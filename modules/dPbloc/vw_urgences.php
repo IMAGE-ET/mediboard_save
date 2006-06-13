823ad80ec633e6653e9d74caa3ec8dfd7eb01a60
Thomas Despoix <tomdesp@gmail.com>
2006-06-13 21:22:29 +0000

    Ref: Passage au CSejour pour dPbloc/
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@128 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/vw_urgences.php
+++ modules/dPbloc/vw_urgences.php
@@ -12,45 +12,49 @@ global $AppUI, $canRead, $canEdit, $m;
 require_once($AppUI->getModuleClass("dPplanningOp", "planning"));
 
 if (!$canRead) {
-	$AppUI->redirect( "m=system&a=access_denied" );
+  $AppUI->redirect( "m=system&a=access_denied" );
 }
 
 // Date du jour
 $today = date("Y-m-d");
 
+$where = array();
+$where["plageop_id"] = "IS NULL";
+$where["pat_id"] = "IS NOT NULL";
+$where["annulee"] = "= 0";
+$ljoin = array();
+$ljoin["patients"] = "operations.pat_id = patients.patient_id";
+$ljoin["sejour"] = "operations.sejour_id = sejour.sejour_id";
+$order = "operations.time_adm, patients.nom, patients.prenom";
+
 // Récupération urgences de chaque jour
 // Admissions passées
-for($i = -5; $i < 0; $i++) {
-  $curr_day = mbDate("+ $i DAYS", $today);
+for ($i = -5; $i < 0; $i++) {
+  $curr_day = mbDate("$i DAYS", $today);
+  $next_day = mbDate("+ 1 DAY", $curr_day);
+
+  $operation = new COperation;
+  $where["entree_prevue"] = "BETWEEN '$curr_day' and '$next_day'";
+  
+  mbTrace($where, "where");
+  
   $list1[$i]["date"] = $curr_day;
-  $list1[$i]["urgences"] = new COperation;
-  $where = array();
-  $where["date_adm"] = "= '$curr_day'";
-  $where["plageop_id"] = "IS NULL";
-  $where["pat_id"] = "IS NOT NULL";
-  $where["annulee"] = "= 0";
-  $ljoin = array();
-  $ljoin["patients"] = "operations.pat_id = patients.patient_id";
-  $order = "operations.time_adm, patients.nom, patients.prenom";
-  $list1[$i]["urgences"] = $list1[$i]["urgences"]->loadList($where, $order, null, null, $ljoin);
+  $list1[$i]["urgences"] = $operation->loadList($where, $order, null, null, $ljoin);
   foreach($list1[$i]["urgences"] as $key => $value) {
     $list1[$i]["urgences"][$key]->loadRefsFwd();
   }
 }
 
 //Admissions à venir
-for($i = 0; $i < 5; $i++) {
-  $curr_day = mbDate("+ $i DAYS", $today);
+for ($i = 0; $i < 5; $i++) {
+  $curr_day = mbDate("$i DAYS", $today);
+  $next_day = mbDate("+ 1 DAY", $curr_day);
+  
+  $operation = new COperation;
+  $where["entree_prevue"] = "BETWEEN '$curr_day' and '$next_day'";
+  
   $list2[$i]["date"] = $curr_day;
-  $list2[$i]["urgences"] = new COperation;
-  $where = array();
-  $where["date_adm"] = "= '$curr_day'";
-  $where["plageop_id"] = "IS NULL";
-  $where["pat_id"] = "IS NOT NULL";
-  $ljoin = array();
-  $ljoin["patients"] = "operations.pat_id = patients.patient_id";
-  $order = "operations.time_adm, patients.nom, patients.prenom";
-  $list2[$i]["urgences"] = $list2[$i]["urgences"]->loadList($where, $order, null, null, $ljoin);
+  $list2[$i]["urgences"] = $operation->loadList($where, $order, null, null, $ljoin);
   foreach($list2[$i]["urgences"] as $key => $value) {
     $list2[$i]["urgences"][$key]->loadRefsFwd();
   }
