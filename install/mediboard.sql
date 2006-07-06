6fac8365c970e883b791828f0a83069e2a288ec3
Thomas Despoix <tomdesp@gmail.com>
2006-07-06 08:56:08 +0000

    Bug: initialisation des modules dans la base de données mediboard.sql
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@285 a21d6be2-e512-0410-a060-91f5a2946631

--- install/mediboard.sql
+++ install/mediboard.sql
@@ -44,8 +44,8 @@ CREATE TABLE `modules` (
 -- Dumping data for table `modules`
 -- 
 
-INSERT INTO `modules` VALUES (18, 'User Administration', 'admin', '1.0.0', '', 'core', 1, 'User Admin', 'helix-setup-users.png', 9, 1, '');
-INSERT INTO `modules` VALUES (19, 'System Administration', 'system', '1.0.0', '', 'core', 1, 'System Admin', '48_my_computer.png', 10, 1, '');
+INSERT INTO `modules` VALUES (18, 'User Administration', 'admin', '1.0.0', '', 'core', 1, 'User Admin', 'admin.png', 9, 1, '');
+INSERT INTO `modules` VALUES (19, 'System Administration', 'system', '1.0.0', '', 'core', 1, 'System Admin', 'system.png', 10, 1, '');
 
 -- --------------------------------------------------------
 
