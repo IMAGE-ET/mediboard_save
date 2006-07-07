27a902c98524debbae9b126ba80ab3dc6e6d14f4
Romain Ollivier <ollivier@openxtrem.com>
2006-07-07 16:38:47 +0000

    nettoyage index : chargement automatiques de classes supprimées
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@302 a21d6be2-e512-0410-a060-91f5a2946631

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ $phpChrono->start();
 require_once("./includes/config.php");
 
 // Check that the user has correctly set the root directory
-is_file( "{$dPconfig['root_dir']}/includes/config.php" ) 
-  or die( "ERREUR FATALE: le repertoire racine est probablement mal configuré" );
+is_file($dPconfig["root_dir"]."/includes/config.php") 
+  or die("ERREUR FATALE: le repertoire racine est probablement mal configuré");
 
 require_once("./includes/main_functions.php");
 require_once("./includes/errors.php");
@@ -32,12 +32,12 @@ require_once("./includes/errors.php");
 ini_set("memory_limit", "64M");
 
 // manage the session variable(s)
-session_name( 'dotproject' );
-if (get_cfg_var( 'session.auto_start' ) > 0) {
+session_name("dotproject");
+if (get_cfg_var("session.auto_start") > 0) {
 	session_write_close();
 }
 session_start();
-session_register( 'AppUI' ); 
+session_register("AppUI"); 
   
 // write the HTML headers
 header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");	// Date in the past
@@ -46,21 +46,21 @@ header ("Cache-Control: no-cache, must-revalidate");	// HTTP/1.1
 header ("Pragma: no-cache");	// HTTP/1.0
 
 // check if session has previously been initialised
-if (!isset( $_SESSION['AppUI'] ) || isset($_GET['logout'])) {
-    $_SESSION['AppUI'] = new CAppUI();
+if (!isset($_SESSION["AppUI"]) || isset($_GET["logout"])) {
+    $_SESSION["AppUI"] = new CAppUI();
 }
 
-$AppUI =& $_SESSION['AppUI'];
-$AppUI->setConfig( $dPconfig );
+$AppUI =& $_SESSION["AppUI"];
+$AppUI->setConfig($dPconfig);
 $AppUI->checkStyle();
  
 // load the db handler
-require_once( "./includes/db_connect.php" );
+require_once("./includes/db_connect.php");
 
 // load the commonly used classes
-require_once( $AppUI->getSystemClass('date'));
-require_once( $AppUI->getSystemClass('dp'));
-require_once( $AppUI->getSystemClass('mbmodule'));
+require_once($AppUI->getSystemClass("date"));
+require_once($AppUI->getSystemClass("dp"));
+require_once($AppUI->getSystemClass("mbmodule"));
 
 // load default preferences if not logged in
 if ($AppUI->doLogin()) {
@@ -68,41 +68,41 @@ if ($AppUI->doLogin()) {
 }
 
 // check if the user is trying to log in
-if (isset($_POST['login'])) {
-	$username = dPgetParam( $_POST, 'username', '' );
-	$password = dPgetParam( $_POST, 'password', '' );
-	$redirect = dPgetParam( $_REQUEST, 'redirect', '' );
-	$ok = $AppUI->login( $username, $password );
+if (isset($_POST["login"])) {
+	$username = dPgetParam($_POST, "username", "");
+	$password = dPgetParam($_POST, "password", "");
+	$redirect = dPgetParam($_REQUEST, "redirect", "");
+	$ok = $AppUI->login($username, $password);
 	if (!$ok) {
-		@include_once( "./locales/core.php" );
+		@include_once("./locales/core.php");
 		$AppUI->setMsg("Login Failed", UI_MSG_ERROR);
 	}
-	$AppUI->redirect( "$redirect" );
+	$AppUI->redirect("$redirect");
 }
 
 
 // Get the user preference
-$uistyle = $AppUI->getPref('UISTYLE');
+$uistyle = $AppUI->getPref("UISTYLE");
 
 // clear out main url parameters
-$m = '';
-$a = '';
-$u = '';
+$m = "";
+$a = "";
+$u = "";
 
 // check if we are logged in
 if ($AppUI->doLogin()) {
     $AppUI->setUserLocale();
 	// load basic locale settings
-	@include_once( "./locales/$AppUI->user_locale/locales.php" );
-	@include_once( "./locales/core.php" );
-	setlocale( LC_TIME, $AppUI->user_locale );
+	@include_once("./locales/$AppUI->user_locale/locales.php");
+	@include_once("./locales/core.php");
+	setlocale(LC_TIME, $AppUI->user_locale);
 
-	$redirect = @$_SERVER['QUERY_STRING'];
-	if (strpos( $redirect, 'logout' ) !== false) {
-		$redirect = '';
+	$redirect = @$_SERVER["QUERY_STRING"];
+	if (strpos($redirect, "logout") !== false) {
+		$redirect = "";
 	}
 
-	if (isset( $locale_char_set )) {
+	if (isset($locale_char_set)) {
 		header("Content-type: text/html;charset=$locale_char_set");
 	}
 
@@ -114,22 +114,22 @@ if ($AppUI->doLogin()) {
 }
 
 // bring in the rest of the support and localisation files
-require_once( "./includes/permissions.php" );
+require_once("./includes/permissions.php");
 
 
 // set the module and action from the url
-$m     = $AppUI->checkFileName(mbGetValueFromGet( 'm', getReadableModule() ));
-$a     = $AppUI->checkFileName(mbGetValueFromGet( 'a', 'index' ));
-$u     = $AppUI->checkFileName(mbGetValueFromGet( 'u', '' ));
-$dosql = $AppUI->checkFileName(mbGetValueFromPost( 'dosql', '' ));
+$m     = $AppUI->checkFileName(mbGetValueFromGet("m", getReadableModule()));
+$a     = $AppUI->checkFileName(mbGetValueFromGet("a", "index"));
+$u     = $AppUI->checkFileName(mbGetValueFromGet("u", ""));
+$dosql = $AppUI->checkFileName(mbGetValueFromPost("dosql", ""));
 
 // load module based locale settings
-@include_once( "./locales/$AppUI->user_locale/locales.php" );
-@include_once( "./locales/core.php" );
+@include_once("./locales/$AppUI->user_locale/locales.php");
+@include_once("./locales/core.php");
 
 $user_locale = $AppUI->user_locale;
 
-setlocale( LC_TIME, $user_locale );
+setlocale(LC_TIME, $user_locale);
 
 // check overall module permissions
 // these can be further modified by the included action files
@@ -139,25 +139,21 @@ $canAuthor = $canEdit;
 $canDelete = $canEdit;
 
 // Don't output anything. Usefull for fileviewers, popup dialogs, ajax requests, etc.
-$suppressHeaders = dPgetParam( $_GET, 'suppressHeaders');
+$suppressHeaders = dPgetParam($_GET, "suppressHeaders");
 
 // Output the charset header in cas of an ajax request
-$ajax = dPgetParam( $_GET, 'ajax', false);
+$ajax = dPgetParam($_GET, "ajax", false);
  
 if (!$suppressHeaders || $ajax) {
 	// output the character set header
-	if (isset( $locale_char_set )) {
+	if (isset($locale_char_set)) {
 		header("Content-type: text/html;charset=$locale_char_set");
 	}
 }
 
-// include the module class file
-@include_once( $AppUI->getModuleClass( $m ) );
-@include_once( "./modules/$m/" . ($u ? "$u/" : "") . "$u.class.php" );
-
 // do some db work if dosql is set
-if (isset( $_REQUEST["dosql"]) ) {
-    $mDo = isset( $_REQUEST["m"]) ? $_REQUEST["m"] : $m;
+if (isset($_REQUEST["dosql"])) {
+    $mDo = isset($_REQUEST["m"]) ? $_REQUEST["m"] : $m;
     require ("./modules/$mDo/$dosql.php");
 }
 
