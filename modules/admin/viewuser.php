e6f5a05ce8f77af826749b3ac9f07f86f7851899
Thomas Despoix <tomdesp@gmail.com>
2006-07-13 17:23:31 +0000

    ref : simple aux doubles quotes
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@341 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/viewuser.php
+++ modules/admin/viewuser.php
@@ -1,12 +1,12 @@
 <?php /* ADMIN $Id$ */
 $AppUI->savePlace();
 
-$user_id = isset( $_GET['user_id'] ) ? $_GET['user_id'] : 0;
+$user_id = isset( $_GET["user_id"] ) ? $_GET["user_id"] : 0;
 
-if (isset( $_GET['tab'] )) {
-	$AppUI->setState( 'UserVwTab', $_GET['tab'] );
+if (isset( $_GET["tab"] )) {
+	$AppUI->setState( "UserVwTab", $_GET["tab"] );
 }
-$tab = $AppUI->getState( 'UserVwTab' ) !== NULL ? $AppUI->getState( 'UserVwTab' ) : 0;
+$tab = $AppUI->getState( "UserVwTab" ) !== NULL ? $AppUI->getState( "UserVwTab" ) : 0;
 
 // pull data
 $sql = "
@@ -15,13 +15,13 @@ FROM users
 WHERE user_id = $user_id
 ";
 if (!db_loadHash( $sql, $user )) {
-	$titleBlock = new CTitleBlock( 'Invalid User ID', 'helix-setup-user.png', $m, "$m.$a" );
+	$titleBlock = new CTitleBlock( "Invalid User ID", "helix-setup-user.png", $m, "$m.$a" );
 	$titleBlock->addCrumb( "?m=admin", "users list" );
 	$titleBlock->show();
 } else {
 
 // setup the title block
-	$titleBlock = new CTitleBlock( 'View User', 'helix-setup-user.png', $m, "$m.$a" );
+	$titleBlock = new CTitleBlock( "View User", "helix-setup-user.png", $m, "$m.$a" );
 	if ($canRead) {
 	  $titleBlock->addCrumb( "?m=admin", "users list" );
 	}
@@ -29,7 +29,7 @@ if (!db_loadHash( $sql, $user )) {
 	      $titleBlock->addCrumb( "?m=admin&amp;a=addedituser&amp;user_id=$user_id", "edit this user" );
 	      $titleBlock->addCrumb( "?m=system&amp;a=addeditpref&amp;user_id=$user_id", "edit preferences" );
 	      $titleBlock->addCrumbRight(
-			'<a href="#" onclick="popChgPwd();return false">' . $AppUI->_('change password') . '</a>'
+			'<a href="#" onclick="popChgPwd();return false">' . $AppUI->_("change password") . '</a>'
 	      );
 	}
 	$titleBlock->show();
@@ -45,38 +45,38 @@ function popChgPwd() {
 	<td width="50%">
 		<table cellspacing="1" cellpadding="2" border="0" width="100%">
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Login Name');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Login Name");?>:</td>
 			<td class="hilite" width="100%"><?php echo $user["user_username"];?></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('User Type');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("User Type");?>:</td>
 			<td class="hilite" width="100%"><?php echo $utypes[$user["user_type"]];?></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Real Name');?>:</td>
-			<td class="hilite" width="100%"><?php echo $user["user_first_name"].' '.$user["user_last_name"];?></td>
+			<td align="right" nowrap><?php echo $AppUI->_("Real Name");?>:</td>
+			<td class="hilite" width="100%"><?php echo $user["user_first_name"]." ".$user["user_last_name"];?></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Phone');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Phone");?>:</td>
 			<td class="hilite" width="100%"><?php echo @$user["user_phone"];?></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Home Phone');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Home Phone");?>:</td>
 			<td class="hilite" width="100%"><?php echo @$user["user_home_phone"];?></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Mobile');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Mobile");?>:</td>
 			<td class="hilite" width="100%"><?php echo @$user["user_mobile"];?></td>
 		</tr>
 		<tr valign=top>
-			<td align="right" nowrap><?php echo $AppUI->_('Address');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Address");?>:</td>
 			<td class="hilite" width="100%"><?php
 				echo @$user["user_address1"]
-					.( ($user["user_address2"]) ? '<br />'.$user["user_address2"] : '' )
-					.'<br />'.$user["user_city"]
-					.'&nbsp;&nbsp;'.$user["user_state"]
-					.'&nbsp;&nbsp;'.$user["user_zip"]
-					.'<br />'.$user["user_country"]
+					.( ($user["user_address2"]) ? "<br />".$user["user_address2"] : "" )
+					."<br />".$user["user_city"]
+					."&nbsp;&nbsp;".$user["user_state"]
+					."&nbsp;&nbsp;".$user["user_zip"]
+					."<br />".$user["user_country"]
 					;
 			?></td>
 		</tr>
@@ -86,7 +86,7 @@ function popChgPwd() {
 	<td width="50%">
 		<table width="100%">
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Birthday');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Birthday");?>:</td>
 			<td class="hilite" width="100%"><?php echo @$user["user_birthday"];?></td>
 		</tr>
 		<tr>
@@ -98,11 +98,11 @@ function popChgPwd() {
 			<td class="hilite" width="100%"><a href="aim:<?php echo @$user["user_aol"];?>"><?php echo @$user["user_aol"];?></a></td>
 		</tr>
 		<tr>
-			<td align="right" nowrap><?php echo $AppUI->_('Email');?>:</td>
+			<td align="right" nowrap><?php echo $AppUI->_("Email");?>:</td>
 			<td class="hilite" width="100%"><?php echo '<a href="mailto:'.@$user["user_email"].'">'.@$user["user_email"].'</a>';?></td>
 		</tr>
 		<tr>
-			<td colspan="2"><strong><?php echo $AppUI->_('Signature');?>:</strong></td>
+			<td colspan="2"><strong><?php echo $AppUI->_("Signature");?>:</strong></td>
 		</tr>
 		<tr>
 			<td class="hilite" width="100%" colspan="2">
@@ -117,7 +117,7 @@ function popChgPwd() {
 <?php	
 	// tabbed information boxes
 	$tabBox = new CTabBox( "?m=admin&amp;a=viewuser&amp;user_id=$user_id", "{$AppUI->cfg['root_dir']}/modules/admin/", $tab );
-  $tabBox->add( 'vw_usr_perms', 'Permissions Mediboard' );
+  $tabBox->add( "vw_usr_perms", "Permissions Mediboard" );
 	$tabBox->show();
 }
 ?>
