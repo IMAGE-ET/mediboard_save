fc1cd6ac46e8c795c2493dcd0293f7af5b090674
Romain Ollivier <ollivier@openxtrem.com>
2006-07-27 16:17:25 +0000

    ref : chargement de tous les fichiers de locales
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@488 a21d6be2-e512-0410-a060-91f5a2946631

--- locales/core.php
+++ locales/core.php
@@ -1,15 +1,8 @@
 <?php
 ob_start();
-	@readfile( "{$AppUI->cfg['root_dir']}/locales/$AppUI->user_locale/common.inc" );
-	@readfile( "{$AppUI->cfg['root_dir']}/locales/$AppUI->user_locale/$m.inc" );
-	switch ($m) {
-	case 'departments':
-		@readfile( "{$AppUI->cfg['root_dir']}/locales/$AppUI->user_locale/companies.inc" );
-		break;
-	case 'system':
-		@readfile( "{$AppUI->cfg['root_dir']}/locales/{$AppUI->cfg['host_locale']}/styles.inc" );
-		break;
-	}
+  foreach(glob($AppUI->cfg["root_dir"]."/locales/$AppUI->user_locale/*.inc") as $file) {
+	  readfile($file);
+  }
 	eval( "\$GLOBALS['translate']=array(".ob_get_contents()."\n'0');" );
 ob_end_clean();
 ?>
