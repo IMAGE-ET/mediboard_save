5d50734af3efbdd8bc2fd61a4b54b81b1aacbd12
Romain Ollivier <ollivier@openxtrem.com>
2006-08-11 12:34:21 +0000

    passage du $g en smarty
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@621 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/smartydp.class.php
+++ classes/smartydp.class.php
@@ -59,7 +59,7 @@ class CSmartyDP extends Smarty {
    * Standard data assignment
    */
   function CSmartyDP($doubleTagMod = 0) {
-    global $AppUI, $dbChronos, $dPconfig, $canRead, $canEdit, $m, $a, $tab, $dialog, $mb_version_build;
+    global $AppUI, $dbChronos, $dPconfig, $canRead, $canEdit, $m, $a, $tab, $g, $dialog, $mb_version_build;
     $root = $AppUI->getConfig( 'root_dir' );
     
     // Directories initialisation
@@ -88,6 +88,7 @@ class CSmartyDP extends Smarty {
     $this->assign("m", $m);
     $this->assign("a", $a);
     $this->assign("tab", $tab);
+    $this->assign("g", $g);
     $this->assign("dialog", $dialog);
     $this->assign("mb_version_build", $mb_version_build);
     
