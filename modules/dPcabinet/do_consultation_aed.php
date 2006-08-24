694b30935e50bb9101580d4248f0b4618aeb005b
Romain Ollivier <ollivier@openxtrem.com>
2006-08-24 14:44:57 +0000

    ref : vérification de la supression de la consultation avant de suprimmer la consultation d'anesthésie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@682 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/do_consultation_aed.php
+++ modules/dPcabinet/do_consultation_aed.php
@@ -13,22 +13,30 @@ require_once($AppUI->getSystemClass("doobjectaddedit"));
 require_once($AppUI->getModuleClass("dPcabinet", "consultation"));
 require_once($AppUI->getModuleClass("dPcabinet", "consultAnesth"));
 
-if ($chir_id = dPgetParam( $_POST, 'chir_id'))
-  mbSetValueToSession('chir_id', $chir_id);
+if ($chir_id = mbGetValueFromPost("chir_id")) {
+  mbSetValueToSession("chir_id", $chir_id);
+}
 
 $do = new CDoObjectAddEdit("CConsultation", "consultation_id");
 $do->createMsg = "Consultation créée";
 $do->modifyMsg = "Consultation modifiée";
 $do->deleteMsg = "Consultation supprimée";
 $do->doBind();
-if (intval(dPgetParam($_POST, 'del'))) {
+if (intval(mbGetValueFromPost("del"))) {
   $consultAnesth = new CConsultAnesth;
   $where = array();
   $where["consultation_id"] = "= '".$do->_obj->consultation_id."'";
   $consultAnesth->loadObject($where);
-  if($consultAnesth->consultation_anesth_id)
-    $consultAnesth->delete();
-  $do->doDelete();
+  if($do->_obj->canDelete() && $consultAnesth->consultation_anesth_id) {
+    if($consultAnesth->delete()) {
+      $do->doDelete();
+    } else {
+      $AppUI->setMsg("Impossible de supprimer la consultation d'anesthésie associée", UI_MSG_ERROR );
+      $do->doRedirect();
+    }
+  } else {
+    $do->doDelete();
+  }
 
 } else {
   $do->doStore();
