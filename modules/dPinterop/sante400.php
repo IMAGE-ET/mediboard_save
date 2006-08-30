940c216383b9f853fdd3b84e5770f4004159ead7
Thomas Despoix <tomdesp@gmail.com>
2006-08-30 09:22:18 +0000

    Fnc: Fichier d'intégration pour Sante400
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@696 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPinterop/sante400.php
+++ modules/dPinterop/sante400.php
@@ -2,57 +2,100 @@
 
 require_once "DB.php";
 
-$dsn = array (
-    "phptype"  => "odbc",
-    "username" => "GMB",
-    "password" => "GMB  ",
-    "hostspec" => "sante400",
-//    "database" => "sante400",
-);
-
-//$dsn = "sante400";
-
 $chrono = new Chronometer();
 
 $db_user = "GMB";
 $db_pass = "GMB";
 
 $sql = "SELECT * FROM API";
-  for ($index = 0; $index < 1; $index++) {
 
-  // Create a new DB connection object and connect to the ODBC database.
-  $db =& DB::connect($dsn);
-  if (DB::isError($db)) {
-    die("Unable to connect to database: " 
-      . $db->getMessage() . "\n"
-      . $db->getDebugInfo() . "\n");
+switch ($mode = mbGetValueFromGet("mode", "pear")) {
+  /**
+   * Mode PEAR::DB
+   */
+	case "pear":
+  $dsn = array (
+      "phptype"  => "odbc",
+      "username" => "GMB",
+      "password" => "GMB  ",
+      "hostspec" => "sante400",
+  //    "database" => "sante400",
+  );
+  
+  for ($index = 0; $index < 1; $index++) {
+    $chrono->start();
+
+    // Create a new DB connection object and connect to the ODBC database.
+    $db =& DB::connect($dsn);
+    if (DB::isError($db)) {
+      die("Unable to connect to database: " 
+        . $db->getMessage() . "\n"
+        . $db->getDebugInfo() . "\n");
+    }
+    
+  
+    mbTrace("Yes", "Connection successful");
+    
+    // Execute Query
+    $list = $db->getAll($sql);
+    if (PEAR::isError($list)) {
+        
+        mbTrace($list->getMessage(), "Erreur");
+        mbTrace($list->getDebugInfo(), "Debug Info");
+        die();
+    }
+   
+    $chrono->stop();
+  }
+		
+	break;
+
+  /**
+   * Mode ODBC
+   */
+  case "odbc":
+  $dsn = "sante400";
+
+  for ($index = 0; $index < 1; $index++) {
+    $chrono->start();
+
+    // Connect to the ODBC database.
+    $link = odbc_connect($dsn, $db_user, $db_pass);
+    if (odbc_error()) {
+      mbTrace(odbc_errormsg(), "Could no connect");
+      die;
+    }
+
+    // Execute Query
+    $res = odbc_exec($link, $sql);
+    while ($obj = odbc_fetch_object($res)) {
+  //    mbTrace($obj, "Found Object");
+    }
+  
+    $chrono->stop();
   }
   
+  break;
+
+	default:
+  mbTrace($mode, "mode not available");
+	break;
+}
+
+
+//$dsn = "sante400";
+
 //  $link = odbc_connect($dsn, $db_user, $db_pass);
 //  if (odbc_error()) {
 //    mbTrace(odbc_errormsg(), "Could no connect");
 //    die;
 //  }
 
-  mbTrace("Yes", "Connection successful");
-  $chrono->start();
-  
-  $list = $db->getAll($sql);
-  if (PEAR::isError($list)) {
-      
-      mbTrace($list->getMessage(), "Erreur");
-      mbTrace($list->getDebugInfo(), "Debug Info");
-      die();
-  }
-   
-  
 //  $res = odbc_exec($link, $sql);
 //  while ($obj = odbc_fetch_object($res)) {
 ////    mbTrace($obj, "Found Object");
 //  }
 
-  $chrono->stop();
-}
 
 mbTrace($index, "How many identical selects");
 mbTrace($chrono, "Chrono");
