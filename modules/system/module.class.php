0fa769b57a004180be0b6c166fe2a45c4628ed52
Romain Ollivier <ollivier@openxtrem.com>
2006-09-14 08:57:49 +0000

    Ref : réunification de la classe CModule
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@777 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/system/module.class.php
@@ -0,0 +1,211 @@
+<?php /* $Id: $ */
+
+/**
+ * @package Mediboard
+ * @subpackage system
+ * @version $Revision: $
+ * @author Romain Ollivier
+*/
+ 
+global $AppUI, $m;
+
+require_once($AppUI->getSystemClass("mbobject"));
+
+/**
+* Module class
+*/
+
+global $module_installed;
+global $module_active;
+global $module_visible;
+
+class CModule extends CMbObject {
+  
+  // primary key
+  var $mod_id;
+  
+  // DB Fields
+  var $mod_name      = null;
+  var $mod_type      = null; // Core or User
+  var $mod_version   = null; // Current Installed version MM.mmm
+  var $mod_active    = null; // active module
+  var $mod_ui_active = null; // visible module
+  var $mod_ui_order  = null; // UI Position
+    
+  // Form Fields
+  var $_upgradable = null; // Check if upgradable
+  
+  // Static Collections
+  var $_registered = array();
+/*
+  var $mod_directory   = null; : removed
+  var $mod_setup_class = null; : removed
+  var $mod_ui_name     = null; : removed
+  var $mod_ui_icon     = null; : removed
+  var $mod_description = null; : removed
+*/
+  function CModule() {
+    $this->CMbObject("modules", "mod_id");
+
+    $this->_props["mod_name"]      = "notNull|str|maxLength|20";
+    $this->_props["mod_type"]      = "notNull|enum|core|user";
+    $this->_props["mod_version"]   = "notNull|str|maxLength|6";
+    $this->_props["mod_ui_active"] = "notNull|num|length|1"; // should be "bool"
+    $this->_props["mod_ui_order"]  = "notNull|nul|length|2";
+  }
+  
+  function updateFormFields() {
+    $this->_view = $this->mod_name;
+  }
+  
+  function loadByName($name) {
+    $where = array();
+    $where["mod_name"] = "= '$name'";
+    return $this->loadObject($where);
+  }
+    
+  function registerSetup() {
+    global $registeredModules;
+
+    if (array_key_exists($this->name, $registeredModules)) {
+      trigger_error("Module '$this->name' already registered", E_USER_ERROR);
+    }
+    
+    $registeredModules[$this->name] = $this;
+  }
+
+  function loadModules() {
+    $modules = new CModule;
+    $order = "mod_ui_order";
+    $where = array();
+    $modules = $modules->loadList($where, $order);
+    
+    global $module_installed;
+    global $module_active;
+    global $module_visible;
+
+    $module_installed = array();
+    $module_active = array();
+    $module_visible = array();
+    
+    foreach ($modules as $keyModule => $valModule) {
+      $module =& $modules[$keyModule];
+        
+      $module_installed[$module->mod_name] = $module;
+      
+      if ($module->mod_active == 1) {
+        $module_active[$module->mod_name] =& $module;
+      }
+    
+      if ($module->mod_ui_active == 1) {
+        $module_visible[$module->mod_name] =& $module;
+      }
+    }
+  }
+  
+  /**
+   * Returns all or a named installed module
+   */
+  function getInstalled($moduleName = null) {
+    global $module_installed;
+
+    if ($moduleName) {
+      return $module_installed[$moduleName];
+    }
+
+    return $module_installed;
+  }
+
+  /**
+   * Returns all or a named active module
+   */
+  function getActive($moduleName = null) {
+    global $module_active;
+
+    if ($moduleName) {
+      return $module_active[$moduleName];
+    }
+
+    return $module_active;
+  }
+   
+  /**
+   * Returns all or a named visible module
+   */
+  function getVisible($moduleName = null) {
+    global $module_visible;
+
+    if ($moduleName) {
+      return $module_visible[$moduleName];
+    }
+
+    return $module_visible;
+  }
+  
+  function reorder() {
+    $sql = "SELECT * FROM modules ORDER BY mod_ui_order";
+    $result = db_exec($sql);
+    $i = 1;
+    while($row = db_fetch_array($result)) {
+      $sql = "UPDATE modules SET mod_ui_order = '$i' WHERE mod_id = '".$row["mod_id"]."'";
+      db_exec($sql);
+      $i++;
+    }
+  }
+
+  function install() {
+    $sql = "SELECT mod_name FROM modules WHERE mod_name = '$this->mod_name'";
+    $temp = null;
+    if (db_loadHash($sql, $temp)) {
+      // the module is already installed
+      // TODO: check for older version - upgrade
+      return false;
+    }
+    $this->store();
+    $this->reorder();
+    return true;
+  }
+
+  function remove() {
+    $sql = "DELETE FROM modules WHERE mod_id = $this->mod_id";
+    if (!db_exec( $sql )) {
+      return db_error();
+    } else {
+      $this->reorder();
+      return null;
+    }
+  }
+
+  function move($dirn) {
+    $temp = $this->mod_ui_order;
+    if($dirn == "moveup") {
+      $temp--;
+      $sql = "UPDATE modules SET mod_ui_order = (mod_ui_order+1) WHERE mod_ui_order = $temp";
+      db_exec($sql);
+    } else if($dirn == "movedn") {
+      $temp++;
+      $sql = "UPDATE modules SET mod_ui_order = (mod_ui_order-1) WHERE mod_ui_order = $temp";
+      db_exec($sql);
+    }
+    $sql = "UPDATE modules SET mod_ui_order = $temp WHERE mod_id = $this->mod_id";
+    db_exec($sql);
+
+    $this->mod_id = $temp;
+    
+    $this->reorder();
+  }
+// overridable functions
+  function moduleInstall() {
+    return null;
+  }
+  function moduleRemove() {
+    return null;
+  }
+  function moduleUpgrade() {
+    return null;
+  }
+}
+
+CModule::loadModules();
+
+?>
