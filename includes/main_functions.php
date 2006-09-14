c4c4fe3cbbea09f3d253654943ff53ff28a53c96
Romain Ollivier <ollivier@openxtrem.com>
2006-09-14 09:45:07 +0000

    ref : nettoyage de main_functions.php
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@779 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/main_functions.php
+++ includes/main_functions.php
@@ -6,28 +6,8 @@
 require_once("mb_functions.php");
 
 $CR = "\n";
-define('SECONDS_PER_DAY', 60 * 60 * 24);
 
-##
-## Returns the best color based on a background color (x is cross-over)
-##
-function bestColor( $bg, $lt='#ffffff', $dk='#000000' ) {
-// cross-over color = x
-	$x = 128;
-	$r = hexdec( substr( $bg, 0, 2 ) );
-	$g = hexdec( substr( $bg, 2, 2 ) );
-	$b = hexdec( substr( $bg, 4, 2 ) );
-
-	if ($r < $x && $g < $x || $r < $x && $b < $x || $b < $x && $g < $x) {
-		return $lt;
-	} else {
-		return $dk;
-	}
-}
-
-##
-## returns a select box based on an key,value array where selected is based on key
-##
+// returns a select box based on an key,value array where selected is based on key
 function arraySelect( &$arr, $select_name, $select_attribs, $selected, $translate=false ) {
 	GLOBAL $AppUI;
 	reset( $arr );
@@ -48,42 +28,7 @@ function arraySelect( &$arr, $select_name, $select_attribs, $selected, $translat
 	return $s;
 }
 
-##
-## returns a select box based on an key,value array where selected is based on key
-##
-function arraySelectTree( &$arr, $select_name, $select_attribs, $selected, $translate=false ) {
-	GLOBAL $AppUI;
-	reset( $arr );
-
-	$children = array();
-	// first pass - collect children
-	foreach ($arr as $k => $v ) {
-		$id = $v[0];
-		$pt = $v[2];
-		$list = @$children[$pt] ? $children[$pt] : array();
-		array_push($list, $v);
-	    $children[$pt] = $list;
-	}
-	$list = tree_recurse($arr[0][2], '', array(), $children);
-	return arraySelect( $list, $select_name, $select_attribs, $selected, $translate );
-}
-
-function tree_recurse($id, $indent, $list, $children) {
-	if (@$children[$id]) {
-		foreach ($children[$id] as $v) {
-			$id = $v[0];
-			$txt = $v[1];
-			$pt = $v[2];
-			$list[$id] = "$indent $txt";
-			$list = tree_recurse($id, "$indent--", $list, $children);
-		}
-	}
-	return $list;
-}
-
-##
-## Merges arrays maintaining/overwriting shared numeric indicees
-##
+// Merges arrays maintaining/overwriting shared numeric indicees
 function arrayMerge( $a1, $a2 ) {
 	foreach ($a2 as $k => $v) {
 		$a1[$k] = $v;
@@ -91,33 +36,7 @@ function arrayMerge( $a1, $a2 ) {
 	return $a1;
 }
 
-##
-## breadCrumbs - show a colon separated list of bread crumbs
-## array is in the form url => title
-##
-function breadCrumbs( &$arr ) {
-	GLOBAL $AppUI;
-	$crumbs = array();
-	foreach ($arr as $k => $v) {
-		$crumbs[] = "<a href=\"$k\">".$AppUI->_( $v )."</a>";
-	}
-	return implode( ' <strong>:</strong> ', $crumbs );
-}
-##
-## generate link for context help -- old version
-##
-function contextHelp( $title, $link='' ) {
-	return dPcontextHelp( $title, $link );
-}
-
-function dPcontextHelp( $title, $link='' ) {
-	global $AppUI;
-	return "<a href=\"#$link\" onClick=\"javascript:window.open('?m=help&dialog=1&hid=$link', 'contexthelp', 'width=400, height=400, left=50, top=50, scrollbars=yes, resizable=yes')\">".$AppUI->_($title)."</a>";
-}
-
-##
-## displays the configuration array of a module for informational purposes
-##
+// displays the configuration array of a module for informational purposes
 function dPshowModuleConfig( $config ) {
 	GLOBAL $AppUI;
 	$s = '<table cellspacing="2" cellpadding="2" border="0" class="std" width="50%">';
@@ -129,13 +48,9 @@ function dPshowModuleConfig( $config ) {
 	return ($s);
 }
 
-/**
- *	Function to recussively find an image in a number of places
- *	@param string The name of the image
- *	@param string Optional name of the current module
- */
+// Function to recussively find an image in a number of places
 function dPfindImage( $name, $module=null ) {
-// uistyle must be declared globally
+  // uistyle must be declared globally
 	global $AppUI, $uistyle;
 
 	if (file_exists( "{$AppUI->cfg['root_dir']}/style/$uistyle/images/$name" )) {
@@ -155,10 +70,6 @@ function dPfindImage( $name, $module=null ) {
  *	Workaround removed due to problems in Opera and other issues
  *	with IE6.
  *	Workaround to display png images with alpha-transparency in IE6.0
- *	@param string The name of the image
- *	@param string The image width
- *	@param string The image height
- *	@param string The alt text for the image
  */
 function dPshowImage( $src, $wid='', $hgt='', $alt='' ) {
 	if (strpos( $src, '.png' ) > 0 && strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE 6.0' ) !== false) {
@@ -168,153 +79,12 @@ function dPshowImage( $src, $wid='', $hgt='', $alt='' ) {
   }
 }
 
-#
-# function to return a default value if a variable is not set
-#
-
-function defVal($var, $def) {
-	return isset($var) ? $var : $def;
-}
-
-/**
-* Utility function to return a value from a named array or a specified default
-*/
+// Utility function to return a value from a named array or a specified default
 function dPgetParam( &$arr, $name, $def=null ) {
 	return isset( $arr[$name] ) ? $arr[$name] : $def;
 }
 
-#
-# add history entries for tracking changes
-#
-
-function addHistory( $description, $project_id = 0, $module_id = 0) {
-	global $AppUI;
-	/*
-	 * TODO:
-	 * 1) description should be something like:
-	 * 		command(arg1, arg2...)
-	 *  for example:
-	 * 		new_forum('Forum Name', 'URL')
-	 *
-	 * This way, the history module will be able to display descriptions
-	 * using locale definitions:
-	 * 		"new_forum" -> "New forum '%s' was created" -> "Se ha creado un nuevo foro llamado '%s'"
-	 *
-	 * 2) project_id and module_id should be provided in order to filter history entries
-	 *
-	 */
-	if(!$AppUI->cfg['log_changes']) return;
-	$description = str_replace("'", "\'", $description);
-	$hsql = "select * from modules where mod_name = 'History' and mod_active = 1";
-	$qid = db_exec($hsql);
-
-	if (! $qid || db_num_rows($qid) == 0) {
-	  $AppUI->setMsg("History module is not loaded, but your config file has requested that changes be logged.  You must either change the config file or install and activate the history module to log changes.", UI_MSG_ALERT);
-	  return;
-	}
-
-	$psql =	"INSERT INTO history " .
-			"( history_description, history_user, history_date ) " .
-	  		" VALUES ( '$description', " . $AppUI->user_id . ", now() )";
-	db_exec($psql);
-	echo db_error();
-}
-
-##
-## Looks up a value from the SYSVALS table
-##
-function dPgetSysVal( $title ) {
-	$sql = "
-	SELECT syskey_type, syskey_sep1, syskey_sep2, sysval_value
-	FROM sysvals,syskeys
-	WHERE sysval_title = '$title'
-		AND syskey_id = sysval_key_id
-	";
-	db_loadHash( $sql, $row );
-// type 0 = list
-	$sep1 = $row['syskey_sep1'];	// item separator
-	$sep2 = $row['syskey_sep2'];	// alias separator
-
-	// A bit of magic to handle newlines and returns as separators
-	// Missing sep1 is treated as a newline.
-	if (!isset($sep1))
-	  $sep1 = "\n";
-	if ($sep1 == "\\n")
-	  $sep1 = "\n";
-	if ($sep1 == "\\r")
-	  $sep1 = "\r";
-
-	$temp = explode( $sep1, $row['sysval_value'] );
-	$arr = array();
-	// We use trim() to make sure a numeric that has spaces
-	// is properly treated as a numeric
-	foreach ($temp as $item) {
-		if($item) {
-			$temp2 = explode( $sep2, $item );
-			if (isset( $temp2[1] )) {
-				$arr[trim($temp2[0])] = $temp2[1];
-			} else {
-				$arr[trim($temp2[0])] = $temp2[0];
-			}
-		}
-	}
-	return $arr;
-}
-
-function dPuserHasRole( $name ) {
-	global $AppUI;
-	$uid = $AppUI->user_id;
-	$sql = "SELECT r.role_id FROM roles AS r,user_roles AS ur WHERE ur.user_id=$uid AND ur.role_id=r.role_id AND r.role_name='$name'";
-	return db_loadResult( $sql );
-}
-
-function dPformatDuration($x) {
-    global $dPconfig;
-    global $AppUI;
-    $dur_day = floor($x / $dPconfig['daily_working_hours']);
-    //$dur_hour = fmod($x, $dPconfig['daily_working_hours']);
-    $dur_hour = $x - $dur_day*$dPconfig['daily_working_hours'];
-    $str = '';
-    if ($dur_day > 1) {
-        $str .= $dur_day .' '. $AppUI->_('days'). ' ';
-    } elseif ($dur_day == 1) {
-        $str .= $dur_day .' '. $AppUI->_('day'). ' ';
-    }
-
-    if ($dur_hour > 1 ) {
-        $str .= $dur_hour .' '. $AppUI->_('hours');
-    } elseif ($dur_hour > 0 and $dur_hour <= 1) {
-        $str .= $dur_hour .' '. $AppUI->_('hour');
-    }
-
-    if ($str == '') {
-        $str = $AppUI->_("n/a");
-    }
-
-    return $str;
-
-}
-
-/**
-*/
-function dPsetMicroTime() {
-	global $microTimeSet;
-	list($usec, $sec) = explode(" ",microtime());
-	$microTimeSet = (float)$usec + (float)$sec;
-}
-
-/**
-*/
-function dPgetMicroDiff() {
-	global $microTimeSet;
-	$mt = $microTimeSet;
-	dPsetMicroTime();
-	return sprintf( "%.3f", $microTimeSet - $mt );
-}
-
-/**
-* Make text safe to output into double-quote enclosed attirbutes of an HTML tag
-*/
+// Make text safe to output into double-quote enclosed attirbutes of an HTML tag
 function dPformSafe( $txt, $deslash=false ) {
 	if (is_object( $txt )) {
 		foreach (get_object_vars($txt) as $k => $v) {
@@ -340,24 +110,6 @@ function dPformSafe( $txt, $deslash=false ) {
 		}
 	}
 	return $txt;
-}
-
-function convert2days( $durn, $units ) {
-	global $AppUI;
-	switch ($units) {
-	case 0:
-		return $durn / $AppUI->cfg['daily_working_hours'];
-		break;
-	case 24:
-		return $durn;
-	}
-}
-
-function formatTime( $uts ) {
-	global $AppUI;
-	$date = new CDate();
-	$date->setDate($uts, DATE_FORMAT_UNIXTIME);	
-	return $date->format( $AppUI->getPref('SHDATEFORMAT') );
 }
 
 function formatCurrency( $number, $format ) {
@@ -414,9 +166,6 @@ function formatCurrency( $number, $format ) {
 				break;
 		}
 	}
-	// Avant
-	//$currency .= $currency_prefix . $mondat['int_curr_symbol'] . $currency_suffix;
-	// Apres
 	$currency = $currency_prefix . $mondat['int_curr_symbol'] . $currency_suffix;
 	$space = "";
 	if ($mondat[$letter . "_sep_by_space"])
