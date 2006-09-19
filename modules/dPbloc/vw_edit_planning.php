e1007fd1f4face1dc485f4c63b292e4a831010f8
Romain Ollivier <ollivier@openxtrem.com>
2006-09-19 11:57:13 +0000

    test distant
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@835 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/vw_edit_planning.php
+++ modules/dPbloc/vw_edit_planning.php
@@ -15,7 +15,7 @@ $plageop_id = mbGetValueFromGetOrSession("plageop_id");
 // Liste des Salles
 $listSalles = new CSalle();
 $where = array();
-//$where["group_id"] = "= '$g'";
+$where["group_id"] = "= '$g'";
 $order = "'nom'";
 $listSalles = $listSalles->loadList($where, $order);
 
@@ -25,7 +25,7 @@ $plagesel = new CPlageOp;
 $plagesel->load($plageop_id);
 if($plagesel->plageop_id){
   $arrKeySalle = array_keys($listSalles);
-  if(!in_array($plagesel->salle_id,$arrKeySalle)){
+  if(!in_array($plagesel->salle_id,$arrKeySalle)) {
     $plageop_id = 0;
     $plagesel = new CPlageOp;
   }
