a3bd73f60eb018a0b3d86b4b31a4e3d2b66a87b0
Sébastien Fillonneau <s.fillonneau@openxtrem.com>
2006-09-22 08:01:47 +0000

    Bug : Chargement des données de l'utilisateur demandé
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@897 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/edit_prefs.php
+++ modules/admin/edit_prefs.php
@@ -14,19 +14,28 @@ $user_id = mbGetValueFromGet("user_id" , 0);
 // Vérification des droit
 if ($canEdit){
   $user_id = mbGetValueFromGetOrSession("user_id", $AppUI->user_id);
+  $user_id = intval($user_id);
 }else{
   $user_id = $AppUI->user_id;
 }  
 
 // Chargement User demandé
 $user = null;
-if($user_id){
+if($user_id!==null){
   $user = new CUser;
   $user->load($user_id);
+  
+  if($user_id == $AppUI->user_id){
+    $prefs = $AppUI->user_prefs;
+  }else{
+    $sql = "SELECT pref_name, pref_value
+          FROM user_preferences
+          WHERE pref_user = $user_id";
+    $prefs = db_loadHashList( $sql );
+  }
 }
 
 // load the preferences
-$prefs = $AppUI->user_prefs;
 
 $prefsUser = array();
 
@@ -34,9 +43,9 @@ $prefsUser = array();
 $array_list_pref_generale = array("LOCALE","UISTYLE");
 foreach($array_list_pref_generale as $namePref){
   if(!array_key_exists("$namePref",$prefs)){
-    $prefs["$namePref"] = null;
+    $prefs[$namePref] = null;
   }
-  $prefsUser["GENERALE"]["$namePref"] = $prefs["$namePref"];
+  $prefsUser["GENERALE"][$namePref] = $prefs[$namePref];
 }
 
 
@@ -51,9 +60,9 @@ foreach($array_list_module_pref as $modulename => $listPrefs){
   if(($user_id!==0 && $prefModule->mod_id && CPermModule::getInfoModule("view", $prefModule->mod_id, PERM_READ, $user_id)) || $user_id===0){
     foreach($listPrefs as $namePref){
     	if(!array_key_exists("$namePref",$prefs)){
-    	  $prefs["$namePref"] = null;
+    	  $prefs[$namePref] = null;
     	}
-      $prefsUser["$modulename"]["$namePref"] = $prefs["$namePref"];
+      $prefsUser[$modulename][$namePref] = $prefs[$namePref];
     }    
   }
 }
