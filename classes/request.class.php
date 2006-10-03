3fc44c9c527658de7bf69c1900fe21300c86cd21
Romain Ollivier <ollivier@openxtrem.com>
2006-10-03 16:46:23 +0000

    fct : classe de création de requete
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1005 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ classes/request.class.php
@@ -0,0 +1,135 @@
+<?php /* CLASSES $Id: $ */
+
+/**
+ *  @package Mediboard
+ *  @subpackage classes
+ *  @author  Romain Ollivier
+ *  @version $Revision: $
+ */
+
+// Request
+class CRequest {
+  //Table name
+  var $tbl = "";
+  
+  // params
+  var $ljoin = array();
+  var $where = array();
+  var $group = array();
+  var $order = array();
+  var $limit = "";
+    
+  function CRequest($tbl) {
+    $this->tbl = $tbl;
+  }
+  
+  function resetParams() {
+    $this->ljoin = array();
+    $this->where = array();
+    $this->group = array();
+    $this->order = array();
+    $this->limit = "";
+  }
+
+  function addLJoin($ljoin) {
+    if(is_array($ljoin)) {
+      $this->ljoin = array_merge($this->ljoin, $ljoin);
+    }
+  }
+
+  function addWhere($where) {
+    if(is_array($where)) {
+      $this->where = array_merge($this->where, $where);
+    } elseif(is_string($where)) {
+      $this->where[] = $where;
+    }
+  }
+
+  function addGroup($group) {
+    if(is_array($group)) {
+      $this->group = array_merge($this->group, $group);
+    } elseif(is_string($group)) {
+      $this->group[] = $group;
+    }
+  }
+
+  function addOrder($order) {
+    if(is_array($order)) {
+      $this->order = array_merge($this->order, $order);
+    } elseif(is_string($order)) {
+      $this->order[] = $order;
+    }
+  }
+
+  function setLimit($limit) {
+    $this->limit = $limit;
+  }
+
+  function getRequest() {
+    $sql = "SELECT `$this->tbl`.* FROM `$this->tbl`";
+
+    // Left join clauses
+    if ($this->ljoin) {
+      assert(is_array($this->ljoin));
+      foreach ($this->ljoin as $table => $condition) {
+        $sql .= "\nLEFT JOIN `$table` ON $condition";
+      }
+    }
+    
+    // Where clauses
+    if (is_array($this->where)) {
+      foreach ($this->where as $field => $eq) {
+        if (is_string($field)) {
+          if($pos = strpos($field, ".")) {
+            $point_table = substr($field, 0, $pos);
+            $point_field = substr($field, $pos + 1);
+            $this->where[$field] = "`$point_table`.`$point_field` $eq";
+          } else {
+            $this->where[$field] = "`$field` $eq";
+          }
+        }
+        
+        $this->where[$field] = "(" . $this->where[$field] . ")";
+      }
+    }
+    
+    if ($this->where) {
+      $sql .= "\nWHERE ";
+      $sql .= is_array($this->where) ? implode("\nAND ", $this->where) : $this->where;
+    }
+      
+    // Group by fields
+    if (is_array($this->group)) {
+      foreach ($this->group as $key => $field) {
+        $this->group[$key] = "`$field`";
+      }
+    }
+    
+    if ($this->group) {
+      $sql .= "\nGROUP BY ";
+      $sql .= is_array($this->group) ? implode(", ", $this->group) : $this->group;
+    }
+      
+    // Order by fields
+    if (is_array($this->order)) {
+      foreach ($this->order as $key => $field) {
+        // We cannot use the `` syntax because it wont work
+        // with table.field syntax, neither the ASC/DESC one
+        //$this->$order[$key] = "`$field`";
+        $this->order[$key] = "$field";
+      }
+    }
+    
+    if ($this->order) {
+      $sql .= "\nORDER BY ";
+      $sql .= is_array($this->order) ? implode(", ", $this->order) : $this->order;
+    }
+    
+    // Limits
+    if ($this->limit) {
+      $sql .= "\nLIMIT $this->limit";
+    }
+    return $sql;
+  }
+}
+?>
