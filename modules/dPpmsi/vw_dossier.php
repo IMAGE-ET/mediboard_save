b36c34707cdcd2f0d0bf28affb343c815f55e696
Sébastien Fillonneau <s.fillonneau@openxtrem.com>
2006-10-05 08:41:09 +0000

    Ref: Amelioration du Code
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1026 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpmsi/vw_dossier.php
+++ modules/dPpmsi/vw_dossier.php
@@ -15,68 +15,57 @@ if (!$canEdit) {
 
 $pat_id = mbGetValueFromGetOrSession("pat_id");
 
-$fileModule    = CModule::getInstalled("dPfiles");
-$fileCptRendus = CModule::getInstalled("dPcompteRendu");
-
-$canReadFiles     = $fileModule->canRead();
-$canEditFiles     = $fileModule->canEdit();
-$canReadCptRendus = $fileCptRendus->canRead();
-$canEditCptRendus = $fileCptRendus->canEdit();
-
-// Liste des modèles
-$listModeleAuth = array();
-$listModelePrat = new CCompteRendu;
-$listModeleFct = new CCompteRendu;
-if ($pat_id) {
-  $listPrat = new CMediusers();
-  $listPrat = $listPrat->loadPraticiens(PERM_READ);
-  $listFct = new CMediusers();
-  $listFct = $listFct->loadFonctions(PERM_READ);
-  
-  $where = array();
-  $where["chir_id"] = db_prepare_in(array_keys($listPrat));
-  $where["object_id"] = "IS NULL";
-  $where["type"] = "= 'patient'";
-  $order = "chir_id, nom";
-  $listModelePrat = $listModelePrat->loadlist($where, $order);
- 
-  $where = array();
-  $where["function_id"] = db_prepare_in(array_keys($listFct));
-  $where["object_id"] = "IS NULL";
-  $where["type"] = "= 'patient'";
-  $order = "chir_id, nom";
-  $listModeleFct = $listModeleFct->loadlist($where, $order);
-}
-
-// Liste des Category pour les fichiers
-$listCategory = new CFilesCategory;
-$listCategory = $listCategory->listCatClass("CPatient");
-
 // Chargement des praticiens
 $listPrat = new CMediusers;
 $listPrat = $listPrat->loadPraticiens(PERM_READ);
 
-// Chargement complet du dossier patient
+
+// Chargement du dossier patient
 $patient = new CPatient;
 $patient->load($pat_id);
+
 if ($patient->patient_id) {
-  $patient->loadDossierComplet();
+	//$patient->loadDossierComplet();
+  $patient->loadRefsConsultations();
+  $patient->loadRefsSejours();
+  $patient->loadRefsAntecedents();
+  $patient->loadRefsTraitements();
+  $patient->loadRefsAffectations();
+  
+  //Affectation
+  $affectation =& $patient->_ref_curr_affectation;
+  if ($affectation->affectation_id) {
+      $affectation->loadRefsFwd();
+      $affectation->_ref_lit->loadCompleteView();
+    }
     
-  // Chargements complémentaires sur les opérations
+    $affectation =& $patient->_ref_next_affectation;
+    if ($affectation->affectation_id) {
+      $affectation->loadRefsFwd();
+      $affectation->_ref_lit->loadCompleteView();
+    }
+    
+  // Consultation
+  foreach ($patient->_ref_consultations as $keyConsult => $valueConsult) {
+    $consult =& $patient->_ref_consultations[$keyConsult];
+    $consult->loadRefsFwd(); //loadRefs();
+  }
+
+  // Sejours
   foreach ($patient->_ref_sejours as $keySejour => $valueSejour) {
     $sejour =& $patient->_ref_sejours[$keySejour];
+    $sejour->loadRefs();
     $sejour->loadRefGHM();
-
     foreach ($sejour->_ref_operations as $keyOp => $valueOp) {
       $operation =& $sejour->_ref_operations[$keyOp];
+      $operation->loadRefsFwd();
+      
       $operation->loadRefsDocuments();
-
-      $operation->loadRefsActesCCAM();  
+      $operation->loadRefsActesCCAM();
       foreach ($operation->_ref_actes_ccam as $keyActe => $valueActe) {
         $acte =& $operation->_ref_actes_ccam[$keyActe];
         $acte->loadRefsFwd();
       }
-      
       if($operation->plageop_id) {
         $plage =& $operation->_ref_plageop;
         $plage->loadRefsFwd();
@@ -94,24 +83,16 @@ if ($patient->patient_id) {
 $moduleCabinet = CModule::getInstalled("dPcabinet");
 $canEditCabinet = $moduleCabinet->canEdit();
 
-$affichageNbFile = CFile::loadNbFilesByCategory($patient);
 
 // Création du template
 $smarty = new CSmartyDP(1);
 
 $smarty->debugging = false;
 
-$smarty->assign("affichageNbFile", $affichageNbFile);
 $smarty->assign("patient"       , $patient       );
 $smarty->assign("listPrat"      , $listPrat      );
 $smarty->assign("canEditCabinet", $canEditCabinet);
-$smarty->assign("listCategory"  , $listCategory  );
-$smarty->assign("canReadFiles"  , $canReadFiles                              );
-$smarty->assign("canEditFiles"  , $canEditFiles                              );
-$smarty->assign("canReadCptRendus", $canReadCptRendus                        );
-$smarty->assign("canEditCptRendus", $canEditCptRendus                        );
-$smarty->assign("listModelePrat", $listModelePrat                            );
-$smarty->assign("listModeleFct" , $listModeleFct                             );
+
 
 $smarty->display("vw_dossier.tpl");
 
