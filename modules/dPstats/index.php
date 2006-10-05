171b5b35f3fcc95b9fba8c13cf3cb9eb6b4ed59a
Romain Ollivier <ollivier@openxtrem.com>
2006-10-05 13:18:43 +0000

    act : suppression de l'affichage du tab trop gourmand
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1036 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstats/index.php
+++ modules/dPstats/index.php
@@ -11,7 +11,7 @@ $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
 $module->registerTab("vw_hospitalisation", "Hospitalisation"  , TAB_READ);
 $module->registerTab("vw_bloc"           , "Bloc opératoire"  , TAB_READ);
-$module->registerTab("vw_bloc2"          , "Bloc opératoire 2", TAB_READ);
+//$module->registerTab("vw_bloc2"          , "Bloc opératoire 2", TAB_READ);
 $module->registerTab("vw_time_op"        , "Temps opératoires", TAB_READ);
 $module->registerTab("vw_users"          , "Utilisateurs"     , TAB_READ);
 
