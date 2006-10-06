30eb9a1f57356df998ec33ac4f83600069ef8beb
Romain Ollivier <ollivier@openxtrem.com>
2006-10-06 11:52:44 +0000

    erg : faute d'orth
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1052 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstats/index.php
+++ modules/dPstats/index.php
@@ -11,7 +11,7 @@ $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
 $module->registerTab("vw_hospitalisation", "Hospitalisation"    , TAB_READ);
 $module->registerTab("vw_bloc"           , "Bloc opératoire"    , TAB_READ);
-$module->registerTab("vw_bloc2"          , "Jjournée opératoire", TAB_READ);
+$module->registerTab("vw_bloc2"          , "Journée opératoire", TAB_READ);
 $module->registerTab("vw_time_op"        , "Temps opératoires"  , TAB_READ);
 $module->registerTab("vw_users"          , "Utilisateurs"       , TAB_READ);
 
