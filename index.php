943a61fbec4efe33acf6c65ea49d304b759ec6d6
Romain Ollivier <ollivier@openxtrem.com>
2006-10-09 14:48:37 +0000

    fct : choix du module par défaut
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1072 a21d6be2-e512-0410-a060-91f5a2946631

--- index.php
+++ index.php
@@ -133,8 +133,19 @@ if($AppUI->doLogin()) {
 	exit;
 }
 
+// Select the default module
+$defmodule = null;
+if($pref_defmodule = $AppUI->getPref("DEFMODULE")) {
+  $defmodule = CModule::getVisible($AppUI->getPref("DEFMODULE"));
+}
+if(!$defmodule) {
+  $defmodule_name = CPermModule::getVisibleModule();
+} else {
+  $defmodule_name = $defmodule->mod_name;
+}
+
 // set the module and action from the url
-$m     = $AppUI->checkFileName(mbGetValueFromGet("m"     , CPermModule::getVisibleModule()));
+$m     = $AppUI->checkFileName(mbGetValueFromGet("m"     , $defmodule_name));
 $a     = $AppUI->checkFileName(mbGetValueFromGet("a"     , "index"));
 $u     = $AppUI->checkFileName(mbGetValueFromGet("u"     , ""));
 $dosql = $AppUI->checkFileName(mbGetValueFromPost("dosql", ""));
