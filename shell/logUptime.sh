8573115e4a2100d53eccdc5f548eedba87b7c0ea
Thomas Despoix <tomdesp@gmail.com>
2006-10-26 12:04:17 +0000

    Fnc: script shell pour logger l'uptime
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1195 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ shell/logUptime.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BASH_PATH=$(dirname $BASH_SOURCE)
+. $BASH_PATH/utils.sh
+
+########
+# Uptime logger for server load analysis
+########
+
+announce_script "Uptime logger"
+
+if [ "$#" -lt 1 ]
+then 
+  echo "Usage: $0 <file>
+  echo "  <file> is the target for log, ie /var/log/uptime.log"
+  exit 1
+fi
+   
+file=$1
+
+## Make the log line
+dt=$(date '+%Y-%m-%dT%H:%M:%S'); 
+up=$(uptime | awk '{print $10 $11 $12 }'); 
+
+## Log the line
+echo "$dt $up" >> /var/log/uptime.log
