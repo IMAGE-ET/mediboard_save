c1fa49b17e2186b39041bd3afe476c034f6b211b
Romain Ollivier <ollivier@openxtrem.com>
2006-10-27 08:47:34 +0000

    bug : transmission du reorder
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1207 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/operation.class.php
+++ modules/dPplanningOp/operation.class.php
@@ -267,7 +267,7 @@ class COperation extends CMbObject {
     } elseif($this->rank != 0) {
       $this->rank = 0;
       $this->time_operation = "00:00:00";
-      $this->store();
+      $this->store($reorder);
     }
     
     // Vérification qu'on a pas des actes CCAM codés obsolètes
