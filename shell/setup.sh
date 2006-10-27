5741242b9ce39879382e7e3de381778ce9be6637
Romain Ollivier <ollivier@openxtrem.com>
2006-10-27 09:05:44 +0000

    fct : possibilité d'executer le setup uniquement sur modules ou style
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1208 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/setup.sh
+++ shell/setup.sh
@@ -17,21 +17,36 @@ APACHE_GROUP=$1
     exit 0
   fi
 
+# Check optionnal sub-directory
+SUB_DIR=$2
+  if [ "$SUB_DIR" = "modules" ]
+  then
+    BASE_PATH="modules/*"
+    SUB_PATH="modules/*/templates_c/"
+  else
+    if [ "$SUB_DIR" = "style" ]
+    then
+      BASE_PATH="style/*"
+      SUB_PATH="style/*/templates_c/"
+    else
+      BASE_PATH="*"
+      SUB_PATH="lib/ tmp/ files/ includes/ modules/*/templates_c/ style/*/templates_c/ modules/dPinterop/hprim/"
+    fi
+  fi
+
 # Change to Mediboard directory
 MB_PATH=$(dirname $BASH_PATH)
 cd $MB_PATH
 
 # Change group to allow Appache to access files as group
-chgrp -R $APACHE_GROUP *
+echo $BASE
+chgrp -R $APACHE_GROUP $BASE_PATH
 check_errs $? "Failed to change files group to '$APACHE_GROUP'" "Files group changed to '$APACHE_GROUP'!"
 
 # Remove write access to all files for group and other
-chmod -R go-w *
+chmod -R go-w $BASE_PATH
 check_errs $? "Failed to protect all files from writing" "Files protected from writing!"
 
 # Give write access to Apache for some directories
-chmod -R g+w lib/ tmp/ files/ includes/ modules/*/templates_c/ style/*/templates_c/ modules/dPinterop/hprim/
+chmod -R g+w $SUB_PATH
 check_errs $? "Failed to allow Apache writing to mandatory files" "Apache writing allowed for mandatory files!"
-
-
-
