35902fbde7f0c0ed31f62ef45728ef2146c7478f
Romain Ollivier <ollivier@openxtrem.com>
2006-11-23 12:19:34 +0000

    ref : on ne logge plus si il n'y a pas de modification
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1305 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/doobjectaddedit.class.php
+++ classes/doobjectaddedit.class.php
@@ -10,60 +10,60 @@
 global $AppUI, $canRead, $canEdit, $m;
 
 class CDoObjectAddEdit {
-  var $className = null;
+  var $className           = null;
   var $objectKeyGetVarName = null;
-  var $createMsg = null;
-  var $modifyMsg = null;
-  var $deleteMsg = null;
-  var $redirect = null;
-  var $redirectStore  = null;
-  var $redirectError  = null;
-  var $redirectDelete = null;
-  var $ajax = null;
-  var $callBack = null;
-  var $suppressHeaders = null;
-  var $_obj = null;
-  var $_objBefore = null;
-  var $_logIt = null;
-    
+  var $createMsg           = null;
+  var $modifyMsg           = null;
+  var $deleteMsg           = null;
+  var $redirect            = null;
+  var $redirectStore       = null;
+  var $redirectError       = null;
+  var $redirectDelete      = null;
+  var $ajax                = null;
+  var $callBack            = null;
+  var $suppressHeaders     = null;
+  var $_obj                = null;
+  var $_objBefore          = null;
+  var $_logIt              = null;
+
   function CDoObjectAddEdit($className, $objectKeyGetVarName) {
     global $m,$AppUI;
-    
+
     $this->className           = $className;
     $this->objectKeyGetVarName = $objectKeyGetVarName;
     $this->redirect            = "m={$m}";
     $this->redirectStore       = null;
     $this->redirectError       = null;
     $this->redirectDelete      = null;
-    
+
     $this->createMsg           = $AppUI->_("msg-".$className."-create");
     $this->modifyMsg           = $AppUI->_("msg-".$className."-modify");
     $this->deleteMsg           = $AppUI->_("msg-".$className."-delete");
-    
+
     $this->_logIt              = true;
     $this->_obj                = new $this->className();
     $this->_objBefore          = new $this->className();
   }
-  
+
   function doBind() {
     global $AppUI;
-    
+
     $this->ajax = mbGetValueFromPost("ajax");
     $this->suppressHeaders = mbGetValueFromPost("suppressHeaders");
     $this->callBack = mbGetValueFromPost("callback");
     unset($_POST["ajax"]);
     unset($_POST["suppressHeaders"]);
     unset($_POST["callback"]);
-    
+
     // UTF8 issue for Ajax
     if ($this->ajax) {
       foreach($_POST as $key => $value) {
-      	if(is_scalar($value)){
+        if(is_scalar($value)){
           $_POST[$key] = utf8_decode($value);
         }
       }
     }
-    
+
     // Object binding
     if (!$this->_obj->bind( $_POST )) {
       $AppUI->setMsg( $this->_obj->getError(), UI_MSG_ERROR );
@@ -72,10 +72,10 @@ class CDoObjectAddEdit {
       }
       $this->doRedirect();
     }
-    
+
     $this->_objBefore->load($this->_obj->_id);
   }
-  
+
   function doDelete() {
     global $AppUI;
 
@@ -93,8 +93,8 @@ class CDoObjectAddEdit {
       }
     }
   }
-  
-  function doStore () {
+
+  function doStore() {
     global $AppUI;
 
     if ($msg = $this->_obj->store()) {
@@ -113,7 +113,7 @@ class CDoObjectAddEdit {
       }
     }
   }
-  
+
   function doRedirect() {
     global $AppUI;
 
@@ -126,13 +126,13 @@ class CDoObjectAddEdit {
         echo "\n<script type='text/javascript'>$callBack($idValue);</script>";
       }
       exit;
-    } 
-    
+    }
+
     if ($this->redirect !== null) {
       $AppUI->redirect($this->redirect);
     }
   }
-  
+
   function doLog($type) {
     global $AppUI;
 
@@ -145,21 +145,21 @@ class CDoObjectAddEdit {
         }
       }
     }
-    
+
     $object_id = $this->_obj->_id;
     $type = "store";
     if ($this->_objBefore->_id == null) {
-      $type = "create"; 
+      $type = "create";
       $fields = array();
-    } 
-    
+    }
+
     if ($this->_obj->_id == null) {
-      $type = "delete"; 
+      $type = "delete";
       $object_id = $this->_objBefore->_id;
       $fields = array();
-    } 
+    }
 
-    if ($this->_logIt) {
+    if ($this->_logIt && count($fields)) {
       $log = new CuserLog;
       $log->user_id = $AppUI->user_id;
       $log->object_id = $object_id;
@@ -173,14 +173,14 @@ class CDoObjectAddEdit {
 
   function doIt() {
     $this->doBind();
-    
+
     if (intval(dPgetParam($_POST, 'del'))) {
       $this->doDelete();
     } else {
       $this->doStore();
     }
-    
+
     $this->doRedirect();
   }
-  
+
 }
