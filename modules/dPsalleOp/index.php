f5733120049530cbaa626a63422e7ee85fef5210
Sébastien Fillonneau <s.fillonneau@openxtrem.com>
2007-01-26 15:13:16 +0000

    Erg: Non affichage de l'onglet anesthesie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1511 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPsalleOp/index.php
+++ modules/dPsalleOp/index.php
@@ -13,6 +13,6 @@ $module->registerTab("vw_operations" , "Salles d'opération", TAB_READ);
 $module->registerTab("vw_reveil"     , "Salle de reveil"   , TAB_READ);
 //$module->registerTab("vw_brancardage", "Brancardage"       , TAB_READ);
 $module->registerTab("vw_urgences"   , "Liste des urgences", TAB_READ);
-$module->registerTab("vw_anesthesie" , "Anesthésie"        , TAB_READ);
+//$module->registerTab("vw_anesthesie" , "Anesthésie"        , TAB_READ);
 
 ?>
