4516c79963de2bbe9891d56f2ba437343bdf2929
Thomas Despoix <tomdesp@gmail.com>
2007-02-14 17:00:55 +0000

    Ref: Chrono with report based on tags
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1575 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/chrono.class.php
+++ classes/chrono.class.php
@@ -6,6 +6,7 @@
  *  @author  Thomas Despoix
  *  @version $Revision$
  */
+ 
 // Chronometer
 class Chronometer {
   var $total = 0;
@@ -13,6 +14,8 @@ class Chronometer {
   var $maxStep = 0;
   var $avgStep = 0;
   var $nbSteps = 0;
+  
+  var $report = array();
     
   function Chronometer() {
   }
@@ -27,15 +30,30 @@ class Chronometer {
     $this->step = $this->microtimeFloat();
   }
   
-  function stop() {
+  function stop($key = "") {
     $this->step = $this->microtimeFloat() - $this->step;
     $this->total += $this->step;
-    
-    if ($this->step > $this->maxStep) {
-			$this->maxStep = $this->step;
-		}
-    
+    $this->maxStep = max($this->maxStep, $this->step);
     $this->avgStep = $this->total/$this->nbSteps;
+
+    if ($key) {
+      if (!array_key_exists($key, $this->report)) {
+        $this->report[$key] = new CObject;
+        $report =& $this->report[$key];
+        $report->nbSteps = 0;
+        $report->step = 0;
+        $report->total = 0;
+        $report->maxStep = 0;
+        $report->avgStep = 0;
+      }
+      
+      $report =& $this->report[$key];
+      $report->nbSteps++;
+      $report->step = $this->step;
+      $report->total += $report->step;
+      $report->maxStep = max($report->maxStep, $report->step);
+      $report->avgStep = $report->total/$report->nbSteps;
+    } 
   }
 }
 ?>
