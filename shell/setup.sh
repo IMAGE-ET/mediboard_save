039827600eb9a897aeae4975e400fd8ca9cb999f
Romain Ollivier <ollivier@openxtrem.com>
2007-02-20 12:50:13 +0000

    fct : ajout du droit sur les fichiers de locales
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1615 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/setup.sh
+++ shell/setup.sh
@@ -39,7 +39,7 @@ SUB_DIR=$2
       SUB_PATH="style/*/templates_c/"
     else
       BASE_PATH="*"
-      SUB_PATH="lib/ tmp/ files/ includes/ modules/*/templates_c/ style/*/templates_c/ modules/dPinterop/hprim/"
+      SUB_PATH="lib/ tmp/ files/ includes/ modules/*/templates_c/ style/*/templates_c/ modules/dPinterop/hprim/ locales/*/"
     fi
   fi
 
