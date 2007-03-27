adbe80bc9a76ae10d77c8584a10244c77bc7882a
Thomas Despoix <tomdesp@gmail.com>
2007-03-27 14:00:24 +0000

    Erg: Warning sur la gestion des FSE dans l'index du module dPcabinet
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1768 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/index.php
+++ modules/dPcabinet/index.php
@@ -18,7 +18,7 @@ $module->registerTab("vw_dossier"       , null , TAB_EDIT);
 $module->registerTab("form_print_plages", null , TAB_READ);
 $module->registerTab("vw_compta"        , null , TAB_EDIT);
 
-if ($AppUI->user_prefs["GestionFSE"]) {
+if (@$AppUI->user_prefs["GestionFSE"]) {
   $module->registerTab("vw_intermax", null , TAB_EDIT);
 }
 
