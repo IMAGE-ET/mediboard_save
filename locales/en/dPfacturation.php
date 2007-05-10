57851125c1a326a0e6088b9cc13d40fb8455f51c
Yohann Poiron <poiron@openxtrem.com>
2007-05-10 15:42:48 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1931 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ locales/en/dPfacturation.php
@@ -0,0 +1,6 @@
+<?php
+$locales['mod-dPfacturation-tab-vw_idx_facture'] = '';
+$locales['base_url'] = 'http://localhost/';
+$locales['root_dir'] = '';
+$locales['site_domain'] = 'localhost';
+?>
---
+++ locales/fr/dPfacturation.php
@@ -0,0 +1,6 @@
+<?php
+$locales['mod-dPfacturation-tab-vw_idx_facture'] = 'Gestion des factures';
+$locales['base_url'] = 'http://localhost/';
+$locales['root_dir'] = '';
+$locales['site_domain'] = 'localhost';
+?>
