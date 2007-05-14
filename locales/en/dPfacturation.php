dc32cb5760c722e6224b48344fc5752ffd66fce1
Sébastien Fillonneau <s.fillonneau@openxtrem.com>
2007-05-14 07:11:55 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1947 a21d6be2-e512-0410-a060-91f5a2946631

--- locales/en/dPfacturation.php
+++ locales/en/dPfacturation.php
@@ -1,10 +1,13 @@
 <?php
+$locales['CFactureItem-taxe'] = '';
 $locales['base_url'] = 'http://localhost/';
 $locales['CFacture-date'] = 'Date';
 $locales['CFacture-prix'] = '';
 $locales['CFacture-sejour_id'] = '';
+$locales['CFactureItem-libelle'] = '';
+$locales['CFactureItem-prix_ht'] = '';
 $locales['mod-dPfacturation-tab-vw_idx_facture'] = '';
-$locales['mod-dPfacturation-tab-vw_idx_factureitem '] = '';
+$locales['mod-dPfacturation-tab-vw_idx_factureitem'] = '';
 $locales['root_dir'] = '';
 $locales['site_domain'] = 'localhost';
 ?>
--- locales/fr/dPfacturation.php
+++ locales/fr/dPfacturation.php
@@ -1,10 +1,13 @@
 <?php
+$locales['CFactureItem-taxe'] = 'Taxe';
 $locales['base_url'] = 'http://localhost/';
 $locales['CFacture-date'] = 'Date';
 $locales['CFacture-prix'] = 'Prix';
 $locales['CFacture-sejour_id'] = 'Sejour';
+$locales['CFactureItem-libelle'] = 'Libellé';
+$locales['CFactureItem-prix_ht'] = 'Prix HT';
 $locales['mod-dPfacturation-tab-vw_idx_facture'] = 'Gestion des factures';
-$locales['mod-dPfacturation-tab-vw_idx_factureitem '] = 'Gestion des éléments';
+$locales['mod-dPfacturation-tab-vw_idx_factureitem'] = 'Gestion des éléments';
 $locales['root_dir'] = '';
 $locales['site_domain'] = 'localhost';
 ?>
