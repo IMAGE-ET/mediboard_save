c03074605bf7e9ef5b558003850113ce2026cc9d
Romain Ollivier <ollivier@openxtrem.com>
2007-05-22 10:21:30 +0000

    bug : gestion des longeurs de champs input
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@1986 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/fieldSpecs/numSpec.class.php
+++ classes/fieldSpecs/numSpec.class.php
@@ -178,7 +178,7 @@ class CNumSpec extends CMbFieldSpec {
   }
 
   function getFormHtmlElement($object, $params, $value, $className){
-    if(!array_key_exists($params["size"])) {
+    if(!array_key_exists("size", $params)) {
       $length = max($this->length, $this->maxLength);
       $params["size"] = $length ? $length : 8;
     }
