b702de023a253ea23cf67ca778fe5f1977fd70df
Thomas Despoix <tomdesp@gmail.com>
2007-07-03 14:31:47 +0000

    Erg: Le SystemMsg s'éfface à la fin de l'initialisation
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2149 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/javascript/functions.js
+++ includes/javascript/functions.js
@@ -105,7 +105,7 @@ SystemMessage = {
 
   // Check message type (loading, notice, warning, error) from given div
   checkType: function(div) {
-    this.autohide = $A(div.childNodes).pluck("className").compact().last() == ["message"];
+    this.autohide = $A(div.childNodes).pluck("className").compact() == ["message"];
   },
 
   // Catches the innerHTML watch event
@@ -161,6 +161,11 @@ SystemMessage = {
     
     // Watch for content manipulation
     this.div.watch("innerHTML", this.refresh.bind(this));
+    
+    // Hide empty message
+    if (!this.div.innerHTML.strip()) {
+      new Effect.Fade(this.div);
+    }
   }
 }
 
