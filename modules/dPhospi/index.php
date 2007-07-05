6b8e7bdc1f1e7212e5a47dcbfa1235cb1a330949
Romain Ollivier <ollivier@openxtrem.com>
2007-07-05 07:10:52 +0000

    erg : déplacement de l'onglet des pathologies
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2169 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/index.php
+++ modules/dPhospi/index.php
@@ -13,9 +13,9 @@ $module->registerTab("form_print_planning", "Impression plannings"  , TAB_READ);
 $module->registerTab("edit_sorties"       , "Déplacements / Sorties", TAB_READ);
 $module->registerTab("vw_recherche"       , "Chercher une chambre"  , TAB_READ);
 $module->registerTab("vw_affectations"    , "Affectations"          , TAB_READ);
+$module->registerTab("vw_idx_pathologies" , "Pathologies"           , TAB_READ);
 $module->registerTab("vw_idx_sejour"      , "Consulter un séjour"   , TAB_READ);
 $module->registerTab("vw_suivi_bloc"      , "Suivi bloc"            , TAB_READ);
 $module->registerTab("vw_idx_chambres"    , "Chambres"              , TAB_ADMIN);
 $module->registerTab("vw_idx_services"    , "Services"              , TAB_ADMIN);
-$module->registerTab("vw_idx_pathologies"  , "Pathologies"          , TAB_READ);
 ?>
