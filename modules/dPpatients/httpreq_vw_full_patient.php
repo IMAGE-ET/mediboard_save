6498c7b8eed7b5f9fac2cd201ca8b59cf0d762d8
Romain Ollivier <ollivier@openxtrem.com>
2007-07-08 19:07:55 +0000

    bug : mise à jour du httpreq_vw_full_patient par rapport au vw_full_patient
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2194 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/httpreq_vw_full_patient.php
+++ modules/dPpatients/httpreq_vw_full_patient.php
@@ -7,7 +7,7 @@
 * @author Sébastien Fillonneau
 */
 
-global $AppUI, $can, $m;
+global $AppUI, $can, $m, $dPconfig;
 
 $can->needsRead();
 
@@ -31,11 +31,40 @@ $patient->loadRefsTraitements();
 
 $diagnosticsInstall = CModule::getActive("dPImeds") && CModule::getActive("dPsante400");
 
+$etablissements = CMediusers::loadEtablissements(PERM_EDIT);
+$codePraticienEc = null;
+$userSel = new CMediusers;
+$userSel->load($AppUI->user_id);
+
+$patient->makeDHEUrl();
+if(CModule::getInstalled("dPsante400") && ($dPconfig["interop"]["mode_compat"] == "medicap")) {
+  $tmpEtab = array();
+  foreach($etablissements as $etab) {
+    $idExt = new CIdSante400;
+    $idExt->loadLatestFor($etab);
+    if($idExt->id400) {
+      $tmpEtab[$idExt->id400] = $etab;
+    }
+  }
+  $etablissements = $tmpEtab;
+
+  $idExt = new CIdSante400;
+  $idExt->loadLatestFor($patient);
+  $patIdentEc = $idExt->id400;
+  $patient->_urlDHEParams["patIdentEc"]      = $patIdentEc;
+
+  $idExt = new CIdSante400;
+  $idExt->loadLatestFor($userSel);
+  $codePraticienEc = $idExt->id400;
+  $patient->_urlDHEParams["codePraticienEc"] = $codePraticienEc;
+}
+
 // Création du template
 $smarty = new CSmartyDP();
 
 $smarty->assign("canCabinet", CModule::getCanDo("dPcabinet"));
 
+$smarty->assign("codePraticienEc"    , $codePraticienEc   );
 $smarty->assign("listPrat"           , $listPrat          );
 $smarty->assign("patient"            , $patient           );
 $smarty->assign("diagnosticsInstall" , $diagnosticsInstall);
