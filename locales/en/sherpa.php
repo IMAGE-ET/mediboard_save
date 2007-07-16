c04cf727a2fd2b6245304744830bf0be9198458f
Thomas Despoix <tomdesp@gmail.com>
2007-07-16 16:11:22 +0000

    Erg: Locales de sherpa
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2272 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ locales/en/sherpa.php
@@ -0,0 +1,8 @@
+<?php
+$locales['config-object_handlers-CSpObjectHandler'] = '';
+$locales['CSpEtablissement.create'] = '';
+$locales['CSpObjectHandler'] = '';
+$locales['mod-sherpa-tab-view_etablissements'] = '';
+$locales['module-sherpa-court'] = '';
+$locales['module-sherpa-long'] = '';
+?>
---
+++ locales/fr/sherpa.php
@@ -0,0 +1,8 @@
+<?php
+$locales['config-object_handlers-CSpObjectHandler'] = 'Activer le gestionnaire Sherpa';
+$locales['CSpEtablissement.create'] = 'Créer un nouvel établissement';
+$locales['CSpObjectHandler'] = 'Gestionnaire Sherpa';
+$locales['mod-sherpa-tab-view_etablissements'] = 'Etablissements';
+$locales['module-sherpa-court'] = 'Sherpa';
+$locales['module-sherpa-long'] = 'Integration de e-Sherpa';
+?>
