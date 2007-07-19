3ab871fd1f8dbaa6fb563cf981d87ec3721898bb
Thomas Despoix <tomdesp@gmail.com>
2007-07-19 08:36:05 +0000

    Ref: Import base insee au format SQL dump
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2326 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/httpreq_do_add_insee.php
+++ modules/dPpatients/httpreq_do_add_insee.php
@@ -10,11 +10,11 @@
 global $AppUI, $can;
 
 $can->needsAdmin();
-
 $sourcePath = "modules/dPpatients/INSEE/insee.tar.gz";
 $targetDir = "tmp/insee";
+$targetPath = "tmp/insee/insee.sql";
 
-// Extract the CSV Files dump
+// Extract the SQL dump
 if (null == $nbFiles = CMbPath::extract($sourcePath, $targetDir)) {
   $AppUI->stepAjax("Erreur, impossible d'extraire l'archive", UI_MSG_ERROR);
 } 
@@ -22,61 +22,11 @@ if (null == $nbFiles = CMbPath::extract($sourcePath, $targetDir)) {
 $AppUI->stepAjax("Extraction de $nbFiles fichier(s)", UI_MSG_OK);
 
 $ds = CSQLDataSource::get("INSEE");
-
-// Create communes table
-$query = "DROP TABLE IF EXISTS `communes_france`";
-$ds->exec($query);
-$query = "CREATE TABLE `communes_france` (
-          `commune` varchar(25) NOT NULL default '',
-          `code_postal` varchar(5) NOT NULL default '',
-          `departement` varchar(25) NOT NULL default '',
-          `INSEE` varchar(5) NOT NULL default '',
-          PRIMARY KEY  (`INSEE`),
-          KEY `commune` (`commune`),
-          KEY `code_postal` (`code_postal`)
-        ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des informations sur les communes françaises';";
-$ds->exec($query);
-
-// Load communes data from CSV
-$targetPath = $AppUI->getTmpPath("insee/insee.csv");
-$query = "LOAD DATA LOCAL INFILE '$targetPath'" .
-    "\nINTO TABLE `communes_france`" .
-    "\nFIELDS TERMINATED BY ';'" .
-    "\nLINES TERMINATED BY '\r\n'";
-$ds->exec($query);
-if ($msg = $ds->error()) {
-  $AppUI->stepAjax("Erreur d'import CSV : $msg", UI_MSG_ERROR);
+if (null == $lineCount = $ds->queryDump($targetPath)) {
+  $msg = $ds->error();
+  $AppUI->stepAjax("Erreur de requête SQL: $msg", UI_MSG_ERROR);
 }
 
-$nbRows = $ds->affectedRows();
-$AppUI->stepAjax("Import villes INSEE effectué avec succès : $nbRows enregistrements", UI_MSG_OK);
-
-// Create pays table
-$query = "DROP TABLE IF EXISTS `pays`";
-$ds->exec($query);
-$query = "CREATE TABLE `pays` (
-          `numerique` mediumint(3) unsigned zerofill NOT NULL,
-          `alpha_3` char(3) NOT NULL default '',
-          `alpha_2` char(2) NOT NULL default '',
-          `nom_fr` varchar(255) NOT NULL default '',
-          `nom_ISO` varchar(255) NOT NULL default '',
-          PRIMARY KEY (`alpha_3`)
-        ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table des nom de pays';";
-$ds->exec($query);
-
-// Load pays data from CSV
-$targetPath = $AppUI->getTmpPath("insee/pays-iso.csv");
-$query = "LOAD DATA LOCAL INFILE '$targetPath'" .
-    "\nINTO TABLE `pays`" .
-    "\nFIELDS TERMINATED BY '|'" .
-    "\nENCLOSED BY ''" .
-    "\nLINES TERMINATED BY '\r\n'";
-$ds->exec($query);
-if ($msg = $ds->error()) {
-  $AppUI->stepAjax("Erreur d'import CSV : $msg", UI_MSG_ERROR);
-}
-
-$nbRows = $ds->affectedRows();
-$AppUI->stepAjax("Import pays ISO effectué avec succès : $nbRows enregistrements", UI_MSG_OK);
+$AppUI->stepAjax("import effectué avec succès de $lineCount lignes", UI_MSG_OK);
 
 ?>
