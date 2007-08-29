132388739e47ae0b319ab8ab4d6c78686efcc928
Thomas Despoix <tomdesp@gmail.com>
2007-08-29 12:02:06 +0000

    Bug: Amélioration du data source Ingres Erg: Cosmétique sur les login.tpl
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2458 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/system/httpreq_create_dsn.php
@@ -0,0 +1,49 @@
+<?php /* $Id: httpreq_do_empty_templates.php 982 2006-09-30 17:52:38Z MyttO $ */
+
+/**
+ * @package Mediboard
+ * @subpackage system
+ * @version $Revision: 982 $
+ * @author Thomas Despoix
+ * @license GNU GPL 
+ **/
+
+global $AppUI, $can, $dPconfig;
+
+$can->needsAdmin();
+
+// Check params
+if (null == $dsn = mbGetValueFromGet("dsn")) {
+  $AppUI->stepAjax("Aucun DSN spécifié", UI_MSG_ERROR);
+}
+
+if (null == @$dPconfig["db"][$dsn]) {
+  $AppUI->stepAjax("Configuration pour le DSN '$dsn' inexistante", UI_MSG_ERROR);
+}
+
+$dsConfig =& $dPconfig["db"][$dsn];
+if ("mysql" != $dbtype = $dsConfig["dbtype"]) {
+  $AppUI->stepAjax("Seules les DSN MySQL peuvent être créées par un accès administrateur", UI_MSG_ERROR);
+}
+
+// Substitute admin access
+$user = $dsConfig["dbuser"];
+$pass = $dsConfig["dbpass"];
+$name = $dsConfig["dbname"];
+
+$dsConfig["dbuser"] = mbGetValueFromGet("master_user");
+$dsConfig["dbpass"] = mbGetValueFromGet("master_pass");
+$dsConfig["dbname"] = "";
+
+$ds = CSQLDataSource::get($dsn);
+$AppUI->stepAjax("Connexion vers la DSN '$dsn' réussie");
+
+foreach ($ds->queriesForDSN($user, $pass, $name) as $key => $query) {
+  if (!$ds->exec($query)) {
+    $AppUI->stepAjax("Requête '$key' échouée", UI_MSG_WARNING);
+    continue;
+  }
+  
+  $AppUI->stepAjax("Requête '$key' effectuée", UI_MSG_OK);
+}
+
---
+++ modules/system/httpreq_test_dsn.php
@@ -0,0 +1,21 @@
+<?php /* $Id: httpreq_do_empty_templates.php 982 2006-09-30 17:52:38Z MyttO $ */
+
+/**
+ * @package Mediboard
+ * @subpackage system
+ * @version $Revision: 982 $
+ * @author Thomas Despoix
+ * @license GNU GPL 
+ **/
+
+global $AppUI, $can;
+
+$can->needsAdmin();
+
+// Check params
+if (null == $dsn = mbGetValueFromGet("dsn")) {
+  $AppUI->stepAjax("Aucun DSN spécifié", UI_MSG_ERROR);
+}
+
+$ds = CSQLDataSource::get($dsn);
+$AppUI->stepAjax("Connexion vers la DSN '$dsn' réussie");
