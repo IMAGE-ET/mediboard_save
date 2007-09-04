386c53016e3c98e55b63f17db794c99d3b3a54cc
Alexis Granger <granger@openxtrem.com>
2007-09-04 15:34:52 +0000

    fnc: affichage d'un fichier contenant les patients dans les lits à la date actuelle
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2515 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPhospi/get_etat_lits_txt.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+* @package Mediboard
+* @subpackage dPhospi
+* @version $Revision:  $
+* @author Alexis Granger
+*/
+
+/*
+Pour acceder à cette page ==>
+http://localhost/mediboard/index.php?m=dPhospi&a=get_etat_lits_txt&dialog=1&suppressHeaders=1;  
+*/
+
+global $AppUI, $can, $m, $g;
+
+// Date actuelle
+$date = mbDateTime();
+
+// Chargement des services
+$services = new CService;
+$services = $services->loadListWithPerms(PERM_READ);
+
+// Affectation a la date $date
+$affectation = new CAffectation();
+$whereAffect["entree"] = "<= '$date'";
+$whereAffect["sortie"] = ">= '$date'";
+$whereAffect["sejour_id"] = "!= '0'";
+$groupAffect = "sejour_id";
+
+$affectations = $affectation->loadList($whereAffect,null,null,$groupAffect);
+
+$list_affectations = array();
+
+foreach($affectations as $key=>$_affectation){
+   $_affectation->loadRefLit();
+   $_affectation->_ref_lit->loadRefChambre();
+   $_affectation->_ref_lit->_ref_chambre->loadRefsFwd();
+   $_affectation->loadRefSejour();
+   $_affectation->_ref_sejour->loadRefPraticien();
+   $_affectation->_ref_sejour->loadRefPatient();
+   
+   $list_affectations[$key]["nom"] = $_affectation->_ref_sejour->_ref_patient->nom;
+   $list_affectations[$key]["prenom"] = $_affectation->_ref_sejour->_ref_patient->prenom;
+   $list_affectations[$key]["id"] = $_affectation->_ref_sejour->_ref_patient->_id;
+   $list_affectations[$key]["service"] = $_affectation->_ref_lit->_ref_chambre->_ref_service->_id;
+   $list_affectations[$key]["chambre"] = $_affectation->_ref_lit->_ref_chambre->_id;
+   $list_affectations[$key]["lit"] = $_affectation->_ref_lit->_id;
+   $list_affectations[$key]["sexe"] = $_affectation->_ref_sejour->_ref_patient->sexe;
+   $list_affectations[$key]["naissance"] = mbTranformTime(null, $_affectation->_ref_sejour->_ref_patient->naissance, "%Y%m%d");
+   $list_affectations[$key]["date_entree"] = mbTranformTime(null, mbDate($_affectation->_ref_sejour->entree_reelle), "%Y%m%d"); 
+   $list_affectations[$key]["heure_entree"] = mbTranformTime(null, mbTime($_affectation->_ref_sejour->entree_reelle), "%H%M");
+   $list_affectations[$key]["date_sortie"] = mbTranformTime(null, mbDate($_affectation->_ref_sejour->sortie_reelle), "%Y%m%d"); 
+   $list_affectations[$key]["heure_sortie"] = mbTranformTime(null, mbTime($_affectation->_ref_sejour->sortie_reelle), "%H%M");
+}
+
+
+// Création du template
+$smarty = new CSmartyDP();
+
+$smarty->assign("list_affectations", $list_affectations);
+
+$smarty->display("get_etat_lits_txt.tpl");
+
+
+?>
