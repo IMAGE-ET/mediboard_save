27bf6f933384b34d7f591e05e6b57f7335918354
Romain Ollivier <ollivier@openxtrem.com>
2007-09-04 12:59:23 +0000

    bug : modification de la table t_malade
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2509 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/sherpa/setup.php
+++ modules/sherpa/setup.php
@@ -10,7 +10,7 @@
 // MODULE CONFIGURATION DEFINITION
 $config = array();
 $config["mod_name"]        = "sherpa";
-$config["mod_version"]     = "0.13";
+$config["mod_version"]     = "0.14";
 $config["mod_type"]        = "user";
 
 class CSetupsherpa extends CSetup {
@@ -21,7 +21,6 @@ class CSetupsherpa extends CSetup {
     $this->mod_name = "sherpa";
     
     $this->makeRevision("all");
-    
     $sql = "CREATE TABLE `t_malade` (" .
         "\n `malade_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT," .
         "\n `malnum` INT(6) UNSIGNED ZEROFILL," .
@@ -30,8 +29,8 @@ class CSetupsherpa extends CSetup {
         "\n `datnai` INT(8) UNSIGNED ZEROFILL," .
         "\nPRIMARY KEY (`malade_id`)) TYPE=MYISAM";
     $this->addQuery($sql);
+    
     $this->makeRevision("0.10");
-
     $sql = "CREATE TABLE `sp_etablissement` (" .
         "\n `sp_etab_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT," .
         "\n `group_id` INT(11) UNSIGNED NOT NULL," .
@@ -39,16 +38,24 @@ class CSetupsherpa extends CSetup {
         "\n `increment_patient` INT," .
         "\nPRIMARY KEY (`sp_etab_id`)) TYPE=MYISAM";
     $this->addQuery($sql);
+    
     $this->makeRevision("0.11");
-        
     $sql = "ALTER TABLE `t_malade` " .
         "\nDROP `malade_id`," .
         "\nCHANGE `malnum` `malnum` INT( 6 ) UNSIGNED ZEROFILL NOT NULL DEFAULT '0'," .
         "\nADD PRIMARY KEY ( `malnum` ) ;";
     $this->addQuery($sql);
+    
     $this->makeRevision("0.12");
+    
+    $this->makeRevision("0.13");
+    $sql = "ALTER TABLE `t_malade` " .
+          "\nCHANGE `malnom` `malnom` VARCHAR(20), " .
+          "\nCHANGE `malpre` `malpre` VARCHAR(10), " .
+          "\nCHANGE `datnai` `datnai` CHAR(10);";
+    $this->addQuery($sql);
 
-    $this->mod_version = "0.13";
+    $this->mod_version = "0.14";
     
   }
 }
