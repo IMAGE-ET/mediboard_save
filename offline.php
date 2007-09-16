bc441ea1abd59570bd7d94a14f6d537465ad11e8
Thomas Despoix <tomdesp@gmail.com>
2007-09-16 07:07:36 +0000

    Ref: Mode offline
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2628 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ offline.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html">
+<title>Mediboard offline</title>
+</head>
+<body>
+  Le système est désactivé pour cause de maintenance.
+  <br/> 
+  Merci de réessayer ultérieurement.
+</body>
+</html>
