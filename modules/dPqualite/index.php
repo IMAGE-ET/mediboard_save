675772d7891dbb611f4375d34c195371e9ee389a
Romain Ollivier <ollivier@openxtrem.com>
2007-09-18 06:42:41 +0000

    erg : rajout des fonctions de procedure
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2657 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPqualite/index.php
+++ modules/dPqualite/index.php
@@ -9,13 +9,13 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-//$module->registerTab("vw_procedures"         , null , TAB_READ);
-//$module->registerTab("vw_procencours"        , null , TAB_EDIT);
-//$module->registerTab("vw_procvalid"          , null , TAB_ADMIN);
-//$module->registerTab("vw_edit_classification", null , TAB_ADMIN);
-//$module->registerTab("vw_modeles"            , null , TAB_EDIT);
 $module->registerTab("vw_incident"           , null , TAB_READ);
 $module->registerTab("vw_incidentvalid"      , null , TAB_READ);
 $module->registerTab("vw_edit_ei"            , null , TAB_ADMIN);
+$module->registerTab("vw_procedures"         , null , TAB_READ);
+$module->registerTab("vw_procencours"        , null , TAB_EDIT);
+$module->registerTab("vw_procvalid"          , null , TAB_ADMIN);
+$module->registerTab("vw_edit_classification", null , TAB_ADMIN);
+$module->registerTab("vw_modeles"            , null , TAB_EDIT);
 
 ?>
