c2a60791041b9818ea90326c32cbeef3a0202fdf
Romain Ollivier <ollivier@openxtrem.com>
2007-09-25 13:51:14 +0000

    erg : correction de la légende
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2689 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/legende.php
+++ modules/dPbloc/legende.php
@@ -6,19 +6,19 @@
     <th colspan="2">Etat des salles</th>
   </tr>
   <tr>
-    <td style="background-image:url(images/icons/ray.gif); background-repeat:repeat;"></td>
-    <td class="text">Intervention terminée</td>
-  </tr>        
+    <td style="background-color:#ffa"></td>
+    <td class="text">Patient entré au bloc</td>
+  </tr>       
   <tr>
     <td style="background-color:#cfc"></td>
     <td class="text">Intervention en cours</td>
   </tr>
   <tr>
+    <td style="background-image:url(images/icons/ray.gif); background-repeat:repeat;"></td>
+    <td class="text">Intervention terminée</td>
+  </tr> 
+  <tr>
     <td style="background-color:#fcc"></td>
     <td class="text">Problème de timing</td>
-  </tr>
-  <tr>
-    <td style="background-color:#ffa"></td>
-    <td class="text">Intervention à venir</td>
   </tr>
 </table>
