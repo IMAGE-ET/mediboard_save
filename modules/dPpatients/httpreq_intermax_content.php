73ae2a09590ff5620fb4dada4b03ae6248fe40b3
Thomas Despoix <tomdesp@gmail.com>
2007-09-25 16:21:05 +0000

    Ref: callbakAjax supports json Ref: lecture vitale et framework intermax Ref: nettoyage url.js Fnc: Ajout de champs de régime santé dans la classe CPatient
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2693 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPpatients/httpreq_intermax_content.php
@@ -0,0 +1,20 @@
+<?php /* $Id:  $ */
+
+/**
+* @package Mediboard
+* @subpackage dPpatients
+* @version $Revision:  $
+* @author Thomas Despoix
+*/
+
+global $AppUI, $can;
+
+$can->needsRead();
+
+$intermax = mbGetValueFromGetOrSession("intermax", array());
+
+$fonction = @$intermax["FONCTION"]["NOM"];
+
+$AppUI->stepAjax("Fonction Intermax '$fonction', reçue", UI_MSG_OK);
+$AppUI->callbackAjax("Intermax.handleResult", $fonction);
+?>
