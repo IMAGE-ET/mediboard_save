b29ebb27fb10c2229a37eca5bcf271a8d905e8ed
Alexis Granger <granger@openxtrem.com>
2007-09-25 13:00:35 +0000

    erg: ajout de la legende pour les salles
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2685 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPbloc/legende.php
@@ -0,0 +1,24 @@
+<table class="tbl">
+  <tr>
+    <th class="title">Icone</th><th class="title">Description</th>
+  </tr>
+  <tr>
+    <th colspan="2">Etat des salles</th>
+  </tr>
+  <tr>
+    <td style="background-image:url(images/icons/ray.gif); background-repeat:repeat;"></td>
+    <td class="text">Intervention terminée</td>
+  </tr>        
+  <tr>
+    <td style="background-color:#cfc"></td>
+    <td class="text">Intervention en cours</td>
+  </tr>
+  <tr>
+    <td style="background-color:#fcc"></td>
+    <td class="text">Problème de timing</td>
+  </tr>
+  <tr>
+    <td style="background-color:#ffa"></td>
+    <td class="text">Intervention à venir</td>
+  </tr>
+</table>
--- modules/dPbloc/templates/vw_suivi_salles.tpl
+++ modules/dPbloc/templates/vw_suivi_salles.tpl
@@ -11,10 +11,21 @@ function printFeuilleBloc(oper_id) {
   url.addParam("operation_id", oper_id);
   url.popup(700, 600, 'FeuilleBloc');
 }
+
+function showLegend() {
+  var url = new Url;
+  url.setModuleAction("dPbloc", "legende");
+  url.popup(500, 150, "Legend");
+}
+
+
 </script>
 
 <table class="main">
   <tr>
+    <td>
+      <a href="#" onclick="showLegend()" class="buttonsearch">Légende</a>
+    </td>
     <th colspan="{{$listSalles|@count}}">
       {{$date_suivi|date_format:"%A %d %B %Y"}}
       <img id="changeDate" src="./images/icons/calendar.gif" title="Choisir la date" alt="calendar" />
