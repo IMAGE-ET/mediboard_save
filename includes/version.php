a19758a309cc6b70320ed8553b833e95b6d04a53
Thomas Despoix <tomdesp@gmail.com>
2007-10-04 16:43:48 +0000

    Erg: Affichage des statuts de date Ref: Amélioration de du system message sans leak Fnc: Amélioration de la recherche d'affectations de personnel
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@2772 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 143,
+  "build" => 144,
 );
 
 $version["string"] = join($version, ".");
