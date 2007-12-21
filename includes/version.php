266f4b0c6fda3922e564acc68ec48becfa16e391
Alexis Granger <granger@openxtrem.com>
2007-12-21 09:27:34 +0000

    ref: avancement du build (modification du CSS)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3132 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 163,
+  "build" => 164,
 );
 
 $version["string"] = join($version, ".");
