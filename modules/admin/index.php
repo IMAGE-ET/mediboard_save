655ae302326441065163be11c1d4197091962b31
Romain Ollivier <ollivier@openxtrem.com>
2008-01-11 13:07:29 +0000

    ref : droit à la grille en edit
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3176 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/index.php
+++ modules/admin/index.php
@@ -12,6 +12,6 @@ $module = CModule::getInstalled(basename(dirname(__FILE__)));
 $module->registerTab("vw_edit_users", null, TAB_READ);
 $module->registerTab("edit_perms"   , null, TAB_EDIT);
 $module->registerTab("edit_prefs"   , null, TAB_EDIT);
-$module->registerTab("vw_all_perms" , null, TAB_ADMIN);
+$module->registerTab("vw_all_perms" , null, TAB_EDIT);
 
 ?>
