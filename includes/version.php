703df75708e23ad557130d31e1ebe1bc222a786c
Thomas Despoix <tomdesp@gmail.com>
2008-01-28 16:00:35 +0000

    Erg: Numéro de version javascript
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3308 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 169,
+  "build" => 170,
 );
 
 $version["string"] = join($version, ".");
