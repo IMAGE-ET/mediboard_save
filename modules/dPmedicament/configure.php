aa3997ce35ff7d14b150cc6828148aceb1e7fb40
Alexis Granger <granger@openxtrem.com>
2008-01-29 14:34:26 +0000

    fnc: fichier de configuration de dPmedicament
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3314 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPmedicament/configure.php
@@ -0,0 +1,19 @@
+<?php /* $Id: $ */
+
+/**
+ * @package Mediboard
+ * @subpackage dPmedicament
+ * @version $Revision: $
+ * @author Alexis Granger
+ */
+
+global $can;
+
+$can->needsAdmin();
+
+// Création du template
+$smarty = new CSmartyDP();
+
+$smarty->display("configure.tpl");
+
+?>
---
+++ modules/dPmedicament/templates/configure.tpl
@@ -0,0 +1,1 @@
+{{include file="../../system/templates/configure_dsn.tpl" dsn=bcb}}
