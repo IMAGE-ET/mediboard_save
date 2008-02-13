5a1e0dd60c0e75bfb42bbc543c9eb99eb4dee29b
Romain Ollivier <ollivier@openxtrem.com>
2008-02-13 09:02:40 +0000

    edit : fin de la phase de test ftp
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3425 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/ftp.class.php
+++ classes/ftp.class.php
@@ -71,8 +71,8 @@ class CFTP {
     
     // Upload the file
     $upload = ftp_put($conn_id, $destination_file, $source_file, $mode);
-    $this->logError("Phase de test, document non envoyé");
-    return false;
+    //$this->logError("Phase de test, document non envoyé");
+    //return false;
     if (!$upload) {
       $this->logError("Impossible de copier le fichier source $source_base en fichier cible $destination_file");
       return false;
