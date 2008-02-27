6c7911f0b01ac2930f136e4564ad698ef7870ced
Romain Ollivier <ollivier@openxtrem.com>
2008-02-27 19:59:40 +0000

    bug : utilisation du numéro provisoire pour l'appel soap
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3537 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPlabo/prescriptionlabo.class.php
+++ modules/dPlabo/prescriptionlabo.class.php
@@ -104,8 +104,24 @@ class CPrescriptionLabo extends CMbObject {
     // Chargement de l'id externe de la prescription (tag: Imeds)
     $idExterne->loadLatestFor($this, "iMeds");
     if(!$idExterne->_id) {
+      // Afactoriser : assez complexe (concatenation du code 4 praticien et du code 4 prescription)
+      $this->loadRefsFwd();
+      $prat =& $this->_ref_praticien;
+
+      $tagCode4 = "labo code4";
+      $idSantePratCode4 = new CIdSante400();
+      $idSantePratCode4->loadLatestFor($prat, $tagCode4);
+	
+      $idPresc = new CIdSante400();
+      $idPresc->tag = "$tagCatalogue Prat:".str_pad($idSantePratCode4->id400, 4, '0', STR_PAD_LEFT); // tag LABO Prat: 0017
+      $idPresc->object_class = "CPrescriptionLabo";
+      $idPresc->loadMatchingObject("id400 DESC");
+      
+      $numprovisoire = str_pad($idSantePratCode4->id400, 4, '0', STR_PAD_LEFT).str_pad($idPresc->id400, 4, '0', STR_PAD_LEFT);
+
+      // Fin de la partie à factoriser
       $client = new SoapClient($dPconfig["dPlabo"]["CPrescriptionLabo"]["url_ws_id_prescription"], array('exceptions' => 0));
-      $result = $client->NDOSLAB(array("NumMedi" => $this->_id, "pwd" => $dPconfig["dPlabo"]["CPrescriptionLabo"]["pass_ws_id_prescription"]));
+      $result = $client->NDOSLAB(array("NumMedi" => $numprovisoire, "pwd" => $dPconfig["dPlabo"]["CPrescriptionLabo"]["pass_ws_id_prescription"]));
       if (is_soap_fault($result)) {
         trigger_error("SOAP Fault: (faultcode: {$result->faultcode}, faultstring: {$result->faultstring})", E_USER_ERROR);
       }
