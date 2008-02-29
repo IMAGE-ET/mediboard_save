e966e05dc13b2caf66761ea31950c3a7966e3b0c
Fabien Ménager <fabien.menager@openxtrem.com>
2008-02-29 13:30:06 +0000

    erg: Modele selector : accessibilité aux modeles de documents dits "incompatibles"
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3560 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/modele_selector.php
+++ modules/dPcompteRendu/modele_selector.php
@@ -32,17 +32,30 @@ $order = "nom";
 
 $wherePrat = array();
 $whereFunc = array();
+
 // Chargement des modeles pour chaque classe, pour les praticiens et leur fonction
 foreach($templateClasses as $class => $id) {
-  // Pour le praticien
-  $wherePrat["chir_id"] = "= '$praticien->_id'";
-  $wherePrat["object_class"] = "= '$class'";
-	$modeles[$class]['prat'] = CCompteRendu::loadModeleByCat(null, $wherePrat, $order, true);
-
-	// Pour la fonction du praticien
-  $whereFunc["function_id"] = "= '$praticien->function_id'";
-	$whereFunc["object_class"] = "= '$class'";
-	$modeles[$class]['func'] = CCompteRendu::loadModeleByCat(null, $whereFunc, $order, true);
+  if ($id) {
+    // Pour le praticien
+    $wherePrat["chir_id"] = "= '$praticien->_id'";
+    $wherePrat["object_class"] = "= '$class'";
+    $modelesCompat[$class]['prat'] = CCompteRendu::loadModeleByCat(null, $wherePrat, $order, true);
+  
+    // Pour la fonction du praticien
+    $whereFunc["function_id"] = "= '$praticien->function_id'";
+    $whereFunc["object_class"] = "= '$class'";
+    $modelesCompat[$class]['func'] = CCompteRendu::loadModeleByCat(null, $whereFunc, $order, true);
+  } else {
+    // Pour le praticien
+    $wherePrat["chir_id"] = "= '$praticien->_id'";
+    $wherePrat["object_class"] = "= '$class'";
+    $modelesNonCompat[$class]['prat'] = CCompteRendu::loadModeleByCat(null, $wherePrat, $order, true);
+  
+    // Pour la fonction du praticien
+    $whereFunc["function_id"] = "= '$praticien->function_id'";
+    $whereFunc["object_class"] = "= '$class'";
+    $modelesNonCompat[$class]['func'] = CCompteRendu::loadModeleByCat(null, $whereFunc, $order, true);
+  }
 }
 
 
@@ -51,8 +64,10 @@ $smarty = new CSmartyDP();
 
 $smarty->assign("target_id", $object->_id);
 $smarty->assign("target_class", $object->_class_name);
-$smarty->assign("templateClasses", $templateClasses);
-$smarty->assign("modeles", $modeles);
+
+$smarty->assign("modelesCompat", $modelesCompat);
+$smarty->assign("modelesNonCompat", $modelesNonCompat);
+$smarty->assign("modelesId", $templateClasses);
 
 $smarty->display("modele_selector.tpl");
 
