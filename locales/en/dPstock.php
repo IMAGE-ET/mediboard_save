fd2eadb7195bbf3df58e349449d0c6f25d156168
Fabien Ménager <fabien.menager@openxtrem.com>
2008-03-10 08:54:42 +0000

    Fnc: initial commit dPstock
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3631 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ locales/en/dPstock.php
@@ -0,0 +1,11 @@
+<?php
+$locales['mod-dPstock-tab-vw_idx_reference'] = "";
+$locales['base_url'] = "http://192.168.0.6/~fabien";
+$locales['mod-dPstock-tab-vw_idx_category'] = "";
+$locales['mod-dPstock-tab-vw_idx_order'] = "";
+$locales['mod-dPstock-tab-vw_idx_product'] = "";
+$locales['mod-dPstock-tab-vw_idx_societe'] = "";
+$locales['mod-dPstock-tab-vw_idx_stock'] = "";
+$locales['root_dir'] = "/home/fabien/www/mediboard";
+$locales['site_domain'] = "192.168.0.6";
+?>
