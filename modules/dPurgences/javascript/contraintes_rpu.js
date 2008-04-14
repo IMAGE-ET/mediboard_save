e4aa7d375107ab657b35ae2ace1de6b34605815f
Thomas Despoix <tomdesp@gmail.com>
2008-04-14 20:12:56 +0000

    Fnc: Sherpa: Retouches UPATOU Ref: Factorisation des ContraintesRPU Erg: Navigation UPATOU
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3881 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPurgences/javascript/contraintes_rpu.js
@@ -0,0 +1,108 @@
+ContraintesRPU = {
+  contraintesProvenance: [],
+  contraintesDestination: [],
+  contraintesOrientation: [],
+  
+	updateProvenance: function(mode_entree, clearField) {
+		var oSelect = document.editRPU.provenance;
+	
+		// Le champ peut être caché
+		if (!oSelect) {
+		  return;
+		}
+		
+		// On remet la valeur à zéro
+		if (clearField) {
+			oSelect.value = "";
+		}
+		
+	  if (mode_entree == "") {
+	    $A(oSelect).each( function(input) {
+	      input.disabled = false;
+	    });
+	    return;
+	  }
+	  
+	  var valeursPossibles = this.contraintesProvenance[mode_entree];
+
+	  if (!valeursPossibles) {
+	    $A(oSelect).each( function(input) {
+	      input.disabled = true;
+	    });
+	    return;
+	  }
+	 
+	  $A(oSelect).each( function(input) {
+	    input.disabled = !valeursPossibles.include(input.value);
+	  });
+  },
+  
+	updateDestination: function(mode_sortie, clearField) {
+		var oSelect = document.editRPUDest.destination;
+
+		// Le champ peut être caché
+		if (!oSelect) {
+		  return;
+		}
+		
+		// On remet la valeur à zéro
+		if (clearField) {
+			oSelect.value = "";
+		}
+		
+	  if (mode_sortie == "") {
+	    $A(oSelect).each( function(input) {
+	      input.disabled = false;
+	    });
+	    return;
+	  }
+	  
+	  var valeursPossibles = this.contraintesDestination[mode_sortie];
+
+	  if(!valeursPossibles){
+	    $A(oSelect).each( function(input) {
+	      input.disabled = true;
+	    });
+	    return;
+	  }
+	 
+	  $A(oSelect).each( function(input) {
+	    input.disabled = !valeursPossibles.include(input.value);
+	  });
+	},
+	
+	updateOrientation: function(mode_sortie, clearField) {
+		var oSelect = document.editRPUDest.orientation;
+
+		// Le champ peut être caché
+		if (!oSelect) {
+		  return;
+		}
+		
+		// On remet la valeur à zéro
+		if (clearField) {
+			oSelect.value = "";
+		}
+		
+	  if (mode_sortie == "") {
+	    $A(oSelect).each( function(input) {
+	      input.disabled = false;
+	    });
+	    return;
+	  }
+	  
+	  var valeursPossibles = this.contraintesOrientation[mode_sortie];
+
+	  if(!valeursPossibles){
+	    $A(oSelect).each( function(input) {
+	      input.disabled = true;
+	    });
+	    return;
+	  }
+	 
+	  $A(oSelect).each( function(input) {
+	    input.disabled = !valeursPossibles.include(input.value);
+	  });
+	}
+}
+
---
+++ modules/dPurgences/templates/configure.tpl
@@ -0,0 +1,35 @@
+<form name="editConfig" action="?m={{$m}}&amp;{{$actionType}}=configure" method="post" onsubmit="return checkForm(this)">
+
+<input type="hidden" name="m" value="system" />
+<input type="hidden" name="dosql" value="do_configure" />
+
+<table class="form">
+
+  <!-- Champs RPU -->  
+  <tr>
+    <th class="category" colspan="100">Mode RPU</th>
+  </tr>
+  
+  <tr>
+    {{assign var=var value=old_rpu}}
+    <th colspan="3">
+      <label for="{{$m}}[{{$var}}]" title="{{tr}}config-{{$m}}-{{$var}}{{/tr}}">
+        {{tr}}config-{{$m}}-{{$var}}{{/tr}}
+      </label>  
+    </th>
+    <td colspan="3">
+      <select class="bool" name="{{$m}}[{{$var}}]">
+        <option value="0" {{if 0 == $dPconfig.$m.$var}} selected="selected" {{/if}}>{{tr}}config-{{$m}}-{{$var}}-0{{/tr}}</option>
+        <option value="1" {{if 1 == $dPconfig.$m.$var}} selected="selected" {{/if}}>{{tr}}config-{{$m}}-{{$var}}-1{{/tr}}</option>
+      </select>
+    </td>
+  </tr>
+
+  <tr>
+    <td class="button" colspan="6">
+      <button class="modify" type="submit">{{tr}}Modify{{/tr}}</button>
+    </td>
+  </tr>
+</table>
+
+</form>
