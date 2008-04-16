b68243d646d6bd3101baf168c44c6f42f7e59773
Romain Ollivier <ollivier@openxtrem.com>
2008-04-16 08:03:23 +0000

    test : log des résultats Imeds
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3914 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPImeds/httpreq_soap_labo_results.php
+++ modules/dPImeds/httpreq_soap_labo_results.php
@@ -30,10 +30,9 @@ $idCIDC->loadLatestFor($etab, "Imeds cidc");
 $urlImeds = parse_url($dPconfig["dPImeds"]["url"]);
 
 $serviceAdresse = $urlImeds["scheme"]."://".$urlImeds["host"]."/dllimeds/webimeddll.asmx";
-
-mbTrace("avant", "log", true);
+mbTrace(class_exists("SoapClient"), "log", true);
 $client = new SoapClient($serviceAdresse."?WSDL", array('exceptions' => 0));
-mbTrace("apres", "log", true);
+
 $requestParams = array("strIDC"           => "$idCIDC->id400",
                        "strDIV"           => "$idCSDV->id400",
                        "strSDV"           => "$idCSDV->id400",
