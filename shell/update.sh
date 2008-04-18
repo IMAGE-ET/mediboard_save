5fe1069db3257aa62b9980d47e5ae90765158c3d
Thomas Despoix <tomdesp@gmail.com>
2008-04-18 15:12:46 +0000

    Sys: Script de mise à jour avec deux mode : show et do
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@3946 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/update.sh
+++ shell/update.sh
@@ -21,43 +21,49 @@ fi
 log=tmp/svnlog.txt
 prefixes="erg|fnc|fct|bug|warn|edit|syst|svn"
 
-case "$1" in \
-  show) \
-    echo "show" \
-    ;; \
-  do) \
-    echo "hide" \
-    ;; \
-  *) \
+case "$1" in
+  show)
+    svn info | awk 'NR==5'
+    svn log -r BASE:HEAD | grep -i -E "(${prefixes})" | tac
+    svn info -r HEAD | awk 'NR==5'
+    ;;
+    
+  do)
+    # Ensure log file exists
+    touch $log;
+    
+    # Log file is revered, make it straight
+    tac $log > $log.reversed
+    mv -f $log.reversed $log
+    
+    # Concat the source (BASE) revision number : 5th line of SVN info (!)
+    echo >> $log
+    svn info | awk 'NR==5' >> $log
+
+    # Concat SVN Log from BASE to HEAD
+    svn log -r BASE:HEAD | grep -i -E "(${prefixes})" >> $log
+    check_errs $? "Failed to append SVN log" "SVN log appended!"
+    
+    if [ "$1" = "do" ]
+    then
+      echo "+++ SHOULD UPDATE HERE +++" >> $log
+    fi
+    
+    # Concat the target (HEAD) revision number
+    echo >> $log
+    svn info | awk 'NR==5' >> $log
+    
+    # Concat dating info
+    echo "--- Updated Mediboard on $(date) ---" >> $log
+    echo >> $log
+    
+    ## Reverse the log file for user convenience
+    tac $log > $log.straight
+    mv -f $log.straight $log
+    ;;
+
+  *)
     echo "Action $1 unknown" \
     ;; \
 esac
-
-# Ensure log file exists
-touch $log;
-
-# Log file is revered, make it straight
-tac $log > $log.reversed
-mv -f $log.reversed $log
-
-# Concat SVN Log from BASE to HEAD
-svn log -r BASE:HEAD | grep -i -E "(${prefixes})" >> $log
-check_errs $? "Failed to append SVN log" "SVN log appended!"
-
-if [ "$1" = "do" ]
-then
-  echo "+++ SHOULD UPDATE HERE +++" >> $log
-fi
-
-# Concat the target (HEAD) revision number : 5th line of SVN info (!)
-echo >> $log
-svn info | awk 'NR==5' >> $log
-
-# Concat dating info
-echo "--- Updated Mediboard on $(date) ---" >> $log
-echo >> $log
-
-## Reverse the log file for user convenience
-tac $log > $log.straight
-mv -f $log.straight $log
 
