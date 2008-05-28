1f2d8596ce8df46235eb1fdbbacab755bd24133b
Fabien Ménager <fabien.menager@openxtrem.com>
2008-05-28 09:40:36 +0000

    bug: Modification de la librairie Flotr de .zip vers .tar.gz
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4221 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 207,
+  "build" => 208,
 );
 
 $version["string"] = join($version, ".");
