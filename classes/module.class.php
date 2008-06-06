c760795dcbf6cff7d7a24d843d460d9bc4738058
Thomas Despoix <tomdesp@gmail.com>
2008-06-06 13:20:14 +0000

    Bug: Légère notice sur l'upgrade de modules (user log sur CModule)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4279 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/module.class.php
+++ classes/module.class.php
@@ -95,7 +95,8 @@ class CModule extends CMbObject {
       "mod_name"      => "notNull str maxLength|20",
       "mod_type"      => "notNull enum list|core|user",
       "mod_version"   => "notNull str maxLength|6",
-      "mod_ui_active" => "num length|1", // should be "bool"
+      "mod_active" => "bool",
+      "mod_ui_active" => "bool",
       "mod_ui_order"  => "num"
     );
     return array_merge($specsParent, $specs);
