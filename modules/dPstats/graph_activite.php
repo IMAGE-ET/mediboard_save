1c6e5bc1c30377e9a12f6e655b0e33f18ded215e
Romain Ollivier <ollivier@openxtrem.com>
2008-07-01 14:38:22 +0000

    bug: récupération des données en get et pas à travers la session
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4400 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstats/graph_activite.php
+++ modules/dPstats/graph_activite.php
@@ -9,18 +9,18 @@
 
 global $m;
 
-$debutact      = mbGetValueFromGetOrSession("debut", mbDate("-1 YEAR"));
+$debutact      = mbGetValueFromGet("debut", mbDate("-1 YEAR"));
 $rectif        = mbTransformTime("+0 DAY", $debutact, "%d")-1;
 $debutact      = mbDate("-$rectif DAYS", $debutact);
-$finact        = mbGetValueFromGetOrSession("fin", mbDate());
+$finact        = mbGetValueFromGet("fin", mbDate());
 $rectif        = mbTransformTime("+0 DAY", $finact, "%d")-1;
 $finact        = mbDate("-$rectif DAYS", $finact);
 $finact        = mbDate("+ 1 MONTH", $finact);
 $finact        = mbDate("-1 DAY", $finact);
-$prat_id       = mbGetValueFromGetOrSession("prat_id", 0);
-$salle_id      = mbGetValueFromGetOrSession("salle_id", 0);
-$discipline_id = mbGetValueFromGetOrSession("discipline_id", 0);
-$codes_ccam    = strtoupper(mbGetValueFromGetOrSession("codes_ccam", ""));
+$prat_id       = mbGetValueFromGet("prat_id", 0);
+$salle_id      = mbGetValueFromGet("salle_id", 0);
+$discipline_id = mbGetValueFromGet("discipline_id", 0);
+$codes_ccam    = strtoupper(mbGetValueFromGet("codes_ccam", ""));
 
 CAppUI::requireModuleFile($m, "inc_graph_activite");
 // Finally send the graph to the browser
