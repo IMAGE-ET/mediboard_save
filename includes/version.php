e1ab265a9317c0f1fe92413c7d5ba045402f1347
Fabien Ménager <fabien.menager@openxtrem.com>
2008-07-03 12:01:33 +0000

    bug: resolution des bugs de superposition des elements transparents au dessus des tooltips et time pickers
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4425 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 215,
+  "build" => 216,
 );
 
 $version["string"] = join($version, ".");
