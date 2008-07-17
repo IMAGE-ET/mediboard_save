7b433fc32df87aec8b409e4e8deddcf957fb9e3c
Alexandre Germonneau <a.germonneau@openxtrem.com>
2008-07-17 08:10:00 +0000

    erg: Internationalisation du module cell saver.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4513 a21d6be2-e512-0410-a060-91f5a2946631

--- locales/en/bloodSalvage.php
+++ locales/en/bloodSalvage.php
@@ -1,3 +1,45 @@
 <?php
+$locales['CBloodSalvage-desc'] = "Description";
+$locales['mod-dPportail-tab-vw_blood_salvage'] = "Blood salvage";
+$locales['module-bloodSalvage-court'] = "Blood salvage";
+$locales['module-bloodSalvage-long'] = "intraoperative blood salvage";
+$locales['CBloodSalvage-_recuperation_start'] = 'start recuperation';
+$locales['CBloodSalvage-nurse_sspi'] = 'recovery room nurses';
+$locales['CBloodSalvage-_recuperation_end'] = 'end recuperation';
+$locales['CBloodSalvage-_transfusion_start'] = 'start transfusion';
+$locales['CBloodSalvage-_transfusion_end'] = 'end transfusion';
+$locales['CBloodSalvage-wash_volume'] = 'wash volume';
+$locales['CBloodSalvage-saved_volume'] = 'saved blood volume';
+$locales['CBloodSalvage-transfused_volume'] = 'transfused volume';
+$locales['CBloodSalvage-hgb_pocket'] = 'pocket hgb';
+$locales['CBloodSalvage-hgb_patient'] = 'post-transfusion patient hgb';
+$locales['mod-bloodSalvage-tab-vw_bloodSalvage'] = 'Cell Saver';
+$locales['mod-bloodSalvage-tab-vw_bloodSalvage_sspi'] = 'Recovery room';
+$locales['mod-bloodSalvage-tab-vw_cellSaver'] = 'Cell Saver manager';
+$locales['mod-bloodSalvage-tab-vw_typeEi_manager'] = 'Incident model manager';
+$locales['CBloodSalvage-anticoagulant_cip'] = 'Anticoagulant';
+$locales['CBloodSalvage-receive_kit'] = 'receive kit';
+$locales['CBloodSalvage-wash_kit'] = 'wash kit';
+$locales['CTypeEi.concerne.mat'] = 'Element';
+$locales['CTypeEi.concerne.med'] = 'Pill';
+$locales['CTypeEi.concerne.pat'] = 'Patient';
+$locales['CTypeEi.concerne.pers'] = 'Member';
+$locales['CTypeEi.concerne.vis'] = 'Guest';
+$locales['CTypeEi.concerne'] = 'Concerne';
+$locales['CTypeEi.name'] = 'Name';
+$locales['CTypeEi.desc'] = 'Description';
+$locales['CTypeEi-concerne'] = 'looks';
+$locales['CTypeEi-name'] = 'Name';
+$locales['CTypeEi-desc'] = 'Description';
+$locales['CTypeEi.modify'] = 'Modify model';
+$locales['CTypeEi.create'] = 'Create model';
+$locales['CTypeEi'] = 'Models';
+$locales['CCellSaver'] = 'Available cells saver list';
+$locales['CCellSaver.marque'] = 'Brand';
+$locales['CCellSaver-marque'] = 'Brand';
+$locales['CCellSaver.modele'] = 'Model';
+$locales['CCellSaver-modele'] = 'Model';
+$locales['CCellSaver.modify'] = 'Modify  Cell Saver';
+$locales['CCellSaver.create'] = 'Add a Cell Saver';
 
 ?>
