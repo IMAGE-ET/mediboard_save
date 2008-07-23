4611d3ef48c0d71f3ec43116d984df1aaa0152c4
Fabien Ménager <fabien.menager@openxtrem.com>
2008-07-23 12:05:55 +0000

    ref: mise à jour des locales de la pharmacie et des stocks
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4570 a21d6be2-e512-0410-a060-91f5a2946631

--- locales/en/pharmacie.php
+++ locales/en/pharmacie.php
@@ -3,5 +3,6 @@ $locales['mod-pharmacie-tab-vw_idx_delivrance'] = '';
 $locales['mod-pharmacie-tab-vw_idx_dispensation'] = '';
 $locales['mod-pharmacie-tab-vw_idx_prescriptions_sejour'] = '';
 $locales['mod-pharmacie-tab-vw_idx_destockage_service'] = '';
-$locales['mod-pharmacie-tab-vw_idx_dispensation_nominative'] = ''; 
+$locales['mod-pharmacie-tab-vw_idx_dispensation_nominative'] = '';
+$locales['mod-pharmacie-tab-vw_idx_restockage_service'] = '';
 ?>
