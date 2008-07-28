e4c552e233e4a2eae47a8ea8b2d3d503c768ec0c
Thomas Despoix <tomdesp@gmail.com>
2008-07-28 15:55:40 +0000

    Fnc: Purge du livret thérapeutique
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4619 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPmedicament/httpreq_purge_livret.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* @package Mediboard
+* @subpackage dPmedicament
+* @version $Revision: $
+* @author Alexis Granger
+*/
+
+global $can;
+$can->needsAdmin();
+
+// Comptage
+$count = CBcbProduitLivretTherapeutique::countProduits();
+CAppUI::stepAjax("Il y a $count produits dans le livret");
+
+// Purge
+if (-1 === $purge = CBcbProduitLivretTherapeutique::purgeProduits()) {
+  CAppUI::stepAjax("Impossible de supprimer les produits", UI_MSG_ERROR);
+}
+
+CAppUI::stepAjax("$purge produits supprimés dans le livret", UI_MSG_WARNING);
+?>
