db06b9317cd2604d5b5d50bf244e40977ce6e9c6
Romain Ollivier <ollivier@openxtrem.com>
2008-07-29 05:40:04 +0000

    edit : avancé du build
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4623 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -10,7 +10,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 238,
+  "build" => 239,
 );
 
 $version["string"] = join($version, ".");
