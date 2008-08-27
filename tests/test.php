d311d40ca52c72c1037383098c587d24fc8af219
Fabien Ménager <fabien.menager@openxtrem.com>
2008-08-27 18:13:19 +0000

    ref: retour a l'ancienne version du fichier de tests
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4702 a21d6be2-e512-0410-a060-91f5a2946631

--- tests/test.php
+++ tests/test.php
@@ -1,65 +1,22 @@
 <?php
-// ByteRun.com encode file decoder
 
-// Open and read the content of the encoded file into a variable
-$file = file_get_contents('BCBDci.php');
+// Creation de la connexion au serveur SOAP
+$soapAddress = "https://127.0.0.1/medilab/medilab.asmx?WSDL";
+$client = new SoapClient($soapAddress, array('exceptions' => 0)); 
 
-// Strip php tags
-$file = str_replace('<?php', "", $file);
-$file = str_replace('<?', "", $file);   // Make sure to get rid of short tags....
-$file = str_replace('?>', "", $file);
-$file = str_replace('$_F=__FILE__;', "", $file); // This stays the same in byterun.com encode files
+// Liste des fonctions disponibles
+var_dump($client->__getFunctions());
 
-// Create $_F variable
-$_F=__FILE__; // Always in the byterun.com encode files
+$NumMedi = "12345";
+$Pwd     = "67890";
 
-// Get $_X
-preg_match('/\$_X=\'.*?\';/', $file, $match);
+$result = $client->NDOSLAB(array("NumMedi" => $NumMedi, "pwd" => $Pwd));
 
-// Strip $_X='*'; from the string
-$file = str_replace($match[0], "", $file);
+if (is_soap_fault($result)) {
+trigger_error("SOAP Fault: (faultcode: {$result->faultcode}, faultstring: {$result->faultstring})", E_USER_ERROR);
+}
 
-//Create the $_X variable with the contents
-$_X = str_replace('$_X=\'', "", $match[0]);
-$_X = str_replace('\';', "", $_X);
+// Affichage de la requete
+var_dump($result);
 
-// Change the Eval function
-$file = str_replace('eval', 'echo ', $file);
-
-// Function to eval the new string
-function deval()
- {
-  global $file, $_F, $_X;
-
-  ob_start();
-  eval($file);
-  $contents = ob_get_contents();
-  ob_end_clean();
-  return($contents);
- }
-
-// Run the code thru once
-$file = deval();
-
-// We know that $_X=base64_decode($_X); is always there so lets go ahead and run that
-$_X = base64_decode($_X);
-
-// Strip it from the string
-$file = str_replace('$_X=base64_decode($_X);', "", $file);
-
-// Get the contents for $_X=strtr(*);
-preg_match('/\$_X=strtr.*?\'\);/', $file, $match2);
-
-// Strip it
-$file = str_replace($match2[0], "", $file);
-
-// Run it
-eval($match2[0]);
-
-$_R=ereg_replace('__FILE__',"'".$_F."'",$_X);
-
-$_R = ltrim($_R, '?><?php');
-$_R = rtrim($_R, '?>');
-
-echo $_R;
 ?>
