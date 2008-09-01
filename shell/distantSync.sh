1dce932298f894a31688108da43680b1a5b4e474
Romain Ollivier <ollivier@openxtrem.com>
2008-09-01 10:39:24 +0000

    fnc: prise en compte d'un paramètre PORT pour les synchronisations distantes
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4721 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/distantSync.sh
+++ shell/distantSync.sh
@@ -11,17 +11,24 @@ announce_script "Mediboard synchronisation"
 
 if [ "$#" -lt 1 ]
 then 
-  echo "Usage: $0 <location> <source directory> <destination>"
-  echo "  <source location> <source directory> <destination> is the remote location to be rsync-ed, ie root@oxmytto.homelinux.com"
+  echo "Usage: $0 <location> <source directory> <destination> <port (optionnal)>"
+  echo "  <source location>  is the remote location to be rsync-ed, ie root@oxmytto.homelinux.com"
   echo "  <source directory> is the remote directory to be rsync-ed, /var/www/"
-  echo "  <destination> is the target remote location, /var/backup/"
+  echo "  <destination>      is the target remote location, /var/backup/"
+  echo "  <port> (optionnal) is the ssh port af the target remote location, 22"
   exit 1
 fi
    
 location=$1
 directory=$2
 destination=$3
+if [ $4 ]
+then
+  port=$4
+else
+  port=22
+fi
 
 # Backups directory
-rsync -e ssh -avz $location:$directory $destination/$(echo $location | cut -d'@' -f2)
+rsync -e "ssh -p $port" -avz $location:$directory $destination/$(echo $location | cut -d'@' -f2)
 check_errs $? "Failed to rsync Backups directory" "Succesfully rsync-ed Backups directory!"
