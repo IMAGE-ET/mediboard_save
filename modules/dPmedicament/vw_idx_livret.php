c167e9b383b011e8375d9699265e77f792f54f37
Fabien Ménager <fabien.menager@openxtrem.com>
2008-09-03 09:17:34 +0000

    ref: suppression de chargements inutiles dans l'affichge du livret thérapeutique qui ralentissaient vraiment la page
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4734 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPmedicament/vw_idx_livret.php
+++ modules/dPmedicament/vw_idx_livret.php
@@ -9,21 +9,19 @@
 
 
 
-global $AppUI, $can, $m, $g;
+global $g;
 
 $lettre = mbGetValueFromGet("lettre");
 
 $listProduits = array();
+
 // Chargement de l'etablissement courant
-$etablissement = new CGroups();
-$etablissement->load($g);
+$etablissement = CGroups::loadCurrent();
 
 // Chargement des produits du livret therapeutique
 $etablissement->loadRefLivretTherapeutique();
 
-
-$tabLettre = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Z");
-
+$tabLettre = range('A', 'Z');
 
 // --- Chargement de l'arbre ATC ---
 $codeATC = mbGetValueFromGet("codeATC");
@@ -32,29 +30,19 @@ $classeATC = new CBcbClasseATC();
 $chapitreATC = $classeATC->getLibelle($codeATC);
 // Chargements des sous chapitres
 $arbreATC = $classeATC->loadArbre($codeATC);
-// Chargement des produits par classes ATC
-$classeATC->loadRefsProduits($codeATC);
-// Calcul du niveau du code
-$niveauCodeATC = $classeATC->getNiveau($codeATC);
-// Calcul du code de niveau superieur
-$codeNiveauSup = $classeATC->getCodeNiveauSup($codeATC);
 
 
 // Création du template
 $smarty = new CSmartyDP();
 
 $smarty->assign("listProduits", $listProduits);
-
 $smarty->assign("arbreATC", $arbreATC);
 $smarty->assign("codeATC", $codeATC);
-$smarty->assign("classeATC", $classeATC);
 $smarty->assign("chapitreATC", $chapitreATC);
-$smarty->assign("niveauCodeATC", $niveauCodeATC);
-$smarty->assign("codeNiveauSup", $codeNiveauSup);
-
 $smarty->assign("lettre", $lettre);
 $smarty->assign("produits_livret", $etablissement->_ref_produits_livret);
 $smarty->assign("tabLettre", $tabLettre);
+
 $smarty->display("vw_idx_livret.tpl");
 
 ?>
