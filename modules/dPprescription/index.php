a5ac65cc1ba69c7203021fbdedcbf3645d4d842b
Alexis Granger <granger@openxtrem.com>
2008-09-16 13:05:06 +0000

    erg: passage de tous les onglets de la prescription en ADMIN sauf celui de gestion des protocoles (EDIT)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4779 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPprescription/index.php
+++ modules/dPprescription/index.php
@@ -9,11 +9,11 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("vw_edit_prescription", null, TAB_READ);
-$module->registerTab("vw_edit_category", null, TAB_READ);
-$module->registerTab("vw_edit_element", null, TAB_READ);
-$module->registerTab("vw_edit_executant", null, TAB_READ);
-$module->registerTab("vw_edit_protocole", null, TAB_READ);
+//$module->registerTab("vw_edit_prescription", null, TAB_READ);
+$module->registerTab("vw_edit_category", null, TAB_ADMIN);
+$module->registerTab("vw_edit_element", null, TAB_ADMIN);
+$module->registerTab("vw_edit_executant", null, TAB_ADMIN);
+$module->registerTab("vw_edit_protocole", null, TAB_EDIT);
 $module->registerTab("vw_edit_associations_moments", null, TAB_ADMIN);
-$module->registerTab("vw_edit_moments_unitaires", null, TAB_EDIT);
+$module->registerTab("vw_edit_moments_unitaires", null, TAB_ADMIN);
 ?>
