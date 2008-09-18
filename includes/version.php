73ec0b4dbe6020a5349b3ec6c47bb34d4a2f1357
Fabien Ménager <fabien.menager@openxtrem.com>
2008-09-18 13:20:05 +0000

    erg: optgroups des la salle d'op agrandis
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4799 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -8,7 +8,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 261,
+  "build" => 262,
 );
 
 $version["string"] = implode(".", $version);
