8c71e89f3c4881dba8a949666e60a46e121f3ac7
Thomas Despoix <tomdesp@gmail.com>
2008-10-01 08:25:58 +0000

    Bug: La vérification de doublons à la volée posait de problème sur la date de naissance
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@4899 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -8,7 +8,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 272,
+  "build" => 273,
 );
 
 $version["string"] = implode(".", $version);
