1e61450edc8fa737635432b6477e33714064a945
Romain Ollivier <ollivier@openxtrem.com>
2008-10-17 08:17:44 +0000

    erg: réorganisation des onglets du module administration
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5015 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/index.php
+++ modules/system/index.php
@@ -9,12 +9,12 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("view_messages"       , null, TAB_READ);
 $module->registerTab("view_modules"        , null, TAB_READ);
+$module->registerTab("view_messages"       , null, TAB_READ);
 $module->registerTab("view_translate"      , null, TAB_READ);
-$module->registerTab("view_install"        , null, TAB_READ);
 $module->registerTab("view_history"        , null, TAB_READ);
 $module->registerTab("view_access_logs"    , null, TAB_READ);
 $module->registerTab("view_ressources_logs", null, TAB_READ);
+$module->registerTab("view_install"        , null, TAB_READ);
 
 ?>
