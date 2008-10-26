3c302c6829cfab313f668d7203484bca466a2ffd
Thomas Despoix <tomdesp@gmail.com>
2008-10-26 00:59:31 +0000

    Erg: Quelques locales manquantes dans Imeds
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5081 a21d6be2-e512-0410-a060-91f5a2946631

--- locales/en/dPImeds.php
+++ locales/en/dPImeds.php
@@ -1,7 +1,8 @@
 <?php
 $locales['module-dPImeds-court'] = 'Imeds';
 $locales['module-dPImeds-long'] = '';
-$locales['mod-dPImeds-tab-vw_id_imeds'] = "";
-$locales['config-dPImeds-url'] = "";
-$locales['mod-dPImeds-tab-vw_results'] = "";
+$locales['mod-dPImeds-tab-vw_id_imeds'] = '';
+$locales['config-dPImeds-url'] = '';
+$locales['mod-dPImeds-tab-vw_results'] = '';
+$locales['mod-dPImeds-tab-vw_soap_services'] = 'Web services';
 ?>
--- locales/fr/dPImeds.php
+++ locales/fr/dPImeds.php
@@ -1,7 +1,8 @@
 <?php
 $locales['module-dPImeds-court'] = 'Imeds';
 $locales['module-dPImeds-long'] = 'Interface Imeds';
-$locales['mod-dPImeds-tab-vw_id_imeds'] = "Identifiants Imeds";
-$locales['config-dPImeds-url'] = "Adresse du serveur Imeds";
-$locales['mod-dPImeds-tab-vw_results'] = "Consulter les résultats";
+$locales['mod-dPImeds-tab-vw_id_imeds'] = 'Identifiants Imeds';
+$locales['config-dPImeds-url'] = 'Adresse du serveur Imeds';
+$locales['mod-dPImeds-tab-vw_results'] = 'Consulter les résultats';
+$locales['mod-dPImeds-tab-vw_soap_services'] = 'Services web';
 ?>
