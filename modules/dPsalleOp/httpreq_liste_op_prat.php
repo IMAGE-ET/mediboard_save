9f292a5954b6d5bfc2279840a380d3142a53dc31
Thomas Despoix <tomdesp@gmail.com>
2008-10-28 21:33:03 +0000

    Bug: On ne chargeait pas les blocs pour permettre le transfert de plages dans la salle d'op en mode praticien
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5126 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPsalleOp/httpreq_liste_op_prat.php
+++ modules/dPsalleOp/httpreq_liste_op_prat.php
@@ -14,9 +14,8 @@ $can->needsRead();
 $date  = mbGetValueFromGetOrSession("date", mbDate());
 $operation_id = mbGetValueFromGetOrSession("operation_id");
 
-// Liste des salles
-$listSalles = new CSalle;
-$listSalles = $listSalles->loadGroupList();
+// Selection des salles
+$listBlocs = CGroups::loadCurrent()->loadBlocs(PERM_READ);
 
 // Chargement des chirurgiens
 $listPermPrats = new CMediusers;
@@ -57,7 +56,7 @@ $smarty = new CSmartyDP();
 $smarty->assign("vueReduite"    , false        );
 $smarty->assign("praticien"     , $praticien   );
 $smarty->assign("salle"         , new CSalle   );
-$smarty->assign("listSalles"    , $listSalles  );
+$smarty->assign("listBlocs"     , $listBlocs   );
 $smarty->assign("listPrats"     , $listPrats   );
 $smarty->assign("date"          , $date        );
 $smarty->assign("operation_id"  , $operation_id);
