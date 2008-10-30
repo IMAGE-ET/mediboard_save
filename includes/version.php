409f1dc456ff1b109466a7f1a9b4de61aad86b39
Fabien Ménager <fabien.menager@openxtrem.com>
2008-10-30 10:27:09 +0000

    erg: cases d'administration selectionnées entourées d'un bord plein plutot que pointillé
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5146 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 299,
+  "build" => 300,
 );
 
 $version["string"] = implode(".", $version);
