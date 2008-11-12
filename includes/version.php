431651231a91b65189c111e7fe2ec7dad6bc35eb
Fabien Ménager <fabien.menager@openxtrem.com>
2008-11-12 14:37:46 +0000

    bug: buttons class="notext" s'affichaient mal sous Firefox 2
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5209 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 311,
+  "build" => 312,
 );
 
 $version["string"] = implode(".", $version);
