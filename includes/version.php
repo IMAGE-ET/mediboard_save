aab7a942c4c3582bc3d1aec2c62639e2f361d8c7
Fabien Ménager <fabien.menager@openxtrem.com>
2008-11-13 09:48:14 +0000

    erg: bouton inutilisables sur le theme mediboard dans la salle d'op
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5211 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 312,
+  "build" => 313,
 );
 
 $version["string"] = implode(".", $version);
