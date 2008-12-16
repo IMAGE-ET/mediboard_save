6161cc5c6e43c40d0ec19d80d6bc73bd635098fd
Fabien Ménager <fabien.menager@openxtrem.com>
2008-12-16 13:14:33 +0000

    erg: reduction de la taille des champs de la page de login pour mieux coincider avec le bouton, quon ne peut pas trop agrandir
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5444 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 331,
+  "build" => 332,
 );
 
 $version["string"] = implode(".", $version);
