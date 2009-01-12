5bf037b42b903f2a631f68d7c434937b7c666996
Fabien Ménager <fabien.menager@openxtrem.com>
2009-01-12 11:09:00 +0000

    erg: mise a jour de quelques styles de formulaires dans le theme PhenX
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5503 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 337,
+  "build" => 338,
 );
 
 $version["string"] = implode(".", $version);
