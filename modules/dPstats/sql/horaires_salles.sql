1d48a59202b7e8a896b7e9905b58fe68d8fd0c32
Thomas Despoix <tomdesp@gmail.com>
2009-01-28 09:49:29 +0000

    Fnc: Script SQL des horaires de salles
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5603 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPstats/sql/horaires_salles.sql
@@ -0,0 +1,43 @@
+CREATE TEMPORARY TABLE operations_salle_date (
+  op_id INT(11) UNSIGNED,
+  entree TIME,
+  sortie TIME,
+  salle_id INT(11) UNSIGNED,
+  `date` DATE
+);
+
+INSERT INTO operations_salle_date (op_id, entree, sortie, salle_id, `date`)
+SELECT operation_id, entree_salle, sortie_salle, `salle_id` , `date`
+FROM `operations`
+WHERE `salle_id` IS NOT NULL
+AND `date` IS NOT NULL;
+
+INSERT INTO operations_salle_date (op_id, entree, sortie, salle_id, `date`)
+SELECT operation_id, entree_salle, sortie_salle, `operations` . `salle_id` , `plagesop` . `date`
+FROM `operations`, `plagesop` 
+WHERE `operations` . `salle_id` IS NOT NULL
+AND `plagesop` . plageop_id = operations.plageop_id;
+
+CREATE TEMPORARY TABLE es_minmax_salle_date (
+  nb_op INT(11) UNSIGNED,
+  entree TIME,
+  sortie TIME,
+  salle_id INT(11) UNSIGNED,
+  `date` DATE
+);
+
+INSERT INTO es_minmax_salle_date (nb_op, entree, sortie, salle_id, `date`)
+SELECT COUNT(op_id), MIN(entree), MAX(sortie), `salle_id`, `date`
+FROM operations_salle_date
+GROUP BY  `date`, `salle_id`;
+
+SELECT 
+  salle_id AS Salle,
+  SUM(nb_op) AS total_op, 
+  FORMAT(AVG(nb_op), 1) AS moyenne_op, 
+  sec_to_time(AVG(time_to_sec(entree))) AS moyenne_entree, 
+  sec_to_time(SQRT(VARIANCE(time_to_sec(entree)))) AS ecart_entree, 
+  sec_to_time(AVG(time_to_sec(sortie))) AS moyenne_sortie,
+  sec_to_time(SQRT(VARIANCE(time_to_sec(sortie)))) AS ecart_sortie
+FROM es_minmax_salle_date
+GROUP BY salle_id;
