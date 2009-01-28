169e7d158e3585563def67206e9a3996f3290847
Thomas Despoix <tomdesp@gmail.com>
2009-01-28 09:38:24 +0000

    Erg: Localisation des onglets du module de stats
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5602 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstats/index.php
+++ modules/dPstats/index.php
@@ -9,11 +9,11 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("vw_hospitalisation", "Hospitalisation"   , TAB_READ);
-$module->registerTab("vw_bloc"           , "Bloc opératoire"   , TAB_READ);
-$module->registerTab("vw_bloc2"          , "Journée opératoire", TAB_READ);
-$module->registerTab("vw_time_op"        , "Stats de durées"   , TAB_READ);
-$module->registerTab("vw_personnel_salle", "Personnel en salle", TAB_READ);
-$module->registerTab("vw_users"          , "Utilisateurs"      , TAB_ADMIN);
+$module->registerTab("vw_hospitalisation", NULL, TAB_READ);
+$module->registerTab("vw_bloc"           , NULL, TAB_READ);
+$module->registerTab("vw_bloc2"          , NULL, TAB_READ);
+$module->registerTab("vw_time_op"        , NULL, TAB_READ);
+$module->registerTab("vw_personnel_salle", NULL, TAB_READ);
+$module->registerTab("vw_users"          , NULL, TAB_ADMIN);
 
 ?>
