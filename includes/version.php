ccf401893a65f3641c8e8e088aaea63ac54e9604
Fabien Ménager <fabien.menager@openxtrem.com>
2009-02-06 14:21:51 +0000

    erg: ajout de quelques classes pour des couleurs de perenité au thème PhenX
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5643 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 353,
+  "build" => 354,
 );
 
 $version["string"] = implode(".", $version);
