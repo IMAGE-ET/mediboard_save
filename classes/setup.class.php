d0596675c29ee3de7c794a4588af6a66b9cb3387
Romain Ollivier <ollivier@openxtrem.com>
2009-03-02 09:57:21 +0000

    test : vérification de l'existence d'une préférence utilisateur avant de la rajouter
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@5735 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/setup.class.php
+++ classes/setup.class.php
@@ -96,8 +96,7 @@ class CSetup {
    * @param string $default Default value of the preference
    */
   function addPrefQuery($name, $default) {
-    $sqlTest = "SELECT * FROM `user_preferences` WHERE `pref_user` = '0' && `pref_name` = '$name' && `pref_value` = '$default'";
-    //mbTrace($sqlTest); exit;
+    $sqlTest = "SELECT * FROM `user_preferences` WHERE `pref_user` = '0' && `pref_name` = '$name'";
     $result = $this->ds->exec($sqlTest);
     if(!$this->ds->numRows($result)) {
       $sql = "INSERT INTO `user_preferences` ( `pref_user` , `pref_name` , `pref_value` )
