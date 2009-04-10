8902a69d7f379b0e67655dee0b98c45c58623b77
Yohann Poiron <poiron@openxtrem.com>
2009-04-10 07:09:00 +0000

    ref : Ajout du délais et temps de répétitions pour effectuer une requête sur mediboard ref : Changement de l'appel Curl par Wget
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6056 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/request.sh
+++ shell/request.sh
@@ -16,47 +16,43 @@ then
   echo "  <username> is the name of the user requesting, ie cron"
   echo "  <password is the password of the user requesting, ie ****"
   echo "  <params> is the GET param string for request, ie m=dPpatients&tab=vw_medecins"
+  echo "  <times> is the number of repetition, ie 4"
+  echo "  <delay> is the time between each repetition, ie 2"
   exit 1
 fi
    
-url=$1
-user=$2
-pass=$3
-params="$4"
+url="$1"
+login=login=1
+user=&username=$2
+pass=&password=$3
+params="&$4"
+times=$5
+delay=$6
 
 dir=/var/www/html/cron
 file=$dir/cron.html
-cookie=$dir/cookie.txt
-
-mediboard_connect() 
-{
-  curl $url/index.php \
-    --output $file \
-    --create-dirs \
-    --silent \
-    --insecure \
-    --data login=ok \
-    --data username=$user \
-    --data password=$pass \
-    --cookie-jar $cookie \
-    --cookie $cookie
-  check_errs $? "Failed to connect to Mediboard" "Connected to Mediboard!"
-}
 
 mediboard_request() 
 {
-  curl $url/index.php?$params\
-    --output $file \
-    --create-dirs \
-    --silent \
-    --write-out '- cUrled URL: %{url_effective}\n- download size %{size_download}\n- total time %{time_total}\n' \
-    --insecure \
-    --cookie $cookie
-  check_errs $? "Failed to request to Mediboard" "Mediboard requested!"
+   wget $url/index.php?$login$user$pass$params\
+     --output-file="$file"\
+     --force-directories\
+     --no-check-certificate
+   echo "wget URL : $url/index.php?$login$user$pass$params."
+   check_errs $? "Failed to request to Mediboard" "Mediboard requested!"  
 }
 
 rm -f $file
 check_errs $? "Failed to remove output file" "Output file removed!"
 
-mediboard_connect
-mediboard_request
+if [ "$#" -ne 6 ]
+then
+  mediboard_request
+else
+while [ $times -gt 0 ]
+do
+  times=$(($times - 1))
+  mediboard_request &
+  sleep $delay
+done
+fi
