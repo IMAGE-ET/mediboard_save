0f1e54959dff6730754353a9ca523e2440236b5b
Thomas Despoix <tomdesp@gmail.com>
2009-04-17 06:59:39 +0000

    Erg: Nouvelle version du BCB updater
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6108 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPmedicament/shell/updateBCB.sh
+++ modules/dPmedicament/shell/updateBCB.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 BASH_PATH=$(dirname $0)
-. $BASH_PATH/utils.sh
+ROOT_PATH="$BASH_PATH/../../.."
+. $ROOT_PATH/shell/utils.sh
+TMP_PATH="$ROOT_PATH/tmp"
 
 ########
 # Mediboard BCB updater
@@ -9,18 +11,69 @@ BASH_PATH=$(dirname $0)
 
 announce_script "Mediboard BCB updater"
 
-if [ "$#" -lt 4 ]
+if [ "$#" -lt 1 ]
 then 
-  echo "Usage: $0 <httpword> <database> <username> <password>
-  echo "  <httpword> is the Mediboard portal password to access bcb/ folder
-  echo "  <database> is the name of the database you want to empty and fill in
-  echo "  <username> is the the database username
-  echo "  <password> is the the database password
+  echo "Usage: $0 <password> [--skip-download]"
+  echo "  <password> is the Mediboard portal password to access bcb/ folder"
+  echo "  [--skip-download] to skip downloading the latest BCB dump"
   exit 1
 fi
-   
-httpword=$1
-database=$2
-username=$3
-password=$4
 
+password=$1
+archive="$TMP_PATH/bcbdump_latest.zip"
+
+if [[ $2 == "--skip-download" ]]
+then 
+  echo "BCB latest dump download skipped!"
+else
+	unlink $archive
+	check_errs $? "Failed to remove former BCB dump archive" "Former BCB dump archive removed!"   
+	
+	wget http://mediboard:$password@www.mediboard.org/bcb/bcbdump_latest.zip \
+	  --output-document $archive
+	check_errs $? "Failed to download BCB dump" "BCBDump downloaded!"
+fi
+
+dump="$TMP_PATH/bcbdump_latest.sql"
+unzip $archive -d $TMP_PATH
+check_errs $? "Failed to extract BCB dump" "BCBDump extracted!"
+
+config_path="$ROOT_PATH/includes/config.php"
+
+find_property() 
+{
+	property_pattern=$(echo $1 | sed 's/ /.*/g')
+	value_pattern=".*= [']\([a-z0-9]*\)['];"
+	replacement="\1"
+	$property_path
+	grep "$property_pattern" $config_path | sed "s/$value_pattern/$replacement/"
+}
+
+current_dsn=$(find_property "dPmedicament CBcbObject dsn")
+echo "Current BCB DSN detected is '$current_dsn'"
+
+update_dsn=""
+if [[ $current_dsn == "bcb1" ]]
+then 
+  update_dsn="bcb2"
+fi
+
+if [[ $current_dsn == "bcb2" ]]
+then
+  update_dsn="bcb1"
+fi
+
+if [[ -z $update_dsn ]]
+then 
+	echo "Current BCB DSN is neither 'bcb1' nor 'bcb2', 'bcb1' will be updated"
+  update_dsn="bcb1"
+fi
+
+echo "BCB DSN to update is '$update_dsn'"
+
+name=$(find_property "db $update_dsn dbname");
+user=$(find_property "db $update_dsn dbuser");
+pass=$(find_property "db $update_dsn dbpass");
+
+mysql -u $user -p $pass $name -e "show tables" | grep -v Tables_in | grep -v "+" | \ gawk '{print "drop table " $1 ";"}' | mysql -u $user -p $pass $name
+check_errs $? "Failed drop all tables in DSN '$update_dsn'" "All tables dropped in DSN '$update_dsn'"
