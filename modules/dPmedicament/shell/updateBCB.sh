3456ebd6117f830c0311dac8dd07031e093458ee
Thomas Despoix <tomdesp@gmail.com>
2009-04-17 13:43:21 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6118 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPmedicament/shell/updateBCB.sh
+++ modules/dPmedicament/shell/updateBCB.sh
@@ -21,23 +21,24 @@ fi
 
 password=$1
 archive="$TMP_PATH/bcbdump_latest.zip"
+dump="$TMP_PATH/mysqldump.sql"
 
-# Download latest BCB dump
-if [[ $2 == "--skip-download" ]]
+if [ $2 = "--skip-download" ]
 then 
   echo "BCB latest dump download skipped!"
 else
-	rm $archive
-	
-	wget http://mediboard:$password@www.mediboard.org/bcb/bcbdump_latest.zip \
-	  --output-document $archive
-	check_errs $? "Failed to download BCB dump" "BCBDump downloaded!"
-fi
+  # Download latest BCB Dump
+  rm -f $archive
+  wget http://mediboard:$password@www.mediboard.org/bcb/bcbdump_latest.zip \
+    --output-document $archive
+  check_errs $? "Failed to download BCB dump" "BCBDump downloaded!"
 
-# Extract latest BCB dump
-dump="$TMP_PATH/bcbdump_latest.sql"
-unzip $archive -d $TMP_PATH
-check_errs $? "Failed to extract BCB dump" "BCBDump extracted!"
+  # Extract latest BCB dump
+  dump="$TMP_PATH/mysqldump.sql"
+  rm -f $dump
+  unzip $archive -d $TMP_PATH
+  check_errs $? "Failed to extract BCB dump" "BCBDump extracted!"
+fi
 
 config_path="$ROOT_PATH/includes/config.php"
 
@@ -45,11 +46,10 @@ config_path="$ROOT_PATH/includes/config.php"
 # Retrieve a config in Mediboard config file
 find_property() 
 {
-	property_pattern=$(echo $1 | sed 's/ /.*/g')
-	value_pattern=".*= [']\([a-z0-9]*\)['];"
-	replacement="\1"
-	$property_path
-	grep "$property_pattern" $config_path | sed "s/$value_pattern/$replacement/"
+  property_pattern=$(echo $1 | sed "s/[a-z0-9]*/'&'/g" | sed "s/ /.*/g")
+  value_pattern=".*= [']\([a-z0-9]*\)['];"
+  replacement="\1"
+  grep "$property_pattern" $config_path | sed "s/$value_pattern/$replacement/"
 }
 
 # Detect current DSN
@@ -57,22 +57,18 @@ current_dsn=$(find_property "dPmedicament CBcbObject dsn")
 echo "Current BCB DSN detected is '$current_dsn'"
 
 # Compute DSN to be updated
-update_dsn=""
-if [[ $current_dsn == "bcb1" ]]
-then 
-  update_dsn="bcb2"
-fi
-
-if [[ $current_dsn == "bcb2" ]]
-then
-  update_dsn="bcb1"
-fi
-
-if [[ -z $update_dsn ]]
-then 
-	echo "Current BCB DSN is neither 'bcb1' nor 'bcb2', 'bcb1' will be updated"
-  update_dsn="bcb1"
-fi
+case "$current_dsn" in
+  bcb1)
+    update_dsn="bcb2"
+    ;;
+  bcb2)
+    update_dsn="bcb1"
+    ;;
+  *)
+    echo "Current BCB DSN is neither 'bcb1' nor 'bcb2'"
+    update_dsn="bcb1"
+    ;;
+esac
 
 echo "BCB DSN to update is '$update_dsn'"
 
@@ -80,10 +76,16 @@ echo "BCB DSN to update is '$update_dsn'"
 name=$(find_property "db $update_dsn dbname");
 user=$(find_property "db $update_dsn dbuser");
 pass=$(find_property "db $update_dsn dbpass");
+echo "BCB data source detail is: name '$name', user '$user', pass '$pass'"
 
 # Empty the BCB data source
-mysql -u $user -p $pass $name -e "show tables" | grep -v Tables_in | grep -v "+" | \ gawk '{print "drop table " $1 ";"}' | mysql -u $user -p $pass $name
-check_errs $? "Failed drop all tables in DSN '$update_dsn'" "All tables dropped in DSN '$update_dsn'"
+mysql -u $user -p$pass $name -e "show tables" \
+ | grep -v Tables_in  \
+ | grep -v "+" \
+ | awk '{print "drop table " $1 ";"}' \
+ | mysql -u $user -p$pass $name
+check_errs $? "Failed to empty '$name' database" "Database '$name' emptied!"
 
-# Fill inEmpty the BCB data source
-
+# Fill in BDB dump
+mysql -u $user -p$pass $name < $dump
+check_errs $? "Failed to fill '$name' database with BCB dump" "BCB dump filled in '$name' database!"
