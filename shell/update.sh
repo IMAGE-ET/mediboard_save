edab50e7e542b43be0f4c3c2cdbd052ab37b761d
Thomas Despoix <tomdesp@gmail.com>
2009-05-11 13:32:45 +0000

    Bug: Chemins relatifs pour le script de mise à jour
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6257 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/update.sh
+++ shell/update.sh
@@ -19,8 +19,9 @@ then
   exit 1
 fi
    
-log=$BASH_PATH/../tmp/svnlog.txt
-tmp=$BASH_PATH/../tmp/svnlog.tmp
+MB_PATH=$BASH_PATH/..
+log=$MB_PATH/tmp/svnlog.txt
+tmp=$MB_PATH/tmp/svnlog.tmp
 prefixes="erg|fnc|fct|bug|war|edi|sys|svn"
 revision=HEAD
 
@@ -32,28 +33,28 @@ fi
 
 case "$1" in
   info)
-    svn info | awk 'NR==5'
-    svn log -r BASE:$revision | grep -i -E "(${prefixes})"
-    svn info -r $revision | awk 'NR==5'
+    svn info $MB_PATH | awk 'NR==5'
+    svn log  $MB_PATH -r BASE:$revision | grep -i -E "(${prefixes})"
+    svn info $MB_PATH -r $revision | awk 'NR==5'
     ;;
     
   real)
     # Concat the source (BASE) revision number : 5th line of SVN info (!)
-    svn info | awk 'NR==5' > $tmp
+    svn info $MB_PATH | awk 'NR==5' > $tmp
     check_errs $? "Failed to get source revision info" "SVN Revision source info written!"
     echo >> $tmp
 
     # Concat SVN Log from BASE to target revision
-    svn log -r BASE:$revision | grep -i -E "(${prefixes})" >> $tmp
+    svn log $MB_PATH -r BASE:$revision | grep -i -E "(${prefixes})" >> $tmp
     check_errs $? "Failed to parse SVN log" "SVN log parsed!"
     
     # Perform actual update
-    svn update --revision $revision
+    svn update $MB_PATH --revision $revision
     check_errs $? "Failed to perform SVN update" "SVN updated performed!"
 
     # Concat the target revision number
     echo >> $tmp
-    svn info | awk 'NR==5' >> $tmp
+    svn info $MB_PATH | awk 'NR==5' >> $tmp
     check_errs $? "Failed to get target revision info" "SVN Revision target info written!"
 
     # Concat dating info
