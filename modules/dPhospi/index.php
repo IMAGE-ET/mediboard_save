777f4b5bb8021e277b3cedb56c387b079618168a
Alexis Granger <granger@openxtrem.com>
2009-06-03 07:39:36 +0000

    erg: suppression des onglets "Consulter un sejour" et "Bilan des prescriptions" dans dPhospi
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6385 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/index.php
+++ modules/dPhospi/index.php
@@ -14,10 +14,12 @@ $module->registerTab("edit_sorties"               , null, TAB_READ);
 $module->registerTab("vw_recherche"               , null, TAB_READ);
 $module->registerTab("vw_affectations"            , null, TAB_READ);
 $module->registerTab("vw_idx_pathologies"         , null, TAB_READ);
+/*
 $module->registerTab("vw_idx_sejour"              , null, TAB_READ);
 if(CModule::getActive("dPprescription")){
 $module->registerTab("vw_bilan_prescription"      , null, TAB_READ);
 }
+*/
 $module->registerTab("vw_suivi_bloc"              , null, TAB_READ);
 $module->registerTab("vw_idx_chambres"            , null, TAB_ADMIN);
 ?>
