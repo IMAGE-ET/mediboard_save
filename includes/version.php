6639ae2b61f1a428c1927208d6c1332c7873732d
Fabien Ménager <fabien.menager@openxtrem.com>
2009-06-23 15:22:05 +0000

    erg: ajout de menus deroulants qui apparaissent en cliquant sur les fleches de changement de mois et d'années
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6466 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 435,
+  "build" => 436,
 );
 
 $version["string"] = implode(".", $version);
