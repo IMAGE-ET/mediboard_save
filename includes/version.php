cd7809ab96bab1f3f977e607f51b2c0aa4ff9ae8
Alexis Granger <granger@openxtrem.com>
2009-06-26 09:00:19 +0000

    bug: version.php
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6500 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 441,
+  "build" => 442,
 );
 
 $version["string"] = implode(".", $version);
