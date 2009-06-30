33bc2944a695473557a890bd7f405fc3ef2f497e
Fabien Ménager <fabien.menager@openxtrem.com>
2009-06-30 15:48:19 +0000

    erg: l'icone de pliage du menu sous le theme e-cap et medilab decalaient le nom du premier module
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6526 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 444,
+  "build" => 445,
 );
 
 $version["string"] = implode(".", $version);
