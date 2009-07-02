94fa3aafdfd1301bc4b646cf0ae2a4cd989933de
Fabien Ménager <fabien.menager@openxtrem.com>
2009-07-02 14:04:18 +0000

    erg: le plan de cueillette effectue le tri par nom de produit
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6550 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/pharmacie/print_prepare_plan.php
+++ modules/pharmacie/print_prepare_plan.php
@@ -20,17 +20,19 @@ mbSetValueToSession('_date_max', $date_max);
 $date_min = "$date_min 00:00:00";
 $date_max = "$date_max 23:59:59";
 
-$order_by = 'product_stock_location.position ASC';
+$order_by = 'product_stock_location.position ASC, product.name ASC';
 
 $where = array (
   "product_delivery.date_dispensation BETWEEN '$date_min' AND '$date_max'",
   'product_delivery.quantity' => " > 0",
+  //'product_delivery.order' => " != '1'",
   'product_delivery.patient_id' => "IS NULL",
 );
 
 $ljoin = array(
   'product_stock_group' => 'product_stock_group.stock_id = product_delivery.stock_id',
   'product_stock_location' => 'product_stock_location.stock_location_id = product_stock_group.location_id',
+  'product' => 'product.product_id = product_stock_group.product_id',
 );
 
 $list_services = new CService();
