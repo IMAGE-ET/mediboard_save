5ccb49cde3e9fc81351da17b84721b366b33f421
Fabien Ménager <fabien.menager@openxtrem.com>
2009-07-03 08:26:08 +0000

    erg: petite amélioration des numericField
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6555 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 449,
+  "build" => 450,
 );
 
 $version["string"] = implode(".", $version);
