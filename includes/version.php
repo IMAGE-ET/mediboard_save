fa0316b9327a00c9c29c260c25a62f135e055329
Alexis Granger <granger@openxtrem.com>
2009-07-06 12:38:20 +0000

    erg: affichage des tick au niveau des tarifs
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6589 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 452,
+  "build" => 453,
 );
 
 $version["string"] = implode(".", $version);
