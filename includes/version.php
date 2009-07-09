104727a7543150a454ef48b636d49502369219bb
Fabien Ménager <fabien.menager@openxtrem.com>
2009-07-09 12:59:27 +0000

    ref: increment de la version
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6623 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 456,
+  "build" => 457,
 );
 
 $version["string"] = implode(".", $version);
