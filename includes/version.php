712f6880cbd08b7d5e6a2734f70d0f31f4ff276a
Fabien Ménager <fabien.menager@openxtrem.com>
2009-07-10 14:22:39 +0000

    bug: meilleur affichage des a.button
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6633 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 459,
+  "build" => 460,
 );
 
 $version["string"] = implode(".", $version);
