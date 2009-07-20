28f0a246b44063e5f39f43a42d70c3e9df56b1c1
Fabien Ménager <fabien.menager@openxtrem.com>
2009-07-20 11:53:36 +0000

    ref: pas de recherche par nom de naissance par lecture VitaleVision
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6664 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 462,
+  "build" => 463,
 );
 
 $version["string"] = implode(".", $version);
