09c552bac6f80d5fe7a43e6dfd8844e01eaadfea
Fabien Ménager <fabien.menager@openxtrem.com>
2009-08-26 13:22:10 +0000

    erg: amélioration des numeric field en mode tactile
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6800 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 485,
+  "build" => 486,
 );
 
 $version["string"] = implode(".", $version);
