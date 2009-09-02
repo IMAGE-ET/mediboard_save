dc75a5236b2750ccbceb62adf4527aa519dc8b28
Fabien Ménager <fabien.menager@openxtrem.com>
2009-09-02 08:30:27 +0000

    bug: les a.button s'affichaient mal
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6834 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 492,
+  "build" => 493,
 );
 
 $version["string"] = implode(".", $version);
