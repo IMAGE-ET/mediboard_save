2bbbfd1990e9b5bd0360428b8cafcda66759a082
Fabien Ménager <fabien.menager@openxtrem.com>
2009-09-02 10:00:23 +0000

    erg: les controles sont plus grand de quelques pixels en mode touchscreen
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6836 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 493,
+  "build" => 494,
 );
 
 $version["string"] = implode(".", $version);
