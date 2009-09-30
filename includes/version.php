084dfb3dbd5a028b815acc0a1b656575417dba8f
Fabien Ménager <fabien.menager@openxtrem.com>
2009-09-30 13:12:15 +0000

    ref: mise à jour du version.php
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@6975 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 506,
+  "build" => 507,
 );
 
 $version["string"] = implode(".", $version);
