2a9add1c46c405fb51fae54310359eafa15ab3e1
Yohann Poiron <poiron@openxtrem.com>
2009-10-08 10:58:02 +0000

    fnc: Ajout des codes (CG, SP et SF) pour la NGAP
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7020 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPccam/setup.php
+++ modules/dPccam/setup.php
@@ -39,9 +39,9 @@ class CSetupdPccam extends CSetup {
 
     // Data source query
     $query = "SELECT *
-			FROM `codes_ngap`
-			WHERE code = 'FSD' 
-			AND tarif = '35'";
+      FROM `codes_ngap`
+      WHERE code = 'CG' 
+      AND tarif = '19'";
     $this->addDatasource("ccamV2", $query);
   }
 }
