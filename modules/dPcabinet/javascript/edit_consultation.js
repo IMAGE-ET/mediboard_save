8dbcecef0031b474c861d5c2c4f6ed70b08337ea
Thomas Despoix <tomdesp@gmail.com>
2009-10-28 15:56:21 +0000

    SVN: Suppression anormale d'un fichier
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7149 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPcabinet/javascript/edit_consultation.js
@@ -0,0 +1,34 @@
+// $Id: $
+
+var ListConsults = {
+  target: "listConsult",
+  request: null,
+
+  init: function(consult_id, prat_id, date, vue, current_m) {
+    var url = new Url("dPcabinet", "httpreq_vw_list_consult");
+    url.addParam("selConsult", consult_id);
+    url.addParam("prat_id", prat_id);
+    url.addParam("date", date);
+    url.addParam("vue2", vue);
+    url.addParam("current_m", current_m);
+    this.request = url.periodicalUpdate(this.target, { frequency: 90, waitingText: null } );
+
+    if (!consult_id || Preferences.dPcabinet_show_program == "0") {
+      $(this.hide());    
+    }
+  },
+  
+  hide: function() {
+    this.request.stop();
+    $(this.target).hide();    
+  },
+  
+  show: function() {
+    this.request.start();
+    $(this.target).appear();    
+  },
+  
+  toggle: function() {
+    this[$(this.target).visible() ? "hide" : "show"](); 
+  }
+}
