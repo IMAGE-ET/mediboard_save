05f3adb409ee49cefcb2c73152dac2de30b588b0
Thomas Despoix <tomdesp@gmail.com>
2009-11-09 10:28:06 +0000

    Sys: Ping logger with auto log path
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7263 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/logPing.sh
+++ shell/logPing.sh
@@ -11,19 +11,20 @@ announce_script "Ping logger"
 
 if [ "$#" -lt 1 ]
 then 
-  echo "Usage: $0 <host> <file>"
+  echo "Usage: $0 <host>"
   echo "  <host> is the target of the ping, could be a hostname, domain name, or ip address, yet anything pingable, ie mediboard.org"
-  echo "  <file> is the target for log, ie /var/log/ping.log"
   exit 1
 fi
 
 host=$1
-file=$2
 
 ## Make the log line
 dt=$(date '+%Y-%m-%dT%H:%M:%S'); 
 ping=$(ping $host -c 4 | tail -n 1); 
 
 ## Log the line
+dir="/var/log/ping"
+file="$dir/$host.log"
+force_dir $dir
 echo "$dt $ping" >> $file
 check_errs $? "Failed to log ping" "Ping logged!"
