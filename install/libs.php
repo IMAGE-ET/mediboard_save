7d775220313765aface2e33b4322a55f56b6409f
Fabien Ménager <fabien.menager@openxtrem.com>
2009-11-16 14:45:01 +0000

    ref: mise à jour de Flotr (quelques corrections sur les bar graph et les pie charts)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7336 a21d6be2-e512-0410-a060-91f5a2946631

--- install/libs.php
+++ install/libs.php
@@ -305,12 +305,12 @@ CLibrary::$all[$library->name] = $library;
 $library = new CLibrary;
 $library->name = "Flotr plotting library";
 $library->url = "http://solutoire.com/flotr/";
-$library->fileName = "flotr.r150.tar.gz";
+$library->fileName = "flotr.r152.tar.gz";
 $library->description = "Création de graphiques en JS";
 $library->sourceDir = "flotr";
 $library->targetDir = "flotr";
 $library->versionFile = "flotr.js";
-$library->versionString = '$Id: flotr.js 150';
+$library->versionString = '$Id: flotr.js 152';
 
 CLibrary::$all[$library->name] = $library;
 
