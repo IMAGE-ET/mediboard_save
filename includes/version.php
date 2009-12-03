d0bb4a880218f3dfaff59ced72ef1c0ab85580ea
Fabien Ménager <fabien.menager@openxtrem.com>
2009-12-03 11:09:08 +0000

    erg: mise a jour des couleurs des differents control_tabs du theme phenx
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7504 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 541,
+  "build" => 542,
 );
 
 $version["string"] = implode(".", $version);
