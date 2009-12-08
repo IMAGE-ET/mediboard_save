c98e783547515ee8a3d67243b33d39821fe9c546
Fabien Ménager <fabien.menager@openxtrem.com>
2009-12-08 13:28:26 +0000

    bug: l'impression de documents multiple ne fonctionnait pas bien si plusieurs copies du meme document
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7553 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/print_docs.php
+++ modules/dPcabinet/print_docs.php
@@ -24,48 +24,44 @@ $can->needsObject($consult);
 $headerFound = $footerFound = false;
 
 $consult->loadRefsDocs();
-$docs_ids = array_keys($consult->_ref_documents);
+
 foreach($nbDoc as $compte_rendu_id => $nb_print){
-  if($nb_print > 0 && in_array($compte_rendu_id, $docs_ids)){
+  if(($nb_print > 0) && isset($consult->_ref_documents[$compte_rendu_id])){
     for($i = 1; $i <= $nb_print; $i++){
-      $doc = $consult->_ref_documents[$compte_rendu_id];
-      
-      // Suppression des headers et footers en trop (tous sauf le premier)
-      $xml = new DOMDocument;
-      $source = utf8_encode("<div>$doc->source</div>");
-      $source = preg_replace("/&\w+;/i", "", $source);
-      
-      @$xml->loadXML($source);
-      $xpath = new DOMXPath($xml);
-      
-      $nodeList = $xpath->query("//*[@id='header']");
-      if ($nodeList->length) {
-        if ($headerFound) {
-          $header = $nodeList->item(0);
-          $header->parentNode->removeChild($header);
-        }
-        $headerFound = true;
-      }
-      
-      $nodeList = $xpath->query("//*[@id='footer']");
-      if ($nodeList->length) {
-        if ($footerFound) {
-          $footer = $nodeList->item(0);
-          $footer->parentNode->removeChild($footer);
-        }
-        $footerFound = true;
-      }
-      
-      $doc->source = $xml->saveHTML();
-      
-      $documents[] = $doc;
+      $documents[] = $consult->_ref_documents[$compte_rendu_id];
     }
   }
 }
 
 $source = "";
-foreach($documents as $key => $doc) {
-  $source .= $doc->source . '<br style="page-break-after: always;" />';
+foreach($documents as $doc) {
+  // Suppression des headers et footers en trop (tous sauf le premier)
+  $xml = new DOMDocument;
+  $_source = utf8_encode("<div>$doc->source</div>");
+  $_source = preg_replace("/&\w+;/i", "", $_source);
+  
+  @$xml->loadXML($_source);
+  $xpath = new DOMXPath($xml);
+  
+  $nodeList = $xpath->query("//*[@id='header']");
+  if ($nodeList->length) {
+    if ($headerFound) {
+      $header = $nodeList->item(0);
+      $header->parentNode->removeChild($header);
+    }
+    $headerFound = true;
+  }
+  
+  $nodeList = $xpath->query("//*[@id='footer']");
+  if ($nodeList->length) {
+    if ($footerFound) {
+      $footer = $nodeList->item(0);
+      $footer->parentNode->removeChild($footer);
+    }
+    $footerFound = true;
+  }
+    
+  $source .= $xml->saveHTML() . '<br style="page-break-after: always;" />';
 }
 
 // Initialisation de FCKEditor
