a43c594db387d06b68f2c7af1e558edac007a8aa
Romain Ollivier <ollivier@openxtrem.com>
2009-12-10 10:32:00 +0000

    ref : ajout du besoin du droit en ecriture pour certains onglets du module système
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7568 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/index.php
+++ modules/system/index.php
@@ -11,8 +11,8 @@ $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
 $module->registerTab("view_modules"        , TAB_EDIT);
 $module->registerTab("view_messages"       , TAB_READ);
-$module->registerTab("view_translate"      , TAB_READ);
-$module->registerTab("object_merger"       , TAB_READ);
+$module->registerTab("view_translate"      , TAB_EDIT);
+$module->registerTab("object_merger"       , TAB_EDIT);
 $module->registerTab("view_history"        , TAB_READ);
 $module->registerTab("view_access_logs"    , TAB_READ);
 $module->registerTab("view_ressources_logs", TAB_READ);
