48a5efaa35462801f32bc2be4a8476fff8598c9c
Alexis Granger <granger@openxtrem.com>
2009-12-18 11:31:08 +0000

    erg: affichage de l'entete de la prescription
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7656 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 551,
+  "build" => 552,
 );
 
 $version["string"] = implode(".", $version);
