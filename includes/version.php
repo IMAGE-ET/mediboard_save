23206a9c26ec19e71ceb2e4be8ef645fd396c862
Alexis Granger <granger@openxtrem.com>
2010-01-13 11:12:40 +0000

    fnc: affichage des bons de commande / version.php
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@7823 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 565,
+  "build" => 566,
 );
 
 $version["string"] = implode(".", $version);
