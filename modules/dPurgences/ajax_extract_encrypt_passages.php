47e66862749ea1dcde97c482444b7960f0b37896
Yohann Poiron <poiron@openxtrem.com>
2010-02-17 08:19:04 +0000

    bug: Erreur sur le nom du module d'inclusion
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8083 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPurgences/ajax_extract_encrypt_passages.php
+++ modules/dPurgences/ajax_extract_encrypt_passages.php
@@ -8,8 +8,8 @@
  * @license OXPL
  */
 
-include_once('modules/oscour/ajax_extract_passages.php');
+include_once('modules/dPurgences/ajax_extract_passages.php');
 
-include_once('modules/oscour/ajax_encrypt_passages.php');
+include_once('modules/dPurgences/ajax_encrypt_passages.php');
 
 ?>
