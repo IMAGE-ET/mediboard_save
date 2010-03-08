d21a25ae1196dedb51034244df51dc55122541b7
Fabien Ménager <fabien.menager@openxtrem.com>
2010-03-08 13:22:45 +0000

    bug: mauvais affichage du menu des modules en haut dans le theme mediboard dans IE7
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8257 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 590,
+  "build" => 591,
 );
 
 $version["string"] = implode(".", $version);
