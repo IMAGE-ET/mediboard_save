3409f6c5fe87ee57c3b6af4391f7439516701f0d
Fabien Ménager <fabien.menager@openxtrem.com>
2010-03-15 13:52:14 +0000

    bug: double escaping des antislashes des user prefs (du a la non-utilisation de CDoObjectAddEdit)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8331 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/controllers/do_preference_aed.php
+++ modules/admin/controllers/do_preference_aed.php
@@ -13,13 +13,14 @@ global $AppUI;
 $prefs = CValue::post("pref", array());
 $user_id = CValue::post("user_id", 0);
 
+// @todo: voir à utiliser CDoObjectAddEdit
 foreach ($prefs as $key => $value) {
 	$pref = new CPreferences();
 	$pref->user_id = $user_id;
 	$pref->key = $key;
   $pref->loadMatchingObject();
   
-	$pref->value = $value;
+	$pref->value = stripslashes($value);
 
 	if ($msg = $pref->store()) {
 		CAppUI::setMsg($msg, UI_MSG_ERROR);
