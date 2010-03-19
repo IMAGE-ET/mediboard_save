48b649696b178c66a3cd1e78e210fe70c8ab9490
Flavien Crochard <flavien.crochard@openxtrem.com>
2010-03-19 13:47:42 +0000

    Fnc: Ajout d'un volet pour l'établissement courant.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8386 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPdeveloppement/templates/view_metrique.tpl
+++ modules/dPdeveloppement/templates/view_metrique.tpl
@@ -1,19 +1,61 @@
-<table class="tbl main">
-  <tr>
-    <th>Type de données</th>
-    <th>Quantité</th>
-    <th>Dernière mise à jour</th>
-  </tr>
-  {{foreach from=$result item=curr_result key=class}}
-  <tr>
-    <td>{{tr}}{{$class}}{{/tr}}</td>
-    <td>{{$curr_result.Rows}}</td>
-    <td>
-      {{assign var=relative value=$curr_result.Update_relative}}
-      <label title="{{$curr_result.Update_time|date_format:$dPconfig.datetime}}">
-      	{{$relative.count}} {{tr}}{{$relative.unit}}{{if $relative.count > 1}}s{{/if}}{{/tr}}
-      </label>
-    </td>
-  </tr>
-  {{/foreach}}
-</table>
+<script type="text/javascript">
+function viewGeneral() {
+  $("general").show();
+	$("current").hide();
+}
+
+function viewCurrent() {
+  $("current").show();
+	$("general").hide();
+}
+
+Main.add(function () {
+  searchTabs = new Control.Tabs('main_tab_group');
+  // Au chargement, vue de l'établissement courant
+	viewGeneral();
+});
+</script>
+
+<ul id="main_tab_group" class="control_tabs">
+  <li><a href="#general">Général</a></li>
+	<li><a href="#current">Etablissement courant</a></li>
+</ul>
+
+<hr class="control_tabs" />
+
+<div id="general" style="display: none;">
+	<table class="tbl main">
+	  <tr>
+	    <th>Type de données</th>
+	    <th>Quantité</th>
+	    <th>Dernière mise à jour</th>
+	  </tr>
+	  {{foreach from=$result item=curr_result key=class}}
+	  <tr>
+	    <td>{{tr}}{{$class}}{{/tr}}</td>
+	    <td>{{$curr_result.Rows}}</td>
+	    <td>
+	      {{assign var=relative value=$curr_result.Update_relative}}
+	      <label title="{{$curr_result.Update_time|date_format:$dPconfig.datetime}}">
+	      	{{$relative.count}} {{tr}}{{$relative.unit}}{{if $relative.count > 1}}s{{/if}}{{/tr}}
+	      </label>
+	    </td>
+	  </tr>
+	  {{/foreach}}
+	</table>
+</div>
+
+<div id="current" style="display: none;">
+  <table class="tbl main">
+    <tr>
+      <th width="50%">Type de données</th>
+      <th width="50%">Quantité</th>
+    </tr>
+		{{foreach from=$res_current_etab item=curr_res key=field_res}}
+		<tr>
+			<td>{{tr}}{{$field_res}}{{/tr}}</td>
+			<td>{{$curr_res}}</td>
+    </tr>
+		{{/foreach}}
+	</table>
+</div>
--- modules/dPdeveloppement/view_metrique.php
+++ modules/dPdeveloppement/view_metrique.php
@@ -27,9 +27,73 @@ foreach ($listeClasses as $class){
 	}
 }
 
+// Pour l'établissement courant
+$current_group = CGroups::loadCurrent()->_id;
+$res_current_etab = array();
+$where = array();
+$ljoin= array();
+
+// - Nombre de séjours
+$sejour = new CSejour;
+$where["sejour.group_id"] = " = $current_group";
+$res_current_etab["CSejour"] = $sejour->countList($where);
+
+
+// - Nombre de consultations
+$where = array();
+$consultation = new CConsultation;
+$ljoin["plageconsult"]        = "consultation.plageconsult_id = plageconsult.plageconsult_id";
+$ljoin["users_mediboard"]     = "plageconsult.chir_id = users_mediboard.user_id";
+$ljoin["functions_mediboard"] = "users_mediboard.function_id = functions_mediboard.function_id";
+$where["functions_mediboard.group_id"] = " = $current_group";
+$res_current_etab["CConsultation"] = $consultation->countList($where, null, null, null, $ljoin);
+
+// - Entrées de journal
+$ljoin = array();
+$where = array();
+$user_log = new CUserLog;
+$ljoin["users_mediboard"] = "user_log.user_id = users_mediboard.user_id";
+$ljoin["functions_mediboard"] = "users_mediboard.function_id = functions_mediboard.function_id";
+$where["functions_mediboard.group_id"] = " = $current_group";
+$res_current_etab["CUserLog"] = $user_log->countList($where, null, null, null, $ljoin);
+
+// - Utilisateurs
+$ljoin = array();
+$where = array();
+$mediuser = new CMediusers;
+$ljoin["functions_mediboard"]   = "users_mediboard.function_id = functions_mediboard.function_id";
+$where["functions_mediboard.group_id"] = " = $current_group";
+$res_current_etab["CMediusers"] = $mediuser->countList($where, null, null, null, $ljoin);
+ 
+// - Patients IPP
+$where = array();
+$tag_ipp = CAppUI::conf("dPpatients CPatient tag_ipp");
+str_replace('$g', $current_group, $tag_ipp);
+$where["tag"] = " = '$tag_ipp'";
+$id400 = new CIdSante400;
+$res_current_etab["CIdSante400"] = $id400->countList($where);
+
+// - Lits
+$ljoin = array();
+$where = array();
+$lit = new CLit;
+$ljoin["chambre"] = "lit.chambre_id = chambre.chambre_id";
+$ljoin["service"] = "chambre.service_id = service.service_id";
+$where["service.group_id"] = " = $current_group";
+$res_current_etab["CLit"] = $lit->countList($where, null, null, null, $ljoin);
+
+// - Chambres
+$ljoin = array();
+$where = array();
+$chambre = new CChambre;
+$ljoin["service"] = "chambre.service_id = service.service_id";
+$where["service.group_id"] = " = $current_group";
+$res_current_etab["CChambre"] = $chambre->countList($where, null, null, null, $ljoin);
+
 // Création du template
 $smarty = new CSmartyDP();
 $smarty->assign("result" , $result);
+$smarty->assign("res_current_etab", $res_current_etab);
 $smarty->display("view_metrique.tpl");
 
 ?>
