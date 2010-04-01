5cedc5bda70901a669c62d76ee0e9d47c992abc9
Fabien Ménager <fabien.menager@openxtrem.com>
2010-04-01 12:05:38 +0000

    bug: fonction recurse() redéclarée a chaque appel à array_replace_recursive
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8447 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/compat.php
+++ includes/compat.php
@@ -69,23 +69,23 @@ function in_array_recursive($needle, $haystack, $strict = false) {
  * cf. http://php.net/array_replace_recursive
  */
 if (!function_exists('array_replace_recursive')) {
-  function array_replace_recursive($array, $array1) {
-    function recurse($array, $array1) {
-      foreach ($array1 as $key => $value) {
-        // create new key in $array, if it is empty or not an array
-        if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key]))) {
-          $array[$key] = array();
-        }
- 
-        // overwrite the value in the base array
-        if (is_array($value)) {
-          $value = recurse($array[$key], $value);
-        }
-        $array[$key] = $value;
+  function array_replace_recursive__recurse($array, $array1) {
+    foreach ($array1 as $key => $value) {
+      // create new key in $array, if it is empty or not an array
+      if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key]))) {
+        $array[$key] = array();
       }
-      return $array;
+
+      // overwrite the value in the base array
+      if (is_array($value)) {
+        $value = array_replace_recursive__recurse($array[$key], $value);
+      }
+      $array[$key] = $value;
     }
- 
+    return $array;
+  }
+  
+  function array_replace_recursive($array, $array1) {
     // handle the arguments, merge one by one
     $args = func_get_args();
     $array = $args[0];
@@ -94,7 +94,7 @@ if (!function_exists('array_replace_recursive')) {
     }
     for ($i = 1; $i < count($args); $i++) {
       if (is_array($args[$i])) {
-        $array = recurse($array, $args[$i]);
+        $array = array_replace_recursive__recurse($array, $args[$i]);
       }
     }
     return $array;
