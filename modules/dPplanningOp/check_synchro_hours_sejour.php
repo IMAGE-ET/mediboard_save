cc0fb7f31205e05bf5557d81cfee34d53b74362f
Thomas Despoix <tomdesp@gmail.com>
2010-04-15 20:54:49 +0000

    Ref: La vérification de synchro des horaires de séjours utilise désormais les stepAjax
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8583 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/check_synchro_hours_sejour.php
+++ modules/dPplanningOp/check_synchro_hours_sejour.php
@@ -9,47 +9,44 @@
  */
 
 $type = CValue::get("type", 'check_entree');
-//$ds = new CMySQLDataSource();
 $ds = CSQLDataSource::get("std");
 $result = "";
 
 switch($type) {
   case 'check_entree' :
     $message = " entrée(s) erronée(s)";
-    $sql = "SELECT COUNT(*) AS total FROM `sejour`
-            WHERE `sejour`.`entree` != IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)";
+    $sql = "SELECT COUNT(*) AS total 
+		  FROM `sejour`
+      WHERE `sejour`.`entree` != IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)";
     $result = $ds->loadResult($sql);
     break;
   case 'check_sortie' :
     $message = " sortie(s) erronnée(s)";
-    $sql = "SELECT COUNT(*) AS total FROM `sejour`
-            WHERE `sejour`.`sortie` != IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)";
+    $sql = "SELECT COUNT(*) AS total 
+		  FROM `sejour`
+      WHERE `sejour`.`sortie` != IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)";
     $result = $ds->loadResult($sql);
     break;
   case 'fix_entree' :
     $message = " entrée(s) corrigée(s)";
     $sql = "UPDATE `sejour` SET
-              `sejour`.`entree` = IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)
-            WHERE `sejour`.`entree` != IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)";
+		  `sejour`.`entree` = IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)
+      WHERE `sejour`.`entree` != IF(`sejour`.`entree_reelle`,`sejour`.`entree_reelle`,`sejour`.`entree_prevue`)";
     $ds->query($sql);
     $result = $ds->affectedRows();
     break;
   case 'fix_sortie' :
     $message = " sortie(s) corrigée(s)";
     $sql = "UPDATE `sejour` SET
-              `sejour`.`sortie` = IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)
-            WHERE `sejour`.`sortie` != IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)";
+		  `sejour`.`sortie` = IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)
+      WHERE `sejour`.`sortie` != IF(`sejour`.`sortie_reelle`,`sejour`.`sortie_reelle`,`sejour`.`sortie_prevue`)";
     $ds->query($sql);
     $result = $ds->affectedRows();
     break;
   default: 
-    $message = "commande non reconnue";
+    CAppUI::stepAjax("Commande non reconnue", UI_MSG_ERROR);
 }
 
-if($result != "") {
-  $message = $result . $message;
-}
-
-echo $message;
+CAppUI::stepAjax(CValue::first($result, "Aucune") . $message, $result ? UI_MSG_WARNING : UI_MSG_OK);
 
 ?>
