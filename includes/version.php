caf42fa815df62393821474a9cf1b8ea48aef1e7
Yohann Poiron <poiron@openxtrem.com>
2010-04-27 08:18:52 +0000

    ref: Incrémentation du version.js
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8705 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 616,
+  "build" => 617,
 );
 
 $version["string"] = implode(".", $version);
