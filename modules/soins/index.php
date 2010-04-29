f0e7ea0fd433416a590a18517f33e9ca52601fc8
Alexis Granger <granger@openxtrem.com>
2010-04-29 06:47:18 +0000

    bug: suppression des onglets bilan des prescriptions et pancarte si la prescription n'est pas activée
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8740 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/index.php
+++ modules/soins/index.php
@@ -11,8 +11,10 @@
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
 $module->registerTab("vw_idx_sejour"        , TAB_READ);
-$module->registerTab("vw_bilan_prescription", TAB_READ);
-$module->registerTab("vw_pancarte_service"  , TAB_READ);
+if(CModule::getActive('dPprescription')){
+	$module->registerTab("vw_bilan_prescription", TAB_READ);
+  $module->registerTab("vw_pancarte_service"  , TAB_READ);
+}
 
 if (isset(CModule::$active["dPstock"])) {
   $module->registerTab("vw_stocks_service"  , TAB_EDIT);
