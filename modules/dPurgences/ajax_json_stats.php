512791d7d3061c80d840ed1e5db397bdca2acf5d
Fabien Ménager <fabien.menager@openxtrem.com>
2010-04-29 08:05:16 +0000

    bug: les droits necessaires pour voir les stats d'urgence sont les droits admin
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8743 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPurgences/ajax_json_stats.php
+++ modules/dPurgences/ajax_json_stats.php
@@ -8,7 +8,7 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
  */
 
-CCanDo::checkRead();
+CCanDo::checkAdmin();
 
 $axe    = CValue::getOrSession('axe');
 $entree = CValue::getOrSession('entree', mbDate());
--- modules/dPurgences/vw_stats.php
+++ modules/dPurgences/vw_stats.php
@@ -8,8 +8,7 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
  */
 
-global  $can;
-$can->needsRead();
+CCanDo::checkAdmin();
 
 $axe    = CValue::getOrSession('axe');
 $entree = CValue::getOrSession('entree', mbDate());
