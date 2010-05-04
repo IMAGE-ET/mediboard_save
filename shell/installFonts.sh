507946ac2ffc621e2bd94163a8b0d02fed65361e
Flavien Crochard <flavien.crochard@openxtrem.com>
2010-05-04 09:34:44 +0000

    Ref: Amélioration du script d'installation de polices pour domPDF
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8830 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/installFonts.sh
+++ shell/installFonts.sh
@@ -5,8 +5,6 @@ mkdir temp_fonts
 
 cd temp_fonts
 
-announce_script "Installation des Microsoft's core fonts"
-
 # Récupération des polices
 wget -i ../font_list.txt
 
@@ -14,13 +12,13 @@ wget -i ../font_list.txt
 cabextract --lowercase *.exe
 
 # Conversion en afm de ces polices et installation
-php ../../lib/dompdf/load_font.php Arial arial.ttf arialbd.ttf ariali.ttf arialbi.ttf
-php ../../lib/dompdf/load_font.php 'Comic Sans MS'  comic.ttf comicbd.ttf
-php ../../lib/dompdf/load_font.php Georgia georgia.ttf georgiab.ttf georgiai.ttf georgiaz.ttf
-php ../../lib/dompdf/load_font.php 'Trebuchet MS' trebuc.ttf trebucbd.ttf trebucit.ttf trebucbi.ttf
-php ../../lib/dompdf/load_font.php Verdana verdana.ttf verdanab.ttf verdanai.ttf verdanaz.ttf
-php ../../lib/dompdf/load_font.php 'Times New Roman' times.ttf timesbd.ttf timesi.ttf timesbi.ttf
+php ../lib/dompdf/load_font.php Arial arial.ttf arialbd.ttf ariali.ttf arialbi.ttf
+php ../lib/dompdf/load_font.php 'Comic Sans MS'  comic.ttf comicbd.ttf
+php ../lib/dompdf/load_font.php Georgia georgia.ttf georgiab.ttf georgiai.ttf georgiaz.ttf
+php ../lib/dompdf/load_font.php 'Trebuchet MS' trebuc.ttf trebucbd.ttf trebucit.ttf trebucbi.ttf
+php ../lib/dompdf/load_font.php Verdana verdana.ttf verdanab.ttf verdanai.ttf verdanaz.ttf
+php ../lib/dompdf/load_font.php 'Times New Roman' times.ttf timesbd.ttf timesi.ttf timesbi.ttf
 
 # Nettoyage
+cd ../
 rm -Rf temp_fonts
-
