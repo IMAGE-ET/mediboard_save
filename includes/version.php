0c331057541eadb11c782a19a6f999299a15e2ef
Alexis Granger <granger@openxtrem.com>
2010-05-19 08:10:59 +0000

    ref: version.php
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@8953 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 639,
+  "build" => 640,
 );
 
 $version["string"] = implode(".", $version);
