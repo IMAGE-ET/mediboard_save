bcac9a744766e9291d93fa279526ec94412abd7e
Fabien Ménager <fabien.menager@openxtrem.com>
2010-05-28 08:57:44 +0000

    bug: le cover ne fonctionnait plus
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9047 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/javascript/functions.js
+++ includes/javascript/functions.js
@@ -75,16 +75,13 @@ var WaitingMessage = {
     element = $(element);
     
     var coverContainer = new Element("div", {style: "border:none;background:none;padding:0;margin:0;position:relative;"}).addClassName("cover-container").hide(),
-        cover = new Element("div").addClassName("ajax-loading");
+        cover = new Element("div").addClassName("ajax-loading"),
+        descendant = element.down();
 
     coverContainer.insert(cover);
     
     /** If the element is a TR, we add the div to the firstChild to avoid a bad page render (a div in a <table> or a <tr>)*/
-    var receiver = element;
-    
-    if (/^tr|tbody$/i.test(element.tagName)) {
-      receiver = element.down("td, th");
-    }
+    var receiver = (descendant && /^tr$/i.test(element.tagName)) ? descendant : element;
     
     receiver.insert({top: coverContainer});
     
--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 647,
+  "build" => 648,
 );
 
 $version["string"] = implode(".", $version);
