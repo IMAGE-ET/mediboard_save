b6dcc7685b97d9237f4a425b352115f08fec19f6
Fabien Ménager <fabien.menager@openxtrem.com>
2010-06-15 09:25:50 +0000

    bug: correction des listes de delivrance incompletes
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9206 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/pharmacie/httpreq_vw_deliveries_list.php
+++ modules/pharmacie/httpreq_vw_deliveries_list.php
@@ -25,6 +25,14 @@ if (!in_array($mode, array("global", "nominatif"))) {
 
 $service = new CService;
 $services = $service->loadGroupList();
+$services["none"] = new CService;
+$services["none"]->_view = "";
+$service_keys = array_keys($services);
+
+$deliveries_by_service = array_fill_keys($service_keys, array());
+$delivered_counts = array_fill_keys($service_keys, 0);
+$order_by_product = false;
+$stocks_service = array();
 
 $order_by = 'service_id, patient_id, date_dispensation DESC';
 $where = array();
@@ -33,69 +41,62 @@ if ($mode == "global")
   $where['patient_id'] = "IS NULL";
 else
   $where['patient_id'] = "IS NOT NULL";
-
-$where[] = 'service_id '.CSQLDataSource::prepareIn(array_keys($services))." OR service_id IS NULL OR service_id = ''";
+  
 $where[] = "date_dispensation BETWEEN '$date_min 00:00:00' AND '$date_max 23:59:59'";
 $where['quantity'] = " > 0";
 $where['stock_id'] = "IS NOT NULL";
-$where[] = "`order` != '1' OR `order` IS NULL";
+//$where[] = "`order` != '1' OR `order` IS NULL";
 
 if (!$display_delivered) {
   $where[] = "date_delivery IS NULL OR date_delivery = ''";
 }
-
+  
 $delivery = new CProductDelivery();
-$deliveries = $delivery->loadList($where, $order_by, 200);
 
-foreach($deliveries as $_id => $_delivery) {
-  $delivered = $_delivery->date_delivery || $_delivery->isDelivered();
-  if (!$delivered || ($delivered && $display_delivered)) {
-    // can't touch this !
-  }
-  else {
-    unset($deliveries[$_id]);
-  }
-}
-
-$stocks_service = array();
-foreach($deliveries as $_delivery){
-  $_delivery->loadRefsFwd();
-  $_delivery->loadRefsBack();
-  $_delivery->_ref_stock->loadRefsFwd();
+foreach($services as $_service_id => $_service) {
+  if ($_service_id == "none")
+    $where['service_id'] = "IS NULL OR service_id = ''";
+  else
+    $where['service_id'] = " = '$_service->_id'";
   
-  $stocks_service[$_delivery->_id] = CProductStockService::getFromCode($_delivery->_ref_stock->_ref_product->code, $_delivery->service_id);
-}
-
-$services["none"] = new CService;
-$services["none"]->_view = "";
-
-$service_keys = array_keys($services);
-$deliveries_by_service = array_fill_keys($service_keys, array());
-$delivered_counts = array_fill_keys($service_keys, 0);
-
-$order_by_product = false;
-
-foreach ($deliveries as $_delivery) {
-  $service_id = $_delivery->service_id ? $_delivery->service_id : 'none';
+  $deliveries = $delivery->loadList($where, $order_by, 200);
   
-  if ($order_by_product) {
-    $key = str_pad($_delivery->_ref_stock->_ref_product->_view, 50, " ", STR_PAD_RIGHT).$_delivery->date_dispensation;
-  }
-  else {
-    $key = str_pad(mbMinutesRelative($_delivery->date_dispensation, mbDateTime())+1, 20, " ", STR_PAD_LEFT).
-           str_pad($_delivery->_ref_stock->_ref_product->_view, 50, " ", STR_PAD_RIGHT).
-           $_delivery->_id;
+  foreach($deliveries as $_id => $_delivery) {
+    $_delivery->loadRefsFwd();
+    $_delivery->_ref_stock->loadRefsFwd();
+    $_delivery->isDelivered();
+    
+    $delivered = /*$_delivery->date_delivery || */$_delivery->_delivered;
+    if (!$delivered || ($delivered && $display_delivered)) {
+      // can't touch this !
+    }
+    else {
+      unset($deliveries[$_id]);
+    }
   }
   
-  if (/*$_delivery->date_delivery || */$_delivery->isDelivered()) {
-    $delivered_counts[$service_id]++;
+  foreach($deliveries as $_id => $_delivery) {
+    $stocks_service[$_delivery->_id] = CProductStockService::getFromCode($_delivery->_ref_stock->_ref_product->code, $_delivery->service_id);
+
+    if ($order_by_product) {
+      $key = str_pad($_delivery->_ref_stock->_ref_product->name, 50, " ", STR_PAD_RIGHT).$_delivery->date_dispensation;
+    }
+    else {
+      $key = str_pad(mbMinutesRelative($_delivery->date_dispensation, mbDateTime())+1, 20, " ", STR_PAD_LEFT).
+             str_pad($_delivery->_ref_stock->_ref_product->name, 50, " ", STR_PAD_RIGHT).
+             $_delivery->_id;
+    }
+    
+    if ($_delivery->date_delivery || $_delivery->_delivered) {
+      $delivered_counts[$_service_id]++;
+    }
+    
+    $deliveries_by_service[$_service_id][$key] = $_delivery;
   }
   
-  $deliveries_by_service[$service_id][$key] = $_delivery;
-}
-
-foreach($deliveries_by_service as &$_list) {
-  ksort($_list);
+  foreach($deliveries_by_service as &$_list) {
+    ksort($_list);
+  }
 }
 
 // Création du template
