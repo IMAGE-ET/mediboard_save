9d3529d1eaf333859008341bebfea5da134925b4
Thomas Despoix <tomdesp@gmail.com>
2010-06-21 14:41:09 +0000

    Fnc: détail cosmétique pour les textarea autocomplete
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9258 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 659,
+  "build" => 660,
 );
 
 $version["string"] = implode(".", $version);
