c4609add06aaeb644c0ccf7c9a3b48c2dc477323
Yohann Poiron <poiron@openxtrem.com>
2010-06-30 07:17:59 +0000

    ref: Modification des données Urgences : structure d'un élément par journée
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9317 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPurgences/ajax_extract_passages_urg.php
+++ modules/dPurgences/ajax_extract_passages_urg.php
@@ -22,62 +22,65 @@ if (!$debut_selection || !$fin_selection) {
 
 // Chargement des rpu de la main courante
 $sejour = new CSejour;
-
 $where = array();
 $ljoin["rpu"] = "sejour.sejour_id = rpu.sejour_id";
-
 $where[] = "sejour.entree_reelle BETWEEN '$debut_selection' AND '$fin_selection' 
   OR (sejour.sortie_reelle IS NULL AND sejour.entree_reelle BETWEEN '$debut_selection' AND '$fin_selection')";
-
 // RPUs
 $where[] = "rpu.rpu_id IS NOT NULL";
 $where["sejour.group_id"] = "= '".CGroups::loadCurrent()->_id."'";
-
 $order = "sejour.entree_reelle ASC";
-
 $sejours = $sejour->loadList($where, $order, null, null, $ljoin);
 
-$stats = array (
-  "total" => 0,
-  "less_than_1" => 0,
-  "more_than_75" => 0,
-  "transferts_count" => 0, 
-  "mutations_count" => 0,
-  "mutations_uhcd_count" => 0
-);
-
+$count_sejour = 0;
+$stats = array();
 // Détail du chargement
 foreach ($sejours as &$_sejour) {
+  $count_sejour++;
+  
   $_sejour->loadRefsFwd(1);
-  $_sejour->loadRefRPU();  
+  $_sejour->loadRefRPU();
   $_sejour->_ref_rpu->loadRefSejourMutation();
   
-  $stats["total"]++;
+  $entree_patient = mbDate($_sejour->entree);
+  
+  if (!array_key_exists($entree_patient, $stats)) {
+    $stats[$entree_patient] = array (
+      "total"                => 0,
+      "mutations_count"      => 0,
+      "mutations_uhcd_count" => 0,
+      "transferts_count"     => 0, 
+      "less_than_1"          => 0,
+      "more_than_75"         => 0,
+    );
+  }
+ 
+  $stats[$entree_patient]["total"]++;
 
   // Statistiques de mutations de sejours
   $service_mutation = $_sejour->_ref_service_mutation;
   if ($service_mutation->_id) {
-    $stats["mutations_count"]++;
+    $stats[$entree_patient]["mutations_count"]++;
     if ($service_mutation->uhcd) {
-      $stats["mutations_uhcd_count"]++;
+      $stats[$entree_patient]["mutations_uhcd_count"]++;
     }
   }
 
   // Statistiques de transferts de sejours
   $etablissement_tranfert = $_sejour->_ref_etabExterne;
   if ($etablissement_tranfert->_id) {
-    $stats["transferts_count"]++;
+    $stats[$entree_patient]["transferts_count"]++;
   }
   
   // Statistiques  d'âge de patient
   $patient =& $_sejour->_ref_patient;
   if ($patient->_age < "1") {
-    $stats["less_than_1"]++;
+    $stats[$entree_patient]["less_than_1"]++;
   }
     
   if ($patient->_age >= "75") {
-    $stats["more_than_75"]++;
-  }
+    $stats[$entree_patient]["more_than_75"]++;
+  } 
 }
 
 $extractPassages = new CExtractPassages();
@@ -96,7 +99,7 @@ if (!$rpuSender) {
 }
 $extractPassages = $rpuSender->extractURG($extractPassages, $stats);
 
-CAppUI::stepAjax("Extraction de ".$stats['total']." RPUs du ".mbDateToLocale($debut_selection)." au ".mbDateToLocale($fin_selection)." terminée.", UI_MSG_OK);
+CAppUI::stepAjax("Extraction de $count_sejour séjours du ".mbDateToLocale($debut_selection)." au ".mbDateToLocale($fin_selection)." terminée.", UI_MSG_OK);
 if (!$extractPassages->message_valide)
   CAppUI::stepAjax("Le document produit n'est pas valide.", UI_MSG_WARNING);
 else 
