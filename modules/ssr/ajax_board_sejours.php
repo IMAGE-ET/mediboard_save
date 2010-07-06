71ce054ad1a6ae8ca3d699056e8831526ec75c67
Alexis Granger <granger@openxtrem.com>
2010-07-06 08:00:26 +0000

    fnc: chargement de sejours planifiables
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9370 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/ssr/ajax_board_sejours.php
+++ modules/ssr/ajax_board_sejours.php
@@ -13,6 +13,9 @@ CCanDo::checkEdit();
 $date    = CValue::getOrSession("date", mbDate());
 $kine_id = CValue::getOrSession("kine_id");
 
+$mediuser = new CMediusers();
+$mediuser->load($kine_id);
+
 $planning = new CPlanningWeek($date);
 
 // Sejour SSR
@@ -50,7 +53,51 @@ $where["evenement_ssr.therapeute_id"] = "= '$kine_id'";
 $sejours["planned"] = $sejour->loadList($where, null, null, null, $join);
 
 // Sejours pour lesquels le rééducateur est exécutant pour des lignes prescrites mais n'a pas encore d'evenement planifiés
-//$sejours["plannable"] = array();
+$sejours["plannable"] = array();
+
+$where = array();
+$where["sejour.entree"] = "<= '$planning->date_max'";
+$where["sejour.sortie"] = ">= '$planning->date_min'";
+$_sejours = $sejour->loadList($where);
+
+foreach($_sejours as $_sejour){
+	$_sejour->loadRefBilanSSR();
+	$bilan_ssr =& $_sejour->_ref_bilan_ssr;
+	$bilan_ssr->loadRefTechnicien();
+	$kine_referent_id = $bilan_ssr->_ref_technicien->kine_id;
+	
+	if(($kine_referent_id == $kine_id) && !array_key_exists($_sejour->_id, $sejours["planned"])){
+    $sejours["plannable"][$_sejour->_id] = $_sejour;      		
+	}
+
+	// Chargement de la prescrtiption su sejour
+	$_sejour->loadRefPrescriptionSejour();
+	$prescription =& $_sejour->_ref_prescription_sejour;
+	// Chargement des lignes de la prescription
+	$prescription->loadRefsLinesElementByCat();
+	// Parcours des lignes d'element
+	foreach($prescription->_ref_prescription_lines_element as $_line_element){
+		// Si le sejour est deja dans la liste des sejours
+		if(array_key_exists($_sejour->_id, $sejours["plannable"])){
+			continue;
+		}
+		
+		// Recuperation de la categorie de prescription de la ligne
+		$category_prescription_id = $_line_element->_ref_element_prescription->category_prescription_id;
+		
+		// Chargement des associations 
+		$function_categorie = new CFunctionCategoryPrescription();
+	  $function_categorie->category_prescription_id = $category_prescription_id;
+	  $functions_cat = $function_categorie->loadMatchingList();
+	  
+		$functions = array();
+		foreach($functions_cat as $function_cat){
+		  if(($mediuser->function_id == $function_cat->function_id) && !array_key_exists($_sejour->_id, $sejours["planned"])){
+		    $sejours["plannable"][$_sejour->_id] = $_sejour;      
+		  }	
+		}
+	}
+}
 
 foreach ($sejours as &$_sejours) {
 	foreach ($_sejours as $_sejour) {
