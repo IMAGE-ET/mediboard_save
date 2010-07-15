428e156e8d21af6c3d05b6bafb446707ab304b90
Flavien Crochard <flavien.crochard@openxtrem.com>
2010-07-15 13:24:15 +0000

    Ref: Téléchargement du fichier txt pour l'échange soap
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9493 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/webservices/download_echange.php
@@ -0,0 +1,34 @@
+<?php /* $Id $ */
+
+/**
+ * @package Mediboard
+ * @subpackage webservices
+ * @version $Revision: $
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ */
+
+$echange_soap_id = CValue::get("echange_soap_id");
+$echange_soap = new CEchangeSOAP();
+$echange_soap->load($echange_soap_id);
+$echange_soap->loadRefs();
+
+$input  = print_r(unserialize($echange_soap->input), true);
+$output = print_r(unserialize($echange_soap->output), true);
+
+$function_name = $echange_soap->function_name;
+$content = "Parametres :\n
+$input
+Resultat :\n
+$output";
+
+$echange = utf8_decode($content);
+
+header("Content-Disposition: attachment; filename={$function_name}-{$echange_soap_id}.txt");
+header("Content-Type: text/plain; charset=".CApp::$encoding);
+header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
+header( "Cache-Control: post-check=0, pre-check=0", false );
+header("Content-Length: ".strlen($echange));
+echo $echange;
+
+?>
