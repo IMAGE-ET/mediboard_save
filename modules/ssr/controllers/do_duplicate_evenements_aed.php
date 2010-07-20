bc1c001a1229288a78dde210c35ac345442bbcbd
Thomas Despoix <tomdesp@gmail.com>
2010-07-20 12:50:34 +0000

    Bug: La duplication d'événements SSR ne dupliquait pas les actes CdARR associés
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9556 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/ssr/controllers/do_duplicate_evenements_aed.php
+++ modules/ssr/controllers/do_duplicate_evenements_aed.php
@@ -14,15 +14,28 @@ $elts_id = explode("|", $token_elts);
 foreach($elts_id as $_elt_id){
   $evenement_ssr = new CEvenementSSR();
   $evenement_ssr->load($_elt_id);
-  
+  $evenement_ssr->loadRefsActesCdARR();
+
+  // Duplication de l'événement  
 	$evenement_ssr->_id = "";
 	$evenement_ssr->realise = 0;
 	$evenement_ssr->debut = mbDateTime("7 days", $evenement_ssr->debut);
-	if($evenement_ssr->seance_collective_id){
+	if ($evenement_ssr->seance_collective_id){
     CAppUI::displayMsg("Impossible de dupliquer des evenements qui sont dans des seances collectives", "CEvenementSSR-msg-store");
-	} else {
+	} 
+	else {
 	  $msg = $evenement_ssr->store();
 	  CAppUI::displayMsg($msg, "CEvenementSSR-msg-store");
+	}
+
+  // Duplication des codes CdARR
+	if ($evenement_ssr->_id) {
+		foreach ($evenement_ssr->_ref_actes_cdarr as $_acte_cdarr) {
+		  $_acte_cdarr->_id = "";
+		  $_acte_cdarr->evenement_ssr_id = $evenement_ssr->_id;
+	    $msg = $_acte_cdarr->store();
+	    CAppUI::displayMsg($msg, "CActeCdARR-msg-store");
+		}
 	}
 }
 
