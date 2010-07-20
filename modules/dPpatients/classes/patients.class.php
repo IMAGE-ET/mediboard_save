5c69684e7a52e7bb0ca944d218dd66ff2d12626e
Flavien Crochard <flavien.crochard@openxtrem.com>
2010-07-20 12:51:12 +0000

    Bug: Utilisation de $can pour tester si l'utilisateur est admin dans l'affichage de la photo du patient.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9557 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/classes/patients.class.php
+++ modules/dPpatients/classes/patients.class.php
@@ -1345,6 +1345,7 @@ class CPatient extends CMbObject {
   }
   
   function loadRefPhotoIdentite() {
+    global $can;
     $file = new CFile();
     $file->setObject($this);
     $file->file_name = 'identite.jpg';
