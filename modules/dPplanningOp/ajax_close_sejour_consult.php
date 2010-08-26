8768355296888aa1b67e9379220b45b52915d45b
Fabien Ménager <fabien.menager@openxtrem.com>
2010-08-26 15:48:12 +0000

    fnc: annulation des consultations sans sortie ni entrée reelle de la veille dans ajax_close_sejour_consult
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@9881 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/ajax_close_sejour_consult.php
+++ modules/dPplanningOp/ajax_close_sejour_consult.php
@@ -8,8 +8,7 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  */
 
-global $can;
-$can->needsEdit();
+CCanDo::checkEdit();
 
 $now       = mbDateTime();
 $yesterday = mbDateTime("-1 DAY");
@@ -17,17 +16,29 @@ $yesterday = mbDateTime("-1 DAY");
 $sejour = new CSejour();
 $where['type']   = "= 'consult'";
 $where['entree'] = "BETWEEN '$yesterday' AND '$now'";
+
+// Cloture des sejours passés
 $where['entree_reelle'] = "IS NOT NULL";
 $where['sortie_reelle'] = "IS NULL";
 $sejours = $sejour->loadList($where);
 
-CAppUI::stepAjax(count($sejours)." séjours à clôturer.", UI_MSG_OK);
+CAppUI::stepAjax(count($sejours)." séjours à clôturer", UI_MSG_OK);
 
 foreach ($sejours as $_sejour) {
   $_sejour->sortie_reelle = $now;
 	$msg = $_sejour->store();  
-	
 	CAppUI::stepAjax($msg ? "Séjour non clôturé" : "Séjour clôturé", $msg ? UI_MSG_WARNING : UI_MSG_OK);	
 }
 
-?>
+// Annulation des séjours sans entrée reelle ni sortie reelle
+$where['entree_reelle'] = "IS NULL";
+$where['sortie_reelle'] = "IS NULL";
+$sejours = $sejour->loadList($where);
+
+CAppUI::stepAjax(count($sejours)." séjours à annuler", UI_MSG_OK);
+
+foreach ($sejours as $_sejour) {
+  $_sejour->annule = 1;
+  $msg = $_sejour->store();
+  CAppUI::stepAjax($msg ? "Séjour non annulé" : "Séjour annulé", $msg ? UI_MSG_WARNING : UI_MSG_OK);  
+}
