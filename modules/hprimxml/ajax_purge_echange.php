c0afa36ea5ef0d17206d94fa2bbc84d27cc4ae0a
Yohann Poiron <poiron@openxtrem.com>
2010-09-08 12:21:21 +0000

    ref: Passage de la purge pour les content_xml
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10001 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hprimxml/ajax_purge_echange.php
+++ modules/hprimxml/ajax_purge_echange.php
@@ -22,25 +22,29 @@ if (!$date_min || !$date_max) {
 $itemEchangeHprim = new CEchangeHprim;
 
 // Requêtes
+$ljoin = "";
+$ljoin["content_xml"] = "`content_xml`.`content_id` = `echange_hprim`.`message_content_id`";
+    
 $where = array();
-$where["date_production"] = "BETWEEN '".$date_min."' AND '".$date_max."'";
-$where["purge"] = "= '0'";
+$where["echange_hprim.date_production"] = "BETWEEN '".$date_min."' AND '".$date_max."'";
+$where["content_xml.content"] = "IS NOT NULL";
 
 if (!$do_purge) {
-  $count = $itemEchangeHprim->countList($where);
+  $count = $itemEchangeHprim->countList($where, null, null, null, $ljoin);
   CAppUI::stepAjax($count." échanges H'XML à purger");
 } else {
 	$order = "date_production ASC";
 	
   // Récupération de la liste des echanges HPRIM
-  $echangesHprim = $itemEchangeHprim->loadList($where, $order, "0, 100");
+  $echangesHprim = $itemEchangeHprim->loadList($where, $order, "0, 500", null, $ljoin);
   $count  = 0;
   foreach($echangesHprim as $_echange_hprim) {  
+    $_echange_hprim->loadContent();
+    
     // Suppression du champ message et acquittement de l'échange
-    $_echange_hprim->_message = "";
+    $_echange_hprim->_message      = "";
 		$_echange_hprim->_acquittement = "";
-		
-    $_echange_hprim->purge = 1;
+
     if ($msg = $_echange_hprim->store()) {
       CAppUI::stepAjax("#$_echange_hprim->_id : Impossible de sauvegarder l'échange H'XML", UI_MSG_WARNING);
       CAppUI::stepAjax($msg, UI_MSG_WARNING);
