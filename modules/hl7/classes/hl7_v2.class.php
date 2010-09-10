6f9f2dabf8156a872753d6eda7eb3db69dbd68ff
Yohann Poiron <poiron@openxtrem.com>
2010-09-10 14:04:33 +0000

    fnc: Ajout des classes primaires pour HL7
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10048 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/hl7/classes/hl7_v2.class.php
@@ -0,0 +1,16 @@
+<?php /* $Id:$ */
+
+/**
+ * @package Mediboard
+ * @subpackage hl7
+ * @version $Revision: 10041 $
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ */
+
+class CHL7V2 {  
+  
+  
+}
+
+?>
