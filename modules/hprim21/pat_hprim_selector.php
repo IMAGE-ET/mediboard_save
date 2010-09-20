fbf2ccfd63f15ccf59beb022f93558c3a1a10e55
Yohann Poiron <poiron@openxtrem.com>
2010-09-20 07:35:49 +0000

    ref: Suppression du check sur le selecteur de patient et séjour
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10110 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hprim21/pat_hprim_selector.php
+++ modules/hprim21/pat_hprim_selector.php
@@ -8,8 +8,6 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  */
 
-CCanDo::checkRead();
-
 $patient_id    = CValue::get("patient_id");
 $patient = new CPatient;
 if($patient_id) {
