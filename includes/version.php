bef245d747707b237b3f73c965df892b98f0edd6
Fabien Ménager <fabien.menager@openxtrem.com>
2010-10-20 12:59:55 +0000

    erg: decallage leger de la barre de boutons des textareas autocompletés pour laisser la place pour l'ascenseur du textera
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10440 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 751,
+  "build" => 752,
 );
 
 $version["string"] = implode(".", $version);
