5feb012fe254687b8d86f48665a530433974483c
Thomas Despoix <tomdesp@gmail.com>
2010-10-22 20:41:23 +0000

    Bug: Le setup de dPetablissement 0.25 était faux Ref: Nettoyage du setup de dPetablissement
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10472 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPetablissement/setup.php
+++ modules/dPetablissement/setup.php
@@ -18,119 +18,120 @@ class CSetupdPetablissement extends CSetup {
     $this->makeRevision("all");
     
     $this->makeRevision("0.1");
-    $sql = "ALTER TABLE `groups_mediboard`" .
-            "\nADD `raison_sociale` VARCHAR( 50 ) ," .
-            "\nADD `adresse` TEXT ," .
-            "\nADD `cp` VARCHAR( 5 ) ," .
-            "\nADD `ville` VARCHAR( 50 ) ," .
-            "\nADD `tel` VARCHAR( 10 ) ," .
-            "\nADD `directeur` VARCHAR( 50 ) ," .
-            "\nADD `domiciliation` VARCHAR( 9 ) ," .
-            "\nADD `siret` VARCHAR( 14 );";
-    $this->addQuery($sql);
-    $sql = "INSERT INTO `groups_mediboard` ( `group_id` , `text` )
-						VALUES (NULL , 'Etablissement');";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard`
+		  ADD `raison_sociale` VARCHAR( 50 ) ,
+			ADD `adresse` TEXT ,
+			ADD `cp` VARCHAR( 5 ) ,
+			ADD `ville` VARCHAR( 50 ) ,
+			ADD `tel` VARCHAR( 10 ) ,
+			ADD `directeur` VARCHAR( 50 ) ,
+			ADD `domiciliation` VARCHAR( 9 ) ,
+			ADD `siret` VARCHAR( 14 );";
+    $this->addQuery($query);
+    $query = "INSERT INTO `groups_mediboard` ( `group_id` , `text` )
+			VALUES (NULL , 'Etablissement');";
+    $this->addQuery($query);
 
     $this->makeRevision("0.11");
-    $sql = "ALTER TABLE `groups_mediboard` ADD `ape` VARCHAR( 4 ) DEFAULT NULL;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard` 
+		  ADD `ape` VARCHAR( 4 ) DEFAULT NULL;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.12");
-    $sql = "ALTER TABLE `groups_mediboard` " .
-               "\nCHANGE `group_id` `group_id` int(11) unsigned NOT NULL AUTO_INCREMENT," .
-               "\nCHANGE `cp` `cp` int(5) unsigned zerofill NULL," .
-               "\nCHANGE `tel` `tel` bigint(10) unsigned zerofill NULL," .
-               "\nCHANGE `text` `text` varchar(255) NOT NULL;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard`
+		  CHANGE `group_id` `group_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			CHANGE `cp` `cp` int(5) unsigned zerofill NULL,
+			CHANGE `tel` `tel` bigint(10) unsigned zerofill NULL,
+			CHANGE `text` `text` varchar(255) NOT NULL;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.13");
-    $sql = "ALTER TABLE `groups_mediboard`" .
-            "\nADD `fax` bigint(10) unsigned zerofill NULL AFTER `tel`,".
-            "\nADD `mail` varchar(50) DEFAULT NULL," .
-            "\nADD `web` varchar(255) DEFAULT NULL;" ;
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard`
+		  ADD `fax` bigint(10) unsigned zerofill NULL AFTER `tel`,
+			ADD `mail` varchar(50) DEFAULT NULL,
+			ADD `web` varchar(255) DEFAULT NULL;" ;
+    $this->addQuery($query);
 
     
     $this->makeRevision("0.14");
-    $sql = "ALTER TABLE `groups_mediboard`  
-            ADD `tel_anesth` BIGINT(10) UNSIGNED ZEROFILL;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard`
+      ADD `tel_anesth` BIGINT(10) UNSIGNED ZEROFILL;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.15");
-    $sql = "CREATE TABLE `etab_externe` (
-           `etab_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
-           `nom` VARCHAR(255) NOT NULL, 
-           `raison_sociale` VARCHAR(50), 
-           `adresse` TEXT, 
-           `cp` INT(5) UNSIGNED ZEROFILL, 
-           `ville` VARCHAR(50),  
-           `tel` BIGINT(10) UNSIGNED ZEROFILL, 
-           `fax` BIGINT(10) UNSIGNED ZEROFILL, 
-           `finess` INT(9) UNSIGNED ZEROFILL, 
-           PRIMARY KEY (`etab_id`)) TYPE=MYISAM;";
-    $this->addQuery($sql);
+    $query = "CREATE TABLE `etab_externe` (
+			`etab_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
+			`nom` VARCHAR(255) NOT NULL, 
+			`raison_sociale` VARCHAR(50), 
+			`adresse` TEXT, 
+			`cp` INT(5) UNSIGNED ZEROFILL, 
+			`ville` VARCHAR(50),  
+			`tel` BIGINT(10) UNSIGNED ZEROFILL, 
+			`fax` BIGINT(10) UNSIGNED ZEROFILL, 
+			`finess` INT(9) UNSIGNED ZEROFILL, 
+			PRIMARY KEY (`etab_id`)) TYPE=MYISAM;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.16");
-    $sql = "ALTER TABLE `etab_externe`
-            ADD `siret` CHAR(14), 
-            ADD `ape` CHAR(4);";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `etab_externe`
+      ADD `siret` CHAR(14), 
+      ADD `ape` CHAR(4);";
+    $this->addQuery($query);
     
     $this->makeRevision("0.17");
-    $sql = "ALTER TABLE `groups_mediboard`
-            ADD `service_urgences_id` INT(11) UNSIGNED;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard`
+      ADD `service_urgences_id` INT(11) UNSIGNED;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.18");
-    $sql = "ALTER TABLE `etab_externe`
-            CHANGE `ape` `ape` VARCHAR(6);";
-    $this->addQuery($sql);
-    $sql = "ALTER TABLE `groups_mediboard`
-            CHANGE `ape` `ape` VARCHAR(6);";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `etab_externe`
+      CHANGE `ape` `ape` VARCHAR(6);";
+    $this->addQuery($query);
+    $query = "ALTER TABLE `groups_mediboard`
+      CHANGE `ape` `ape` VARCHAR(6);";
+    $this->addQuery($query);
     
     $this->makeRevision("0.19");
-    $sql = "ALTER TABLE `groups_mediboard` 
-              ADD INDEX (`service_urgences_id`);";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard` 
+      ADD INDEX (`service_urgences_id`);";
+    $this->addQuery($query);
     
     $this->makeRevision("0.20");
-    $sql = "ALTER TABLE `groups_mediboard` 
-              ADD `finess` INT (9) UNSIGNED ZEROFILL;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard` 
+      ADD `finess` INT (9) UNSIGNED ZEROFILL;";
+    $this->addQuery($query);
     
     $this->makeRevision("0.21");
-    $sql = "ALTER TABLE `groups_mediboard` 
-              ADD `pharmacie_id` INT (11) UNSIGNED,
-              ADD INDEX (`pharmacie_id`);";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard` 
+      ADD `pharmacie_id` INT (11) UNSIGNED,
+      ADD INDEX (`pharmacie_id`);";
+    $this->addQuery($query);
     
     $this->makeRevision("0.22");
-    $sql = "ALTER TABLE `groups_mediboard` 
-              ADD `chambre_particuliere` ENUM ('0','1') NOT NULL DEFAULT '0'";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_mediboard` 
+      ADD `chambre_particuliere` ENUM ('0','1') NOT NULL DEFAULT '0'";
+    $this->addQuery($query);
     
     $this->makeRevision("0.23");
-    $sql = "CREATE TABLE `groups_config` (
-              `groups_config_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
-              `object_id` INT (11) UNSIGNED,
-              `max_comp` INT (11) UNSIGNED,
-              `max_ambu` INT (11) UNSIGNED
-            ) TYPE=MYISAM;";
-    $this->addQuery($sql);
+    $query = "CREATE TABLE `groups_config` (
+      `groups_config_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+      `object_id` INT (11) UNSIGNED,
+      `max_comp` INT (11) UNSIGNED,
+      `max_ambu` INT (11) UNSIGNED
+    ) TYPE=MYISAM;";
+    $this->addQuery($query);
     
-    $sql = "ALTER TABLE `groups_config` 
-              ADD INDEX (`object_id`);";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_config` 
+      ADD INDEX (`object_id`);";
+    $this->addQuery($query);
     
     $this->makeRevision("0.24");
-    $sql = "ALTER TABLE `groups_config` 
-              ADD `codage_prat` ENUM ('0','1');";
-    $this->addQuery($sql);
-    $sql = "UPDATE SET TABLE `groups_config` 
-              ADD `codage_prat` ENUM ('0','1') DEFAULT 0;";
-    $this->addQuery($sql);
+    $query = "ALTER TABLE `groups_config` 
+      ADD `codage_prat` ENUM ('0','1');";
+    $this->addQuery($query);
+    $query = "ALTER TABLE `groups_config` 
+      CHANGE `codage_prat` `codage_prat` ENUM ('0','1') DEFAULT '0';";
+    $this->addQuery($query);
     
     $this->mod_version = "0.25";
   } 
