f4f222975e9aef0ab5d516f26d4c5e164bba926e
Fabien Ménager <fabien.menager@openxtrem.com>
2010-11-08 09:20:52 +0000

    ref: ajout de la possibilité de concatener des schemas XML
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10596 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hprimxml/ajax_do_cfg_action.php
+++ modules/hprimxml/ajax_do_cfg_action.php
@@ -62,15 +62,91 @@ function extractFiles($schemaDir, $schemaFile, $delOldDir = false) {
   $archivePath = "$baseDir/$schemaFile";
   if ($delOldDir && file_exists($destinationDir)) {
     if (CMbPath::remove($destinationDir)) {
-      echo "<div class='message'>Suppression de $destinationDir</div>";
+      echo "<div class='info'>Suppression de $destinationDir</div>";
     } else {
       echo "<div class='error'>Impossible de supprimer le dossier $destinationDir</div>";
+      return;
     }
   }
   if (false != $nbFiles = CMbPath::extract($archivePath, $destinationDir)) {
-    echo "<div class='message'>Extraction de $nbFiles fichiers pour $schemaDir</div>";
+    echo "<div class='info'>Extraction de $nbFiles fichiers pour $schemaDir</div>";
   } else {
     echo "<div class='error'>Impossible d'extraire l'archive $schemaFile</div>";
+    return;
+  }
+  
+  if(CAppUI::conf("hprimxml concatenate_xsd")) {
+    $rootFiles = glob("$destinationDir/msg*.xsd");
+    $includeFiles = array_diff(glob("$destinationDir/*.xsd"), $rootFiles);
+    $importFiles = array();
+    
+    foreach($rootFiles as $rootFile) {
+      $xsd = new DOMDOcument();
+      $xsd->loadXML(file_get_contents($rootFile));
+      
+      // suppression des includes actuels
+      $xpath = new DOMXPath($xsd);
+      $includeNodes = $xpath->query("//xsd:include");
+      foreach($includeNodes as $node) {
+        $node->parentNode->removeChild($node);
+      }
+      
+      // ... et des imports
+      $includeNodes = $xpath->query("//xsd:import");
+      foreach($includeNodes as $node) {
+        $node->parentNode->removeChild($node);
+      }
+      
+      foreach($includeFiles as $includeFile) {
+        $include = new DOMDOcument();
+        $include->loadXML(file_get_contents($includeFile));
+        
+        // suppression des includes actuels
+        $xpath = new DOMXPath($xsd);
+        $includeNodes = $xpath->query("//xsd:include");
+        foreach($includeNodes as $node) {
+          $node->parentNode->removeChild($node);
+        }
+        
+        // ... et des imports
+        $includeNodes = $xpath->query("//xsd:import[@schemaLocation]");
+        foreach($includeNodes as $node) {
+          $schemaLocation = $node->getAttribute("schemaLocation");
+          $importFiles[$schemaLocation] = true;
+          $node->removeAttribute("schemaLocation");
+          //$node->parentNode->removeChild($node);
+        }
+        
+        $isImport = false;
+        foreach($importFiles as $key => $value) {
+          if (strpos($includeFile, $key) !== false) {
+            $isImport = true;
+            break;
+          }
+        }
+        
+        foreach($include->documentElement->childNodes as $child) {
+          $impNode = $xsd->importNode($child, true);
+          
+          if ($isImport) {
+            $xsd->documentElement->setAttribute("xmlns:insee", "http://www.hprim.org/inseeXML");
+            if (!$child instanceof DOMText && !$child instanceof DOMComment) {
+              if ($name = $impNode->getAttribute("name")) {
+                $name = "insee:$name";
+                $impNode->setAttribute("name", $name);
+              }
+            }
+          }
+          
+          $xsd->documentElement->appendChild($impNode);
+        }
+  
+      }
+      
+      file_put_contents(substr($rootFile, 0, -4).".xml", $xsd->saveXML());
+      
+      echo "<div class='info'>Schéma concatené</div>";
+    }
   }
 }
 
