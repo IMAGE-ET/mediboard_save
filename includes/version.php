b8e398a4041ba5f72a66a68c825ccd6507ad621c
Flavien Crochard <flavien.crochard@openxtrem.com>
2010-11-10 09:11:38 +0000

    Ref: La div systemMsg ne doit pas être visible à l'impression.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10618 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 765,
+  "build" => 766,
 );
 
 $version["string"] = implode(".", $version);
