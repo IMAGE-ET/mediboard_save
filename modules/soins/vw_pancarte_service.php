da251313f1c7681aeffc79c668a303ed9b6d9f8c
Alexis Granger <granger@openxtrem.com>
2010-11-15 14:58:59 +0000

    bug: dateTime non defini
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10633 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/vw_pancarte_service.php
+++ modules/soins/vw_pancarte_service.php
@@ -174,12 +174,6 @@ foreach($prescriptions as $_prescription){
 			if($_planif->_ref_object->_recent_modification){
         $new[$_prescription->_id]["$_date $time"] = 1;
         $pancarte[$_prescription->_id]["$_date $time"][$type][$_planif->object_id]["new"] = 1;
-      }
-      // Creation du tableau d'urgences
-      if(is_array($_planif->_ref_object->_dates_urgences) && array_key_exists($date_reelle, $_planif->_ref_object->_dates_urgences) 
-         && in_array($dateTime, $_planif->_ref_object->_dates_urgences[$date_reelle])){
-        $urgences[$_prescription->_id]["$_date $time"] = 1;
-        $pancarte[$_prescription->_id]["$_date $time"][$type][$_planif->object_id]["urgence"] = 1;
       }				
 		}
 		
@@ -253,12 +247,6 @@ foreach($prescriptions as $_prescription){
       if($_administration->_ref_object->_recent_modification){
         $new[$_prescription->_id]["$_date $time"] = 1;
         $pancarte[$_prescription->_id]["$_date $time"][$type][$_administration->object_id]["new"] = 1;
-      }
-      // Creation du tableau d'urgences
-      if(is_array($_administration->_ref_object->_dates_urgences) && array_key_exists($date_reelle, $_administration->_ref_object->_dates_urgences) 
-         && in_array($dateTime, $_administration->_ref_object->_dates_urgences[$date_reelle])){
-        $urgences[$_prescription->_id]["$_date $time"] = 1;
-        $pancarte[$_prescription->_id]["$_date $time"][$type][$_administration->object_id]["urgence"] = 1;
       }
 
 			// Suppression d'une planification systeme replanifiée
