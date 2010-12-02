7dc559af952465b544de15f0c264ab77578579a5
Thomas Despoix <tomdesp@gmail.com>
2010-12-02 15:16:10 +0000

    Ref: Classe CMbRange complète
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10780 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/mbRange.class.php
+++ classes/mbRange.class.php
@@ -10,19 +10,57 @@
 
 /**
  * Generic range calculation.
- * A range is a given pair with a min and max bound
- * A null bound is infinite
+ * A range is a given pair with a lower and upper bound
+ * A null bound is infinite (minus for lower, plus for upper)
  */
 abstract class CMbRange {
+  /**
+   * Tell whether range is void (empty)
+   * @param object $lower The lower bound
+   * @param object $upper The upper bound
+   * @return boolean 
+   */
+  static function void($lower, $upper) {
+    return ($upper < $lower || $lower === null || $upper === null);
+  }
+
 	/**
-	 * Check if given value is in given range or equals to limit
-	 * @param $value mixed The value to check
-	 * @param $min mixed The min bound
-	 * @param $max mixed The max bound
-	 * @return bool 
+	 * Tell whether given value is in range (permissive)
+	 * @param object $value The value to check
+	 * @param object $lower The lower bound
+	 * @param object $upper The upper bound
+	 * @return boolean 
 	 */
-	static function in($value, $min, $max) {
-	  return ($value <= $max || $max === null) && ($value >= $min || $min === null);
+	static function in($value, $lower, $upper) {
+	  return ($value <= $upper || $upper === null) && ($value >= $lower || $lower === null);
 	}
+	
+	/**
+	 * Tell whether two ranges collide (permissive)
+	 * @param object $lower1
+	 * @param object $upper1
+	 * @param object $lower2
+	 * @param object $upper2
+	 * @return boolean
+	 */
+	static function collides($lower1, $upper1, $lower2, $upper2) {
+		return ($lower1 <= $upper2 || $upper2 === null) && ($upper1 >= $lower2 || $lower1 === null);
+	}
+
+  /**
+   * Get the intersection of two ranges (permissive)
+   * Result intersection might be empty, that is with upper < lower bound
+   * @param object $lower1
+   * @param object $upper1
+   * @param object $lower2
+   * @param object $upper2
+   * @return array($lower, $upper)
+   */
+  static function intersection($lower1, $upper1, $lower2, $upper2) {
+    return array (
+      ($lower1 !== null && $lower2 !== null ) ? max($lower1, $lower2) : null,
+      ($upper1 !== null && $upper2 !== null ) ? min($upper1, $upper2) : null,
+		);
+  }
 }
 ?>
