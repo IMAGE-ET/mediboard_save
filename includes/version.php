313c2f309a725d71240ee0366a8fcdbc75c69796
Thomas Despoix <tomdesp@gmail.com>
2010-12-09 13:19:28 +0000

    Erg: Alignement globalement vertical des images
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10849 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 786,
+  "build" => 787,
 );
 
 $version["string"] = implode(".", $version);
