35b494b4d5fa8c11c5dc13a28f1f27df74ac15d9
Yohann Poiron <poiron@openxtrem.com>
2010-12-10 08:12:55 +0000

    fnc: Ajout d'une classe générique pour les handlers SOAP
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@10853 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/webservices/classes/soap_handler.class.php
@@ -0,0 +1,45 @@
+<?php /* $Id$ */
+
+/**
+ * @package Mediboard
+ * @subpackage webservices
+ * @version $Revision$
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ */
+
+class CSoapHandler {
+  static $paramSpecs = array(
+    "calculatorAuth" => array ( 
+      "operation" => "string",
+      "entier1"   => "int",
+      "entier2"   => "int")
+  );
+  
+  /**
+   * Calculation method (add/substract)
+   * @param operation Type de l'opération (add / substract)
+   * @param entier1 Premier entier
+   * @param entier2 Deuxième entier
+   * @return result Operation result 
+   **/
+  function calculatorAuth($operation, $entier1, $entier2) {
+    $result = 0;
+
+    if (($operation != "add") && ($operation != "substract")) {
+      return "Veuillez utiliser une methode d'operation valable (add/substract).";
+    } 
+    if (!$entier1 || !$entier2) {
+      return "Veuillez indiquer 2 entiers.";
+    } 
+    if ($operation == "add") {
+      $result = $entier1 + $entier2;
+    }
+    if ($operation == "substract") {
+      $result = $entier1 -$entier2;
+    }
+    
+    return $result;
+  }
+}
+?>
