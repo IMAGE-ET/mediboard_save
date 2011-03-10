27fc8e06c54d0af9c963fe3d88dd10110b2ee14e
Romain Ollivier <ollivier@openxtrem.com>
2011-03-10 15:04:41 +0000

    erg : classement des protocoles par libelle de l'intervention avant le libelle du séjour
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@11554 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/httpreq_vw_list_protocoles.php
+++ modules/dPplanningOp/httpreq_vw_list_protocoles.php
@@ -49,7 +49,7 @@ $ljoin = array(
   "users" => "users.user_id = protocole.chir_id"
 );
 
-$list_protocoles  = $protocole->loadList($where, "users.user_username, libelle_sejour, libelle, codes_ccam", "{$page[$type]},40", null, $ljoin);
+$list_protocoles  = $protocole->loadList($where, "users.user_username, libelle, libelle_sejour, codes_ccam", "{$page[$type]},40", null, $ljoin);
 $total_protocoles = $protocole->countList($where, null, null, null, $ljoin);
 
 foreach ($list_protocoles as $_prot){
