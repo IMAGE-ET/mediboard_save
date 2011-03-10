fd02746aeca0b9e1b43f85c4941c817b819ac19c
Fabien Ménager <fabien.menager@openxtrem.com>
2011-03-10 21:59:59 +0000

    fnc: ajout d'un test de generateur de sprite pour les icones des modules
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@11560 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ tests/icons_sprite.php
@@ -0,0 +1,49 @@
+<?php
+
+$size_orig = 48;
+$ttl = 60*60; // seconds
+
+$size = isset($_GET["size"]) ? $_GET["size"] : $size_orig;
+
+$icons = glob("../modules/*/images/icon.png");
+
+$hash = md5(implode("", $icons));
+$cachefile = "../tmp/$hash.sprite.$size.png";
+
+$out_of_date = !file_exists($cachefile) || (filemtime($cachefile)+$ttl < time());
+
+if (!$out_of_date) {
+  $last_update = filemtime($cachefile);
+  
+  foreach($icons as $_icon) {
+    if ($last_update < filemtime($_icon)) {
+      $out_of_date = true;
+      break;
+    }
+  }
+}
+
+if ($out_of_date) {
+  $img = imagecreatetruecolor($size, $size*count($icons));
+  $background = imagecolorallocate($img, 0, 0, 0);
+  imagecolortransparent($img, $background); // make the new temp image all transparent
+  imagesavealpha($img, true);
+  imagealphablending($img, false);
+  
+  foreach($icons as $i => $_icon) {
+    $_img = imagecreatefrompng($_icon);
+    
+    if ($size != $size_orig) {
+      imagecopyresampled($img, $_img, 0, $i*$size, 0, 0, $size, $size, $size_orig, $size_orig);
+    }
+    else {
+      imagecopy($img, $_img, 0, $i*$size, 0, 0, $size_orig, $size_orig);
+    }
+  }
+  
+  ob_start();
+  imagepng($img);
+  $content = ob_get_clean();
+  
+  file_put_contents($cachefile, $content);
+}
