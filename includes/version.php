50a04c3eecac33e6be47d38737c1bf0cb51a1840
Fabien Ménager <fabien.menager@openxtrem.com>
2011-03-30 11:11:01 +0000

    ref: increment de $version
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@11704 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 862,
+  "build" => 863,
 );
 
 $version["string"] = implode(".", $version);
