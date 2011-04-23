f06650e08f2558ded110dc5ba66c7de5879902f7
Thomas Despoix <tomdesp@gmail.com>
2011-04-23 13:22:52 +0000

    Ref: version...
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@11981 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 882,
+  "build" => 883,
 );
 
 $version["string"] = implode(".", $version);
