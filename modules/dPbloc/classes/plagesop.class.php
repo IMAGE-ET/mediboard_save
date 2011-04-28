2bac1ba4aebfe4752a87e9168ea993f2b774ae52
Romain Ollivier <ollivier@openxtrem.com>
2011-04-28 12:20:32 +0000

    bug : errur dans la mise à jour des champs de base de données sur les horaires (sur un chiffre au lieu de deux)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12003 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/classes/plagesop.class.php
+++ modules/dPbloc/classes/plagesop.class.php
@@ -300,13 +300,13 @@ class CPlageOp extends CMbObject {
   
   function updateDBFields() {
     if(($this->_heuredeb !== null) && ($this->_minutedeb !== null)) {
-      $this->debut = "$this->_heuredeb:$this->_minutedeb:00";
+      $this->debut = sprintf("%02d:%02d:00", $this->_heuredeb, $this->_minutedeb);
     }
     if(($this->_heurefin !== null) && ($this->_minutefin !== null)) {
-      $this->fin   = "$this->_heurefin:$this->_minutefin:00";
+      $this->fin = sprintf("%02d:%02d:00", $this->_heurefin, $this->_minutefin);
     }
     if(($this->_year !== null) && ($this->_month !== null) && ($this->_day !== null)) {
-      $this->date = "$this->_year-$this->_month-$this->_day";
+      $this->date = sprintf("%02d-%02d-%02d", $this->_year, $this->_month, $this->_day);
     }
     return parent::updateDBFields();
   }
