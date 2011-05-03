8f206f706b91e641fc579a9d06f4d240bf293743
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-05-03 10:54:52 +0000

    Fnc: Script qui convertit n files en pdf (n passé en paramètre).
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12040 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPfiles/ajax_file_to_pdf.php
@@ -0,0 +1,47 @@
+<?php /* $Id: $ */
+
+/**
+ * @package Mediboard
+ * @subpackage includes
+ * @version $Revision: $
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ */
+
+$nb_files = CValue::get("nb_files", 20);
+$file = new CFile();
+$where = array();
+$where["object_class"] = " NOT LIKE 'CFile'";
+
+// Ne convertir que les fichiers dont le nom se finit par une extension convertible
+$rlike = "";
+$types = preg_split("/[\s]+/", CFile::$file_types);
+
+foreach($types as $key => $_type) {
+  $rlike .= " file_name RLIKE '.".$_type."$'";
+  if ($key != (count($types) -1)) {
+    $rlike .= " OR";
+  }
+}
+$where[] = $rlike;
+$where[] = "file_id NOT IN (SELECT object_id from files_mediboard WHERE object_class LIKE 'CFile')";
+
+$files = $file->loadList($where, null, $nb_files);
+
+$converted = 0;
+$not_converted = "";
+
+foreach($files as $_file) {
+  if ($_file->convertToPDF()) {
+    $converted ++;
+  }
+  else {
+    $not_converted .= $_file->_id . " - ";
+  }
+}
+
+if ($converted != count($files)) {
+  trigger_error("Les fichiers suivants n'ont pas été convertis : $not_converted", E_USER_ERROR);
+}
+
+?>
