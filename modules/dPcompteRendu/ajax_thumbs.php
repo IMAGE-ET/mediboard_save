fe52c78314447a99268cba8bd0ee538cb6f34e1c
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-05-13 11:18:28 +0000

    Bug: Pas de génération de vignettes si le pdf est vide.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12163 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/ajax_thumbs.php
+++ modules/dPcompteRendu/ajax_thumbs.php
@@ -16,9 +16,9 @@ $index   = CValue::get("index");
 $file = new CFile;
 $file = $file->load($file_id);
 
-// Si le pdf a été supprimé car on ferme la popup sans enregistrer
+// Si le pdf a été supprimé ou vidé car on ferme la popup sans enregistrer
 // le document, alors on ne génère pas la vignette
-if (!file_exists($file->_file_path)) {
+if (!file_exists($file->_file_path) || file_get_contents($file->_file_path) == "") {
   return;
 }
 
