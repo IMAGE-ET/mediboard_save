1829465fdb63a317384813787175da59a47f3fc2
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-05-19 08:44:26 +0000

    Bug: Les messages de création / modification / suppression de planifications n'étaient pas bons.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12219 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPprescription/controllers/do_administration_aed.php
+++ modules/dPprescription/controllers/do_administration_aed.php
@@ -9,6 +9,19 @@
 */
 
 $do = new CDoObjectAddEdit("CAdministration", "administration_id");
-$do->doIt();
+$do->doBind();
+if ($do->_objBefore->planification == 1) {
+  $do->createMsg = CAppUI::tr("CAdministration-planification-msg-create");
+  $do->modifyMsg = CAppUI::tr("CAdministration-planification-msg-modify");
+  $do->deleteMsg = CAppUI::tr("CAdministration-planification-msg-delete");
+}
+
+if (intval(CValue::read($do->request, 'del'))) {
+  $do->doDelete();
+} else {
+  $do->doStore();
+}
+$do->doRedirect();
+
 
 ?>
