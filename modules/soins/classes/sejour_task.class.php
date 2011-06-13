224b990dd852a9173ea1568f17100eea93def6b4
Fabien Ménager <fabien.menager@openxtrem.com>
2011-06-13 10:48:19 +0000

    bug: mauvais non de proprieté dans CSejourTask
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12416 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/classes/sejour_task.class.php
+++ modules/soins/classes/sejour_task.class.php
@@ -13,16 +13,16 @@ class CSejourTask extends CMbObject {
   // DB Table key
   var $sejour_task_id  = null;
   
-	// DB Fields
-	var $sejour_id       = null;
-	var $description     = null;
-	var $realise         = null;
-	var $resultat        = null;
-	var $prescription_line_element_id = null;
+  // DB Fields
+  var $sejour_id       = null;
+  var $description     = null;
+  var $realise         = null;
+  var $resultat        = null;
+  var $prescription_line_element_id = null;
 
-	var $_ref_sejour     = null;
-  var $_prescription_line_element = null;
-	
+  var $_ref_sejour     = null;
+  var $_ref_prescription_line_element = null;
+  
   function getSpec() {
     $spec = parent::getSpec();
     $spec->table = 'sejour_task';
@@ -32,11 +32,11 @@ class CSejourTask extends CMbObject {
   
   function getProps() {
     $specs = parent::getProps();
-		$specs["sejour_id"]   = "ref notNull class|CSejour";
+    $specs["sejour_id"]   = "ref notNull class|CSejour";
     $specs["description"] = "text notNull helped";
-		$specs["realise"]     = "bool default|0";
-		$specs["resultat"]    = "text helped";
-		$specs["prescription_line_element_id"] = "ref class|CPrescriptionLineElement";
+    $specs["realise"]     = "bool default|0";
+    $specs["resultat"]    = "text helped";
+    $specs["prescription_line_element_id"] = "ref class|CPrescriptionLineElement";
     return $specs;
   }
 
@@ -44,15 +44,15 @@ class CSejourTask extends CMbObject {
     parent::updateFormFields();
     $this->_view = $this->description;
   }
-	
-	function loadRefSejour() {
+  
+  function loadRefSejour() {
     $this->_ref_sejour = new CSejour();
     $this->_ref_sejour = $this->_ref_sejour->getCached($this->sejour_id);
   }
-	
-	function loadRefPrescriptionLineElement(){
-		$this->_ref_prescription_line_element = $this->loadFwdRef("prescription_line_element_id");
-	}
+  
+  function loadRefPrescriptionLineElement(){
+    $this->_ref_prescription_line_element = $this->loadFwdRef("prescription_line_element_id");
+  }
 }
   
 ?>
