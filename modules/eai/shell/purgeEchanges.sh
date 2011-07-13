5703a4a55d452a4922a4b670f1dca5582c4114f5
Thomas Despoix <tomdesp@gmail.com>
2011-07-13 12:54:12 +0000

    Fnc: Script shell de purges des échanges dans le module eai
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12650 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/eai/shell/purgeEchanges.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+username=$1
+password=$2
+
+auth="login=1&username=${username}&password=${password}"
+moda="m=webservices&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
+log="/var/log/mediboard/echanges.purge.log"
+doc="/var/log/mediboard/echanges.purge.html"
+wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
