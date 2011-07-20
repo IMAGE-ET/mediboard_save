2ebb89eeea2bd84673697930fcc448da137e1882
Thomas Despoix <tomdesp@gmail.com>
2011-07-20 19:49:45 +0000

    Fnc: Outil de purge des crazy access logs
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12724 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/crazy_logs.php
+++ modules/system/crazy_logs.php
@@ -17,11 +17,7 @@ $limit = CValue::get("limit", 1000);
 $log = new CAccessLog();
 $ds = $log->_spec->ds;
 
-$query = "SELECT module, action, COUNT(*) AS total FROM `access_log` 
-  WHERE duration / hits > '$ratio'
-  GROUP BY module, action";
-$logs = $ds->loadList($query);
-
+// Purge
 $purged_count = null;
 if ($mode == "purge") {
   $query = "DELETE FROM `access_log` 
@@ -30,6 +26,13 @@ if ($mode == "purge") {
   $ds->exec($query);
   $purged_count = $ds->affectedRows();
 }
+
+// Détection
+$query = "SELECT module, action, COUNT(*) AS total FROM `access_log` 
+  WHERE duration / hits > '$ratio'
+  GROUP BY module, action";
+$logs = $ds->loadList($query);
+
 
 // Création du template
 $smarty = new CSmartyDP();
