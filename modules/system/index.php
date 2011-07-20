b69a17407f1ad8a50d62494192f61193d46b9e0f
Romain Ollivier <ollivier@openxtrem.com>
2011-07-20 15:01:43 +0000

    ref: droits sur les onglets de l'administration
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12720 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/index.php
+++ modules/system/index.php
@@ -9,16 +9,16 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("view_modules"        , TAB_EDIT);
+$module->registerTab("view_modules"        , TAB_ADMIN);
 $module->registerTab("view_messages"       , TAB_READ);
-$module->registerTab("view_translate"      , TAB_ADMIN);
+$module->registerTab("view_translate"      , TAB_EDIT);
 $module->registerTab("object_merger"       , TAB_ADMIN);
-$module->registerTab("view_history"        , TAB_EDIT);
-$module->registerTab("view_access_logs"    , TAB_ADMIN);
-$module->registerTab("view_ressources_logs", TAB_ADMIN);
-$module->registerTab("vw_idx_redirections" , TAB_EDIT);
-$module->registerTab("view_network_address", TAB_EDIT);
-$module->registerTab("idx_view_senders"    , TAB_READ);
+$module->registerTab("view_history"        , TAB_READ);
+$module->registerTab("view_access_logs"    , TAB_READ);
+$module->registerTab("view_ressources_logs", TAB_READ);
+$module->registerTab("vw_idx_redirections" , TAB_ADMIN);
+$module->registerTab("view_network_address", TAB_READ);
+$module->registerTab("idx_view_senders"    , TAB_EDIT);
 $module->registerTab("about"               , TAB_READ);
 
 ?>
