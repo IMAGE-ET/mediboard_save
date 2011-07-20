13861accbbe541e78c60b41e887fe520dd8fda34
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-07-20 13:38:30 +0000

    Fnc: Edition rapide pour les packs de modèles.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12711 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 948,
+  "build" => 949,
 );
 
 $version["string"] = implode(".", $version);
