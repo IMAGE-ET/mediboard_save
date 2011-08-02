0a1ab1644aa97de5529fb2f536328eb97dd4f5e1
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-08-02 07:35:44 +0000

    Ref: Utilisation du CValue::post au lieu de la variable globale post pour l'upload de fichier par yoplet.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12809 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPfiles/ajax_yoplet_upload.php
+++ modules/dPfiles/ajax_yoplet_upload.php
@@ -8,7 +8,7 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
  */
 
-$file_name = $_POST["checksum"];
+$file_name = CValue::post("checksum");
 file_put_contents("tmp/".$file_name, file_get_contents($_FILES["file"]["tmp_name"]));
 
 ?>
