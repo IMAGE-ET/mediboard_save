ff82819b3cc1a503f549c2e12582971ad98ab6b8
Fabien Ménager <fabien.menager@openxtrem.com>
2011-08-04 16:17:27 +0000

    ref: ajout d'une classe CMbSimpleXMLElement
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12821 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ classes/CMbSimpleXMLElement.class.php
@@ -0,0 +1,72 @@
+<?php /* $Id$ */
+
+/**
+ * @package Mediboard
+ * @subpackage classes
+ * @version $Revision$
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ */
+
+if (!class_exists("SimpleXMLElement", false)) {
+  return;
+}
+
+class CMbSimpleXMLElement extends SimpleXMLElement {
+	function getValidPatterns(){
+		return array("*");
+	}
+	
+	function getXpath($prefix = "") {
+		$tokens = array();
+		
+		foreach($this->getValidPatterns() as $patt) {
+			$tokens[] = "$prefix$patt";
+		}
+		
+		return implode(" | ", $tokens);
+	}
+	
+	/**
+	 * @return CMbSimpleXMLElement
+	 */
+  function getParent(){
+    return current($this->xpath($this->getXpath("parent::")));
+  }
+  
+  /**
+   * @return CMbSimpleXMLElement
+   */
+  function getNextSibling(){
+    return current($this->xpath($this->getXpath("following-sibling::")));
+  }
+  
+  /**
+   * @return CMbSimpleXMLElement
+   */
+  function getPreviousSibling(){
+    return current($this->xpath($this->getXpath("preceding-sibling::")));
+  }
+  
+  /**
+   * @return CMbSimpleXMLElement
+   */
+  function getFirstChild(){
+    return current($this->xpath($this->getXpath()));
+  }
+  
+  /**
+   * @return CMbSimpleXMLElement
+   */
+	function getNext(){
+		if ($next = $this->getNextSibling()) {
+			mbTrace("Suivant = sibling");
+			return $next;
+		}
+		
+		if ($parent = $this->getParent()) {
+      mbTrace("Suivant = parent");
+			return $parent->getNext();
+		}
+	}
+}
