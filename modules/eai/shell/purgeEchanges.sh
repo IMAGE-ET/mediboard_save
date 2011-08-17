bd118f618f3f295f2c80d57f7f853ec593e36203
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-08-17 11:50:26 +0000

    Ref: Ajout de la purge des échanges ftp dans le script shell purgeEchange du module eai.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@12878 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/eai/shell/purgeEchanges.sh
+++ modules/eai/shell/purgeEchanges.sh
@@ -12,3 +12,8 @@ moda="m=hprimxml&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
 log="/var/log/mediboard/echanges.purge.log"
 doc="/var/log/mediboard/echanges.purge.html"
 wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
+
+moda="m=ftp&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
+log="/var/log/mediboard/echanges.purge.log"
+doc="/var/log/mediboard/echanges.purge.html"
+wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
