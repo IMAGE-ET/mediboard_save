28ef3706d4ebd93837464b2ed1471250907b144f
Fabien Ménager <fabien.menager@openxtrem.com>
2011-09-20 13:26:19 +0000

    ref: changement de droits sur les onglets du module DMI
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13207 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dmi/index.php
+++ modules/dmi/index.php
@@ -10,14 +10,14 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("vw_commandes"  , TAB_READ);
+$module->registerTab("vw_commandes"  , TAB_EDIT);
 $module->registerTab("vw_elements"   , TAB_EDIT);
 $module->registerTab("vw_categories" , TAB_EDIT);
 //$module->registerTab("vw_peremption" , TAB_READ);
 $module->registerTab("vw_destockage" , TAB_EDIT);
 $module->registerTab("vw_tracabilite", TAB_EDIT);
-$module->registerTab("vw_stats",       TAB_EDIT);
-$module->registerTab("vw_inventory",   TAB_EDIT);
+$module->registerTab("vw_stats",       TAB_READ);
+//$module->registerTab("vw_inventory",   TAB_EDIT);
 $module->registerTab("vw_test_barcode_parser", TAB_ADMIN);
 
 ?>
