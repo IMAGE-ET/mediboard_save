604e41892fa0f20e1623b1f6de39c8569d037ea1
Fabien Ménager <fabien.menager@openxtrem.com>
2011-09-21 15:03:26 +0000

    fnc: ajout de l'argument delete au script de purge des echanges soap et ftp
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13230 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/eai/shell/purgeEchanges.sh
+++ modules/eai/shell/purgeEchanges.sh
@@ -3,17 +3,14 @@ username=$1
 password=$2
 
 auth="login=1&username=${username}&password=${password}"
-moda="m=webservices&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
 log="/var/log/mediboard/echanges.purge.log"
 doc="/var/log/mediboard/echanges.purge.html"
+
+moda="m=webservices&a=ajax_purge_echange&do_purge=1&months=1&max=10000&delete=1"
 wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
 
 moda="m=hprimxml&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
-log="/var/log/mediboard/echanges.purge.log"
-doc="/var/log/mediboard/echanges.purge.html"
 wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
 
-moda="m=ftp&a=ajax_purge_echange&do_purge=1&months=1&max=10000"
-log="/var/log/mediboard/echanges.purge.log"
-doc="/var/log/mediboard/echanges.purge.html"
+moda="m=ftp&a=ajax_purge_echange&do_purge=1&months=1&max=10000&delete=1"
 wget -a ${log} -O ${doc} "http://localhost/mediboard/?${auth}&${moda}"
