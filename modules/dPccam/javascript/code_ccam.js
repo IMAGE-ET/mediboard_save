fb32c79f183cf272153a9acf2a39111c1e8910a7
Romain Ollivier <ollivier@openxtrem.com>
2011-09-23 10:15:36 +0000

    erg: passage de la vue complète d'un code CCAM en modale bug: nom de variable du code CCAM pour la vue complète d'un code
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13250 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPccam/javascript/code_ccam.js
+++ modules/dPccam/javascript/code_ccam.js
@@ -3,9 +3,9 @@
 CodeCCAM = {
 	show: function(code, object_class) {
 	  var url = new Url("dPccam", "vw_full_code");
-	  url.addParam("codeacte", code);
+	  url.addParam("_codes_ccam", code);
 	  url.addParam("object_class", object_class);
 	  url.addParam("hideSelect", "1");
-	  url.popup(700, 550, "Code CCAM");
+	  url.modal();
 	}
 }
