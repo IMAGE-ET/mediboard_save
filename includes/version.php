f2650cf261820cd1da20f12f43dd3d2981ff9e74
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-09-27 10:02:46 +0000

    Erg: Amélioration de la synthèse clinique.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13295 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 981,
+  "build" => 982,
 );
 
 $version["string"] = implode(".", $version);
