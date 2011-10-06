cda87c76cc42b033e281b677ad330b5c162ae4ba
Yohann Poiron <poiron@openxtrem.com>
2011-10-06 10:07:30 +0000

    fnc: Possibilité de rejouer un échange H'XML ou IHE
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13403 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/eai/ajax_send_message.php
+++ modules/eai/ajax_send_message.php
@@ -12,8 +12,51 @@
 
 CCanDo::checkRead();
 
-CAppUI::setMsg("Cette fonctionnalité est désactivée pour le moment...", UI_MSG_ERROR);
+$exchange_guid = CValue::get("exchange_guid");
 
-echo CAppUI::getMsg();
+// Chargement de l'objet
+$exchange = CMbObject::loadFromGuid($exchange_guid);
+$exchange->loadRefsInteropActor();
+
+$receiver = $exchange->_ref_receiver;
+
+$evenement = null;
+if ($receiver instanceof CReceiverIHE) {
+  if ($exchange->type = "PAM") {
+    $evenement = "evenementPatient";
+  }
+}
+
+if ($receiver instanceof CDestinataireHprim) {
+  if ($exchange->type = "patients") {
+    $evenement = "evenementPatient";
+  }
+  if ($exchange->type = "pmsi") {
+    
+  }
+}
+
+if (!$evenement) {
+  CAppUI::stepAjax("Aucun événement défini pour cet échange", UI_MSG_ERROR);
+}
+
+$source = CExchangeSource::get("$receiver->_guid-$evenement");
+$source->setData($exchange->_message);
+$source->send();
+if ($acq = $source->getACQ()) {
+  if ($exchange instanceof CEchangeHprim) {
+    $dom_acq = CHPrimXMLAcquittements::getAcquittementEvenementXML($receiver->_data_format->_family_message);
+    $dom_acq->loadXML($acq);
+    $doc_valid = $dom_acq->schemaValidate();
+    if ($doc_valid) {
+      $exchange->statut_acquittement = $dom_acq->getStatutAcquittement();
+    }
+    $exchange->acquittement_valide = $doc_valid ? 1 : 0;
+    $exchange->_acquittement = $acq;
+    $exchange->store();
+    
+    CAppUI::stepAjax("Le message '".CAppUI::tr("$exchange->_class")."' a été retraité");
+  }
+}
 
 ?>
