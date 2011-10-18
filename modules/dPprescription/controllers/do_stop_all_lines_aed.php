0605654d329ff69adb5ef0c4de89a62ab56e4c4f
Fabien Ménager <fabien.menager@openxtrem.com>
2011-10-18 11:49:47 +0000

    bug: _class_name restant
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13524 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPprescription/controllers/do_stop_all_lines_aed.php
+++ modules/dPprescription/controllers/do_stop_all_lines_aed.php
@@ -20,7 +20,7 @@ foreach($prescription->_ref_prescription_lines as &$_line_med) {
   if(!$_line_med->date_arret && $_line_med->signee) {
     $_line_med->date_arret = mbDate();
     $_line_med->time_arret = mbTime();
-    CAppUI::displayMsg($_line_med->store(), "{$_line_med->_class_name}-msg-store");
+    CAppUI::displayMsg($_line_med->store(), "{$_line_med->_class}-msg-store");
   }
 }
 
@@ -30,7 +30,7 @@ foreach($prescription->_ref_prescription_lines_element as &$_line_elt) {
   if(!$_line_elt->date_arret && $_line_elt->signee) {
     $_line_elt->date_arret = mbDate();
     $_line_elt->time_arret = mbTime();
-    CAppUI::displayMsg($_line_elt->store(), "{$_line_elt->_class_name}-msg-store");
+    CAppUI::displayMsg($_line_elt->store(), "{$_line_elt->_class}-msg-store");
   }
 }
 
@@ -40,7 +40,7 @@ foreach($prescription->_ref_prescription_line_mixes as &$_line_mix) {
   if(!$_line_mix->date_arret && $_line_mix->signature_prat) {
     $_line_mix->date_arret = mbDate();
     $_line_mix->time_arret = mbTime();
-    CAppUI::displayMsg($_line_mix->store(), "{$_line_mix->_class_name}-msg-store");
+    CAppUI::displayMsg($_line_mix->store(), "{$_line_mix->_class}-msg-store");
   }
 }
 
