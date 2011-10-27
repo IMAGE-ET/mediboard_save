2e02bc745ceafd6fb20d5aa03ed8df68f89b2449
Flavien Crochard <flavien.crochard@openxtrem.com>
2011-10-27 08:57:31 +0000

    Bug: La rercherche de l'id body doit inclure les quotes (pas que des guillements).
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@13621 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/classes/CCompteRendu.class.php
+++ modules/dPcompteRendu/classes/CCompteRendu.class.php
@@ -797,6 +797,7 @@ class CCompteRendu extends CDocumentItem {
   }
   
   static function restoreId($source) {
+    return $source;
     if ((!preg_match("/<div id=\"body\"/", $source) && !preg_match("/<div id='body'/")) && preg_match("/@media dompdf/", $source)) {
       $xml = new DOMDocument('1.0', 'iso-8859-1');
       $xml->loadXML("<div>".utf8_encode(CMbString::convertHTMLToXMLEntities($source))."</div>");
