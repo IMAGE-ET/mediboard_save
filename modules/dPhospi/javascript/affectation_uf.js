d8153d6f5d53085d778e0d14e132271d99161226
Aurélie Hébras <aureliehebras@gmail.com>
2011-12-13 09:27:51 +0000

    Ref: erreur dans le mot modal corrigée
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14013 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/javascript/affectation_uf.js
+++ modules/dPhospi/javascript/affectation_uf.js
@@ -6,7 +6,7 @@ AffectationUf  = {
 		var url = new Url('dPhospi'	, 'ajax_affectation_uf');
 		url.addParam('object_guid'	, object_guid);
 		url.requestModal(450);
-		this.modal = url.modaleObject;
+		this.modal = url.modalObject;
 	},
 	
 	affecter: function(curr_affectation_guid, lit_guid) {
@@ -14,7 +14,7 @@ AffectationUf  = {
 		url.addParam('curr_affectation_guid'	, curr_affectation_guid);
 		url.addParam('lit_guid'	, lit_guid);
 		url.requestModal(450);
-		this.modal = url.modaleObject;
+		this.modal = url.modalObject;
 	},
 	onSubmit: function(form) {
 		Control.Modal.close();
