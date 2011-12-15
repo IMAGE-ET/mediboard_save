426e4c054f3ed2cf72d139ceb9b083fdc438f400
Fabien Ménager <fabien.menager@openxtrem.com>
2011-12-15 09:59:26 +0000

    bug: mauvais nom de colonne
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14038 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPsante400/setup.php
+++ modules/dPsante400/setup.php
@@ -57,45 +57,45 @@ class CSetupdPsante400 extends CSetup {
     
     $this->makeRevision("0.14");
     $query = "UPDATE `id_sante400` 
-			SET `tag`='labo code4' 
-			WHERE `tag`='LABO' 
-			AND `object_class`='CMediusers' ;";
+      SET `tag`='labo code4' 
+      WHERE `tag`='LABO' 
+      AND `object_class`='CMediusers' ;";
     $this->addQuery($query);
     
     $this->makeRevision("0.15");
     $query = "UPDATE `id_sante400`
-			SET `tag` = CONCAT('NDOS ', LEFT(`tag`, 8))
-			WHERE `object_class` = 'CSejour'
-			AND `tag` LIKE 'CIDC:___ DMED:________'";
+      SET `tag` = CONCAT('NDOS ', LEFT(`tag`, 8))
+      WHERE `object_class` = 'CSejour'
+      AND `tag` LIKE 'CIDC:___ DMED:________'";
     $this->addQuery($query);
     
     $this->makeRevision("0.16");
     $query = "CREATE TABLE `trigger_mark` (
-			`mark_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
-			`trigger_class` VARCHAR (255) NOT NULL,
-			`trigger_number` VARCHAR (10) NOT NULL,
-			`mark` VARCHAR (255) NOT NULL,
-			`done` ENUM ('0','1') NOT NULL
-		) /*! ENGINE=MyISAM */;";
+      `mark_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+      `trigger_class` VARCHAR (255) NOT NULL,
+      `trigger_number` VARCHAR (10) NOT NULL,
+      `mark` VARCHAR (255) NOT NULL,
+      `done` ENUM ('0','1') NOT NULL
+    ) /*! ENGINE=MyISAM */;";
         
     $this->addQuery($query);
     
     $this->makeRevision("0.17");
     $query = "ALTER TABLE `trigger_mark` ADD UNIQUE `trigger_unique` (
-			`trigger_class` ,
-			`trigger_number`
-		);";
+      `trigger_class` ,
+      `trigger_number`
+    );";
     $this->addQuery($query);
     $query = "ALTER TABLE `trigger_mark` ADD INDEX ( `mark` );";
     $this->addQuery($query);
     
-		$this->makeRevision("0.18");
-		$query = "ALTER TABLE `trigger_mark` 
+    $this->makeRevision("0.18");
+    $query = "ALTER TABLE `trigger_mark` 
               CHANGE `trigger_number` `trigger_number` BIGINT (10) UNSIGNED ZEROFILL NOT NULL;";
-		$this->addQuery($query);
-		
-		$this->makeRevision("0.19");
-		$query = "ALTER TABLE `id_sante400` 
+    $this->addQuery($query);
+    
+    $this->makeRevision("0.19");
+    $query = "ALTER TABLE `id_sante400` 
               CHANGE `id400` `id400` VARCHAR  (25) NOT NULL;";
     $this->addQuery($query);
     
@@ -108,7 +108,7 @@ class CSetupdPsante400 extends CSetup {
     $query = "ALTER TABLE `id_sante400` 
                 CHANGE `object_class` `object_class` VARCHAR (40) NOT NULL;";
     $this->addQuery($query);
-	   
+     
     $this->makeRevision("0.22");
     $query = "CREATE TABLE `incrementer` (
                 `incrementer_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
@@ -123,8 +123,17 @@ class CSetupdPsante400 extends CSetup {
     $query = "ALTER TABLE `incrementer` 
                 ADD INDEX (`last_update`);";
     $this->addQuery($query);
+    
+    $this->makeRevision("0.23");
+    $query = "ALTER TABLE `incrementer` 
+                CHANGE `type` `object_class` ENUM ('CPatient','CSejour') NOT NULL;";
+    $this->addQuery($query);
+    $query = "ALTER TABLE `incrementer` 
+                ADD INDEX (`object_class`),
+                ADD INDEX (`group_id`);";
+    $this->addQuery($query);
               
-    $this->mod_version = "0.23";
+    $this->mod_version = "0.24";
   } 
 }
 ?>
