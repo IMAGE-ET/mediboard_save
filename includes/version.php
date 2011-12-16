f1af48f6f6a7875377df185902ab672f207f6c47
Fabien Ménager <fabien.menager@openxtrem.com>
2011-12-16 13:04:05 +0000

    bug: les soulignements de "onmouseover" s'affichaient lors de l'impression
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14061 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 17,
+  "build" => 18,
 );
 
 $version["string"] = implode(".", $version);
