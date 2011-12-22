9c0f978b4a88a7344823263a119577dba646a9e7
Fabien Ménager <fabien.menager@openxtrem.com>
2011-12-22 15:56:56 +0000

    ref: suppression du forcage d'encodage dans la source Oracle
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14127 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/COracleDataSource.class.php
+++ classes/COracleDataSource.class.php
@@ -16,7 +16,7 @@ class COracleDataSource extends CSQLDataSource {
       return;
     }
     
-    if (false === $this->link = oci_connect($user, $pass, "$host/$name", "AL32UTF8")) {
+    if (false === $this->link = oci_connect($user, $pass, "$host/$name")) {
       $error = $this->error();
       trigger_error( "FATAL ERROR: Connection to Oracle database '$host/$name' failed.\n".$error['message'], E_USER_ERROR );
       return;
