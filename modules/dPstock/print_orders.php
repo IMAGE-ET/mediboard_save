703bc66bc98a1a13a7e9324392326ac0819f01bb
Fabien Ménager <fabien.menager@openxtrem.com>
2012-01-09 14:16:53 +0000

    fnc: ajout du total dans l'impression des commandes et de nouvelles colonnes
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14280 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstock/print_orders.php
+++ modules/dPstock/print_orders.php
@@ -27,20 +27,36 @@ $order = new CProductOrder;
 $orders = $order->search("received", null, null, $where);
 
 $count = $order->_search_count;
+$total = 0;
+$total_ttc = 0;
 
 foreach($orders as $_order) {
-  //$_order->updateCounts();
   $_order->countRenewedItems();
-  if ($_order->object_id) {
-    $_order->loadTargetObject();
-    $_order->_ref_object->loadRefsFwd();
+  
+  foreach ($_order->_ref_order_items as $item) {
+    $item->loadRefsReceptions();
+    $rec = reset($item->_ref_receptions);
+    $_order->_date_received = $rec ? $rec->date : null;
   }
+  
+  if ($_order->object_id) {
+    $_object = $_order->loadTargetObject();
+    $_object->loadRefSejour()->loadNDA();
+    $_object->loadRefsFwd();
+  }
+  
+  $total += $_order->_total;
+  $total_ttc += $_order->_total_tva;
 }
+
+$order->_total = $total;
+$order->_total_tva = $total_ttc;
 
 // Smarty template
 $smarty = new CSmartyDP();
 
 $smarty->assign("orders", $orders);
+$smarty->assign("order", $order);
 $smarty->assign("invoiced", $invoiced);
 $smarty->assign("not_invoiced", $not_invoiced);
 $smarty->assign("date_min", $date_min);
