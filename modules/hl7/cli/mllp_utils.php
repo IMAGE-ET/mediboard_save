98d212b2343fca7b19d88b7c7eafe0584f21e53a
Fabien Ménager <fabien.menager@openxtrem.com>
2012-01-16 12:28:05 +0000

    fnc: possibilité de definir plusieurs types des messages de test pour le serveur MLLP
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14354 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hl7/cli/mllp_utils.php
+++ modules/hl7/cli/mllp_utils.php
@@ -25,3 +25,50 @@ function outln($str){
   $stdout = fopen("php://stdout", "w");
   fwrite($stdout, $str.PHP_EOL);
 }
+
+class ER7Message {
+  /**
+   * @return string An ORU message formatted in ER7
+   */
+  final static function ORU(){
+    $er7 = <<<EOT
+MSH|^~\&||GA0000||VAERS PROCESSOR|20010331||ORU^R01|20010422GA03|T|2.3.1|||AL|
+PID|||1234^^^^SR~1234-12^^^^LR~00725^^^^MR||Doe^John^Fitzgerald^JR^^^L||20001007|M||2106-3^White^HL70005|123 Peachtree St^APT 3B^Atlanta^GA^30210^^M^^GA067||(678) 555-1212^^PRN|
+NK1|1|Jones^Jane^Lee^^RN|VAB^Vaccine administered by (Name)^HL70063|
+NK1|2|Jones^Jane^Lee^^RN|FVP^Form completed by (Name)-Vaccine provider^HL70063|101 Main Street^^Atlanta^GA^38765^^O^^GA121||(404) 554-9097^^WPN|
+ORC|CN|||||||||||1234567^Welby^Marcus^J^Jr^Dr.^MD^L|||||||||Peachtree Clinic|101 Main Street^^Atlanta^GA^38765^^O^^GA121|(404) 554-9097^^WPN|101 Main Street^^Atlanta^GA^38765^^O^^GA121|
+OBR|1|||^Mediboard test|||20010316|
+
+EOT;
+    
+    $obx = array();
+    
+    // Tcore
+    $obx[] = "OBX|1|NM|0002-4b60^Température corporelle^MDIL||".(rand(350, 400)/10)."|0004-17a0^°C^MDIL|";
+    
+    // HR
+    $obx[] = "OBX|1|NM|0002-4182^Rythme cardiaque^MDIL||".rand(40, 90)."|0004-0aa0^bpm^MDIL|";
+    
+    // 
+    //$obx[] = "OBX|1|NM|0002-4182^Rythme cardiaque^MDIL||".rand(40, 90)."|0004-0aa0^bpm^MDIL|";
+    
+    $er7 .= implode("\n", $obx);
+    
+    return $er7;
+  }
+
+  /**
+   * @return array The list of available test messages
+   */
+  static function getList(){
+    $reflection = new ReflectionClass('ER7Message');
+    $list = $reflection->getMethods(ReflectionMethod::IS_FINAL);
+    
+    $types = array();
+    foreach($list as $_method) {
+      $types[] = $_method->name;
+    }
+    
+    return $types;
+  }
+}
