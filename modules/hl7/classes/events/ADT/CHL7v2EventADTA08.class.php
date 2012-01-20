7b2d4497ca56638189ab6aabedd01fbab7031d42
Yohann Poiron <poiron@openxtrem.com>
2012-01-20 13:20:12 +0000

    fnc: Ajout de l'événement A08
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14395 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/hl7/classes/events/ADT/CHL7v2EventADTA08.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * A08 - Update Patient Information - HL7
+ *  
+ * @category HL7
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @version  SVN: $Id:$ 
+ * @link     http://www.mediboard.org
+ */
+
+/**
+ * Class CHL7v2EventADTA08
+ * A08 - Update Patient Information
+ */
+class CHL7v2EventADTA08 extends CHL7v2EventADT implements CHL7EventADTA01 {
+  function __construct($i18n = null) {
+    parent::__construct($i18n);
+        
+    $this->code        = "A08";
+    $this->transaction = CPAM::getTransaction($this->code);
+    $this->msg_codes   = array (
+      array(
+        $this->event_type, $this->code, "{$this->event_type}_A01"
+      )
+    );
+  }
+  
+  function build($sejour) {
+    parent::build($sejour);
+    
+    $patient = $sejour->_ref_patient;
+    // Patient Identification
+    $this->addPID($patient, $sejour);
+    
+    // Patient Additional Demographic
+    $this->addPD1($patient);
+    
+    // Next of Kin / Associated Parties
+    $this->addNK1s($patient);
+    
+    // Patient Visit
+    $this->addPV1($sejour);
+    
+    // Patient Visit - Additionale Info
+    $this->addPV2($sejour);
+  }
+  
+}
+
+?>
