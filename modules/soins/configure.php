d4459a0edd588a465c6219d71cb2276030cacb4a
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-02-07 17:10:14 +0000

    Bug: Problème de variable non définie.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14585 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/configure.php
+++ modules/soins/configure.php
@@ -10,12 +10,20 @@
 
 CCanDo::checkAdmin();
 
+$listHours = range(0, 23);
+foreach($listHours as &$_hour){
+  $_hour = str_pad($_hour,2,"0",STR_PAD_LEFT);
+}
+
 $service  = new CService;
 $services = $service->loadListWithPerms(PERM_READ);
 
 // Création du template
 $smarty = new CSmartyDP();
+
 $smarty->assign("services", $services);
+$smarty->assign("listHours" , $listHours);
+
 $smarty->display("configure.tpl");
 
 ?>
