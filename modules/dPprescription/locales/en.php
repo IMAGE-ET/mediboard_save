e0fb1c631ccec0df6eed272e1b0d45e0c243b45c
Alexis Granger <granger@openxtrem.com>
2012-02-07 09:29:03 +0000

    ref: passage de la prescription en module externe
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14559 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPprescription/locales/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-$locales['CPrescription.protocole.none'] = 'No protocol';
-$locales['CPrescription.protocole.select'] = 'Chose a protocol';
-$locales['CPrescriptionLineElement.jour_decalage_fin.I'] = 'I';
-$locales['CPrescriptionLineElement.jour_decalage_fin.S'] = 'S';
-$locales['CPrescriptionLineMedicament.jour_decalage_fin.I'] = 'I';
-$locales['CPrescriptionLineMedicament.jour_decalage_fin.S'] = 'S';
-?>
