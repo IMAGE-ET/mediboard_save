c74f592023d07bd9a218ffa83566ee2a956a7e22
Alexis Granger <granger@openxtrem.com>
2012-02-13 09:46:29 +0000

    bug: refresh des perfusions
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14618 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/ajax_vw_dossier_soin.php
+++ modules/soins/ajax_vw_dossier_soin.php
@@ -342,7 +342,7 @@ if($object_id && $object_class){
   $smarty->assign("nodebug", true);   
   if($line->_class == "CPrescriptionLineMix"){
     $smarty->assign("_prescription_line_mix", $line);
-    $smarty->display("inc_vw_perf_dossier_soin.tpl");
+    $smarty->display("../../dPprescription/templates/inc_vw_perf_dossier_soin.tpl");
   } else {
     if($line->_class == "CPrescriptionLineElement"){
       $smarty->assign("name_cat", $name_cat);
