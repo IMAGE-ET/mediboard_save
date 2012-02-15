b3b1b9ed90a8ccb9e388eda53390377daf700a03
Thomas Despoix <tomdesp@gmail.com>
2012-02-15 20:39:49 +0000

    Ref: L'envoi de mail diskfull utilise désormais les paramètres de messagerie SMTP system
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14656 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/ajax_send_mail_diskfull.php
+++ modules/system/ajax_send_mail_diskfull.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * aphmOdonto
+ * Send a mail concerning the diskfull problem while backuping
  *  
- * @category aphmOdonto
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
  * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
@@ -11,21 +10,32 @@
  * @link     http://www.mediboard.org
  */
 
-$mail = CValue::get("mail");
-$exchange_source = CExchangeSource::get("mediuser-" . CAppUI::$user->_id);
-
-$exchange_source->init();
+$source = CExchangeSource::get("system-message", "smtp", true);
 
 try {
-  $exchange_source->setRecipient($mail);
-  $exchange_source->setSubject("Mediboard - Alerte d'espace disque");
-  $exchange_source->setBody("L'espace disque du serveur est inférieur à 6 Go. Il faut y remédier au plus vite.");
-  $exchange_source->send();
+  // Source init
+  $source->init();
+  $source->addTo (CAppUI::conf("system CMessage default_email_to"  ));
+  $source->addBcc(CAppUI::conf("system CMessage default_email_from"));
+  $source->addRe (CAppUI::conf("system CMessage default_email_from"));
+  
+  // Email subject
+  $page_title = CAppUI::conf("page_title");
+  $message_subject = CAppUI::tr("system-msg-backup-diskfull");
+  $source->setSubject("$page_title - $message_subject");
+      
+  // Email body
+  $message_body = CAppUI::tr("system-msg-backup-diskfull-desc");
+  $body = "<strong>$page_title</strong>";
+  $body.= "<p>$message_body</p>";
+  $source->setBody($body);
+      
+  // Do send
+  $source->send();
 }
-catch(phpmailerException $e) {
-  throw new CMbException($e->errorMessage());
+catch (CMbException $e) {
+  $e->stepAjax();
 }
-catch(CMbException $e) {
-  throw new CMbException($e->stepAjax());
-}
+
+CAppUI::stepAjax("system-msg-email_sent");
 ?>
