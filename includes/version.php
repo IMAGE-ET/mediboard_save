f32287bee92345b9299bccdc82f2a4126518c3ad
Thomas Despoix <tomdesp@gmail.com>
2012-02-29 23:20:42 +0000

    Erg: La liste RTM courante est toujours rafraichie après une modification dans le formulaire principal
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@14759 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 45,
+  "build" => 46,
 );
 
 $version["string"] = implode(".", $version);
