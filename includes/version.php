5d8ae30b6c9648b6329acd6c56e5ad917391533b
Yohann Poiron <poiron@openxtrem.com>
2012-03-28 13:32:46 +0000

    ref: Incrémentation du version
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15050 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 50,
+  "build" => 51,
 );
 
 $version["string"] = implode(".", $version);
