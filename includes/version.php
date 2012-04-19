dea466e3d367d439a790fcfd7befed3b43c23b5c
Yohann Poiron <poiron@openxtrem.com>
2012-04-19 13:38:42 +0000

    ref: Oubli du version
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15283 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 57,
+  "build" => 58,
 );
 
 $version["string"] = implode(".", $version);
