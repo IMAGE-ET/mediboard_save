9fa2816f164120afc22b2d5545e37df7b7348d42
Aurélie Hébras <aureliehebras@gmail.com>
2012-04-24 08:16:52 +0000

    Ref: Lors de la mutation d'un patient des urgences vers un lit bloqué, non supression du blocage d'urgence
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15318 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/controllers/do_sejour_aed.php
+++ modules/dPplanningOp/controllers/do_sejour_aed.php
@@ -11,8 +11,7 @@ if ($praticien_id = CValue::post("praticien_id")) {
   CValue::setSession("praticien_id", $praticien_id);
 }
 //Pour un séjour ayant comme mode de sortie urgence:
-if(CValue::post("mode_sortie") == "mutation" && CValue::post("type") == "urg" && CValue::post("lit_id")){
-	
+if(CValue::post("mode_sortie") == "mutation" && CValue::post("type") == "urg" && CValue::post("lit_id")){	
 	$sejour_id = CValue::post("sejour_id");
 	$lit_id = CValue::post("lit_id");
   $sejour = new CSejour();
@@ -25,30 +24,6 @@ if(CValue::post("mode_sortie") == "mutation" && CValue::post("type") == "urg" &&
   $affectation->sejour_id = $sejour_id;
   $affectation->sortie = $sejour->sortie_prevue;
   $affectation->store();
-  
-  //Suppression du blocage d'urgence si l'affectation couvre le temps de celui-ci
-  $affectation_urgence =  new CAffectation();
-  $where = array();
-  $where["sortie"] = " BETWEEN '$affectation->entree' AND '$affectation->sortie'";
-  $where["lit_id"] = "= '$lit_id'";
-  $where["function_id"] = "IS NOT NULL";
-  
-  if($affectation_urgence->loadObject($where)){
-  	$affectation_urgence->delete();
-  }
-  //Réduction du blocage d'urgence sinon
-  else{
-    $where = array();
-  	$where[] = "entree <= '$affectation->entree' OR entree <= '$affectation->sortie'";
-    $where[] = "sortie >= '$affectation->sortie' OR sortie BETWEEN '$affectation->entree' AND '$affectation->sortie'";
-	  $where["lit_id"] = "= '$lit_id'";
-	  $where["function_id"] = "IS NOT NULL";
-  	$affectation_urgence->loadObject($where);
-  	
-  	$affectation_urgence->entree = $affectation->sortie;
-  	$affectation_urgence->sejour_id = null;
-  	$msg = $affectation_urgence->store();
-  }
 }
 
 $do = new CDoObjectAddEdit("CSejour", "sejour_id");
