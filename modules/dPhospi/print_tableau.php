4a6501f2ba6dcc4f284a397ca5e218b5ff3bb217
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-05-04 07:21:54 +0000

    Bug: Si les services ne sont pas en session à l'impression de la vue tableau des affectations, les récupérer de la préférence utilisateur ou les charger en accord avec le droit de lecture.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15445 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/print_tableau.php
+++ modules/dPhospi/print_tableau.php
@@ -15,13 +15,34 @@ CAppUI::requireModuleFile("dPhospi", "inc_vw_affectations");
 $g        = CGroups::loadCurrent()->_id;
 $date     = CValue::get("date"    , mbDate());
 $mode     = CValue::get("mode"    , 0);
-$services = CValue::getOrSession("services_ids", "");
+$services_ids = CValue::getOrSession("services_ids", "");
 $g        = CGroups::loadCurrent()->_id;
+
+if (!$services_ids) {
+  $pref_services_ids = json_decode(CAppUI::pref("services_ids_hospi"));
+  
+  // Si la préférence existe, alors on la charge
+  if (isset($pref_services_ids->{"g$g"})) {
+    $services_ids = $pref_services_ids->{"g$g"};
+    if ($services_ids) {
+      $services_ids = explode("|", $services_ids); 
+    }
+    CValue::setSession("services_ids", $services_ids);
+  }
+  // Sinon, chargement de la liste des services en accord avec le droit de lecture
+  else {
+    $service = new CService;
+    $where = array();
+    $where["group_id"] = "= '".$g."'";
+    $services_ids = array_keys($service->loadListWithPerms(PERM_READ, $where, "externe, nom"));
+    CValue::setSession("services_ids", $services_ids);
+  }
+}
 
 // Récupération des chambres/services
 $where = array();
 $where["group_id"] = "= '$g'";
-$where["service_id"] = CSQLDataSource::prepareIn($services);
+$where["service_id"] = CSQLDataSource::prepareIn($services_ids);
 $service = new CService;
 $order = "nom";
 $services = $service->loadListWithPerms(PERM_READ,$where, $order);
