20f7fa5b2ad5e8ff30e1363d3e59011f565cb507
Yohann Poiron <poiron@openxtrem.com>
2012-05-16 10:38:16 +0000

    bug: La date de l'intervention était valuée alors que l'on avait une plage
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15547 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/add_operation_csv.php
+++ modules/dPplanningOp/add_operation_csv.php
@@ -97,7 +97,9 @@ if ($file && ($fp = fopen($file['tmp_name'], 'r'))) {
     $operation->chir_id        = $mediuser->_id;
     $operation->plageop_id     = $plageOp->_id;
     $operation->salle_id       = $salle->_id;
-    $operation->date           = $date_op;
+    if (!$operation->plageop_id) {
+      $operation->date         = $date_op;
+    }
     $operation->temp_operation = $temps_op;
     $operation->time_operation = $time_op;
     $operation->loadMatchingObject();
