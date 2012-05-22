b6b772e8e6ee8a07988a55e2f09a5b18d8a2f684
Thomas Despoix <tomdesp@gmail.com>
2012-05-22 08:12:03 +0000

    Bug: Inclusion du script des UF en Ajax dans le formulaire des mediusers
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15610 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/javascript/affectation_uf.js
+++ modules/dPhospi/javascript/affectation_uf.js
@@ -1,29 +1,29 @@
 AffectationUf  = {
-	modal: null,
-	
-	edit: function(object_guid) {
-		var url = new Url('dPhospi'	, 'ajax_affectation_uf');
-		url.addParam('object_guid'	, object_guid);
-		url.requestModal(450);
-		this.modal = url.modalObject;
-	},
-	
-	affecter: function(curr_affectation_guid, lit_guid, callback) {
-		var url = new Url('dPhospi'				, 'ajax_vw_association_uf');
-		url.addParam('curr_affectation_guid'	, curr_affectation_guid);
-		url.addParam('lit_guid'	, lit_guid);
+  modal: null,
+  
+  edit: function(object_guid) {
+    var url = new Url('hospi'  , 'ajax_affectation_uf');
+    url.addParam('object_guid'  , object_guid);
+    url.requestModal(450, 200);
+    this.modal = url.modalObject;
+  },
+  
+  affecter: function(curr_affectation_guid, lit_guid, callback) {
+    var url = new Url('hospi'        , 'ajax_vw_association_uf');
+    url.addParam('curr_affectation_guid'  , curr_affectation_guid);
+    url.addParam('lit_guid'  , lit_guid);
     url.addParam('callback' , callback);
-		url.requestModal(600, 400);
-		this.modal = url.modalObject;
-	},
+    url.requestModal(600, 400);
+    this.modal = url.modalObject;
+  },
   
-	onSubmit: function(form) {
-		Control.Modal.close();
-		return onSubmitFormAjax(form);	
-	},
+  onSubmit: function(form) {
+    Control.Modal.close();
+    return onSubmitFormAjax(form);  
+  },
   
-	onDeletion: function(form) {
-		Control.Modal.close();
-		return confirmDeletion(form);   
-	}
+  onDeletion: function(form) {
+    Control.Modal.close();
+    return confirmDeletion(form);   
+  }
 };
