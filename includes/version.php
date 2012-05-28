d13fc4081d026cffc40fe6302e9b7050ed97499d
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-05-28 09:03:12 +0000

    Bug: Problème de focus après fermeture de la modale d'impression d'ordonnance dans internet explorer.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15671 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 62,
+  "build" => 63,
 );
 
 $version["string"] = implode(".", $version);
