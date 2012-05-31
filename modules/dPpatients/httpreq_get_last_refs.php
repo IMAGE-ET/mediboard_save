33c87d4de24d307262236104d0c34b63ec7f9d9e
Romain Ollivier <ollivier@openxtrem.com>
2012-05-31 15:05:07 +0000

    erg: cache du dossier d'autres cliniques dans la prise de rdv de consultation
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@15745 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/httpreq_get_last_refs.php
+++ modules/dPpatients/httpreq_get_last_refs.php
@@ -13,8 +13,11 @@ $is_anesth = CValue::get("is_anesth", 1);
 
 $patient = new CPatient;
 $patient->load($patient_id);
-$where = array("group_id" => "= '".CGroups::loadCurrent()->_id."'");
-
+$where = array(
+           "group_id" => "= '".CGroups::loadCurrent()->_id."'",
+           "annule"   => "= '0'"
+         );
+         
 foreach ($patient->loadRefsSejours($where) as $_sejour) {
   foreach ($_sejour->loadRefsConsultations() as $_consult) {
     $_consult->getType();
@@ -22,20 +25,23 @@ foreach ($patient->loadRefsSejours($where) as $_sejour) {
     $_consult->loadRefPraticien()->loadRefFunction();
   }
   
-  foreach ($_sejour->loadRefsOperations() as $_operation) {
+  foreach ($_sejour->loadRefsOperations(array("annulee" => "= '0'")) as $_operation) {
     $_operation->loadRefsFwd();
   }
 }
 
-foreach ($patient->loadRefsConsultations() as $_consult) {
+foreach ($patient->loadRefsConsultations(array("annule" => "= '0'")) as $_consult) {
   if ($_consult->sejour_id) {
     unset($patient->_ref_consultations[$_consult->_id]);
     continue;
   }
   
-  $_consult->loadRefsFwd();
-  $_consult->getType();
   $_consult->loadRefPraticien()->loadRefFunction();
+  if($_consult->_ref_praticien->_ref_function->group_id != CGroups::loadCurrent()->_id) {
+    unset($patient->_ref_consultations[$_consult->_id]);
+    continue;
+  }
+  $_consult->getType();
   $_consult->loadRefPlageConsult();
 }
 
