819f48bf37a5561cfc5c310c045a68600a4f61aa
Kévin Grisel <k.grisel@openxtrem.com>
2012-06-26 13:46:14 +0000

    Ref : Timeout de 2 secondes pour la vérification des accès distants dans mediboard/install.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16002 a21d6be2-e512-0410-a060-91f5a2946631

--- install/testHTTP.php
+++ install/testHTTP.php
@@ -12,34 +12,39 @@ function testHTTPCode( $urls = array(), $type = "Autorisé" ) {
   
   $result = array();
   foreach ( $urls as $url ) {
-    if ( substr( getLastHTTPCode( $url ), 0, 1 ) == $startOfCode ) {
-      $result[$url] = array( "type" => $type, "result" => false );
-    }
-    else {
-      $result[$url] = array( "type" => $type, "result" => true );
-    }
+    $lastHTTPCode = getLastHTTPCode( $url );
+    
+    if ($lastHTTPCode) {
+      if ( substr( $lastHTTPCode, 0, 1 ) == $startOfCode ) {
+        $result[$url] = array( "type" => $type, "result" => false );
+      }
+      else {
+        $result[$url] = array( "type" => $type, "result" => true );
+      }
+    }    
   }
   
   return $result;
 }
 
 function getLastHTTPCode( $url ) {
-  $ctx = stream_context_create( array( 
-     'http' => array( 
-         'timeout' => 5 
-         ) 
-     ) 
- );
-
-  @file_get_contents( $url, false, $ctx );
+  
+  ini_set( "default_socket_timeout", 2 );
+  @file_get_contents( $url );
   $headers = $http_response_header;
 
-  foreach ( $headers as $key => $header ) {
-    if ( substr( $header, 0, 4 ) == "HTTP" ) {
-      $response = split( " ", $header );
-      $response = $response[1];
+  if ( is_array( $headers ) ) {
+    foreach ( $headers as $key => $header ) {
+      if ( substr( $header, 0, 4 ) == "HTTP" ) {
+        $response = split( " ", $header );
+        $response = $response[1];
+      }
     }
   }
+  else {
+    $response = false;
+  }
+  
 
   return $response;
 }
