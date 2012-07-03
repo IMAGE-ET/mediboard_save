15a77b109a665dfef6e5f2f054469e0f116c8a5e
Fabien Ménager <fabien.menager@openxtrem.com>
2012-07-03 12:00:52 +0000

    ref: mauvaise indentation (tabs-spaces)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16065 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/eai/javascript/exchange_data_format.js
+++ modules/eai/javascript/exchange_data_format.js
@@ -10,57 +10,60 @@
  */
 
 ExchangeDataFormat = {
-  evenements : null,	
+  evenements : null,  
   target:      "exchange_data_format",
   modal:       null,
   
   refreshExchanges : function(exchange_class, exchange_type, exchange_group_id){
     var url = new Url("eai", "ajax_refresh_exchanges");
     url.addParam("exchange_class", exchange_class);
-    url.addParam("exchange_type"	  , exchange_type);
+    url.addParam("exchange_type"    , exchange_type);
     url.addParam("exchange_group_id"  , exchange_group_id);
     url.requestUpdate("exchanges", { onComplete : function() {
       if (!exchange_type) {
         return;
       }
       var form = getForm("filterExchange");
-	  if (form) {
-	    ExchangeDataFormat.refreshExchangesList(form);
-	  }
-	} });
+      
+      if (form) {
+        ExchangeDataFormat.refreshExchangesList(form);
+      }
+    } });
   },
-	
+  
   fillSelect : function(source, dest, mod_name) {
-	var selected = $V(source);  
-	dest.update();
+    var selected = $V(source);
+    dest.update();
     dest.insert(new Element('option', {value: ''}).update('&mdash; Liste des événements &mdash;'));
-	dest.insert(new Element('option', {value: 'inconnu'}).update($T(mod_name+'-evt-none')));
-	if (!Object.isArray(ExchangeDataFormat.evenements[selected])) {
-	  $H(ExchangeDataFormat.evenements[selected]).each(function(pair){
-	    var v = pair.key;
-		dest.insert(new Element('option', {value: v}).update($T(mod_name+'-evt_'+selected+'-'+v)));
-	  });
-	}
+    dest.insert(new Element('option', {value: 'inconnu'}).update($T(mod_name+'-evt-none')));
+    
+    if (!Object.isArray(ExchangeDataFormat.evenements[selected])) {
+      $H(ExchangeDataFormat.evenements[selected]).each(function(pair){
+        var v = pair.key;
+        dest.insert(new Element('option', {value: v}).update($T(mod_name+'-evt_'+selected+'-'+v)));
+      });
+    }
   },
   
   refreshExchangesList : function(form) {
-	var url = new Url("eai", "ajax_refresh_echanges_list");
-	url.addFormData(form);
-	url.requestUpdate("exchangesList");
-	return false;
+    var url = new Url("eai", "ajax_refresh_echanges_list");
+    url.addFormData(form);
+    url.requestUpdate("exchangesList");
+    return false;
   },
   
   viewExchange : function(exchange_guid) {
     var url = new Url("eai", "ajax_vw_exchange_details");
     url.addParam("exchange_guid", exchange_guid);
-	url.requestModal(900, 530);
+    url.requestModal(900, 530);
   },
   
   reprocessing : function(exchange_guid){
     var url = new Url("eai", "ajax_reprocessing_exchange");
     url.addParam("exchange_guid", exchange_guid);
     url.requestUpdate("systemMsg", { onComplete:
-    	ExchangeDataFormat.refreshExchange.curry(exchange_guid) });
+      ExchangeDataFormat.refreshExchange.curry(exchange_guid) 
+    });
   },
 
   refreshExchange : function(exchange_guid){
@@ -79,9 +82,10 @@ ExchangeDataFormat = {
     var url = new Url("eai", "ajax_send_message");
     url.addParam("exchange_guid", exchange_guid);
     url.requestUpdate("systemMsg", { onComplete:
-    	ExchangeDataFormat.refreshExchange.curry(exchange_guid) });
+      ExchangeDataFormat.refreshExchange.curry(exchange_guid) 
+    });
   },
-	
+  
   changePage : function(page) {
     $V(getForm('filterExchange').page,page);
   },
@@ -89,11 +93,11 @@ ExchangeDataFormat = {
   hide: function() {
     $(this.target).hide();    
   },
-	  
+    
   show: function() {
     $(this.target).appear();    
   },
-	  
+    
   toggle: function() {
     this[$(this.target).visible() ? "hide" : "show"](); 
   }
