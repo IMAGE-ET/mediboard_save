d36384ce02cce6134cef9c642a7a1244c3dc85fd
Fabien Ménager <fabien.menager@openxtrem.com>
2012-07-12 07:53:54 +0000

    ref: augmentation de la cadence de reparation des consult anesth
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16136 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/ajax_create_missing_consult_anesth.php
+++ modules/dPcabinet/ajax_create_missing_consult_anesth.php
@@ -44,7 +44,7 @@ if (count($ids_consults) == 0) {
   return;
 }
 
-$ids_consults = array_slice($ids_consults, 0, 50);
+$ids_consults = array_slice($ids_consults, 0, 100);
 
 foreach ($ids_consults as $_consult_id) {
   $consult = new CConsultation;
