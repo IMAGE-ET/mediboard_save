c885a91cba6afc8cf153d5077996703891c8aa75
Yohann Poiron <poiron@openxtrem.com>
2012-07-24 13:07:34 +0000

    bug: L'appel au redirect faisait l'inverse de l'urlencode !
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16211 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 80,
+  "build" => 81,
 );
 
 $version["string"] = implode(".", $version);
