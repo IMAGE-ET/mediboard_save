09922a9f967f79998939aff13d118f1bc4af6711
Charles-Etienne Masse <c.masse@openxtrem.com>
2012-08-02 16:05:36 +0000

    Ref : en mode mobile, pas d'appel à CApp::rip()
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16313 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/CDoObjectAddEdit.class.php
+++ classes/CDoObjectAddEdit.class.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 /**
  * $Id$
- * 
+ *
  * @package    Mediboard
  * @subpackage classes
  * @author     SARL OpenXtrem <dev@openxtrem.com>
- * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  * @version    $Revision$
  */
 
@@ -25,7 +25,7 @@ class CDoObjectAddEdit {
   var $callBack            = null;
   var $suppressHeaders     = null;
   var $_logIt              = null;
-  
+
   var $_obj  = null;
   var $_old  = null;
 
@@ -34,7 +34,7 @@ class CDoObjectAddEdit {
       CAppUI::stepAjax("Mode-readonly-title", UI_MSG_ERROR);
       return;
     }
-    
+
     global $m;
 
     $this->className           = $className;
@@ -47,16 +47,16 @@ class CDoObjectAddEdit {
     $this->createMsg           = CAppUI::tr("$className-msg-create");
     $this->modifyMsg           = CAppUI::tr("$className-msg-modify");
     $this->deleteMsg           = CAppUI::tr("$className-msg-delete");
-    
+
     $this->request             =& $_POST;
 
     $this->_logIt              = true;
-    
+
     // @todo : à supprimer cf déplacement dans le doBind()
     $this->_obj                = new $this->className();
     $this->_old                = new $this->className();
     $this->onAfterInstanciation(); // Lancer ceci apres chaque instanciation de _obj et _old !!
-    
+
     $this->objectKey = $objectKey ? $objectKey : $this->_obj->_spec->key;
     $this->objectKeys = $this->objectKey . "s";
   }
@@ -66,26 +66,26 @@ class CDoObjectAddEdit {
     $this->suppressHeaders = CMbArray::extract($this->request, "suppressHeaders");
     $this->callBack        = CMbArray::extract($this->request, "callback");
     $this->postRedirect    = CMbArray::extract($this->request, "postRedirect");
-    
+
     if ($this->postRedirect) {
       $this->redirect = $this->postRedirect;
     }
-    
+
     if ($reinstanciate_objects) {
       $this->_obj            = new $this->className();
       $this->_old            = new $this->className();
       $this->onAfterInstanciation(); // Lancer ceci apres chaque instanciation de _obj et _old !!
     }
-    
+
     // Object binding
     $this->_obj->bind($this->request);
-        
-    // Old object 
+
+    // Old object
     $this->_old->load($this->_obj->_id);
   }
-  
+
   function onAfterInstanciation(){
-    
+
   }
 
   function doDelete() {
@@ -106,13 +106,13 @@ class CDoObjectAddEdit {
       }
       return;
     }
-    
+
     if ($msg = $this->_obj->delete()) {
       CAppUI::setMsg($msg, UI_MSG_ERROR);
       if ($this->redirectError) {
         $this->redirect =& $this->redirectError;
       }
-    } 
+    }
     else {
       CValue::setSession($this->objectKey);
       CAppUI::setMsg($this->deleteMsg, UI_MSG_ALERT);
@@ -128,7 +128,7 @@ class CDoObjectAddEdit {
       if ($this->redirectError) {
         $this->redirect =& $this->redirectError;
       }
-    } 
+    }
     else {
       $id = $this->objectKey;
       CValue::setSession($id, $this->_obj->_id);
@@ -143,7 +143,7 @@ class CDoObjectAddEdit {
     if ($this->redirect === null) {
       return;
     }
-    
+
     // Cas ajax
     if ($this->ajax) {
       $this->doCallback();
@@ -152,26 +152,26 @@ class CDoObjectAddEdit {
     // Cas normal
     CAppUI::redirect($this->redirect);
   }
-  
-  function doCallback(){
+
+  function doCallback() {
     $messages = CAppUI::$instance->messages;
-    
+
     echo CAppUI::getMsg();
-    
+
     $fields = $this->_obj->getProperties();
     $fields["_guid"] = $this->_obj->_guid;
     $fields["_class"] = $this->_obj->_class;
-    
+
     foreach($messages as &$_level) {
       $_keys   = array_map("utf8_encode", array_keys($_level));
       $_values = array_map("utf8_encode", array_values($_level));
       $_level = array_combine($_keys, $_values);
     }
-    
+
     $fields["_ui_messages"] = $messages;
-    
+
     $id = $this->_obj->_id ? $this->_obj->_id : 0;
-    
+
     if ($this->callBack) {
       CAppUI::callbackAjax($this->callBack, $id, $fields);
     }
@@ -179,8 +179,11 @@ class CDoObjectAddEdit {
       $guid = "$this->className-$id";
       CAppUI::callbackAjax("Form.onSubmitComplete", $guid, $fields);
     }
-    
-    CApp::rip();
+
+    if(!$_SESSION["browser"]["mobile"]){
+      CApp::rip();
+    }
+
   }
 
   function doIt() {
@@ -192,20 +195,20 @@ class CDoObjectAddEdit {
         $this->request[$this->objectKey] = $object_id;
         $this->doSingle(true);
       }
-      
+
       $this->doRedirect();
     }
-    
+
     $this->doSingle(false);
     $this->doRedirect();
   }
-  
+
   function doSingle($reinstanciate_objects) {
     $this->doBind($reinstanciate_objects);
-    
+
     if (CMbArray::extract($this->request, 'del')) {
       $this->doDelete();
-    } 
+    }
     else {
       $this->doStore();
     }
@@ -213,7 +216,7 @@ class CDoObjectAddEdit {
 
   /**
    * Sets a error messages and redirects
-   * @param string $msg 
+   * @param string $msg
    */
   function errorRedirect($msg) {
     CAppUI::setMsg($msg, UI_MSG_ERROR);
