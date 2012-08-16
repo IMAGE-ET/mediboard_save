6cdc379c53600778c33167fd952d48325ffea55d
Yohann Poiron <poiron@openxtrem.com>
2012-08-16 14:58:27 +0000

    ref: Gestion de la transaction DEC
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16387 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/ihe/classes/CIHE.class.php
+++ modules/ihe/classes/CIHE.class.php
@@ -25,6 +25,7 @@ class CIHE {
 
   /**
    * Retrieve handlers list
+   * 
    * @return array Handlers list
    */
   static function getObjectHandlers() {
@@ -33,6 +34,7 @@ class CIHE {
   
   /**
    * Retrieve events list of data format
+   * 
    * @return array Events list
    */
   function getEvenements() {
@@ -41,32 +43,43 @@ class CIHE {
   
   /**
    * Retrieve transaction name
-   * @param $code Event code
+   * 
+   * @param string $code Event code
+   * 
    * @return string Transaction name
    */
-  static function getTransaction($code) {}
+  static function getTransaction($code) {
+  }
   
   /**
    * Return data format object
-   * @param exchange Instance of exchange
+   * 
+   * @param CExchangeDataFormat $exchange Instance of exchange
+   * 
    * @return object An instance of data format
    */
   static function getEvent(CExchangeDataFormat $exchange) {
     switch ($exchange->type) {
       case "PAM" :
         return CPAM::getEvent($exchange);
-        break;
       case "PAM_FR" :
         return CPAM::getEvent($exchange);
-        break;
-      case "PAM" :
-        break;
+      case "DEC" :
+        return CDEC::getEvent($exchange);
       default :
         throw new CMbException("CIHE_event-unknown");
         break;
     }
   }
   
+  /**
+   * Return Patient Administration Management (PAM) transaction
+   * 
+   * @param string $code Event code
+   * @param string $i18n Internationalization
+   * 
+   * @return object An instance of PAM transaction
+   */
   static function getPAMTransaction($code, $i18n = null) {
     switch ($i18n) {
       case "FR" :
@@ -78,6 +91,14 @@ class CIHE {
     }
   }
   
+  /**
+   * Return Device Enterprise Communication (DEC) transaction
+   * 
+   * @param string $code Event code
+   * @param string $i18n Internationalization
+   * 
+   * @return object An instance of DEC transaction 
+   */
   static function getDECTransaction($code, $i18n = null) {
     return CDEC::getTransaction($code);
   }
