d2e634d032fe34dcf2e5dd39a521f35feb61202c
Fabien Ménager <fabien.menager@openxtrem.com>
2012-09-10 09:06:21 +0000

    ref: optimisation du charhement des administrations Perop
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16557 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/ajax_vw_perop_administrations.php
+++ modules/soins/ajax_vw_perop_administrations.php
@@ -8,38 +8,59 @@
  * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
  */
 
-$prescription_id   = CValue::get("prescription_id");
+$prescription_id = CValue::get("prescription_id");
 
-  
+// Chargement des administrations
 $administration = new CAdministration();
-$ljoin["prescription_line_medicament"] = "prescription_line_medicament.prescription_line_medicament_id = administration.object_id AND administration.object_class = 'CPrescriptionLineMedicament'";
-$ljoin["prescription_line_element"]    = "prescription_line_element.prescription_line_element_id = administration.object_id AND administration.object_class = 'CPrescriptionLineElement'";
-$ljoin["prescription_line_mix_item"]   = "prescription_line_mix_item.prescription_line_mix_item_id = administration.object_id AND administration.object_class = 'CPrescriptionLineMixItem'";
-$ljoin["prescription_line_mix"]        = "prescription_line_mix.prescription_line_mix_id = prescription_line_mix_item.prescription_line_mix_id";
-                                                                                       
-$ljoin["prescription"] = "(prescription_line_medicament.prescription_id = prescription.prescription_id) OR
-                          (prescription_line_element.prescription_id = prescription.prescription_id) OR
-                          (prescription_line_mix.prescription_id = prescription.prescription_id)";
+$administrations = array();
 
+$where = array();
 $where["prescription.prescription_id"] = " = '$prescription_id'";
 
-$where[] = "prescription_line_medicament.perop = '1' OR 
-            prescription_line_element.perop = '1' OR
-            prescription_line_mix.perop = '1'";
-    
-$order = "dateTime ASC";		
-$administrations = $administration->loadList($where, $order, null, null, $ljoin);
+// CPrescriptionLineMedicament
+$ljoin = array(
+  "prescription_line_medicament" => "prescription_line_medicament.prescription_line_medicament_id = administration.object_id
+                                       AND administration.object_class = 'CPrescriptionLineMedicament'
+                                       AND prescription_line_medicament.perop = '1'",
+  "prescription"                 => "prescription_line_medicament.prescription_id = prescription.prescription_id",
+);
+$administrations = array_merge($administrations, $administration->loadList($where, null, null, null, $ljoin));
 
-foreach($administrations as $_adm){
-	$_adm->loadTargetObject();
-	if($_adm->_ref_object instanceof CPrescriptionLineMedicament || $_adm->_ref_object instanceof CPrescriptionLineMixItem){
-		$_adm->_ref_object->_ref_produit->loadUnitePresentation();
-    $_adm->_ref_object->_ref_produit->loadRapportUnitePriseByCIS();
-    $_adm->_ref_object->_ref_produit->updateRatioMassique();
-    if($_adm->_ref_object->_ref_produit->_ratio_mg){
-      $_adm->_quantite_mg = $_adm->quantite / $_adm->_ref_object->_ref_produit->_ratio_mg;
+// CPrescriptionLineElement
+$ljoin = array(
+  "prescription_line_element"    => "prescription_line_element.prescription_line_element_id = administration.object_id 
+                                       AND administration.object_class = 'CPrescriptionLineElement'
+                                       AND prescription_line_element.perop = '1'",
+  "prescription"                 => "prescription_line_element.prescription_id = prescription.prescription_id",
+);
+$administrations = array_merge($administrations, $administration->loadList($where, null, null, null, $ljoin));
+
+// CPrescriptionLineMixItem
+$ljoin = array(
+  "prescription_line_mix_item"   => "prescription_line_mix_item.prescription_line_mix_item_id = administration.object_id 
+                                       AND administration.object_class = 'CPrescriptionLineMixItem'",
+  "prescription_line_mix"        => "prescription_line_mix_item.prescription_line_mix_id = prescription_line_mix.prescription_line_mix_id
+                                       AND prescription_line_mix.perop = '1'",
+  "prescription"                 => "prescription_line_mix.prescription_id = prescription.prescription_id",
+);
+$administrations = array_merge($administrations, $administration->loadList($where, null, null, null, $ljoin));
+
+$administrations = CMbObject::naturalSort($administrations, array("dateTime"));
+
+foreach ($administrations as $_adm) {
+  $_adm->loadTargetObject();
+  
+  if ($_adm->_ref_object instanceof CPrescriptionLineMedicament || $_adm->_ref_object instanceof CPrescriptionLineMixItem) {
+    $_produit = $_adm->_ref_object->_ref_produit;
+    
+    $_produit->loadUnitePresentation();
+    $_produit->loadRapportUnitePriseByCIS();
+    $_produit->updateRatioMassique();
+    
+    if ($_produit->_ratio_mg) {
+      $_adm->_quantite_mg = $_adm->quantite / $_produit->_ratio_mg;
     }
-	}
+  }
 }
 
 $smarty = new CSmartyDP();
