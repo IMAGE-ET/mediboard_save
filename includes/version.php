63da1f8082dc824ddabc3acca97e2749abf8123f
Alexis Granger <granger@openxtrem.com>
2012-09-17 14:55:46 +0000

    bug: probleme de refresh du plan de soins sous IE (incrementation du version.php)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16650 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 94,
+  "build" => 95,
 );
 
 $version["string"] = implode(".", $version);
