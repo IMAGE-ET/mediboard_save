b9c1b307790a1a26c6246eec62b69e53a2b49156
Romain Ollivier <ollivier@openxtrem.com>
2012-09-20 14:09:23 +0000

    fnc: POC benachmark poste client
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16688 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPdeveloppement/benchmark_client.php
@@ -0,0 +1,14 @@
+<?php /* $Id: benchmark.php 16260 2012-07-27 15:08:25Z lryo $ */
+
+/**
+* @package Mediboard
+* @subpackage dPinterop
+* @version $Revision: 16260 $
+* @author Romain Ollivier
+*/
+
+CCanDo::checkRead();
+
+// Création du template
+$smarty = new CSmartyDP();
+$smarty->display("benchmark_client.tpl");
