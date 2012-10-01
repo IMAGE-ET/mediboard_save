66508f61c7af36c33d527b6d0bec8b314e97e4cb
Yohann Poiron <poiron@openxtrem.com>
2012-10-01 10:16:17 +0000

    fnc: Outils dans l'EAI
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16819 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/eai/index.php
+++ modules/eai/index.php
@@ -12,9 +12,10 @@
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("vw_idx_interop_actors"  , TAB_READ);
+$module->registerTab("vw_idx_interop_actors"      , TAB_READ);
 $module->registerTab("vw_idx_exchange_data_format", TAB_READ);
-$module->registerTab("vw_stats", TAB_ADMIN);
-$module->registerTab("vw_servers_socket", TAB_ADMIN);
+$module->registerTab("vw_stats"                   , TAB_ADMIN);
+$module->registerTab("vw_servers_socket"          , TAB_ADMIN);
+$module->registerTab("vw_tools"                   , TAB_ADMIN);
 
 ?>
---
+++ modules/eai/vw_tools.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * View tools EAI
+ *  
+ * @category EAI
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @version  SVN: $Id:$ 
+ * @link     http://www.mediboard.org
+ */
+ 
+CCanDo::checkRead();
+
+
+
+// Création du template
+$smarty = new CSmartyDP();
+$smarty->display("vw_tools.tpl");
+
+?>
