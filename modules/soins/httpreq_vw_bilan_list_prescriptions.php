550f695bebb3ed85a1075b33dbabf82c843cbccd
Fabien Ménager <fabien.menager@openxtrem.com>
2012-10-12 13:28:56 +0000

    ref: optimisation du script de chargement des prescriptions dans le bilan des prescriptions
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@16952 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/httpreq_vw_bilan_list_prescriptions.php
+++ modules/soins/httpreq_vw_bilan_list_prescriptions.php
@@ -24,49 +24,90 @@ $date_max = $date_max . " 23:59:59";
 $mediuser = new CMediusers();
 $praticiens = $mediuser->loadPraticiens();
 
+$prescriptions = array();
+$prescription = new CPrescription();
+
 // Recherche des prescriptions
 $where = array();
-if($type_prescription == "sejour" || $type_prescription == "sortie_manquante"){
+if ($type_prescription == "sejour" || $type_prescription == "sortie_manquante") {
   $ljoin["sejour"] = "prescription.object_id = sejour.sejour_id";
   $ljoin["patients"] = "patients.patient_id = sejour.patient_id";
+  
   $where["prescription.type"] = " = 'sejour'";
-  $where["sejour.entree"]      = " <= '$date_max'";
-  $where["sejour.sortie"]      = " >= '$date_min'";					
-} else {
-  $where["prescription.type"] = " = 'externe'";
+  $where["sejour.entree"]     = " <= '$date_max'";
+  $where["sejour.sortie"]     = " >= '$date_min'";
+}
+else {
   $ljoin["consultation"] = "prescription.object_id = consultation.consultation_id";
   $ljoin["plageconsult"] = "consultation.plageconsult_id = plageconsult.plageconsult_id";
   $ljoin["patients"] = "patients.patient_id = consultation.patient_id";
+  
+  $where["prescription.type"] = " = 'externe'";
   $where["plageconsult.date"] = "BETWEEN '$date_min' AND '$date_max'";
 }
 
-if($signee == "0"){ 
-  if($praticien_id){
-    $where[] = "(prescription_line_element.praticien_id = '$praticien_id' AND prescription_line_element.signee != '1') 
-                OR (prescription_line_medicament.praticien_id = '$praticien_id' AND prescription_line_medicament.signee != '1' AND prescription_line_medicament.variante_active = '1')
-                OR (prescription_line_mix.praticien_id = '$praticien_id' AND prescription_line_mix.signature_prat != '1' AND prescription_line_mix.variante_active = '1')";
-  } else {
-    $where[] = "(prescription_line_element.signee != '1') OR (prescription_line_medicament.signee != '1' AND prescription_line_medicament.variante_active = '1') OR (prescription_line_mix.signature_prat != '1' AND prescription_line_mix.variante_active = '1')";
+$ljoin_save = $ljoin;
+$wheres = array();
+
+$leftjoins = array(
+  array("prescription_line_element",    "prescription_line_element.prescription_id = prescription.prescription_id"),
+  array("prescription_line_medicament", "prescription_line_medicament.prescription_id = prescription.prescription_id"),
+  array("prescription_line_mix",        "prescription_line_mix.prescription_id = prescription.prescription_id"),
+);
+
+if ($signee == "0") {
+  if ($praticien_id) {
+    $wheres = array(
+      "prescription_line_element.praticien_id    = '$praticien_id' AND prescription_line_element.signee     != '1'",
+      "prescription_line_medicament.praticien_id = '$praticien_id' AND prescription_line_medicament.signee  != '1' AND prescription_line_medicament.variante_active = '1'",
+      "prescription_line_mix.praticien_id        = '$praticien_id' AND prescription_line_mix.signature_prat != '1' AND prescription_line_mix.variante_active = '1'",
+    );
   }
-} else {
-  if($praticien_id){
-    $where[] = "(prescription_line_element.praticien_id = '$praticien_id') 
-             OR (prescription_line_medicament.praticien_id = '$praticien_id')
-             OR (prescription_line_mix.praticien_id = '$praticien_id')";
+  else {
+    $wheres = array(
+      "prescription_line_element.signee     != '1'",
+      "prescription_line_medicament.signee  != '1' AND prescription_line_medicament.variante_active = '1'",
+      "prescription_line_mix.signature_prat != '1' AND prescription_line_mix.variante_active = '1'",
+    );
+  }
+}
+else {
+  if ($praticien_id) {
+    $wheres = array(
+      "prescription_line_element.praticien_id    = '$praticien_id'", 
+      "prescription_line_medicament.praticien_id = '$praticien_id'", 
+      "prescription_line_mix.praticien_id        = '$praticien_id'",
+    );
   }
 }
 
-
-$ljoin["prescription_line_element"] = "prescription_line_element.prescription_id = prescription.prescription_id";
-$ljoin["prescription_line_medicament"] = "prescription_line_medicament.prescription_id = prescription.prescription_id";
-$ljoin["prescription_line_mix"] = "prescription_line_mix.prescription_id = prescription.prescription_id";
-
-$prescriptions = array();
-$prescription = new CPrescription();
-$order = "patients.nom";
-$group_by = "prescription_id";
-
-$prescriptions = $prescription->loadList($where, $order, null, $group_by, $ljoin);
+if (count($wheres)) {
+  $keys = array();
+  foreach($wheres as $_i => $_where) {
+    $where[0] = $_where;
+    
+    // Pour ne pas afficher les prescriptions en double (== group by prescription_id)
+    if (count($keys)) {
+      $where["prescription.prescription_id"] = $prescription->_spec->ds->prepareNotIn($keys);
+    }
+      
+    $ljoin[$leftjoins[$_i][0]] = $leftjoins[$_i][1];
+    $_prescriptions = $prescription->loadList($where, null, null, null, $ljoin);
+    
+    $keys = array_merge($keys, array_keys($_prescriptions));
+    $prescriptions = array_merge($prescriptions, $_prescriptions);
+    
+    $ljoin = $ljoin_save;
+  }
+  
+  foreach ($prescriptions as $_presc) {
+    $_presc->_ref_object->loadRefPatient();
+  }
+  array_multisort(CMbArray::pluck($prescriptions, "_ref_object", "_ref_patient", "nom"), SORT_ASC, $prescriptions);
+}
+else {
+  $prescriptions = $prescription->loadList($where, "patients.nom", null, "prescription_id", $ljoin);
+}
 
 if($type_prescription == "sortie_manquante"){
   foreach($prescriptions as $_prescription){
@@ -91,7 +132,7 @@ if(!$praticien_id && $user->isPraticien()){
 }
 
 foreach($prescriptions as $_prescription){
-	$_prescription->loadRefPatient();
+  $_prescription->loadRefPatient();
 }
 
 // Smarty template
