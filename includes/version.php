d37dbb33ce814536c3a976e7263fba367a820dda
Fabien Ménager <fabien.menager@openxtrem.com>
2012-10-22 09:23:36 +0000

    fnc: ajout du SMR dans la monographie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17012 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 104,
+  "build" => 105,
 );
 
 $version["string"] = implode(".", $version);
