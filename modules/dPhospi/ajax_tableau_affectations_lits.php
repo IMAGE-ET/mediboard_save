4ee8f36472c96c6d09f2afd5c52a87ac74cf88c6
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-10-29 16:27:46 +0000

    Bug: Si on est déconnecté, remettre en session les services dans la vue tableau des affectations.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17126 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/ajax_tableau_affectations_lits.php
+++ modules/dPhospi/ajax_tableau_affectations_lits.php
@@ -34,6 +34,28 @@ $emptySejour->_type_admission = $_type_admission;
 
 $totalLits = 0;
 
+// Détection du changement d'établissement
+if (!$services_ids) {
+  $pref_services_ids = json_decode(CAppUI::pref("services_ids_hospi"));
+  
+  // Si la préférence existe, alors on la charge
+  if (isset($pref_services_ids->{"g$g"})) {
+    $services_ids = $pref_services_ids->{"g$g"};
+    if ($services_ids) {
+      $services_ids = explode("|", $services_ids); 
+    }
+    CValue::setSession("services_ids", $services_ids);
+  }
+  // Sinon, chargement de la liste des services en accord avec le droit de lecture
+  else {
+    $service = new CService;
+    $where = array();
+    $where["group_id"] = "= '".CGroups::loadCurrent()->_id."'";
+    $services_ids = array_keys($service->loadListWithPerms(PERM_READ, $where, "externe, nom"));
+    CValue::setSession("services_ids", $services_ids);
+  }
+}
+
 // Récupération des chambres/services
 $where = array();
 $where["group_id"] = "= '$g'";
