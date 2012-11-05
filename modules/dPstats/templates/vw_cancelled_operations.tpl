d194fbcf9b3afd8decb324a2233b63e4a33db2b7
Romain Ollivier <ollivier@openxtrem.com>
2012-11-05 17:19:17 +0000

    fnc: stats sur les interventions ajoutées le jour même
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17190 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPstats/templates/vw_cancelled_operations.tpl
+++ modules/dPstats/templates/vw_cancelled_operations.tpl
@@ -4,48 +4,89 @@ Main.add(function(){
 });
 </script>
 
-<h2>{{$title}}</h2>
-
 <ul class="control_tabs" id="cancelled-operations">
 {{foreach from=$list item=month key=month_label}}
-  <li><a href="#month-{{$month_label|replace:'/':'-'}}">
-    {{$month_label}}
+  <li><a href="#month-{{$month_label}}">
+    {{$month_label}} ({{$month.total}})
   </a></li>
 {{/foreach}}
 </ul>
+<span style="float: right">
+  <form name="intervs" action="?" method="get" onsubmit="alert('toto'); return checkForm(this)">
+    <input type="hidden" name="m" value="stats" />
+    <input type="hidden" name="tab" value="vw_cancelled_operations" />
+    <select name="type_modif" onchange="this.form.submit()">
+      <option value="annule" {{if $type_modif == "annule"}}selected="selected"{{/if}}>Interventions annulées le jour même</option>
+      <option value="ajoute" {{if $type_modif == "ajoute"}}selected="selected"{{/if}}>Interventions ajoutées le jour même</option>
+    </select>
+    Jusqu'au {{mb_field class=COperation field="_date_max" value=$date_max form="intervs" canNull="false" register=true onchange="this.form.submit()"}}
+  </form>
+</span>
 <hr class="control_tabs" />
   
 {{foreach from=$list item=month key=month_label}}
-  <table id="month-{{$month_label|replace:'/':'-'}}" class="main tbl" style="display: none;">
+  <table id="month-{{$month_label}}" class="main tbl" style="display: none;">
     <tr>
       <th>{{mb_title class=COperation field=date}}</th>
+      <th>{{mb_title class=COperation field=salle_id}}</th>
       <th>{{mb_title class=COperation field=chir_id}}</th>
       <th>{{mb_title class=CSejour field=patient_id}}</th>
       <th>{{mb_title class=CSejour field=type}}</th>
       <th>{{mb_title class=COperation field=libelle}}</th>
+      <th>{{mb_title class=COperation field=rques}}</th>
       <th>{{mb_title class=COperation field=codes_ccam}}</th>
     </tr>
     
-  {{foreach from=$month item=salle key=salle_label}}
     <tr>
-      <th colspan="10">{{$salle_label}}</th>
+      <th colspan="100" class="section">Interventions dans une vacation</th>
     </tr>
-    {{foreach from=$salle item=op}}
+    {{foreach from=$month.inPlage item=op}}
       <tr>
         <td>
           <span onmouseover="ObjectTooltip.createEx(this, '{{$op->_guid}}')">
-            {{mb_value object=$op->_ref_plageop field=date}}
+            {{mb_value object=$op field=_datetime}}
           </span>
           </a>
         </td>
-        <td>{{$op->_ref_chir}}</td>
-        <td>{{$op->_ref_patient}}</td>
+        <td class="text">{{mb_value object=$op field=salle_id}}</td>
+        <td>{{mb_value object=$op field=chir_id}}</td>
+        <td class="text">{{mb_value object=$op->_ref_sejour field=patient_id}}</td>
         <td>{{mb_value object=$op->_ref_sejour field=type}}</td>
-        <td>{{$op->libelle}}</td>
-        <td>{{$op->codes_ccam}}</td>
+        <td class="text">{{mb_value object=$op field=libelle}}</td>
+        <td class="text">{{mb_value object=$op field=rques}}</td>
+        <td>{{mb_value object=$op field=codes_ccam}}</td>
+      </tr>
+    {{foreachelse}}
+      <tr>
+        <td colspan="100" class="empty">{{tr}}COperation.none{{/tr}}</td>
       </tr>
     {{/foreach}}
-  {{/foreach}}
+      
+    
+    <tr>
+      <th colspan="100" class="section">Interventions hors plage</th>
+    </tr>
+    {{foreach from=$month.horsPlage item=op}}
+      <tr>
+        <td>
+          <span onmouseover="ObjectTooltip.createEx(this, '{{$op->_guid}}')">
+            {{mb_value object=$op field=_datetime}}
+          </span>
+          </a>
+        </td>
+        <td>{{mb_value object=$op field=salle_id}}</td>
+        <td>{{mb_value object=$op field=chir_id}}</td>
+        <td>{{mb_value object=$op->_ref_sejour field=patient_id}}</td>
+        <td>{{mb_value object=$op->_ref_sejour field=type}}</td>
+        <td class="text">{{mb_value object=$op field=libelle}}</td>
+        <td class="text">{{mb_value object=$op field=rques}}</td>
+        <td>{{mb_value object=$op field=codes_ccam}}</td>
+      </tr>
+    {{foreachelse}}
+      <tr>
+        <td colspan="100" class="empty">{{tr}}COperation.none{{/tr}}</td>
+      </tr>
+    {{/foreach}}
   
   </table>
 {{/foreach}}
--- modules/dPstats/vw_cancelled_operations.php
+++ modules/dPstats/vw_cancelled_operations.php
@@ -10,12 +10,14 @@
 
 CCanDo::checkRead();
 
-$debut    = CValue::get("debut", mbDate("-1 YEAR"));
-$fin      = CValue::get("fin", mbDate());
-$prat_id  = CValue::get("prat_id");
-$salle_id = CValue::get("salle_id");
-$bloc_id  = CValue::get("bloc_id");
-$codeCCAM = CValue::get("codeCCAM");
+$type_modif = CValue::getOrSession("type_modif", "annule");
+$date_max   = CValue::getOrSession("_date_max", mbDate());
+$date_max   = mbDate("last day of +0 months", $date_max); 
+$date_min   = mbDate("first day of -3 months", $date_max);
+$prat_id    = CValue::get("prat_id");
+$salle_id   = CValue::get("salle_id");
+$bloc_id    = CValue::get("bloc_id");
+$codeCCAM   = CValue::get("codeCCAM");
 
 $prat = new CMediusers;
 $prat->load($prat_id);
@@ -24,66 +26,114 @@ $salle = new CSalle;
 $salle->load($salle_id);
 
 $where = array();
-$where['stats'] = " = '1'";
-if($salle_id) {
-  $where['salle_id'] = " = '$salle_id'";
-} elseif($bloc_id) {
-  $where['bloc_id'] = "= '$bloc_id'";
-}
 
-$salles = $salle->loadList($where);
+$salles = $salle->loadGroupList();
 $list = array();
 
-foreach($salles as $salle) {
-  $query = "SELECT DISTINCT(operations.operation_id) AS op_id,
-              DATE_FORMAT(plagesop.date, '%Y - %m') AS mois,
-              DATE_FORMAT(plagesop.date, '%Y-%m-01') AS orderitem
-            FROM operations
-            INNER JOIN sallesbloc ON operations.salle_id = sallesbloc.salle_id
-            LEFT JOIN plagesop ON plagesop.plageop_id = operations.plageop_id
-            LEFT JOIN user_log ON user_log.object_id = operations.operation_id
-              AND user_log.object_class = 'COperation'
-            WHERE plagesop.date BETWEEN '$debut' AND '$fin'
-              AND user_log.type = 'store'
-              AND DATE(user_log.date) = plagesop.date
-              AND user_log.fields LIKE '%annulee%'
-              AND operations.annulee = '1'";
 
-  if($prat_id)  $query .= "\nAND operations.chir_id = '$prat_id'";
-  if($codeCCAM) $query .= "\nAND operations.codes_ccam LIKE '%$codeCCAM%'";
-
-  $query .= "AND sallesbloc.salle_id = '$salle->_id'
-             ORDER BY orderitem";
-
-  $result = $prat->_spec->ds->loadlist($query);
-  
-  foreach($result as $res) {
-    if (!isset($list[$res['mois']]))
-      $list[$res['mois']] = array();
-      
-    if (!isset($list[$res['mois']][$salle->_view]))
-      $list[$res['mois']][$salle->_view] = array();
-    
-    $operation = new COperation;
-    $operation->load($res['op_id']);
-    $operation->loadRefsFwd();
-    
-    $list[$res['mois']][$salle->_view][$operation->_id] = $operation;
-  }
+if($type_modif == "annule") {
+  $queryInPlage   = "SELECT DISTINCT(operations.operation_id) AS op_id,
+                       DATE_FORMAT(plagesop.date, '%Y - %m') AS mois,
+                       plagesop.date AS orderitem
+                     FROM operations
+                     LEFT JOIN plagesop        ON plagesop.plageop_id = operations.plageop_id
+                     LEFT JOIN sallesbloc      ON sallesbloc.salle_id = operations.salle_id
+                     LEFT JOIN bloc_operatoire ON sallesbloc.bloc_id = bloc_operatoire.bloc_operatoire_id
+                     LEFT JOIN user_log        ON user_log.object_id = operations.operation_id
+                       AND user_log.object_class = 'COperation'
+                     WHERE plagesop.date BETWEEN '$date_min' AND '$date_max'
+                       AND user_log.type = 'store'
+                       AND DATE(user_log.date) = plagesop.date
+                       AND user_log.fields LIKE '%annulee%'
+                       AND operations.annulee = '1'";
+  $queryHorsPlage = "SELECT DISTINCT(operations.operation_id) AS op_id,
+                       DATE_FORMAT(operations.date, '%Y - %m') AS mois,
+                       operations.date AS orderitem
+                     FROM operations
+                     LEFT JOIN sallesbloc      ON sallesbloc.salle_id = operations.salle_id
+                     LEFT JOIN bloc_operatoire ON sallesbloc.bloc_id = bloc_operatoire.bloc_operatoire_id
+                     LEFT JOIN user_log        ON user_log.object_id = operations.operation_id
+                       AND user_log.object_class = 'COperation'
+                     WHERE operations.date BETWEEN '$date_min' AND '$date_max'
+                       AND user_log.type = 'store'
+                       AND DATE(user_log.date) = operations.date
+                       AND user_log.fields LIKE '%annulee%'
+                       AND operations.annulee = '1'";
+}
+else {
+  $queryInPlage   = "SELECT DISTINCT(operations.operation_id) AS op_id,
+                       DATE_FORMAT(plagesop.date, '%Y - %m') AS mois,
+                       plagesop.date AS orderitem
+                     FROM operations
+                     LEFT JOIN plagesop ON plagesop.plageop_id = operations.plageop_id
+                     LEFT JOIN sallesbloc ON sallesbloc.salle_id = operations.salle_id
+                     LEFT JOIN bloc_operatoire ON sallesbloc.bloc_id = bloc_operatoire.bloc_operatoire_id
+                     LEFT JOIN user_log ON user_log.object_id = operations.operation_id
+                       AND user_log.object_class = 'COperation'
+                     WHERE plagesop.date BETWEEN '$date_min' AND '$date_max'
+                       AND user_log.type = 'create'
+                       AND DATE(user_log.date) = plagesop.date
+                       AND operations.annulee = '0'";
+  $queryHorsPlage = "SELECT DISTINCT(operations.operation_id) AS op_id,
+                       DATE_FORMAT(operations.date, '%Y - %m') AS mois,
+                       operations.date AS orderitem
+                     FROM operations
+                     LEFT JOIN sallesbloc ON sallesbloc.salle_id = operations.salle_id
+                     LEFT JOIN bloc_operatoire ON sallesbloc.bloc_id = bloc_operatoire.bloc_operatoire_id
+                     LEFT JOIN user_log ON user_log.object_id = operations.operation_id
+                       AND user_log.object_class = 'COperation'
+                     WHERE operations.date BETWEEN '$date_min' AND '$date_max'
+                       AND user_log.type = 'create'
+                       AND DATE(user_log.date) = operations.date
+                       AND operations.annulee = '0'";
 }
 
-ksort($list);
+if($prat_id) {
+  $queryInPlage   .= "\nAND operations.chir_id = '$prat_id'";
+  $queryHorsPlage .= "\nAND operations.chir_id = '$prat_id'";
+}
+if($codeCCAM) {
+  $queryInPlage   .= "\nAND operations.codes_ccam LIKE '%$codeCCAM%'";
+  $queryHorsPlage .= "\nAND operations.codes_ccam LIKE '%$codeCCAM%'";
+}
+$queryInPlage   .= "\nAND operations.salle_id ".CSQLDataSource::prepareIn(array_keys($salles));
+$queryHorsPlage .= "\nAND operations.salle_id ".CSQLDataSource::prepareIn(array_keys($salles));
+$queryInPlage   .= "\nORDER BY orderitem, bloc_operatoire.nom, sallesbloc.nom";
+$queryHorsPlage .= "\nORDER BY orderitem, bloc_operatoire.nom, sallesbloc.nom";
 
-// Set up the title for the graph
-$title = "Interventions annulées le jour même";
-if($prat_id)  $subtitle .= " - Dr $prat->_view";
-if($salle_id) $subtitle .= " - $salle->nom";
-if($codeCCAM) $subtitle .= " - CCAM : $codeCCAM";
+$resultInPlage   = $prat->_spec->ds->loadlist($queryInPlage);
+$resultHorsPlage = $prat->_spec->ds->loadlist($queryHorsPlage);
+
+for($rangeDate = $date_min; $rangeDate <= $date_max; $rangeDate = mbDate("+1 month", $rangeDate)) {
+  $month = mbTransformTime(null, $rangeDate, "%Y - %m");
+  $list[$month]['total'] = 0;
+  $list[$month]['inPlage'] = array();
+  $list[$month]['horsPlage'] = array();
+}
+
+foreach($resultInPlage as $res) {
+  $operation = new COperation();
+  $operation->load($res['op_id']);
+  $operation->loadRefsFwd();
+  
+  $list[$res['mois']]['total']++;
+  $list[$res['mois']]['inPlage'][$operation->_id] = $operation;
+}
+
+foreach($resultHorsPlage as $res) {
+  $operation = new COperation();
+  $operation->load($res['op_id']);
+  $operation->loadRefsFwd();
+  
+  $list[$res['mois']]['total']++;
+  $list[$res['mois']]['horsPlage'][$operation->_id] = $operation;
+}
 
 // Création du template
 $smarty = new CSmartyDP();
 
-$smarty->assign("list", $list);
-$smarty->assign("title", $title);
+$smarty->assign("list"      , $list);
+$smarty->assign("date_max"  , $date_max);
+$smarty->assign("type_modif", $type_modif);
 
 $smarty->display("vw_cancelled_operations.tpl");
