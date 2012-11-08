797c3e8a6ff7592e387ad30d85947dd4fe8337e8
Fabien Ménager <fabien.menager@openxtrem.com>
2012-11-08 12:57:31 +0000

    fnc: premier jet du testeur de bande passante
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17239 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/javascript/browser_check.js
+++ modules/system/javascript/browser_check.js
@@ -8,7 +8,7 @@ BandwidthTest = {
   uploadLimit: 80,
   launchTest: function() {
     var url = Url.parse();
-    var urlPath = location.origin+url.directory+"/tmp/bandwidth_test/";
+    var urlPath = url.scheme+"://"+location.hostname+url.directory+"tmp/bandwidth_test/";
 
     BandwidthTest.binFileUrl      = urlPath+"big.bin";
     BandwidthTest.binEmptyFileUrl = urlPath+"empty.bin";
@@ -29,9 +29,10 @@ BandwidthTest = {
       parameters: $H({id: BandwidthTest.start}).toQueryString(),
       asynchronous: true,
       evalScripts: false,
-      onComplete: function(transport){
-        BandwidthTest.binfile = transport.responseText;
+      onSuccess: function(transport){
         BandwidthTest.end = new Date().getTime();
+
+        BandwidthTest.binfile = transport.responseText;
 
         var diff = (BandwidthTest.end - BandwidthTest.start) / 1000;
         var speed = (BandwidthTest.binfile.length / diff) / (1024 / 8);
@@ -55,8 +56,9 @@ BandwidthTest = {
       asynchronous: true,
       evalScripts: false,
       contentType: "application/octet-stream",
-      onComplete: function(){
+      onSuccess: function(){
         BandwidthTest.end = new Date().getTime();
+
         var diff = (BandwidthTest.end - BandwidthTest.start) / 1000;
         var speed = (BandwidthTest.binfile.length / diff) / (1024 / 8);
         speed = Math.round(speed*100)/100;
