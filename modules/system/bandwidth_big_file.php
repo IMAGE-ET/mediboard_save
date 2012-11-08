739ee932fec2e05ef37cf8ffe9262b3d1070160c
Fabien Ménager <fabien.menager@openxtrem.com>
2012-11-08 11:53:58 +0000

    fnc: premier jet du testeur de bande passante
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17237 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/bandwidth_big_file.php
+++ modules/system/bandwidth_big_file.php
@@ -16,7 +16,7 @@ $size = min($size, 10*1024); // Cap it to 10MB MAX
 
 $big_file = CAppUI::getTmpPath("bandwidth_test/big.bin");
 CMbPath::forceDir(dirname($big_file));
-file_put_contents($big_file, str_pad("", 1024*$size, "\0"));
+file_put_contents($big_file, str_pad("", 1024*$size, "a")); // Must be a "normal" char so that it's not url encoded
 
 $empty_file = CAppUI::getTmpPath("bandwidth_test/empty.bin");
 file_put_contents($empty_file, "");
