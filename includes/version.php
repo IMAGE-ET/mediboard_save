480fcd1e9e186330fe7a64c7e773831a3d4cd63d
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-11-13 09:13:52 +0000

    Bug: Problème de calcul du débit volumique si des virgules ont été insérées au lieu de points pour le volume et la durée de référence dans la modale de ligne de perfusion.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17284 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 111,
+  "build" => 112,
 );
 
 $version["string"] = implode(".", $version);
