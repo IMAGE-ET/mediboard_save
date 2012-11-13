29d8508c92b99a96946b95d8fcadd232be387533
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-11-13 08:40:50 +0000

    Ref: Mise à jour de la version.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17283 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 110,
+  "build" => 111,
 );
 
 $version["string"] = implode(".", $version);
