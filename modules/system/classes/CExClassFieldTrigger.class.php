44ff13a90c6f45601b60aa18c8be5bf0c4671cce
Fabien Ménager <fabien.menager@openxtrem.com>
2012-11-13 09:43:18 +0000

    ref: ajout de cascade sur des champs des triggers de formulaires
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17285 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/classes/CExClassFieldTrigger.class.php
+++ modules/system/classes/CExClassFieldTrigger.class.php
@@ -5,17 +5,24 @@
  * @subpackage system
  * @version $Revision$
  * @author SARL OpenXtrem
- * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  */
 
 class CExClassFieldTrigger extends CMbObject {
   var $ex_class_field_trigger_id = null;
-  
+
   var $ex_class_field_id = null;
   var $ex_class_triggered_id = null;
   var $trigger_value = null;
-  
+
+  /**
+   * @var CExClassField
+   */
   var $_ref_ex_class_field = null;
+
+  /**
+   * @var CExClassField
+   */
   var $_ref_ex_class_triggered = null;
 
   function getSpec() {
@@ -28,23 +35,33 @@ class CExClassFieldTrigger extends CMbObject {
 
   function getProps() {
     $props = parent::getProps();
-    $props["ex_class_field_id"]     = "ref notNull class|CExClassField";
-    $props["ex_class_triggered_id"] = "ref notNull class|CExClass";
+    $props["ex_class_field_id"]     = "ref notNull class|CExClassField cascade";
+    $props["ex_class_triggered_id"] = "ref notNull class|CExClass cascade";
     $props["trigger_value"]         = "str notNull";
     return $props;
   }
-  
+
   function loadView(){
     parent::loadView();
     $this->loadRefExClassField();
     $this->loadRefExClassTriggered();
     $this->_view = $this->_ref_ex_class_field->_view." > ".$this->_ref_ex_class_triggered->_view;
   }
-  
+
+  /**
+   * @param bool $cache
+   *
+   * @return CExClassField
+   */
   function loadRefExClassField($cache = true){
     return $this->_ref_ex_class_field = $this->loadFwdRef("ex_class_field_id", $cache);
   }
-  
+
+  /**
+   * @param bool $cache
+   *
+   * @return CExClass
+   */
   function loadRefExClassTriggered($cache = true){
     return $this->_ref_ex_class_triggered = $this->loadFwdRef("ex_class_triggered_id", $cache);
   }
