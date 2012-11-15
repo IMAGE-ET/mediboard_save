6ea27a8f1df02845f3e4765008d9cd5f864896b9
Charles-Etienne Masse <c.masse@openxtrem.com>
2012-11-15 10:27:40 +0000

    Fnc: numéro de telephone pour astreinte (setup) dans User et non MediUser
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17316 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/classes/CUser.class.php
+++ modules/admin/classes/CUser.class.php
@@ -5,7 +5,7 @@
  * @subpackage admin
  * @version $Revision$
  * @author SARL OpenXtrem
- * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  */
 
 /**
@@ -14,7 +14,7 @@
 class CUser extends CMbObject {
   // DB key
   var $user_id             = null;
-  
+
   // DB fields
   var $user_username       = null;
   var $user_password       = null;
@@ -25,6 +25,7 @@ class CUser extends CMbObject {
   var $user_email          = null;
   var $user_phone          = null;
   var $user_mobile         = null;
+  var $user_astreinte      = null;
   var $user_address1       = null;
   var $user_city           = null;
   var $user_zip            = null;
@@ -47,16 +48,16 @@ class CUser extends CMbObject {
   var $_user_deb_activite    = null;
   var $_user_fin_activite    = null;
   var $_count_connections    = null;
-  
+
   var $_is_logging           = null;
   var $_user_salt            = null;
 
   // Behaviour fields
   var $_purge_connections = null;
-  
+
   // Form fields
   var $_user_type_view    = null;
-  
+
   // Object references
   var $_ref_preferences = null;
 
@@ -64,10 +65,10 @@ class CUser extends CMbObject {
    * @var CMediusers
    */
   var $_ref_mediuser    = null;
-  
+
   // Object collections
   var $_ref_profiled_users = null;
-  
+
   static $types = array(
     // DEFAULT USER (nothing special)
 //    0  => "-- Choisir un type",
@@ -92,9 +93,9 @@ class CUser extends CMbObject {
     18 => "Aide soignant",
     19 => "Dentiste"
   );
-  
+
   static $ps_types = array(3, 4, 13, 16, 17, 19);
-  
+
   function getSpec() {
     $spec = parent::getSpec();
     $spec->table = 'users';
@@ -118,7 +119,7 @@ class CUser extends CMbObject {
 
   function getProps() {
     $props = parent::getProps();
-    
+
     // Plain fields
     $props["user_username"]       = "str notNull maxLength|20";
     $props["user_password"]       = "str maxLength|64 show|0";
@@ -129,6 +130,7 @@ class CUser extends CMbObject {
     $props["user_email"]          = "str maxLength|255";
     $props["user_phone"]          = "phone";
     $props["user_mobile"]         = "phone";
+    $props["user_astreinte"]      = "phone";
     $props["user_address1"]       = "str";
     $props["user_city"]           = "str maxLength|30";
     $props["user_zip"]            = "str maxLength|11";
@@ -139,29 +141,29 @@ class CUser extends CMbObject {
     $props["template"]            = "bool notNull default|0";
     $props["profile_id"]          = "ref class|CUser";
     $props["dont_log_connection"] = "bool default|0";
-      
+
     // The different levels of security are stored to be usable in JS
     $props["_user_password_weak"]   = "password minLength|4";
     $props["_user_password_strong"] = "password minLength|6 notContaining|user_username notNear|user_username alphaAndNum";
-    
+
     // The actuel config level
     $props["_user_password"] = CAppUI::conf("admin CUser strong_password") ?
       $props["_user_password_strong"] :
       $props["_user_password_weak"];
-    
+
     // Derived fields
     $props["_ldap_linked"]       = "bool";
     $props["_user_type_view"]    = "str";
     $props["_count_connections"] = "num";
     $props["_is_logging"]        = "bool";
     $props["_user_salt"]         = "str";
-    
+
     return $props;
   }
-  
-  /** 
-   * Update the object's specs 
-   * 
+
+  /**
+   * Update the object's specs
+   *
    * @return void
    **/
   function updateSpecs() {
@@ -169,22 +171,22 @@ class CUser extends CMbObject {
 
     $user = new CMediusers();
     $remote = 0;
-    
+
     if ($user->isInstalled()) {
       if ($result = $user->load($this->user_id)) {
         $remote = $user->remote;
       }
     }
-    
+
     $strongPassword = ((CAppUI::conf("admin CUser strong_password") == "1") && ($remote == 0));
-    
+
     // If the global strong password config is set to TRUE and the user can connect remotely
     $this->_specs['_user_password'] = $strongPassword ?
       $this->_specs['_user_password_strong'] :
       $this->_specs['_user_password_weak'];
-    
+
     $this->_specs['_user_password']->fieldName = $oldSpec->fieldName;
-    
+
     $this->_props['_user_password'] = $strongPassword ?
       $this->_props['_user_password_strong'] :
       $this->_props['_user_password_weak'];
@@ -192,22 +194,22 @@ class CUser extends CMbObject {
 
   /**
    * Lazy access to a given user, defaultly connected user
-   * 
+   *
    * @param integer $user_id The user id, connected user if null;
-   * 
+   *
    * @return CUser
    */
   static function get($user_id = null) {
     $user = new CUser;
     return $user->getCached(CValue::first($user_id, CAppUI::$instance->user_id));
   }
-  
+
   function loadView() {
     parent::loadView();
     $this->loadRefMediuser();
     $this->_ref_mediuser->loadView();
   }
-  
+
   /**
    * @return CMediusers
    */
@@ -217,13 +219,13 @@ class CUser extends CMbObject {
       $mediuser->load($this->_id);
       $this->_ref_mediuser = $mediuser;
     }
-    
+
     return $mediuser;
   }
-  
+
   /**
    * Return true if user login count system is ready
-   * 
+   *
    * @return boolean
    */
   function loginErrorsReady() {
@@ -232,43 +234,43 @@ class CUser extends CMbObject {
 
   /**
    * Return true if new hash system is ready
-   * 
+   *
    * @return boolean
    */
   function loginSaltReady() {
     return $this->_spec->ds->loadField($this->_spec->table, "user_salt");
   }
-  
+
   function updatePlainFields() {
     parent::updatePlainFields();
-    
+
     $this->user_password = null;
 
     // If no raw password or already hashed, nothing to do
     if (!$this->_user_password || preg_match('/^[0-9a-f]{32}$/i', $this->_user_password)) {
       return;
     }
-    
+
     // If the new password hashing system is not ready yet
     if (!$this->loginSaltReady()) {
       CValue::setSessionAbs("_pass_deferred", $this->_user_password);
       $this->user_password = md5($this->_user_password);
       return;
     }
-    
+
     // If user is logging, get the salt value in table
     if (!$this->_is_logging) {
       $this->generateUserSalt();
       return;
     }
-    
+
     // If user is trying to log in, we have to compare hashes with corresponding user in table
     $where = array(
       "user_username" => " = '$this->user_username'"
     );
     $_user = new CUser();
     $_user->loadObject($where);
-    
+
     // If user exists, we compare hashes
     if ($_user->_id) {
       // Password is a SHA256 hash, we get user's salt
@@ -294,14 +296,14 @@ class CUser extends CMbObject {
   /**
    * Randomly create a 64 bytes salt according to available methods
    * Compute user_password by hashing _user_password + user_salt
-   * 
+   *
    * @return void
    */
   function generateUserSalt() {
     if ($this->_user_salt) {
       $this->user_salt = $this->_user_salt;
     }
-    
+
     else {
       // Mcrypt has a better CSPRNG method
       if (function_exists('mcrypt_create_iv')) {
@@ -315,17 +317,17 @@ class CUser extends CMbObject {
         $this->user_salt = hash("SHA256", mt_rand());
       }
     }
-    
+
     // Compute the hash
     $this->user_password = hash("SHA256", $this->user_salt.$this->_user_password);
   }
 
   function updateFormFields () {
     parent::updateFormFields();
-    
+
     $user_first_name = CMbString::capitalize($this->user_first_name);
     $user_last_name  = CMbString::upper($this->user_last_name);
-    
+
     $this->_view           = "$user_last_name $user_first_name";
     $this->_login_locked   = $this->user_login_errors >= CAppUI::conf('admin CUser max_login_attempts');
     $this->_user_type_view = CValue::read(self::$types, $this->user_type);
@@ -334,7 +336,7 @@ class CUser extends CMbObject {
   function check() {
     // Chargement des specs des attributs du mediuser
     $this->updateSpecs();
-    
+
     $specs = $this->getSpecs();
 
     // On se concentre dur le mot de passe (_user_password)
@@ -353,12 +355,12 @@ class CUser extends CMbObject {
       if (($target = $pwdSpecs->notContaining) && ($field = $this->$target) && stristr($pwd, $field)) {
         return "Le mot de passe ne doit pas contenir '$field'";
       }
-      
+
       // notNear
       if (($target = $pwdSpecs->notNear) && ($field = $this->$target) && (levenshtein($pwd, $field) < 3)) {
         return "Le mot de passe ressemble trop à '$field'";
       }
-       
+
       // alphaAndNum
       if ($pwdSpecs->alphaAndNum && (!preg_match("/[A-z]/", $pwd) || !preg_match("/\d+/", $pwd))) {
         return 'Le mot de passe doit contenir au moins un chiffre ET une lettre';
@@ -367,41 +369,41 @@ class CUser extends CMbObject {
     else {
       $this->_user_password = null;
     }
-    
+
     return parent::check();
   }
-  
+
   function store() {
     $this->updateSpecs();
-    
+
     if ($msg = $this->purgeConnections()) {
       return $msg;
     }
-    
+
     return parent::store();
   }
-  
+
   /**
    * We need to delete the CMediusers
-   * @return 
+   * @return
    */
   function delete(){
     if ($msg = $this->canDeleteEx()) {
       return $msg;
     }
-    
+
     $mediuser = $this->loadRefMediuser();
-    
+
     if ($mediuser->_id) {
       $mediuser->_keep_user = true;
       if ($msg = $mediuser->delete()) {
         return $msg;
       }
     }
-    
+
     return parent::delete();
   }
-  
+
   /**
    * Purge all connection user log pour user
    * @return string Store-like message
@@ -411,39 +413,39 @@ class CUser extends CMbObject {
     if (!$this->_purge_connections) {
       return;
     }
-    
+
     // Pointless/dangerous if no key
     if (!$this->_id) {
       return;
     }
-    
+
     $query = "DELETE
       FROM user_log
       WHERE (`user_id` = '$this->_id')
       AND (`fields` = 'user_last_login')
       AND (`object_id` = '$this->_id')
       AND (`object_class` = '$this->_class')";
-    
+
     $log = new CUserLog;
     $ds = $log->_spec->ds;
     if (!$ds->exec($query)) {
       return CAppUI::tr("CUser-failed-purge_connections") . $ds->error();
     }
   }
-  
+
   /**
    * Prepare the user log before object persistence
    * @see parent
    * @return CUserLog null if not loggable
-   */ 
+   */
   function prepareLog() {
     if ($this->dont_log_connection && $this->fieldModified("user_last_login")) {
       return;
     }
-    
+
     return parent::prepareLog();
   }
-  
+
   /**
    * Merges an array of objects
    * @see parent
@@ -457,7 +459,7 @@ class CUser extends CMbObject {
     if (!$this->_id) {
       return "CUser-merge-alternative-mode-required";
     }
-    
+
     // Fast merging obligatoire
     $fast = true;
     $mediusers = array();
@@ -466,11 +468,11 @@ class CUser extends CMbObject {
       $mediusers[] = $object->_ref_mediuser;
       $object->removePerms();
     }
-    
+
     $this->loadRefMediuser();
     $this->_ref_mediuser->_force_merge = true;
     $this->_ref_mediuser->merge($mediusers, $fast);
-    
+
     return parent::merge($objects, $fast);
   }
 
@@ -538,58 +540,58 @@ class CUser extends CMbObject {
 
     return null;
   }
-  
+
   /**
    * Tell whether user is linked to an LDAP account
-   * 
-   * @return boolean 
+   *
+   * @return boolean
    */
   function isLDAPLinked() {
     if (!CAppUI::conf("admin LDAP ldap_connection") || !$this->_id) {
       return;
     }
-    
+
     $this->loadLastId400(CAppUI::conf("admin LDAP ldap_tag"));
     return $this->_ldap_linked = ($this->_ref_last_id400->_id) ? 1 : 0;
   }
-  
+
   /**
    * Count connections for user
-   * 
+   *
    * @return integer
    */
   function countConnections() {
     $log = new CUserLog;
-    $log->user_id = $this->_id; 
+    $log->user_id = $this->_id;
     $log->setObject($this);
     $log->fields = "user_last_login";
     return $this->_count_connections = $log->countMatchingList();
   }
-  
+
   /**
    * Get the profiled users when this is a template
-   * 
+   *
    * @return array<CUser> Profiled users collection
    */
   function loadRefProfiledUsers() {
     return $this->_ref_profiled_users = $this->loadBackRefs("profiled_users", "user_last_name, user_first_name");
   }
-  
+
   function canChangePassword() {
     return (CAppUI::conf("admin CUser allow_change_password") || $this->user_type == 1);
   }
-  
+
   static function checkPassword($username, $password, $return_object = false) {
     $new_user = new self;
     $new_user->user_username  = $username;
     $new_user->_user_password = $password;
     $new_user->_is_logging    = true;
     $new_user->loadMatchingObjectEsc();
-    
+
     if ($return_object) {
       return $new_user;
     }
-    
+
     return $new_user->_id != null;
   }
 }
--- modules/admin/setup.php
+++ modules/admin/setup.php
@@ -5,46 +5,46 @@
  * @subpackage admin
  * @version $Revision$
  * @author SARL OpenXtrem
- * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  */
 
 class CSetupadmin extends CSetup {
   function __construct() {
     parent::__construct();
-    
+
     $this->mod_type = "core";
     $this->mod_name = "admin";
-    
+
     $this->makeRevision("all");
-    
+
     $this->makeRevision("1.0.14");
-    $query = "ALTER TABLE `user_preferences` 
+    $query = "ALTER TABLE `user_preferences`
       DROP PRIMARY KEY;";
     $this->addQuery($query);
-    $query = "ALTER TABLE `user_preferences` 
+    $query = "ALTER TABLE `user_preferences`
       ADD `pref_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
       CHANGE `pref_user` `user_id` INT (11) UNSIGNED NOT NULL DEFAULT '0',
       CHANGE `pref_name` `key` VARCHAR (255) NOT NULL,
       CHANGE `pref_value` `value` VARCHAR (255);";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.15");
-    $query = "ALTER TABLE `users` 
+    $query = "ALTER TABLE `users`
       ADD INDEX (`user_birthday`),
       ADD INDEX (`user_last_login`),
       ADD INDEX (`profile_id`);";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.16");
-    $query = "ALTER TABLE `users` 
+    $query = "ALTER TABLE `users`
       CHANGE `user_address1` `user_address1` VARCHAR( 255 );";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.17");
-    $query = "ALTER TABLE `users` 
+    $query = "ALTER TABLE `users`
       ADD `dont_log_connection` ENUM ('0','1') DEFAULT '0';";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.18");
     $query = "CREATE TABLE `source_ldap` (
       `source_ldap_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
@@ -56,46 +56,46 @@ class CSetupadmin extends CSetup {
       `ldap_opt_referrals` ENUM ('0','1') DEFAULT '0'
      ) /*! ENGINE=MyISAM */;";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.19");
-    $query = "ALTER TABLE `source_ldap` 
+    $query = "ALTER TABLE `source_ldap`
                 ADD `bind_rdn_suffix` VARCHAR (255);";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.20");
-    $query = "ALTER TABLE `source_ldap` 
+    $query = "ALTER TABLE `source_ldap`
               ADD `priority` INT (11);";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.21");
-    $query = "ALTER TABLE `source_ldap` 
+    $query = "ALTER TABLE `source_ldap`
               ADD `secured` ENUM ('0','1') DEFAULT '0';";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.22");
-    $query = "ALTER TABLE `users` 
+    $query = "ALTER TABLE `users`
       CHANGE `user_phone`  `user_phone`  VARCHAR (20),
       CHANGE `user_mobile` `user_mobile` VARCHAR (20)";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.23");
-    $query = "ALTER TABLE `users` 
+    $query = "ALTER TABLE `users`
       DROP `user_pic`,
       DROP `user_signature`,
       CHANGE `user_password`     `user_password`     VARCHAR(255),
       CHANGE `user_login_errors` `user_login_errors` TINYINT( 4 ) UNSIGNED NOT NULL DEFAULT '0',
       CHANGE `user_type`         `user_type`         TINYINT( 4 ) UNSIGNED NOT NULL DEFAULT '0';";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.24");
     $query = "ALTER TABLE `users`
       ADD `user_salt` CHAR(64) AFTER `user_password`,
       MODIFY `user_password` CHAR(64);";
     $this->addQuery($query);
-    
+
     $this->makeRevision("1.0.25");
     $this->addDependency("system", "1.1.12");
-    
+
     $query = "CREATE TABLE `view_access_token` (
       `view_access_token_id` INT (11) UNSIGNED NOT NULL auto_increment PRIMARY KEY,
       `user_id` INT (11) UNSIGNED NOT NULL,
@@ -106,7 +106,7 @@ class CSetupadmin extends CSetup {
       `hash` CHAR (40) NOT NULL
      ) /*! ENGINE=MyISAM */;";
     $this->addQuery($query);
-    $query = "ALTER TABLE `view_access_token` 
+    $query = "ALTER TABLE `view_access_token`
       ADD INDEX (`user_id`),
       ADD INDEX (`datetime_start`),
       ADD INDEX (`first_use`),
@@ -114,15 +114,22 @@ class CSetupadmin extends CSetup {
     $this->addQuery($query);
 
     $this->makeRevision("1.0.26");
-    $query = "ALTER TABLE  
+    $query = "ALTER TABLE
       `user_preferences` CHANGE `user_id` `user_id` INT( 11 ) UNSIGNED NULL";
     $this->addQuery($query);
-    $query = "UPDATE `user_preferences` 
+    $query = "UPDATE `user_preferences`
       SET `user_id` = NULL
       WHERE `user_id` = '0'";
     $this->addQuery($query);
-    
-    $this->mod_version = "1.0.27";
+
+    $this->makeRevision("1.0.27");
+
+    $query = "ALTER TABLE `users` ADD `user_astreinte` VARCHAR (20)";
+    $this->addQuery($query);
+
+    $this->mod_version = "1.0.28";
+
+
   }
 }
 ?>
