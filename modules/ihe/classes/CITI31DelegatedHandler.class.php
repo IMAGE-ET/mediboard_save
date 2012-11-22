d289343d10a17143ea4936f503bbdbdadcb89cee
Yohann Poiron <poiron@openxtrem.com>
2012-11-22 09:57:36 +0000

    bug: Chargement sur un objet vide
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17372 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/ihe/classes/CITI31DelegatedHandler.class.php
+++ modules/ihe/classes/CITI31DelegatedHandler.class.php
@@ -236,6 +236,9 @@ class CITI31DelegatedHandler extends CITIDelegatedHandler {
     
     if ($update) {
       if ($movement->original_trigger_code == "A02") {
+        if (!$affectation) {
+          $affectation = new CAffectation();
+        }
         $affectation->load($movement->affectation_id);
       }
       $movement->start_of_movement = $this->getStartOfMovement($movement->original_trigger_code, $sejour, $affectation);
