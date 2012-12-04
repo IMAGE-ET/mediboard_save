44ed92675d4b9ac7876223d20c78a005b2c5c19c
Laurent Le Moine <laurent.le.moine17@gmail.com>
2012-12-04 15:02:26 +0000

    Ref: Suppression du tab "Tables"
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17428 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dicom/index.php
+++ modules/dicom/index.php
@@ -10,4 +10,4 @@
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
 $module->registerTab("vw_idx_sessions", TAB_READ);
-$module->registerTab("vw_idx_tables"  , TAB_ADMIN);
+//$module->registerTab("vw_idx_tables"  , TAB_ADMIN);
