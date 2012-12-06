aa8bc201d0babc11ad2566fc9305eed66e854d84
Flavien Crochard <flavien.crochard@openxtrem.com>
2012-12-06 09:52:47 +0000

    Erg: Ajout d'un bouton ALd / Substituable dans la modale Outils qui ouvre une modale avec toutes les lignes.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17443 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
   
   // Automated numbering (should be incremented at each commit)
-  "build" => 117,
+  "build" => 118,
 );
 
 $version["string"] = implode(".", $version);
