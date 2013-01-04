d49cfb244de2fa2ec7a0b76a83d9caf9f222bae6
Fabien Ménager <fabien.menager@openxtrem.com>
2013-01-04 15:40:17 +0000

    fnc: affichage de documents HPRIM Medecin pour les resultats de biologie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17667 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hprim21/classes/CHprim21.class.php
+++ modules/hprim21/classes/CHprim21.class.php
@@ -27,4 +27,78 @@ class CHprim21 {
     
     return str_replace('$g', $group_id, $tag);
   }
+
+  static function formatHPRIMBiologie($string) {
+    if (strpos($string, "\x8E") !== false) {
+      $string = iconv("macintosh", "iso-8859-1//TRANSLIT//IGNORE", $string);
+    }
+
+    $lines = preg_split("/(\r\n|\n)/", $string, 13);
+
+    $lines[10] = preg_split("/\s/", $lines[10], 2); // Expediteur
+    $lines[11] = preg_split("/\s/", $lines[11], 2); // Destinataire
+
+    $all_text = $lines[12];
+    unset($lines[12]);
+
+    $lab_header = "****LAB****";
+    $fin_header = "****FIN****";
+
+    $lab_pos = strpos($all_text, $lab_header);
+    $fin_pos = strpos($all_text, $fin_header);
+
+    $text  = substr($all_text, 0, $lab_pos);
+    $hprim = trim(substr($all_text, $lab_pos+strlen($lab_header), $fin_pos-$lab_pos-strlen($fin_header)));
+    $hprim = preg_split("/(\r\n|\n)/", $hprim);
+
+    $pattern = "/^RES\|".      // 01
+      "(?P<label>[^\|]*)\|".   // 02
+      "(?P<code>[^\|]*)\|".    // 03
+      "(?P<type>[^\|]*)\|".    // 04
+      "(?P<value>[^\|]*)\|".   // 05
+      "(?P<unit>[^\|]*)\|".    // 06
+      "(?P<min>[^\|]*)\|".     // 07
+      "(?P<max>[^\|]*)\|".     // 08
+      "(?P<anormal>[^\|]*)\|". // 09
+      "(?P<status>[^\|]*)\|".  // 10
+      "(?P<value2>[^\|]*)\|".  // 11
+      "(?P<unit2>[^\|]*)\|".   // 12
+      "(?P<min2>[^\|]*)\|".    // 13
+      "(?P<max2>[^\|]*)\|".    // 14
+      "/";
+
+    $anormalites = array(
+      "L" => "inférieur à la normale",
+      "H" => "supérieur à la normale",
+      "LL" => "inférieur à la valeur panique basse",
+      "HH" => "supérieur à la valeur panique haute",
+      "N" => "normal",
+    );
+
+    $classes = array(
+      "L" => "warning",
+      "H" => "warning",
+      "LL" => "error",
+      "HH" => "error",
+      "N" => "",
+    );
+
+    $results = array();
+    foreach ($hprim as $_hprim) {
+      $matches = array();
+      if (preg_match($pattern, $_hprim, $matches)) {
+        $matches["anormal_text"]  = CValue::read($anormalites, $matches["anormal"]);
+        $matches["anormal_class"] = CValue::read($classes, $matches["anormal"]);
+
+        $results[] = $matches;
+      }
+    }
+
+    $template = new CSmartyDP("modules/hprim21");
+    $template->assign("header",  $lines);
+    $template->assign("text",    $text);
+    $template->assign("results", $results);
+
+    return $template->fetch("inc_hprim_biologie_results.tpl");
+  }
 }
---
+++ modules/hprim21/templates/inc_hprim_biologie_results.tpl
@@ -0,0 +1,76 @@
+<h1>
+  <small style="float: right">[{{$header.0}}]</small>
+  Résultats de laboratoire &ndash; {{$header.9}}
+</h1>
+
+<table class="main form">
+  <tr>
+    <th colspan="4" class="title">{{$header.1}} {{$header.2}} &ndash; {{$header.6}} [{{$header.8}}]</th>
+  </tr>
+  <tr>
+    <th>Expéditeur</th>
+    <td><small>[{{$header.10.0}}]</small> {{$header.10.1}}</td>
+
+    <th>Destinataire</th>
+    <td><small>[{{$header.11.0}}]</small> {{$header.11.1}}</td>
+  </tr>
+  <tr>
+    <td colspan="4"><hr /></td>
+  </tr>
+  <tr>
+    <th>Adresse</th>
+    <td>
+    {{$header.3}}<br />
+    {{$header.4}}<br />
+    {{$header.5}}
+    </td>
+
+    <th>Numéro de sécurité sociale</th>
+    <td>{{$header.7}}</td>
+  </tr>
+</table>
+
+<h1>Courrier</h1>
+<pre>{{$text}}</pre>
+
+<h1>Résultats</h1>
+<table class="main tbl">
+  <tr>
+    <th>Libellé</th>
+  {{*<th>Code</th>*}}
+  {{*<th>Type de résultat</th>*}}
+    <th>Résultat</th>
+    <th>Unité</th>
+    <th>Val. normale inf.</th>
+    <th>Val. normale sup.</th>
+    <th>Anorm.</th>
+    <th>Statut</th>
+    <th>Résultat 2</th>
+    <th>Unité 2</th>
+    <th>Val. norm. inf. 2</th>
+    <th>Val. norm. sup. 2</th>
+  </tr>
+{{foreach from=$results item=_result}}
+  <tr>
+    <td>{{$_result.label}}</td>
+  {{*<td>{{$_result.code}}</td>*}}
+  {{*<td>{{$_result.type}}</td>*}}
+    <td class="{{$_result.anormal_class}}">
+      {{$_result.value}}
+    </td>
+    <td>{{$_result.unit}}</td>
+    <td>{{$_result.min}}</td>
+    <td>{{$_result.max}}</td>
+    <td>
+      {{if $_result.anormal != "N"}}
+          {{$_result.anormal_text}}
+        {{/if}}
+    </td>
+    <td>{{$_result.status}}</td>
+    <td>{{$_result.value2}}</td>
+    <td>{{$_result.unit2}}</td>
+    <td>{{$_result.min2}}</td>
+    <td>{{$_result.max2}}</td>
+  </tr>
+{{/foreach}}
+</table>
