7df0bc2843cbae3c7a2ec8dc4d34371585325e9b
Yohann Poiron <poiron@openxtrem.com>
2013-01-08 15:26:34 +0000

    fnc: Ajout de la réponse K22
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17707 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/hl7/classes/events/QBP/CHL7EventPDQK22.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * K22 - Find Candidates response - HL7
+ *  
+ * @category HL7
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @version  SVN: $Id:$ 
+ * @link     http://www.mediboard.org
+ */
+
+/**
+ * Interface CHL7EventQBPK22
+ * K22 - Find Candidates response
+ */
+interface CHL7EventQBPK22 extends CHL7EventQBP {}
+
+?>
