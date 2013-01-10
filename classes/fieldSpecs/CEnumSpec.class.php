bf5d9ebb76adcdcd4d86a49f409d33af76d9965f
Fabien Ménager <fabien.menager@openxtrem.com>
2013-01-10 17:04:24 +0000

    bug: tous les selects avaient un emptyLabel
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17741 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/fieldSpecs/CEnumSpec.class.php
+++ classes/fieldSpecs/CEnumSpec.class.php
@@ -1,53 +1,53 @@
-<?php 
+<?php
 /**
  * $Id$
- * 
+ *
  * @package    Mediboard
  * @subpackage classes
  * @author     SARL OpenXtrem <dev@openxtrem.com>
- * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
  * @version    $Revision$
  */
 
 class CEnumSpec extends CMbFieldSpec {
-  
+
   var $list = null;
   var $typeEnum = null;
   var $vertical = null;
   var $columns = null;
-  
+
   var $_list = null;
   var $_locales = null;
-  
+
   function __construct($className, $field, $prop = null, $aProperties = array()) {
     parent::__construct($className, $field, $prop, $aProperties);
 
     $this->_list = $this->getListValues($this->list);
     $this->_locales = array();
-    
+
     foreach ($this->_list as $value) {
       $this->_locales[$value] = CAppUI::tr("$className.$field.$value");
     }
   }
-  
+
   protected function getListValues($string){
     $list = array();
-    
+
     if ($string !== "" && $string !== null) {
       $list = explode('|', $string);
     }
-    
+
     return $list;
   }
-  
+
   function getSpecType() {
     return "enum";
   }
-  
+
   function getDBSpec() {
     return "ENUM('".str_replace('|', "','", $this->list)."')";
   }
-  
+
   function getOptions(){
     return array(
       'list'     => 'list',
@@ -56,20 +56,20 @@ class CEnumSpec extends CMbFieldSpec {
       'columns'  => 'num',
     ) + parent::getOptions();
   }
-  
+
   function getValue($object, $smarty = null, $params = array()) {
     $fieldName = $this->fieldName;
     $propValue = $object->$fieldName;
     return CMbString::htmlSpecialChars(CAppUI::tr("$object->_class.$fieldName.$propValue"));
   }
-  
+
   function checkOptions(){
     parent::checkOptions();
     if (!$this->list) {
       trigger_error("Spécification 'list' manquante pour le champ '$this->fieldName' de la classe '$this->className'", E_USER_WARNING);
     }
   }
-  
+
   function checkProperty($object){
     $propValue = $object->{$this->fieldName};
     $specFragments = $this->getListValues($this->list);
@@ -77,13 +77,13 @@ class CEnumSpec extends CMbFieldSpec {
       return "N'a pas une valeur possible";
     }
   }
-  
+
   function sample(&$object, $consistent = true){
     parent::sample($object, $consistent);
     $specFragments = $this->getListValues($this->list);
     $object->{$this->fieldName} = self::randomString($specFragments, 1);
   }
-  
+
   function getFormHtmlElement($object, $params, $value, $className){
     $field         = CMbString::htmlSpecialChars($this->fieldName);
     $typeEnum      = CMbArray::extract($params, "typeEnum", $this->typeEnum ? $this->typeEnum : "select");
@@ -97,16 +97,16 @@ class CEnumSpec extends CMbFieldSpec {
     if ($emptyLabel = CMbArray::extract($params, "emptyLabel")) {
       $emptyLabel = CAppUI::tr($emptyLabel);
     }
-    
+
     // Extra info por HTML generation
     $extra         = CMbArray::makeXmlAttributes($params);
     $locales       = $this->_locales;
     $className     = CMbString::htmlSpecialChars(trim("$className $this->prop"));
     $html          = "";
-    
+
     // Alpha sorting
     if ($alphabet) {
-      asort($locales); 
+      asort($locales);
     }
 
     // Turn readonly to disabled
@@ -116,12 +116,13 @@ class CEnumSpec extends CMbFieldSpec {
     switch ($typeEnum) {
       default:
       case "select":
-        $emptyLabel = "&mdash; $emptyLabel";
-         
+
         $html .= "<select name=\"$field\" class=\"$className\" $disabled $extra>";
-        
+
         // Empty option label
         if ($emptyLabel) {
+          $emptyLabel = "&mdash; $emptyLabel";
+
           if ($value === null) {
             $html .= "\n<option value=\"\" selected=\"selected\">$emptyLabel</option>";
           }
@@ -129,23 +130,23 @@ class CEnumSpec extends CMbFieldSpec {
             $html .= "\n<option value=\"\">$emptyLabel</option>";
           }
         }
-        
+
         // All other options
         foreach ($locales as $key => $item) {
           $selected = "";
           if (($value !== null && $value === "$key") || ($value === null && "$key" === "$this->default" && !$emptyLabel)) {
-            $selected = " selected=\"selected\""; 
+            $selected = " selected=\"selected\"";
           }
-          
+
           $html .= "\n<option value=\"$key\" $selected>$item</option>";
         }
-        
+
         $html .= "\n</select>";
         return $html;
 
       case "radio":
         $compteur = 0;
-        
+
         // Empty radio label
         if ($emptyLabel) {
           if ($value === null) {
@@ -156,32 +157,32 @@ class CEnumSpec extends CMbFieldSpec {
           }
           $html .= "<label for=\"{$field}_\">$emptyLabel</label> ";
         }
-        
+
         // All other radios
         foreach ($locales as $key => $item) {
           $selected = "";
           if (($value !== null && $value === "$key") || ($value === null && "$key" === "$this->default")) {
-            $selected = " checked=\"checked\""; 
+            $selected = " checked=\"checked\"";
           }
-          
+
           $html .= "\n<input type=\"radio\" name=\"$field\" value=\"$key\" $selected class=\"$className\" $disabled $extra />
                        <label for=\"{$field}_{$key}\">$item</label> ";
           $compteur++;
-          
+
           $modulo = $compteur % $cycle;
           if ($separator != null && $modulo == 0 && $compteur < count($locales)) {
             $html  .= $separator;
           }
- 
+
           if ($this->vertical) {
             $html .= "<br />\n";
           }
         }
-        
+
         return $html;
     }
   }
-  
+
   function getLabelForAttribute($object, &$params){
     // to extract the XHTML invalid attribute "typeEnum"
     $typeEnum = CMbArray::extract($params, "typeEnum");
