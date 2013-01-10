869ae11123e78359585be5bd4e225c6341da28c7
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-01-10 10:03:57 +0000

    Bug: Problème de requête dans l'impression planning du PMSI.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17735 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpmsi/print_planning.php
+++ modules/dPpmsi/print_planning.php
@@ -47,7 +47,7 @@ if($filter->_prat_id) {
 // En fonction de la salle
 $listBlocs = CGroups::loadCurrent()->loadBlocs(PERM_READ);
 
-$salle = new CSalle;
+$salle = new CSalle();
 $whereSalle = array('bloc_id' => CSQLDataSource::prepareIn(array_keys($listBlocs)));
 $listSalles = $salle->loadListWithPerms(PERM_READ, $whereSalle);
 
@@ -56,81 +56,91 @@ $where["salle_id"] = CSQLDataSource::prepareIn(array_keys($listSalles), $filter-
 $plagesop = $plagesop->loadList($where, $order);
 $plagesop["urgences"] = new CPlageOp();
 
+$prats = CMbObject::massLoadFwdRef($plagesop, "chir_id");
+CMbObject::massLoadFwdRef($prats, "function_id");
+
 // Operations de chaque plage
 $listUrgencesTraitees = array();
 foreach ($plagesop as $_plage) {
   $where = array();
-  $tempOp = new COperation;
-  
+  $ljoin = array();
+  $tempOp = new COperation();
+
   // Cas des plages normales
   if ($_plage->_id) {
-	  
-	  $_plage->loadRefsFwd();
-	
-	  // Opérations normale
-	  $joins = array();
-	  $where["plageop_id"] = "= '$_plage->_id'";
-	  $where["annulee"] = "= '0'";
-	  
-	  // Intervention ordonnancé
-	  switch ($filter->_ranking) {
-	    case "ok" : $where["rank"] = "!= '0'"; break;
-	    case "ko" : $where["rank"] = "= '0'"; break;
-	  }
-	  
-    // Interventions avec ou sans actes
-    if ($filter->_cotation) {
-      $ljoin["acte_ccam"] = "operations.operation_id = acte_ccam.object_id AND acte_ccam.object_class = 'COperation'";
-      switch ($filter->_cotation) {
-        case "ok" : $where["acte_id"] = "IS NOT NULL"; break;        
-        case "ko" : $where["acte_id"] = "IS NULL"; break;
-      }
+
+    $_plage->loadRefsFwd();
+
+    // Opérations normale
+    $joins = array();
+    $where["plageop_id"] = "= '$_plage->_id'";
+    $where["annulee"] = "= '0'";
+
+    // Intervention ordonnancé
+    switch ($filter->_ranking) {
+      case "ok" :
+        $where["rank"] = "!= '0'";
+        break;
+      case "ko" :
+        $where["rank"] = "= '0'";
     }
-	      
- 	  if ($filter->_codes_ccam) {
-	    $where["codes_ccam"] = "LIKE '%$filter->_codes_ccam%'";
-	  }
-	  
-	  $order = "operations.rank";
-	  
-	  $listOperations = $tempOp->loadList($where, $order);
-	  
-	  // Urgences
-	  $where["plageop_id"]   = "IS NULL";
-	  $where["salle_id"]     = "= '$_plage->salle_id'";
-	  $where["chir_id"]      = "= '$_plage->chir_id'";
-	  $where["date"]         = "= '$_plage->date'";
-	  $where["operation_id"] = $_plage->_spec->ds->prepareNotIn($listUrgencesTraitees);
-	  $listUrgences = $tempOp->loadList($where);
-	  $listUrgencesTraitees = array_merge($listUrgencesTraitees, array_keys($listUrgences));
-	  
-	  // On compile les interventions
-	  $_plage->_ref_operations = array_merge($listOperations, $listUrgences);
-  }
-  else {
-    $ljoin = array();
 
     // Interventions avec ou sans actes
     if ($filter->_cotation) {
       $ljoin["acte_ccam"] = "operations.operation_id = acte_ccam.object_id AND acte_ccam.object_class = 'COperation'";
       switch ($filter->_cotation) {
-        case "ok" : $where["acte_id"] = "IS NOT NULL"; break;        
-        case "ko" : $where["acte_id"] = "IS NULL"; break;
+        case "ok":
+          $where["acte_id"] = "IS NOT NULL";
+          break;
+        case "ko":
+          $where["acte_id"] = "IS NULL";
       }
     }
-    
-    // Cas des urgences restantes
-	  $where["plageop_id"]   = "IS NULL";
-	  $where["date"]         = $ds->prepare("BETWEEN %1 AND %2", $filter->_date_min, $filter->_date_max);
-	  $where["operation_id"] = $ds->prepareNotIn($listUrgencesTraitees);
-	  $order = "date, chir_id";
-    $_plage->_ref_operations = $tempOp->loadList($where, $order, null, null, $ljoin);
-	  if (!count($_plage->_ref_operations)) {
-	    unset($plagesop["urgences"]);
-	    continue;
-	  }
+
+    if ($filter->_codes_ccam) {
+      $where["codes_ccam"] = "LIKE '%$filter->_codes_ccam%'";
+    }
+
+    $order = "operations.rank";
+    $listOperations = $tempOp->loadList($where, $order, null, null, $ljoin);
+
+    // Urgences
+    $where["plageop_id"]   = "IS NULL";
+    $where["salle_id"]     = "= '$_plage->salle_id'";
+    $where["chir_id"]      = "= '$_plage->chir_id'";
+    $where["date"]         = "= '$_plage->date'";
+    $where["operation_id"] = $_plage->_spec->ds->prepareNotIn($listUrgencesTraitees);
+    $listUrgences = $tempOp->loadList($where, $order, null, null, $ljoin);
+    $listUrgencesTraitees = array_merge($listUrgencesTraitees, array_keys($listUrgences));
+
+    // On compile les interventions
+    $_plage->_ref_operations = array_merge($listOperations, $listUrgences);
   }
-  
+  else {
+    // Interventions avec ou sans actes
+    if ($filter->_cotation) {
+      $ljoin["acte_ccam"] = "operations.operation_id = acte_ccam.object_id AND acte_ccam.object_class = 'COperation'";
+      switch ($filter->_cotation) {
+        case "ok":
+          $where["acte_id"] = "IS NOT NULL";
+          break;
+        case "ko":
+          $where["acte_id"] = "IS NULL";
+      }
+    }
+
+    // Cas des urgences restantes
+    $where["plageop_id"]   = "IS NULL";
+    $where["date"]         = $ds->prepare("BETWEEN %1 AND %2", $filter->_date_min, $filter->_date_max);
+    $where["operation_id"] = $ds->prepareNotIn($listUrgencesTraitees);
+    $order = "date, chir_id";
+    $_plage->_ref_operations = $tempOp->loadList($where, $order, null, null, $ljoin);
+    if (!count($_plage->_ref_operations)) {
+      unset($plagesop["urgences"]);
+      continue;
+    }
+  }
+
   foreach ($_plage->_ref_operations as $_operation) {
     $sejour = $_operation->loadRefSejour();
     if ($filterSejour->type && $filterSejour->type != $sejour->type) {
@@ -138,16 +148,16 @@ foreach ($plagesop as $_plage) {
       continue;
     }
 
-    $_operation->loadRefPraticien();
+    $_operation->loadRefPraticien()->loadRefFunction();
     $_operation->loadExtCodesCCAM();
     foreach ($_operation->loadRefsActesCCAM() as $_acte) {
       $_acte->loadRefExecutant();
     }
-    
-    $sejour->loadRefPatient();   
+
+    $sejour->loadRefPatient();
     $sejour->loadRefCurrAffectation($_operation->_datetime);
   }
-  
+
   if (!count($_plage->_ref_operations) && !$filter->_plage) {
     unset($plagesop[$_plage->_id]);
   }
@@ -160,5 +170,3 @@ $smarty->assign("filter"  , $filter);
 $smarty->assign("plagesop", $plagesop);
 
 $smarty->display("print_planning.tpl");
-
-?>
