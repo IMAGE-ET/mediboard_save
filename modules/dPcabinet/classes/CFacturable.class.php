3dc64ff983031743e58fa94941f676b43a78d36a
Aurélie Hébras <aureliehebras@gmail.com>
2013-01-29 13:48:26 +0000

    Fnc: Ajout de la classe CFacturable qui sera prochainement utilisée
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17896 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPcabinet/classes/CFacturable.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * $Id$
+ *
+ * @package    Mediboard
+ * @subpackage dPcabinet
+ * @author     SARL OpenXtrem <dev@openxtrem.com>
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @version    $Revision$
+ */
+
+/**
+ * Facturable
+ *
+ */
+class CFacturable extends CCodable {
+  
+  /**
+   * getBackProps
+   * 
+   * @return $backProps
+  **/
+  function getBackProps() {
+    $backProps = parent::getBackProps();
+    $backProps["facturable"]  = "CFactureLiaison object_id";
+    return $backProps;
+  }
+}
