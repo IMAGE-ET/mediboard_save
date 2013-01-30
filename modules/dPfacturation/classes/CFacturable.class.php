482fa0c2f25972a5bdfd03b7f988be198011a02d
Aurélie Hébras <aureliehebras@gmail.com>
2013-01-30 09:17:44 +0000

    Ref: Déplacement de la classe dans le bon module!
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@17904 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPfacturation/classes/CFacturable.class.php
+++ modules/dPfacturation/classes/CFacturable.class.php
@@ -3,7 +3,7 @@
  * $Id$
  *
  * @package    Mediboard
- * @subpackage dPcabinet
+ * @subpackage dPfacturation
  * @author     SARL OpenXtrem <dev@openxtrem.com>
  * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
  * @version    $Revision$
