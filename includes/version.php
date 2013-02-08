d532b5883906bf43bb2839d2f9f86c9d79a839b8
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-02-08 08:25:31 +0000

    Bug: On ne voyait pas les plages de consultations et opératoires du samedi et dimanche dans la vue semaine du tableau de bord.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18029 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 133,
+  "build" => 134,
 );
 
 $version["string"] = implode(".", $version);
