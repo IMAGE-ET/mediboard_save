3c45514eb00101b065394b1509e147e768f1ad8f
Yohann Poiron <poiron@openxtrem.com>
2013-02-14 16:58:07 +0000

    bug: Le téléphone étrangé n'était pas transmis
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18115 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hl7/classes/segments/CHL7v2SegmentPID.class.php
+++ modules/hl7/classes/segments/CHL7v2SegmentPID.class.php
@@ -181,7 +181,7 @@ class CHL7v2SegmentPID extends CHL7v2Segment {
       $phones[] = $this->getXTN($receiver, $patient->tel2, $receiver->_configs["build_cellular_phone"], "CP");
     }
     if ($patient->tel_autre) {
-      $phones[] = $this->getXTN($receiver, $patient->tel2, "ORN", "PH");
+      $phones[] = $this->getXTN($receiver, $patient->tel_autre, "ORN", "PH");
     }
     if ($patient->email) {
       $phones[] = array(
