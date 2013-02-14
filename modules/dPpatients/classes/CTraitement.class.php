b3c37cba1cec04c827c1b96a1fa55ab4da97f000
Alexis Granger <granger@openxtrem.com>
2013-02-14 09:02:34 +0000

    bug: suppression de l'ajout automatique de code CIM lors de l'ajout de traitement perso en texte simple.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18101 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/classes/CTraitement.class.php
+++ modules/dPpatients/classes/CTraitement.class.php
@@ -51,8 +51,6 @@ class CTraitement extends CMbObject {
     if ($msg = parent::store()) {
       return $msg;
     }
-    // DossierMedical store
-    $this->checkCodeCim10();
   }
   
   function loadRefDossierMedical(){ 
@@ -64,36 +62,6 @@ class CTraitement extends CMbObject {
     parent::loadView();
     $this->loadLogs();
     $this->loadRefDossierMedical();
-  }
-  
-  function checkCodeCim10(){
-    preg_match_all("/[A-Z]\d{2}\.?\d{0,2}/i", $this->traitement, $matches);
-    
-    foreach($matches as $key => $match_){
-      foreach($match_ as &$match){
-        // Transformation du code CIM pour le tester
-        $match = str_replace(".","",$match);
-        $match = strtoupper($match);
-        
-        // Chargement du code CIM 10
-        $code_cim10 = new CCodeCIM10($match, 1);
-    
-        if($code_cim10->libelle != "Code CIM inexistant"){
-          // Cas du code valide, sauvegarde du code CIM
-          $dossier_medical = new CDossierMedical();
-          $dossier_medical->load($this->dossier_medical_id);
-          
-          // si le code n'est pas deja present, on le rajoute
-          if(!array_key_exists($match, $dossier_medical->_ext_codes_cim)){
-            if($dossier_medical->codes_cim != ""){
-              $dossier_medical->codes_cim .= "|";
-            }
-            $dossier_medical->codes_cim .= $match;
-            $dossier_medical->store();
-          }
-        }
-      }
-    }
   }
 }
 
