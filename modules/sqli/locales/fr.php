afb1140b84a8353dd23548c4751decf6a4ebc9f4
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-02-22 08:10:24 +0000

    Bug: suppression module sqli des modules normaux
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18196 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/sqli/locales/fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$locales['Sqli-export-pat-desc'] = 'Export de la base patient pour audit';
-$locales['Sqli-export-type'] = 'Types d\'export vers SQLi Santé';
-$locales['Sqli-warning-load'] = 'Attention, certains exports requièrent de très grosses charges de Mediboard, eviter de les utiliser en heure pleine.';
-$locales['mod-sqli-tab-vw_export_pat'] = 'Exports';
-$locales['module-sqli-court'] = 'SQLi Santé';
-$locales['module-sqli-long'] = 'Outils SQLi ';
-?>
