080743f62901af4734f0b2206b2262e314179993
Aurélie Hébras <aureliehebras@gmail.com>
2013-02-25 14:36:39 +0000

    Ref: La légende utilise à présent un tpl et des traductions
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18218 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/dPurgences/templates/vw_legende.tpl
@@ -0,0 +1,99 @@
+<table class="tbl">
+  <tr>
+    <th colspan="2">Urgences</th>
+  </tr>
+  <tr>
+    <th>Couleur</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td class="text">Patient non pris en charge</td>
+  </tr>
+  <tr>
+    <td style="background-color:#cfc"></td>
+    <td class="text">Patient pris en charge</td>
+  </tr>
+  <tr>
+    <td style="border-right: 5px solid black"></td>
+    <td class="text">Patient sorti</td>
+  </tr>
+  <tr>
+    <th colspan="2">Degrés d'urgences</th>
+  </tr>
+  <tr>
+    <th>Couleur</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td style="background-color:#0F0"></td>
+    <td class="text">{{tr}}CRPU.ccmu.1.desc{{/tr}}</td>
+  </tr>
+  {{if $conf.ref_pays == 1}}
+  <tr>
+    <td style="background-color:#0F0"></td>
+    <td class="text">{{tr}}CRPU.ccmu.P.desc{{/tr}}</td>
+  </tr>
+  {{/if}}
+  <tr>
+    <td style="background-color:#9F0"></td>
+    <td class="text">{{tr}}CRPU.ccmu.2.desc{{/tr}}</td>
+  </tr>
+  <tr>
+    <td style="background-color:#FF0"></td>
+    <td class="text">{{tr}}CRPU.ccmu.3.desc{{/tr}}</td>
+  </tr>  
+  <tr>
+    <td style="background-color:#FFCD00"></td>
+    <td class="text">{{tr}}CRPU.ccmu.4.desc{{/tr}}</td>
+  </tr>
+  {{if $conf.ref_pays == 1}}
+  <tr>
+    <td style="background-color:#F60"></td>
+    <td class="text">{{tr}}CRPU.ccmu.5.desc{{/tr}}</td>
+  </tr>
+  <tr>
+    <td style="background-color:#F00"></td>
+    <td class="text">{{tr}}CRPU.ccmu.D.desc{{/tr}}</td>
+  </tr>
+  {{/if}}
+  <tr>
+    <th colspan="2">{{tr}}CRPU-_attente{{/tr}}</th>
+  </tr>
+  <tr>
+    <th>Image</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>
+      <img src="images/icons/attente_first_part.png"></img>
+    </td>
+    <td>
+      Attente depuis moins de {{$conf.dPurgences.attente_first_part|date_format:"%Hh%M"}}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="images/icons/attente_second_part.png"></img>
+    </td>
+    <td>
+      Attente entre {{$conf.dPurgences.attente_first_part|date_format:"%Hh%M"}} et {{$conf.dPurgences.attente_second_part|date_format:"%Hh%M"}}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="images/icons/attente_third_part.png"></img>
+    </td>
+    <td>
+      Attente entre {{$conf.dPurgences.attente_second_part|date_format:"%Hh%M"}} et {{$conf.dPurgences.attente_third_part|date_format:"%Hh%M"}}
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="images/icons/attente_fourth_part.png"></img>
+    </td>
+    <td>
+      Attente de plus de {{$conf.dPurgences.attente_third_part|date_format:"%Hh%M"}}
+    </td>
+  </tr>
+</table>
--- modules/dPurgences/vw_legende.php
+++ modules/dPurgences/vw_legende.php
@@ -1,95 +1,18 @@
-<table class="tbl">
-  <tr>
-    <th colspan="2">Urgences</th>
-  </tr>
-  <tr>
-    <th>Couleur</th>
-    <th>Description</th>
-  </tr>
-  <tr>
-    <td></td>
-    <td class="text">Patient non pris en charge</td>
-  </tr>
-  <tr>
-    <td style="background-color:#cfc"></td>
-    <td class="text">Patient pris en charge</td>
-  </tr>
-  <tr>
-    <td style="border-right: 5px solid black"></td>
-    <td class="text">Patient sorti</td>
-  </tr>
-  <tr>
-    <th colspan="2">Degrés d'urgences</th>
-  </tr>
-  <tr>
-    <th>Couleur</th>
-    <th>Description</th>
-  </tr>
-  <tr>
-    <td style="background-color:#0F0"></td>
-    <td class="text">Etat stable</td>
-  </tr>
-  <tr>
-    <td style="background-color:#0F0"></td>
-    <td class="text">Psy (Etat stable, terrain psy)</td>
-  </tr>
-  <tr>
-    <td style="background-color:#9F0"></td>
-    <td class="text">Etat stable, examens complémentaires</td>
-  </tr>
-  <tr>
-    <td style="background-color:#FF0"></td>
-    <td class="text">Etat pouvant s'aggraver</td>
-  </tr>  
-  <tr>
-    <td style="background-color:#FFCD00"></td>
-    <td class="text">Pronostic vital engagé</td>
-  </tr>
-  <tr>
-    <td style="background-color:#F60"></td>
-    <td class="text">Réanimation</td>
-  </tr>
-  <tr>
-    <td style="background-color:#F00"></td>
-    <td class="text">Décès</td>
-  </tr>
-  <tr>
-    <th colspan="2">Temps d'attente</th>
-  </tr>
-  <tr>
-    <th>Image</th>
-    <th>Description</th>
-  </tr>
-  <tr>
-    <td>
-      <img src="images/icons/attente_first_part.png"></img>
-    </td>
-    <td>
-      Attente depuis moins de <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_first_part"), null, '%Hh%M')?>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <img src="images/icons/attente_second_part.png"></img>
-    </td>
-    <td>
-      Attente entre <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_first_part"), null, '%Hh%M')?> et <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_second_part"), null, '%Hh%M') ?>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <img src="images/icons/attente_third_part.png"></img>
-    </td>
-    <td>
-      Attente entre <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_second_part"), null, '%Hh%M')?> et <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_third_part"), null, '%Hh%M')?>
-    </td>
-  </tr>
-  <tr>
-    <td>
-      <img src="images/icons/attente_fourth_part.png"></img>
-    </td>
-    <td>
-      Attente de plus de <?php echo mbTransformTime(CAppUI::conf("dPurgences attente_third_part"), null, '%Hh%M')?>
-    </td>
-  </tr>
-</table>
+<?php
+/**
+ * $Id$
+ *
+ * @package    Mediboard
+ * @subpackage dPurgences
+ * @author     SARL OpenXtrem <dev@openxtrem.com>
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @version    $Revision$
+ */
+
+CCanDo::checkRead();
+
+// Création du template
+$smarty = new CSmartyDP();
+
+$smarty->display("vw_legende.tpl");
+?>
