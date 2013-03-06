1143bdd5248fb62b81b27cd1fe3d55bfd0c61a20
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-03-06 14:21:21 +0000

    Fnc: CNotification permettant la gestion de notification sur des classes (Sejour, consultation) et d'un transport (sms, mail ... ) Fnc: Voir les notifications générées pour un jour donné Ref: création d'une classe CMbPhone pour travailler sur les numéros de téléphone
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18327 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ classes/CMbPhone.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * $Id$
+ *
+ * @category Classes
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @link     http://www.mediboard.org
+ */
+ 
+ 
+class CMbPhone {
+
+  /**
+   * transform a phone number to an internation format
+   *
+   * @param string $phonenumber the phone number
+   *
+   * @return string $phonenumber the phone internationalized
+   */
+  static function phoneToInternational($phonenumber) {
+
+    //french default format
+    if (self::checkMobileNumber($phonenumber)) {
+      return preg_replace("/0/", "33", $phonenumber, 1);
+    }
+
+    return $phonenumber;
+  }
+
+  /**
+   * check for mobile number (06..., 07...)
+   *
+   * @param string $number  the number
+   * @param string $country the country to check
+   *
+   * @return bool
+   */
+  static function checkMobileNumber($number, $country ="fr") {
+    $phones = array(
+      "fr"  => "/(0|\+33\s?)(6|7)(\s?\d{2}){4}/"
+    );
+
+    if (!array_key_exists($country, $phones)) {
+      return false;
+    }
+
+    switch ($country) {
+      case "fr":
+        if (preg_match($phones[$country], $number)) {
+          return true;
+        }
+        break;
+    }
+
+
+    return false;
+  }
+}
