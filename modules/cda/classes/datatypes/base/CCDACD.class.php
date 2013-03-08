c957eb054e1b1b63c4c91c5b0c9678eb082b6014
Nicolas Le Dantec <n.ledantec@openxtrem.com>
2013-03-08 15:10:39 +0000

    Fnc : Ajout de nouvelle classe
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18353 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/cda/classes/datatypes/base/CCDACD.class.php
+++ modules/cda/classes/datatypes/base/CCDACD.class.php
@@ -3,7 +3,7 @@
 /**
  * $Id$
  *  
- * @category ${Module}
+ * @category CDA
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
  * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
@@ -42,7 +42,7 @@ class CCDACD extends CCDAANY {
    * this concept descriptor into other code systems.
    * @var CCDACD
    */
-  public $translation;
+  var $translation = array();
 
   /**
    * The plain code symbol defined by the code system.
@@ -79,20 +79,343 @@ class CCDACD extends CCDAANY {
   public $displayName;
 
   /**
+   * Getter code
+   *
+   * @return CCDA_cs CCDA_cs code
+   */
+  public function getCode() {
+    return $this->code;
+  }
+
+  /**
+   * Getter CodeSystem
+   *
+   * @return \CCDA_uid
+   */
+  public function getCodeSystem() {
+    return $this->codeSystem;
+  }
+
+
+  /**
+   * Getter CodeSystemName
+   *
+   * @return \CCDA_st
+   */
+  public function getCodeSystemName() {
+    return $this->codeSystemName;
+  }
+
+  /**
+   * Getter CodeSystemVersion
+   *
+   * @return \CCDA_st
+   */
+  public function getCodeSystemVersion() {
+    return $this->codeSystemVersion;
+  }
+
+  /**
+   * Getter DisplayName
+   *
+   * @return \CCDA_st
+   */
+  public function getDisplayName() {
+    return $this->displayName;
+  }
+
+  /**
+   * Getter OriginalText
+   *
+   * @return \CCDAED
+   */
+  public function getOriginalText() {
+    return $this->originalText;
+  }
+
+  /**
+   * Getter Qualifier
+   *
+   * @return \CCDACR
+   */
+  public function getQualifier() {
+    return $this->qualifier;
+  }
+
+    /**
+   * Getter Translation
+   *
+   * @return \CCDACD
+   */
+  public function getTranslation() {
+    return $this->translation;
+  }
+
+  /**
+   * Setter Code
+   *
+   * @param \CCDA_cs $code CCDA_cs
+   *
+   * @return void
+   */
+  public function setCode($code) {
+    $this->code = $code;
+  }
+
+  /**
+   * Setter CodeSystem
+   *
+   * @param \CCDA_uid $codeSystem CCDA_uid
+   *
+   * @return void
+   */
+  public function setCodeSystem($codeSystem) {
+    $this->codeSystem = $codeSystem;
+  }
+
+  /**
+   * Setter codeSystemName
+   *
+   * @param \CCDA_st $codeSystemName CCDA_st
+   *
+   * @return void
+   */
+  public function setCodeSystemName($codeSystemName) {
+    $this->codeSystemName = $codeSystemName;
+  }
+
+  /**
+   * Setter codeSystemVersion
+   *
+   * @param \CCDA_st $codeSystemVersion CCDA_st
+   *
+   * @return void
+   */
+  public function setCodeSystemVersion($codeSystemVersion) {
+    $this->codeSystemVersion = $codeSystemVersion;
+  }
+
+  /**
+   * Setter displayName
+   *
+   * @param \CCDA_st $displayName CCDA_st
+   *
+   * @return void
+   */
+  public function setDisplayName($displayName) {
+    $this->displayName = $displayName;
+  }
+
+  /**
+   * Setter originalText
+   *
+   * @param \CCDAED $originalText CCDAED
+   * v
+   */
+  public function setOriginalText($originalText) {
+    $this->originalText = $originalText;
+  }
+
+  /**
+   * Setter qualifier
+   *
+   * @param \CCDACR $qualifier CCDACR
+   *
+   * @return void
+   */
+  public function setQualifier($qualifier) {
+    $this->qualifier = $qualifier;
+  }
+
+  /**
+   * Setter translation
+   *
+   * @param \CCDACD $translation CCDACD
+   *
+   * @return void
+   */
+  public function setTranslation($translation) {
+    array_push($this->translation, $translation);
+  }
+
+  /**
 	 * Get the properties of our class as strings
 	 *
 	 * @return array
 	 */
   function getProps() {
     $props = parent::getProps();
-    $props["originalText"] = "CCDAED max:1";
-    $props["qualifier"] = "CCDACR";
-    $props["translation"] = "CCDACD";
-    $props["code"] = "CCDA_cs";
-    $props["codeSystem"] = "CCDA_uid";
-    $props["codeSystemName"] = "CCDA_st";
-    $props["codeSystemVersion"] = "CCDA_st";
-    $props["displayName"] = "CCDA_st";
+    //$props["originalText"] = "CCDAED xml|element max|1";
+    //$props["qualifier"] = "CCDACR xml|element";
+    $props["translation"] = "CCDACD xml|element";
+    $props["code"] = "CCDA_cs xml|attribute";
+    $props["codeSystem"] = "CCDA_uid xml|attribute";
+    $props["codeSystemName"] = "CCDA_st xml|attribute";
+    $props["codeSystemVersion"] = "CCDA_st xml|attribute";
+    $props["displayName"] = "CCDA_st xml|attribute";
     return $props;
+  }
+
+  /**
+   * Fonction permettant de tester la classe
+   *
+   * @return array
+   */
+  function test() {
+    $tabTest = array();
+    /**
+     * Test avec les valeurs null
+     */
+    $tabTest[] = $this->sample("Test avec les valeurs null", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec code erroné
+     */
+    $codeTest = new CCDA_cs();
+    $codeTest->setData(" ");
+    $this->setCode($codeTest);
+
+    $tabTest[] = $this->sample("Test avec code erronée", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec code correct
+     */
+    $codeTest->setData("TEST");
+    $this->setCode($codeTest);
+
+    $tabTest[] = $this->sample("Test avec code correct", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystem incorrecte
+     */
+
+    $codeSystemTest = new CCDA_uid();
+    $codeSystemTest->setData("*");
+    $this->setCodeSystem($codeSystemTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystem incorrecte", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystem correct
+     */
+
+    $codeSystemTest->setData("HL7");
+    $this->setCodeSystem($codeSystemTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystem correct", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystemName incorrecte
+     */
+    $codeSystemNameTest = new CCDA_st();
+    $codeSystemNameTest->setData("");
+    $this->setCodeSystemName($codeSystemNameTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystemName incorrecte", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystemName correct
+     */
+
+
+    $codeSystemNameTest->setData("test");
+    $this->setCodeSystemName($codeSystemNameTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystemName correct", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystemVersion incorrecte
+     */
+    $codeSystemVersionTest = new CCDA_st();
+    $codeSystemVersionTest->setData("");
+    $this->setCodeSystemVersion($codeSystemVersionTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystemVersion incorrecte", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec codeSystemVersion correct
+     */
+
+    $codeSystemVersionTest->setData("test");
+    $this->setCodeSystemVersion($codeSystemVersionTest);
+
+    $tabTest[] = $this->sample("Test avec codeSystemVersion correct", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec displayName incorrecte
+     */
+
+    $displayNameTest = new CCDA_st();
+    $displayNameTest->setData("");
+    $this->setDisplayName($displayNameTest);
+
+    $tabTest[] = $this->sample("Test avec displayName incorrecte", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec displayName correct
+     */
+
+    $displayNameTest->setData("test");
+    $this->setDisplayName($displayNameTest);
+
+    $tabTest[] = $this->sample("Test avec displayName correct", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    if (get_class($this) !== "CCDACD") {
+      return $tabTest;
+    }
+
+    /**
+     * Test avec un translation correct sans valeur
+     */
+    $translation = new CCDACD();
+    $this->setTranslation($translation);
+
+    $tabTest[] = $this->sample("Test avec translation correct sans valeur", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec deux translation correct sans valeur
+     */
+    $translation2 = new CCDACD();
+    $this->setTranslation($translation2);
+
+    $tabTest[] = $this->sample("Test avec deux translation correct sans valeur", "Document valide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    /**
+     * Test avec un translation correct et une correcte valeur avec qualifier
+     */
+    /*
+    $translation->setDataTranslation("test");
+    $this->setTranslation($translation);
+
+    $tabTest[] = $this->sample("Test avec translation correct avec qualifier", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+    return $tabTest;
   }
 }
---
+++ modules/cda/classes/datatypes/base/CCDAthumbnail.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * $Id$
+ *  
+ * @category CDA
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * @link     http://www.mediboard.org */
+ 
+/**
+ * A thumbnail is an abbreviated rendition of the full
+ * data. A thumbnail requires significantly fewer
+ * resources than the full data, while still maintaining
+ * some distinctive similarity with the full data. A
+ * thumbnail is typically used with by-reference
+ * encapsulated data. It allows a user to select data
+ * more efficiently before actually downloading through
+ * the reference.
+ */
+class CCDAthumbnail extends CCDAED {
+
+  
+  /**
+	 * Get the properties of our class as strings
+	 *
+	 * @return array
+	 */
+  function getProps() {
+    $props = parent::getProps();
+    $props["thumbnail"] = "CCDAthumbnail xml|element prohibited";
+    return $props;
+  }
+
+  /**
+   * Fonction permettant de tester la classe
+   *
+   * @return array
+   */
+  function test() {
+    $tabTest = parent::test();
+
+    /**
+     * Test avec une valeur correcte
+     */
+
+    $thum = new CCDAthumbnail();
+    $integrityalgo = new CCDAintegrityCheckAlgorithm();
+    $integrityalgo->setData("SHA-256");
+    $thum->setIntegrityCheckAlgorithm($integrityalgo);
+
+    $this->setThumbnail($thum);
+    $tabTest[] = $this->sample("Test avec thumbnail correcte", "Document invalide");
+
+    /*-------------------------------------------------------------------------------------*/
+
+    return $tabTest;
+  }
+}
