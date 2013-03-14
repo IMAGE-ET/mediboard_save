d63869a7f8a5e17ef90454f27bb72f03ee87f210
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-03-14 14:08:19 +0000

    Bug: commit de la mauvaise suppression de fichier, commit de l'ancien, suppression du bon
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18417 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ modules/messagerie/ajax_open_external_email.php
@@ -0,0 +1,67 @@
+<?php /** $Id$ **/
+
+/**
+ * @package Mediboard
+ * @subpackage messagerie
+ * @version $Revision$
+ * @author SARL OpenXtrem
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ */
+
+CCanDo::checkRead();
+
+$user = CMediusers::get();
+$mail_id = CValue::get("mail_id");
+
+//pop init
+
+//mail
+$mail = new CUserMail();
+$mail->load($mail_id);
+$mail->loadRefsFwd();
+$mail->checkHprim();//HprimMedecin
+//$mail->checkApicrypt();//HprimMedecin
+
+//pop account
+$log_pop = new CSourcePOP();
+$log_pop->load($mail->account_id);
+
+//if not read email, send the seen flag to server
+if (!$mail->date_read) {
+  $pop = new CPop($log_pop);
+  $pop->open();
+  if ($pop->setflag($mail->uid, "\\Seen")) {
+    $mail->date_read = CMbDT::dateTime();
+    $mail->store();
+  }
+  $pop->close();
+}
+
+//get the CFile attachments
+$nbAttachPicked = 0;
+$nbAttach = count($mail->_attachments);
+foreach ($mail->_attachments as $_att) {
+  $_att->loadRefsFwd();
+  if ($_att->_file->_id) {
+    $nbAttachPicked++;
+  }
+}
+
+$mail->checkInlineAttachments();
+
+//hprim
+$headers = preg_split("/(\r\n|\n)/", $mail->_text_plain->content);
+$mail->_text_plain->content = implode("\n", array_splice($headers, 13));
+
+//apicrypt
+if (stripos($mail->_text_plain->content, "****FIN****") !== false) {
+  $mail->_is_apicrypt = 1;
+}
+
+//Smarty
+$smarty = new CSmartyDP();
+$smarty->assign("mail", $mail);
+$smarty->assign("nbAttachPicked", $nbAttachPicked);
+$smarty->assign("nbAttachAll",  $nbAttach);
+$smarty->assign("header", $headers);
+$smarty->display("vw_open_external_email.tpl");
