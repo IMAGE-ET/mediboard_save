e796594f2ec192644bac3bf5e6ed7bfc8709da59
Kévin Grisel <k.grisel@openxtrem.com>
2013-03-20 14:29:31 +0000

    Ref : utf8_decode() était fait après la protection des données.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18476 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/magic_quotes_gpc.php
+++ includes/magic_quotes_gpc.php
@@ -9,18 +9,18 @@
  * @version    $Id$
  */
 
-// Emulates magic quotes when disabled
-if (!get_magic_quotes_gpc()) {
-  $_GET     = array_map_recursive("addslashes", $_GET);
-  $_POST    = array_map_recursive("addslashes", $_POST);
-  $_COOKIE  = array_map_recursive("addslashes", $_COOKIE);
-  $_REQUEST = array_map_recursive("addslashes", $_REQUEST);
-}
-
 // UTF decode inputs from ajax requests
 if (isset($_REQUEST["ajax"]) && empty($_REQUEST["accept_utf8"])) {
   $_GET     = array_map_recursive("utf8_decode", $_GET);
   $_POST    = array_map_recursive("utf8_decode", $_POST);
   $_COOKIE  = array_map_recursive("utf8_decode", $_COOKIE);
   $_REQUEST = array_map_recursive("utf8_decode", $_REQUEST);
+}
+
+// Emulates magic quotes when disabled
+if (!get_magic_quotes_gpc()) {
+  $_GET     = array_map_recursive("addslashes", $_GET);
+  $_POST    = array_map_recursive("addslashes", $_POST);
+  $_COOKIE  = array_map_recursive("addslashes", $_COOKIE);
+  $_REQUEST = array_map_recursive("addslashes", $_REQUEST);
 }
