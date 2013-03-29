5821c54a3399f8566bb79c4031aaa39ba0836472
Yohann Poiron <poiron@openxtrem.com>
2013-03-29 16:04:44 +0000

    fnc: Ajout du segment ZBE pour l'international
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18613 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hl7/classes/events/ADT/CHL7v2EventADTA52.class.php
+++ modules/hl7/classes/events/ADT/CHL7v2EventADTA52.class.php
@@ -69,4 +69,21 @@ class CHL7v2EventADTA52 extends CHL7v2EventADT implements CHL7EventADTA52 {
     // Build specific segments (i18n)
     $this->buildI18nSegments($sejour);
   }
+
+  /**
+   * Build i18n segements
+   *
+   * @param CSejour $sejour Admit
+   *
+   * @see parent::buildI18nSegments()
+   *
+   * @return void
+   */
+  function buildI18nSegments($sejour) {
+
+    // Movement segment only used within the context of the "Historic Movement Management"
+    if ($this->_receiver->_configs["iti31_historic_movement"]) {
+      $this->addZBE($sejour);
+    }
+  }
 }
