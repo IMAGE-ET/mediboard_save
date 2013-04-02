d98903d98e74f864e269fe9caec84d75179b0e0b
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-04-02 14:24:49 +0000

    Ref: affichage de toutes les alertes si pas de bloc_id défini.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18637 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/templates/vw_alertes.tpl
+++ modules/dPbloc/templates/vw_alertes.tpl
@@ -1,34 +1,51 @@
 <table class="tbl">
-  <tr>
-    <th>Date</th>
-    <th>Chirurgien</th>
-    <th>Patient</th>
-    <th>Salle</th>
-    <th>Intervention</th>
-  </tr>
-  {{if $listAlertes|@count}}
-  <tr>
-    <th colspan="10">{{$listAlertes|@count}} alerte(s) sur des interventions</th>
-  </tr>
+  {{if $nbAlertes}}
+    <tr>
+      <th colspan="10" class="title">{{$nbAlertes}} alerte(s) sur des interventions</th>
+    </tr>
+    <tr>
+      <th>Date</th>
+      <th>Chirurgien</th>
+      <th>Patient</th>
+      <th>Salle</th>
+      <th>Intervention</th>
+    </tr>
+    {{foreach from=$blocs item=bloc}}
+      {{foreach from=$bloc->_alertes_intervs item=_alerte}}
+        {{assign var="_operation" value=$_alerte->_ref_object}}
+        {{mb_include module=bloc template=inc_line_alerte is_alerte=1}}
+      {{/foreach}}
+    {{/foreach}}
   {{/if}}
-  {{foreach from=$listAlertes item=_alerte}}
-  {{assign var="_operation" value=$_alerte->_ref_object}}
-    {{mb_include module=bloc template=inc_line_alerte is_alerte=1}}
-  {{/foreach}}
+
   {{if $listNonValidees|@count}}
-  <tr>
-    <th colspan="10">{{$listNonValidees|@count}} intervention(s) non validées</th>
-  </tr>
+    <tr>
+      <th>Date</th>
+      <th>Chirurgien</th>
+      <th>Patient</th>
+      <th>Salle</th>
+      <th>Intervention</th>
+    </tr>
+    <tr>
+      <th colspan="10" class="title">{{$listNonValidees|@count}} intervention(s) non validées</th>
+    </tr>
+    {{foreach from=$listNonValidees item=_operation}}
+      {{mb_include module=bloc template=inc_line_alerte is_alerte=0}}
+    {{/foreach}}
   {{/if}}
-  {{foreach from=$listNonValidees item=_operation}}
-    {{mb_include module=bloc template=inc_line_alerte is_alerte=0}}
-  {{/foreach}}
   {{if $listHorsPlage|@count}}
-  <tr>
-    <th colspan="10">{{$listHorsPlage|@count}} intervention(s) hors plage</th>
-  </tr>
+    <tr>
+      <th colspan="10" class="title">{{$listHorsPlage|@count}} intervention(s) hors plage</th>
+    </tr>
+    <tr>
+      <th>Date</th>
+      <th>Chirurgien</th>
+      <th>Patient</th>
+      <th>Salle</th>
+      <th>Intervention</th>
+    </tr>
+    {{foreach from=$listHorsPlage item=_operation}}
+      {{mb_include module=bloc template=inc_line_alerte is_alerte=0}}
+    {{/foreach}}
   {{/if}}
-  {{foreach from=$listHorsPlage item=_operation}}
-    {{mb_include module=bloc template=inc_line_alerte is_alerte=0}}
-  {{/foreach}}
 </table>
--- modules/dPbloc/vw_alertes.php
+++ modules/dPbloc/vw_alertes.php
@@ -1,4 +1,4 @@
-<?php /* $Id: $ */
+<?php /** $Id: $ **/
 
 /**
  * @package Mediboard
@@ -16,41 +16,54 @@ if ($type == "week") {
   $date = CMbDT::date("last sunday", $date);
   $fin  = CMbDT::date("next sunday", $date);
   $date = CMbDT::date("+1 day", $date);
-} else {
+}
+else {
   $fin = $date;
 }
 
+
+//alerts
+$nbAlertes = 0;
 $bloc = new CBlocOperatoire();
-$bloc->load($bloc_id);
-$bloc->loadRefsSalles();
-$bloc->loadRefsAlertesIntervs();
-foreach($bloc->_alertes_intervs as &$_alerte) {
-  $_alerte->loadTargetObject();
-  $_alerte->_ref_object->loadExtCodesCCAM();
-  $_alerte->_ref_object->loadRefPlageOp();
-  $_alerte->_ref_object->loadRefPraticien();
-  $_alerte->_ref_object->loadRefPatient();
-  $_alerte->_ref_object->updateSalle();
-  $_alerte->_ref_object->_ref_chir->loadRefFunction();
+$where = array();
+if ($bloc_id) {
+  $where["bloc_operatoire_id"] = "= '$bloc_id'";
+}
+$listBlocs = $bloc->loadGroupList($where);
+
+foreach ($listBlocs as $_bloc) {
+  $_bloc->loadRefsSalles();
+  $_bloc->loadRefsAlertesIntervs();
+
+  foreach ($_bloc->_alertes_intervs  as &$_alerte) {
+    $nbAlertes++;
+    $_alerte->loadTargetObject();
+    $_alerte->_ref_object->loadExtCodesCCAM();
+    $_alerte->_ref_object->loadRefPlageOp();
+    $_alerte->_ref_object->loadRefPraticien();
+    $_alerte->_ref_object->loadRefPatient();
+    $_alerte->_ref_object->updateSalle();
+    $_alerte->_ref_object->_ref_chir->loadRefFunction();
+  }
 }
 
 $operation = new COperation();
-
-// Liste des alertes sur les interventions
 
 // Liste des interventions non validées
 $ljoin = array();
 $ljoin["plagesop"] = "operations.plageop_id = plagesop.plageop_id";
 $where = array();
 $where["plagesop.date"]      = "BETWEEN '$date' AND '$fin'";
-$where["plagesop.salle_id"]  = CSQLDataSource::prepareIn(array_keys($bloc->_ref_salles));
+if ($bloc->_id) {
+  $where["plagesop.salle_id"]  = CSQLDataSource::prepareIn(array_keys($bloc->_ref_salles));
+}
 $where["operations.annulee"] = "= '0'";
 $where["operations.rank"]    = "= '0'";
 $order = "plagesop.date, plagesop.chir_id";
 
 $listNonValidees = $operation->loadList($where, $order, null, null, $ljoin);
 
-foreach($listNonValidees as &$op) {
+foreach ($listNonValidees as &$op) {
   $op->loadExtCodesCCAM();
   $op->loadRefPlageOp();
   $op->loadRefPraticien();
@@ -65,13 +78,15 @@ $ljoin["sejour"] = "sejour.sejour_id = operations.sejour_id";
 $where = array();
 $where["operations.date"]    = "BETWEEN '$date' AND '$fin'";
 $where["operations.annulee"] = "= '0'";
-$where[]                     = "operations.salle_id IS NULL OR operations.salle_id ". CSQLDataSource::prepareIn(array_keys($bloc->_ref_salles));
+if ($bloc->_id) {
+  $where[]                   = "operations.salle_id IS NULL OR operations.salle_id ". CSQLDataSource::prepareIn(array_keys($bloc->_ref_salles));
+}
 $where["sejour.group_id"]    = "= '".CGroups::loadCurrent()->_id."'";
 $order = "operations.date, operations.chir_id";
 
 $listHorsPlage = $operation->loadList($where, $order, null, null, $ljoin);
 
-foreach($listHorsPlage as &$op) {
+foreach ($listHorsPlage as &$op) {
   $op->loadRefPlageOp();
   $op->loadExtCodesCCAM();
   $op->loadRefPraticien();
@@ -83,10 +98,9 @@ foreach($listHorsPlage as &$op) {
 // Création du template
 $smarty = new CSmartyDP();
 
-$smarty->assign("listAlertes"    , $bloc->_alertes_intervs);
+$smarty->assign("blocs"    ,       $listBlocs);
+$smarty->assign("nbAlertes"    ,       $nbAlertes);
 $smarty->assign("listNonValidees", $listNonValidees       );
 $smarty->assign("listHorsPlage"  , $listHorsPlage         );
 
 $smarty->display("vw_alertes.tpl");
-
-?>
