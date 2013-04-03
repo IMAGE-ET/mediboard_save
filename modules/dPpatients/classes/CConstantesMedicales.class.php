100894036fc9a11eeb9c5b90c0bd50eb9b5336d9
Fabien Ménager <fabien.menager@openxtrem.com>
2013-04-03 10:36:51 +0000

    bug: variable non definie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18652 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpatients/classes/CConstantesMedicales.class.php
+++ modules/dPpatients/classes/CConstantesMedicales.class.php
@@ -1305,7 +1305,7 @@ class CConstantesMedicales extends CMbObject {
     // Séjour d'urgence
     if ($context instanceof CSejour && $context->type = "urg") {
       $rpu = $context->loadRefRPU();
-      if ($rpu->_id) {
+      if ($rpu && $rpu->_id) {
         $context = $rpu;
       }
     }
