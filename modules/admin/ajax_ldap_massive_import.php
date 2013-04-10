9dd5f690c34f6246ec91e592f4edd705a1845bdb
Yohann Poiron <poiron@openxtrem.com>
2013-04-10 15:34:52 +0000

    ref: Affichage des comptes non associés
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@18752 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/admin/ajax_ldap_massive_import.php
+++ modules/admin/ajax_ldap_massive_import.php
@@ -17,21 +17,26 @@ $count     = CValue::get("count", 5);
 $user = new CUser();
 
 // Requêtes
-$ljoin["id_sante400"] = "`id_sante400`.`object_id` = `users`.`user_id`";
+$ljoin["id_sante400"]     = "`id_sante400`.`object_id` = `users`.`user_id`";
+$ljoin["users_mediboard"] = "`users`.`user_id` = `users_mediboard`.`user_id`";
     
 $where = array();
 $where["id_sante400.object_class"] = "= 'CUser'";
 $where["id_sante400.tag"]          = "= '".CAppUI::conf("admin LDAP ldap_tag")."'";
 $where["id_sante400.id400"]        = "IS NOT NULL";
 $where["users.template"]           = "= '0'";
+$where["users_mediboard.actif"]    = "= '1'";
 
 if (!$do_import) {
   $count_users_ldap = $user->countList($where, null, $ljoin);
   $where = array();
-  $where["users.template"] = "= '0'";
+  $where["users.template"]        = "= '0'";
+  $where["users_mediboard.actif"]  = "= '1'";
+
   $count_users_all  = $user->countList($where);
   CAppUI::stepAjax(($count_users_all - $count_users_ldap)." comptes qui ne sont pas associés");
-} else {
+}
+else {
   $ldaprdn  = CAppUI::conf("admin LDAP ldap_user");
   $ldappass = CAppUI::conf("admin LDAP ldap_password");
 
@@ -41,20 +46,26 @@ if (!$do_import) {
   $where["users.template"] = "= '0'";
   $users_all = $user->loadList($where, null);
 
+  /** @var $users CUser[] */
   $users = array_diff_key($users_all, $users_ldap);
   $users = array_slice($users, $start, $count);
   
   $count = $count_no_associate = $count_associate =  0;
-  foreach($users as $_user) { 
+  foreach ($users as $_user) {
     try {
       $source_ldap = CLDAP::bind($_user, $ldaprdn, $ldappass);
       $_user = CLDAP::searchAndMap($_user, $source_ldap, $source_ldap->_ldapconn, $_user->user_username, null);
-    } catch(CMbException $e) {}
+    }
+    catch(CMbException $e) {
+      $e->stepAjax();
+    }
     
     if ($_user->_count_ldap != 0) {
       $count_associate++;
     }
+
     if ($_user->_count_ldap == 0) {
+      CAppUI::stepAjax("$user->_view non associé", UI_MSG_WARNING);
       $count_no_associate++;
     }
     
@@ -72,5 +83,3 @@ if (!$do_import) {
   CValue::setSession("start", $next);
   CAppUI::stepAjax("On continuera au n° $next / ".count($users)." restants");
 }
-
-?>
