96c72dfe6f7571cf11358ee1ab272a2cba6ccd79
Aurélie Hébras <aureliehebras@gmail.com>
2013-05-29 09:08:37 +0000

    Erg: Ajout de la couleur verte si la facture est acquitée
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19341 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/reservation/ajax_vw_planning.php
+++ modules/reservation/ajax_vw_planning.php
@@ -324,6 +324,7 @@ if ($show_operations) {
         $facture = $sejour->_ref_last_facture;
         if ($facture->_id) {
           $couleur = $facture->cloture ? "blue" : "#FF0";
+          $couleur = $facture->patient_date_reglement ? "green" : $couleur;
           $action_fact = "Facture.edit($facture->_id, '$facture->_class')";
           $libelle.= "<button class=\"calcul notext\" onclick=\"$action_fact\" style=\"border: $couleur 1px solid;\">Facture</button>";
         }
