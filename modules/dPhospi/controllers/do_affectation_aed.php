c343d04f7b051a2777a5940ac134939bbad7e685
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-06-03 13:49:52 +0000

    Ref: nettoyage du setup
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19394 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPhospi/controllers/do_affectation_aed.php
+++ modules/dPhospi/controllers/do_affectation_aed.php
@@ -1,4 +1,4 @@
-<?php /* $Id$ */
+<?php /** $Id$ **/
 
 /**
 * @package Mediboard
@@ -13,51 +13,52 @@ $entree     = CValue::post("entree");
 $sortie     = CValue::post("sortie");
 $function_id = CValue::post("function_id");
 
-if($_lock_all_lits){
+if ($_lock_all_lits) {
+  /** @var CLit $lit */
   $lit = new CLit();
   $lit = $lit->load($lit_id);
   $lit->loadRefChambre()->loadRefService()->loadRefsChambres();
   
-  foreach($lit->_ref_chambre->_ref_service->_ref_chambres as $chambre){
+  foreach ($lit->_ref_chambre->_ref_service->_ref_chambres as $chambre) {
     $chambre->loadRefsLits();
-    foreach($chambre->_ref_lits as $lit){
+    foreach ($chambre->_ref_lits as $lit) {
       $affectation = new CAffectation();
       $affectation->lit_id = $lit->_id;
       $affectation->entree = $entree;
       $affectation->sortie = $sortie;
-	    if ($msg = $affectation->store()) {
-	      CAppUI::setMsg($msg, UI_MSG_ERROR);
-	    }
+      if ($msg = $affectation->store()) {
+        CAppUI::setMsg($msg, UI_MSG_ERROR);
+      }
     }
   }
 
-	echo CAppUI::getMsg();
-	CApp::rip();
+  echo CAppUI::getMsg();
+  CApp::rip();
 }
-elseif($_lock_all_lits_urgences){	
+elseif ($_lock_all_lits_urgences) {
+  /** @var CLit $lit */
   $lit = new CLit();
   $lit = $lit->load($lit_id);
   $lit->loadRefChambre()->loadRefService()->loadRefsChambres();
-  
-  foreach($lit->_ref_chambre->_ref_service->_ref_chambres as $chambre){
+
+  foreach ($lit->_ref_chambre->_ref_service->_ref_chambres as $chambre) {
     $chambre->loadRefsLits();
-    foreach($chambre->_ref_lits as $lit){
+    foreach ($chambre->_ref_lits as $lit) {
       $affectation = new CAffectation();
       $affectation->lit_id = $lit->_id;
       $affectation->entree = $entree;
       $affectation->sortie = $sortie;
       $affectation->function_id = $function_id;
-	    if ($msg = $affectation->store()) {
-	      CAppUI::setMsg($msg, UI_MSG_ERROR);
-	    }
+      if ($msg = $affectation->store()) {
+        CAppUI::setMsg($msg, UI_MSG_ERROR);
+      }
     }
   }
 
-	echo CAppUI::getMsg();
-	CApp::rip();
+  echo CAppUI::getMsg();
+  CApp::rip();
 }
-else{
-	$do = new CDoObjectAddEdit("CAffectation", "affectation_id");
-	$do->doIt();
+else {
+  $do = new CDoObjectAddEdit("CAffectation", "affectation_id");
+  $do->doIt();
 }
-?>
