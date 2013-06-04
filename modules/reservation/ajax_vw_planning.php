bb42abede6882ac903b58779d46bc2e59b43e50e
Charles-Etienne Masse <c.masse@openxtrem.com>
2013-06-04 09:01:56 +0000

    Ref: passage du mode de traitement pour utilisation
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19407 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/reservation/ajax_vw_planning.php
+++ modules/reservation/ajax_vw_planning.php
@@ -302,7 +302,7 @@ if ($show_operations) {
       $libelle = "<span style='display: none;' data-entree_prevue='$sejour->entree_prevue'".
         "data-sortie_prevue='$sejour->sortie_prevue' data-sejour_id='$sejour->_id' data-preop='".
         ($_operation->presence_preop ? CMbDT::transform($_operation->presence_preop, null, "%H:%M") : "00:00")."' data-postop='".
-        ($_operation->presence_postop ? CMbDT::transform($_operation->presence_postop, null, "%H:%M") : "00:00")."'></span>";
+        ($_operation->presence_postop ? CMbDT::transform($_operation->presence_postop, null, "%H:%M") : "00:00")."' data-traitement='".$charge->_id."'></span>";
 
       /** CADRE DROIT */
       $libelle .="<span style=\"float:right; text-align: right\">";
