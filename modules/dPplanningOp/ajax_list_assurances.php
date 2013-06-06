880602e413a9d62e119166b318ac70ca6a26cedc
Aurélie Hébras <aureliehebras@gmail.com>
2013-06-06 08:38:45 +0000

    Ref: Un log était produit par non présence d'une variable
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19442 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/ajax_list_assurances.php
+++ modules/dPplanningOp/ajax_list_assurances.php
@@ -6,7 +6,7 @@
  * @subpackage dPplanningOp
  * @author     SARL OpenXtrem <dev@openxtrem.com>
  * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version    $Revision: 17451 $
+ * @version    $Revision$
  */
 
 CCanDo::checkRead();
@@ -20,13 +20,12 @@ $patient->load($patient_id);
 $patient->loadRefsCorrespondantsPatient();
 
 // On récupére le séjour
-$sejour = new CSejour;
+$sejour = new CSejour();
 if ($sejour_id) {
   $sejour->load($sejour_id);
-  $sejour->loadRefs();
   
   // On vérifie que l'utilisateur a les droits sur le sejour
-  if (!$sejour->canRead()) {
+  if (!$sejour->_canRead) {
     global $m, $tab;
     CAppUI::setMsg("Vous n'avez pas accés à ce séjour", UI_MSG_WARNING);
     CAppUI::redirect("m=$m&tab=$tab&sejour_id=0");
@@ -41,8 +40,8 @@ else {
 // Création du template
 $smarty = new CSmartyDP();
 
-$smarty->assign("sejour"        , $sejour);
-$smarty->assign("patient"       , $patient);
+$smarty->assign("sejour"  , $sejour);
+$smarty->assign("patient" , $patient);
+$smarty->assign("form"    , "editSejour");
 
 $smarty->display("inc_vw_assurances.tpl");
-?>
