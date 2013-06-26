ef39d182f15679093f8a72d7b1c59051d8811275
Yohann Poiron <poiron@openxtrem.com>
2013-06-26 14:52:34 +0000

    bug: Problème sur la récupération du téléphone
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19678 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hl7/classes/CHL7v2RecordPerson.class.php
+++ modules/hl7/classes/CHL7v2RecordPerson.class.php
@@ -721,7 +721,9 @@ class CHL7v2RecordPerson extends CHL7v2MessageXML {
     $ville   = $this->queryTextNode("XAD.3", $NK1_4); 
 
     $NK1_5 = $this->queryNodes("NK1.5", $node)->item(0);
-    $tel   = $this->queryTextNode("XTN.1", $NK1_5);
+    if ($NK1_5) {
+      $tel   = $this->queryTextNode("XTN.1", $NK1_5);
+    }
     
     $relation = $this->queryTextNode("NK1.7/CE.1", $node);
     $relation_autre = null;
