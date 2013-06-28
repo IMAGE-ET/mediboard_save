8405d06fbe129c5d9f3e3241ba5a92dfb07cf01e
Romain Ollivier <ollivier@openxtrem.com>
2013-06-28 07:08:17 +0000

    bug: calcul des dates de la prise de rendez-vous avec php < 5.3
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19703 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/ajax_list_plages.php
+++ modules/dPcabinet/ajax_list_plages.php
@@ -94,8 +94,14 @@ switch ($period) {
     $maxDate = CMbDT::transform("+1 month", $minDate, "%Y-%m-01");
     $maxDate = CMbDT::date("-1 day", $maxDate);
     $refDate = $minDate;
-    $ndate = CMbDT::date("first day of next month"   , $date);
-    $pdate = CMbDT::date("last day of previous month", $date);
+    if (phpversion() >= "5.3") {
+      $ndate = CMbDT::date("first day of next month"   , $date);
+      $pdate = CMbDT::date("last day of previous month", $date);
+    }
+    else {
+      $ndate = CMbDT::date("+1 month"   , CMbDT::format($date, "%Y-%m-01" ));
+      $pdate = CMbDT::date("-1 month"   , CMbDT::format($date, "%Y-%m-01" ));
+    }
     break;
 
   default:
