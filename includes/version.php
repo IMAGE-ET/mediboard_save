069d2343e01c410db0ba3c683609a7877398fc32
Alexis Granger <granger@openxtrem.com>
2013-07-02 08:26:06 +0000

    bug: suppression des produits du livret en mode BCB
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19736 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 172,
+  "build" => 173,
 );
 
 $version["string"] = implode(".", $version);
