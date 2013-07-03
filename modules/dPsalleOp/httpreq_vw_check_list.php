3bd1e69e1fea9d5fb598bd81c0bad7ba6123d40c
Aurélie Hébras <aureliehebras@gmail.com>
2013-07-03 07:32:20 +0000

    Bug: La variable $anesth n'était pas définie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19751 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPsalleOp/httpreq_vw_check_list.php
+++ modules/dPsalleOp/httpreq_vw_check_list.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Id$
+ * $Id:$
  *
  * @package    Mediboard
  * @subpackage SalleOp
  * @author     SARL OpenXtrem <dev@openxtrem.com>
  * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version    $Revision$
+ * @version    $Revision:$
  */
 
 CCanDo::checkRead();
@@ -25,6 +25,8 @@ if ($check_list->object_class == "COperation") {
   $check_list->_ref_object->loadRefChir();
   $anesth_id = $check_list->_ref_object->anesth_id;
 }
+$anesth = new CMediusers();
+$anesth->load($anesth_id);
 
 $personnel = CPersonnel::loadListPers(array("op", "op_panseuse"), true, true);
 
@@ -38,5 +40,6 @@ $smarty = new CSmartyDP();
 $smarty->assign("check_list"           , $check_list);
 $smarty->assign("personnel"            , $personnel);
 $smarty->assign("anesth_id"            , $anesth_id);
+$smarty->assign("anesth"               , $anesth);
 $smarty->assign("check_item_categories", $check_item_categories);
 $smarty->display("inc_edit_check_list.tpl");
