aa4f9c86d8f49326de07b17aa98bf561297c9db2
Alexis Granger <granger@openxtrem.com>
2013-07-11 15:57:37 +0000

    fnc: permettre de masquer le bouton de fermeture d'une ligne de prescription si on peut la prescrire bug: le bouton de signature des perfusions n'etait plus disponible dans le cas d'une posologie à "1 seule fois"
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19880 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 174,
+  "build" => 175,
 );
 
 $version["string"] = implode(".", $version);
