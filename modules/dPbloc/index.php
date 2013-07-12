db294a131f157dfb0128bde7cbb49d3052cddf41
Romain Ollivier <ollivier@openxtrem.com>
2013-07-12 14:26:21 +0000

    erg: suppression de l'onglet de gestion de l'ordre de passage des interventions
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19900 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPbloc/index.php
+++ modules/dPbloc/index.php
@@ -7,27 +7,27 @@
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
  * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version  SVN: $Id:$
+ * @version  SVN: $Id$
  * @link     http://www.mediboard.org
  */
 
 $module = CModule::getInstalled(basename(dirname(__FILE__)));
 
-$module->registerTab("vw_edit_planning"     , TAB_READ);
-$module->registerTab("vw_edit_interventions", TAB_EDIT);
-$module->registerTab("vw_suivi_salles"      , TAB_EDIT);
+$module->registerTab("vw_edit_planning"      , TAB_READ);
+//$module->registerTab("vw_edit_interventions" , TAB_EDIT);
+$module->registerTab("vw_suivi_salles"       , TAB_EDIT);
 if (CModule::getActive("reservation")) {
-  $module->registerTab("vw_planning"        , TAB_READ);
+  $module->registerTab("vw_planning"         , TAB_READ);
 }
-$module->registerTab("vw_urgences"          , TAB_EDIT);
-$module->registerTab("vw_departs_us"        , TAB_EDIT);
+$module->registerTab("vw_urgences"           , TAB_EDIT);
+$module->registerTab("vw_departs_us"         , TAB_EDIT);
 if (CAppUI::conf("dPbloc CPlageOp systeme_materiel") == "standard") {
-  $module->registerTab("vw_idx_materiel"      , TAB_EDIT);
+  $module->registerTab("vw_idx_materiel"     , TAB_EDIT);
 }
 else {
-  $module->registerTab("vw_ressources"        , TAB_EDIT);
+  $module->registerTab("vw_ressources"       , TAB_EDIT);
 }
-$module->registerTab("vw_blocages"          , TAB_EDIT);
+$module->registerTab("vw_blocages"           , TAB_EDIT);
 
-$module->registerTab("vw_idx_blocs"         , TAB_ADMIN);
-$module->registerTab("print_planning"       , TAB_READ);
+$module->registerTab("vw_idx_blocs"          , TAB_ADMIN);
+$module->registerTab("print_planning"        , TAB_READ);
