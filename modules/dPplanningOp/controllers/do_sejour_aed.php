c6a7d2e112d0a7a4ad446963d99e056c8bac8290
Yohann Poiron <poiron@openxtrem.com>
2013-07-16 15:23:49 +0000

    ref: Prise en charge du service de sortie si on a pas de lit
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19938 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/controllers/do_sejour_aed.php
+++ modules/dPplanningOp/controllers/do_sejour_aed.php
@@ -12,19 +12,22 @@
 if ($praticien_id = CValue::post("praticien_id")) {
   CValue::setSession("praticien_id", $praticien_id);
 }
+$lit_id            = CValue::post("lit_id");
+$service_sortie_id = CValue::post("service_sortie_id");
 
 //Pour un séjour ayant comme mode de sortie urgence:
-if (CValue::post("mode_sortie") == "mutation" && CValue::post("type") == "urg" && CValue::post("lit_id")) {
+if (CValue::post("mode_sortie") == "mutation" && CValue::post("type") == "urg" && ($lit_id || $service_sortie_id)) {
   $sejour_id = CValue::post("sejour_id");
-  $lit_id = CValue::post("lit_id");
+
   $sejour = new CSejour();
   $sejour->load($sejour_id);
 
   //Création de l'affectation du patient
   $affectation = new CAffectation();
-  $affectation->entree = CMbDT::dateTime();
-  $affectation->lit_id = $lit_id;
-  $affectation->sejour_id = $sejour_id;
+  $affectation->entree     = CMbDT::dateTime();
+  $affectation->lit_id     = $lit_id;
+  $affectation->service_id = $service_sortie_id;
+  $affectation->sejour_id  = $sejour_id;
   if ($sejour->loadRefRPU()->mutation_sejour_id) {
     $affectation->sejour_id = $sejour->_ref_rpu->mutation_sejour_id;
   }
