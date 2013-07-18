394cef4492b435dfc0b1c3c9949f64d65caab731
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-07-18 16:49:42 +0000

    Erg: Nouveau paramètre pour le script update.sh (révision des modules externes).
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19971 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/update.sh
+++ shell/update.sh
@@ -12,11 +12,12 @@ announce_script "Mediboard SVN updater"
 
 if [ "$#" -lt 1 ]
 then 
-  echo "Usage: $0 <action> [-r <revision>]"
+  echo "Usage: $0 <action> [-r <revision> -e <revision externals>]"
   echo "  <action> The action to perform : info|real"
   echo "     info : Shows the update log"
   echo "     real : Performs the actual update"
   echo "  -r <revision> The revision number you want to update to, HEAD by default"
+  echo "  -e <revision external> The external revision number, same behavior than the above option"
   exit 1
 fi
    
@@ -28,10 +29,17 @@ status=$MB_PATH/tmp/svnstatus.txt
 event=$MB_PATH/tmp/monitevent.txt
 prefixes="erg|fnc|fct|bug|war|edi|sys|svn"
 revision=HEAD
+revision_external=HEAD
+
+# First external
+externals=$(svn propget svn:externals modules)
+first_external=$(echo $externals|head -n 1|cut -d' ' -f 1)
+all_externals=$(echo $externals|awk '{ if ($1 != "") { print "modules/"$1 } }'|tr '\n' ' ')
+path_external=$MB_PATH/modules/$first_external
 
 # Choose the target revision
 
-args=`getopt r: $*`
+args=$(getopt r:e: $*)
 
 if [ $? != 0 ] ; then
   echo "Invalid argument. Check your command line"; exit 0;
@@ -42,6 +50,7 @@ set -- $args
 for i; do
   case "$i" in
     -r) revision=$2; shift 2;;
+    -e) revision_external=$2; shift 2;;
     --) shift ; break ;;
     -*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
   esac
@@ -61,20 +70,39 @@ case "$1" in
     echo >> $tmp
 
     # Concat SVN Log from BASE to target revision
+    # Add the external
+    if [ "$first_external" != "" ]
+    then
+      svn info $path_external | awk 'NR==5' >> $tmp
+    fi
+    
     svn log $MB_PATH -r BASE:$revision > $dif
     check_errs $? "Failed to retrieve SVN log" "SVN log retrieved!"
     
     grep -i -E "(${prefixes})" $dif >> $tmp
     echo "SVN log parsed!"
-    # Don't check beacause grep returns 1 if no occurence found
+    # Don't check because grep returns 1 if no occurence found
     rm -f $dif
     
     # Perform actual update
-    svn update $MB_PATH --revision $revision
+    # If externals, update only the trunk without them and then update the externals
+    if [ "$revision_external" != "HEAD" ] && [ "$first_external" != "" ]
+    then
+      svn update --ignore-externals --revision $revision
+      svn update $all_externals --revision $revision_external
+    else
+      svn update $MB_PATH --revision $revision
+    fi
     check_errs $? "Failed to perform SVN update" "SVN updated performed!"
 
     # Concat the target revision number
     echo >> $tmp
+    # Add the external revision
+    if [ "$first_external" != "" ]
+    then
+      svn info $path_external | awk 'NR==5' >> $tmp
+      check_errs $? "Failed to get target external revision info" "SVN External Revision target info written!"
+    fi
     svn info $MB_PATH | awk 'NR==5' >> $tmp
     check_errs $? "Failed to get target revision info" "SVN Revision target info written!"
 
@@ -107,11 +135,11 @@ case "$1" in
     
     # Write event file
     echo "#$(date +%Y-%m-%dT%H:%M:%S)" >> $event
-    echo "Mise a jour." `svn info $MB_PATH | awk 'NR==5'` >> $event
+    echo "Mise a jour." $(svn info $MB_PATH | awk 'NR==5') >> $event
     ;;
 
   *)
-      check_errs 2 "Action '$1' unknown"
+    check_errs 2 "Action '$1' unknown"
     exit 1
     ;;
 esac
