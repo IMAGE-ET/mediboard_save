522ecfdffc8bb20653f49b16d0131c97a0322017
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-07-19 07:47:33 +0000

    Bug, Problème d'impression d'éléments avec opacité sous firefox.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@19972 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 176,
+  "build" => 177,
 );
 
 $version["string"] = implode(".", $version);
