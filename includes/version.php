c06adbebaa48b7e48a34730d8326db041423b71c
Yohann Poiron <poiron@openxtrem.com>
2013-08-09 14:11:07 +0000

    bug: Problème de réordonnancement des clés d'un tableau sous Chrome
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@20140 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 179,
+  "build" => 180,
 );
 
 $version["string"] = implode(".", $version);
