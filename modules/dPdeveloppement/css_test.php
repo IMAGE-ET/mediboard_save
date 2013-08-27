92b9f5e19228300d1726dce215916d1e5e1fe683
Fabien Ménager <fabien.menager@openxtrem.com>
2013-08-27 14:48:51 +0000

    ref: prise en compte des CSS des modules dans le testeur de CSS (pour les types de boutons)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@20245 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPdeveloppement/css_test.php
+++ modules/dPdeveloppement/css_test.php
@@ -9,16 +9,25 @@
 
 CCanDo::checkRead();
 
-$forms_css = file_get_contents("style/mediboard/forms.css");
+$files = array(
+  "style/mediboard/forms.css",
+);
 
-preg_match_all("/a\.button\.([^\:]+)\:before/", $forms_css, $matches);
+$files = array_merge($files, glob("modules/*/css/main.css"));
+$button_classes = array();
 
-$button_classes = $matches[1];
+foreach ($files as $_file) {
+  $forms_css = file_get_contents($_file);
+
+  $matches = array();
+  preg_match_all("/a\.button\.([^\:]+)\:before/", $forms_css, $matches);
+
+  $button_classes = array_merge($button_classes, $matches[1]);
+}
+
 CMbArray::removeValue("rtl", $button_classes);
 
 // Création du template
 $smarty = new CSmartyDP();
 $smarty->assign('button_classes', array_values($button_classes));
 $smarty->display('css_test.tpl');
-
-?>
