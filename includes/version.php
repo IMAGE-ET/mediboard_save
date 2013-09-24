84a1fb30466808b64a27dabe7e9e86bb0ac1d6e0
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-09-24 09:39:14 +0000

    Bug: Problème d'ajout de posologie pour une ligne d'élément.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@20450 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 184,
+  "build" => 185,
 );
 
 $version["string"] = implode(".", $version);
