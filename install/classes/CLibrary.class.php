bbae51991aaeaca39f2feef06da55de54551bdea
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-10-14 12:47:16 +0000

    Ref: Mise à jour de CKEditor.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@20631 a21d6be2-e512-0410-a060-91f5a2946631

--- install/classes/CLibrary.class.php
+++ install/classes/CLibrary.class.php
@@ -283,7 +283,7 @@ $library->description = "Composant Javascript d'édition de texte au format HTML"
 $library->sourceDir = "ckeditor";
 $library->targetDir = "ckeditor";
 $library->versionFile = "ckeditor.js";
-$library->versionString = "4.2.1b";
+$library->versionString = "new Date()";
 
 CLibrary::$all[$library->name] = $library;
 
