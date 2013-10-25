30fc165da1b5c1a5907f5eb36f759b86a487c137
Romain Ollivier <ollivier@openxtrem.com>
2013-10-25 15:22:34 +0000

    trad: traductions manquantes dans les soins
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@20778 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/locales/fr.php
+++ modules/soins/locales/fr.php
@@ -152,6 +152,7 @@ $locales['mod-soins-tab-vw_ressources_soins'] = 'Charge en soins';
 $locales['mod-soins-tab-vw_sejours'] = 'Consulter un séjour';
 $locales['mod-soins-tab-vw_stocks_service'] = 'Commande / Réception';
 $locales['module-Brancardage-long'] = 'Brancarder';
+$locales['module-soins-court'] = 'Dossier de soins';
 $locales['module-soins-long'] = 'Dossier de soins';
 $locales['offline_sejour'] = 'Procédure dégradée - Séjours';
 $locales['pref-vue_sejours'] = 'Affichage des séjours';
