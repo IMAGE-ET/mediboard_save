22ec46ecce51ce25660b517a1ba929f96e8e01bb
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-12-10 10:17:58 +0000

    Ref: Dans l'impression de l'ordonnance, diminution de l'espace entre la partie ald et non ald.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21341 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 214,
+  "build" => 215,
 );
 
 $version["string"] = implode(".", $version);
