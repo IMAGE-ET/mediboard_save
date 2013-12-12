0ffad79b845d3800295320586007488567a05812
Flavien Crochard <flavien.crochard@openxtrem.com>
2013-12-12 15:38:08 +0000

    Bug: Problème de saut de page à l'impression lors de l'utilisation de la balise h1.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21389 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 216,
+  "build" => 217,
 );
 
 $version["string"] = implode(".", $version);
