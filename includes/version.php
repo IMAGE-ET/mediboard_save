4a674c845224f7f5182609b39a0312e7c9ac1a4b
Kévin Grisel <k.grisel@openxtrem.com>
2013-12-13 10:33:29 +0000

    Ref : Mise à jour de la version.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21396 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 218,
+  "build" => 219,
 );
 
 $version["string"] = implode(".", $version);
