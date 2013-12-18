57724edd5c638c1edb24f1010b8256822b8c8c8d
Alexis Granger <granger@openxtrem.com>
2013-12-18 09:56:18 +0000

    bug: récupération des moments unitaires dans le cas des traitements perso
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21452 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 222,
+  "build" => 223,
 );
 
 $version["string"] = implode(".", $version);
