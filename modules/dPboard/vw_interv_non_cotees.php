bb328af1da0562f693340e8f5a48329290b67759
Aurélie Hébras <aureliehebras@gmail.com>
2014-01-13 08:34:28 +0000

    Ref: Revue de code
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21632 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPboard/vw_interv_non_cotees.php
+++ modules/dPboard/vw_interv_non_cotees.php
@@ -1,18 +1,15 @@
 <?php
-
 /**
- * dPboard
+ * $Id:$
  *
- * @category Board
- * @package  Mediboard
- * @author   SARL OpenXtrem <dev@openxtrem.com>
- * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version  SVN: $Id:$
- * @link     http://www.mediboard.org
+ * @package    Mediboard
+ * @subpackage Board
+ * @author     SARL OpenXtrem <dev@openxtrem.com>
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @version    $Revision:$
  */
 
 CCanDo::checkRead();
-
 $chir = null;
 $mediuser = CMediusers::get();
 
@@ -20,20 +17,19 @@ if ($mediuser->isPraticien()) {
   $chir = $mediuser;
 }
 
-$chirSel      = CValue::getOrSession("praticien_id", $chir ? $chir->user_id : null);
-$all_prats    = CValue::get("all_prats", 0);
+$chirSel    = CValue::getOrSession("praticien_id", $chir ? $chir->user_id : null);
+$all_prats  = CValue::get("all_prats", 0);
+$fin        = CValue::getOrSession("fin"  , CMbDT::date());
+$debut      = CValue::getOrSession("debut", CMbDT::date("-1 week", $fin));
 
 if (!$all_prats) {
   CAppUI::requireModuleFile("dPboard", "inc_board");
 }
 
-$fin   = CValue::getOrSession("fin"  , CMbDT::date());
-$debut = CValue::getOrSession("debut", CMbDT::date("-1 week", $fin));
+$smarty = new CSmartyDP();
 
-$smarty = new CSmartyDP;
-
-$smarty->assign("chirSel", $chirSel);
-$smarty->assign("debut", $debut);
-$smarty->assign("fin", $fin);
-$smarty->assign("all_prats", $all_prats);
+$smarty->assign("chirSel"   , $chirSel);
+$smarty->assign("debut"     , $debut);
+$smarty->assign("fin"       , $fin);
+$smarty->assign("all_prats" , $all_prats);
 $smarty->display("../../dPboard/templates/inc_vw_interv_non_cotees.tpl");
