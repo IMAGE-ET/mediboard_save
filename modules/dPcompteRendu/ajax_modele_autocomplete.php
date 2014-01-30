8b5328d1491526175e6dfe803a2d2dddb9460595
Charles-Etienne Masse <c.masse@openxtrem.com>
2014-01-30 13:40:20 +0000

    Ref: autocomplete des modeles rangé par nom en SQL
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@21836 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/ajax_modele_autocomplete.php
+++ modules/dPcompteRendu/ajax_modele_autocomplete.php
@@ -38,7 +38,7 @@ $where["user_id"] = " = '$curr_user->_id'";
 if ($user->canEdit()) {
   $where["user_id"] = "IN ('$user->_id', '$curr_user->_id')";
 }
-$modeles = $compte_rendu->seek($keywords, $where);
+$modeles = $compte_rendu->seek($keywords, $where, 100, false, null, "nom");
 
 // Niveau fonction
 // Inclusion des fonctions secondaires de l'utilisateur connecté
@@ -55,12 +55,12 @@ $sec_functions = $sec_function->loadIds($whereSecFunc);
 $functions_ids = CMbArray::pluck($sec_functions, "function_id");
 $functions_ids = array_merge($functions_ids, array($user->function_id, $curr_user->function_id));
 $where["function_id"] = CSQLDataSource::prepareIn($functions_ids);
-$modeles = array_merge($modeles, $compte_rendu->seek($keywords, $where));
+$modeles = array_merge($modeles, $compte_rendu->seek($keywords, $where, 100, false, null, "nom"));
 
 // Niveau établissement
 unset($where["function_id"]);
 $where["group_id"] = " = '$user->_group_id'";
-$modeles = array_merge($modeles, $compte_rendu->seek($keywords, $where));
+$modeles = array_merge($modeles, $compte_rendu->seek($keywords, $where, 100, false, null, "nom"));
 
 $modeles = CModelObject::naturalSort($modeles, array("nom"), true);
 
