2720f12d45c9d5e5b17713b612102145e7a0e8db
Alexis Granger <granger@openxtrem.com>
2014-02-13 10:39:57 +0000

    fnc: nouvelle gestion de l'application des protocoles
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@22051 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 224,
+  "build" => 225,
 );
 
 $version["string"] = implode(".", $version);
