62988d088d10d3f5e76c6c9b5129c822213931e0
Yohann Poiron <poiron@openxtrem.com>
2014-02-19 14:50:10 +0000

    fnc: Ajout de la configuration permettant de vérifier ou non les patients similaires
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@22143 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/hprimxml/setup.php
+++ modules/hprimxml/setup.php
@@ -470,6 +470,12 @@ class CSetuphprimxml extends CSetup {
                 ADD `synchronous` ENUM ('0','1') NOT NULL DEFAULT '1';";
     $this->addQuery($query);
 
-    $this->mod_version = "0.53";
+    $this->makeRevision("0.53");
+
+    $query = "ALTER TABLE `hprimxml_config`
+                ADD `check_similar` ENUM ('0','1') NOT NULL DEFAULT '0';";
+    $this->addQuery($query);
+
+    $this->mod_version = "0.54";
   }
 }
