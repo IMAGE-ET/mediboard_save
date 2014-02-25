8080b5f9736fb415aef68b7afd67e2612db9b0a0
Fabien Ménager <fabien.menager@openxtrem.com>
2014-02-25 10:18:14 +0000

    bug: mauvais rechargement des lignes lors de l application d un protocole
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@22201 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 227,
+  "build" => 228,
 );
 
 $version["string"] = implode(".", $version);
