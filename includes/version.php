ee1b3f913fe7e65d3c1365a69f750bb5acdc2ea8
Charles-Etienne Masse <c.masse@openxtrem.com>
2014-03-26 08:52:58 +0000

    Ref: version ++
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@22616 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 233,
+  "build" => 234,
 );
 
 $version["string"] = implode(".", $version);
