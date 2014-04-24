9f3dd885312a9e5a23b7fa8534cd3e9fe0a9bfd6
Aurélie Hébras <aureliehebras@gmail.com>
2014-04-24 07:33:30 +0000

    Ref: Le frais divers ajouté a part défault un numéro de facture supérieur au nombre de facture existant
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@22914 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPccam/ajax_refresh_add_frais_divers.php
+++ modules/dPccam/ajax_refresh_add_frais_divers.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * $Id:$
+ * $Id$
  *
  * @package    Mediboard
  * @subpackage ccam
  * @author     SARL OpenXtrem <dev@openxtrem.com>
  * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version    $Revision:$
+ * @version    $Revision$
  */
 
+CCanDo::checkEdit();
 $object_guid = CValue::get("object_guid");
-$object = CMbObject::loadFromGuid($object_guid);
 
+/* @var CCodable $object*/
+$object = CMbObject::loadFromGuid($object_guid);
 $object->loadRefsFraisDivers();
 
 $frais_divers = new CFraisDivers();
@@ -19,6 +21,10 @@ $frais_divers->setObject($object);
 $frais_divers->quantite = 1;
 $frais_divers->coefficient = 1;
 $frais_divers->num_facture = 1;
+if ($object->_class == "CConsultation" && $object->valide) {
+  $object->loadRefFacture();
+  $frais_divers->num_facture = count($object->_ref_factures)+1;
+}
 $frais_divers->loadListExecutants();
 $frais_divers->loadExecution();
 
