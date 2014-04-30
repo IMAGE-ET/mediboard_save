97f496c6b6ac21c156c7c866a0c07b8cb0b5001c
Nicolas Le Dantec <n.ledantec@openxtrem.com>
2014-04-30 15:04:44 +0000

    Bug : Plus d'association de type de document dans la configuration
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23019 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/cda/controllers/do_cda_association_aed.php
+++ modules/cda/controllers/do_cda_association_aed.php
@@ -11,32 +11,10 @@
  * @link     http://www.mediboard.org
  */
 
-$select     = CValue::post("select");
 $group_type = CValue::post("group_type");
 $group_id   = CValue::post("group_id");
 $group      = new CGroups();
 $group->load($group_id);
-
-foreach ($select as $_id => $_type_code) {
-  $category = new CFilesCategory();
-  $category->load($_id);
-  $idex = new CIdSante400();
-  $idex->tag = "cda_association_code_$group_id";
-  $idex->setObject($category);
-  $idex->loadMatchingObject();
-  $idex->last_update = CMbDT::dateTime();
-  $idex->id400 = $_type_code;
-
-  if (!$_type_code && $idex->_id) {
-    $idex->delete();
-    continue;
-  }
-
-  if ($_type_code && $msg = $idex->store()) {
-    CAppUI::setMsg($msg, UI_MSG_ERROR);
-  }
-
-}
 
 $idex = new CIdSante400();
 $idex->tag = "cda_association_code";
