52085fff73d09b78cab855d432af46f924a088df
Fabien Ménager <fabien.menager@openxtrem.com>
2014-05-30 12:27:21 +0000

    bug: les formulaires soumis en Ajax POST ne permettaient pas d'enregistrer des données non iso-8859-1 comme les "oe" et les €, on convertit donc de UTF-8 vers Windows-1252 plutôt que iso-8859-1
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23379 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/magic_quotes_gpc.php
+++ includes/magic_quotes_gpc.php
@@ -9,10 +9,14 @@
  * @version    $Id$
  */
 
+function fixISOEncoding($str) {
+  return mb_convert_encoding($str, 'Windows-1252', 'UTF-8');
+}
+
 // UTF decode inputs from ajax requests
 if (isset($_REQUEST["ajax"]) && empty($_REQUEST["accept_utf8"])) {
   $_GET     = array_map_recursive("utf8_decode", $_GET);
-  $_POST    = array_map_recursive("utf8_decode", $_POST);
+  $_POST    = array_map_recursive("fixISOEncoding", $_POST);
   $_COOKIE  = array_map_recursive("utf8_decode", $_COOKIE);
   $_REQUEST = array_map_recursive("utf8_decode", $_REQUEST);
 }
