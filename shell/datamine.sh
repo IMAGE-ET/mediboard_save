599037fe4cbaefca67b5da930530455aecab914c
Kévin Grisel <k.grisel@openxtrem.com>
2014-06-06 09:09:21 +0000

    Fnc : Ajout du script datamine.sh dans le shell.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23484 a21d6be2-e512-0410-a060-91f5a2946631

---
+++ shell/datamine.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+BASH_PATH=$(dirname $0)
+. $BASH_PATH/../../../shell/utils.sh
+MB_PATH=$(cd $BASH_PATH/../../../; pwd);
+
+########
+# Miner launcher
+########
+
+announce_script "Miner launcher"
+
+if [ "$#" -lt 3 ]
+then
+  echo "Usage: $0 <instance> <username> <password> Options"
+  echo " <instance> instance name"
+  echo " <username> to access database"
+  echo " <password> authenticate user"
+  echo " Options:"
+  echo "   [-f <file>] output file"
+  exit 1
+fi
+
+args=$(getopt f: $*)
+
+if [ $? != 0 ] ; then
+  echo "Invalid argument. Check your command line"; exit 0;
+fi
+
+file=''
+
+set -- $args
+
+for i; do
+  case "$i" in
+    -f) file="-f $2"; shift 2;;
+    --) shift ; break ;;
+  esac
+done
+
+instance=$1
+username=$2
+password=$3
+
+info_script "Miner launcher"
+
+list=$(ls -C1 $MB_PATH/modules/*/datamine.php|cut -d'/' -f 8)
+
+for i in $list
+do
+  report=$(sh $MB_PATH/shell/request.sh "http://localhost/$instance" $username $password "m=$i&a=datamine&suppressHeaders=1&dialog=1" $file);
+done
