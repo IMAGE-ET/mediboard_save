f9e2d9a8299435c79ccb87fe2897ac9b2d8196e5
Laurent Le Moine <laurent.le.moine17@gmail.com>
2014-06-20 07:55:50 +0000

    bug: L'horaire voulu n'était pas pris en compte dans le calcul de la date d'entrée prévue
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23635 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/classes/COperation.class.php
+++ modules/dPplanningOp/classes/COperation.class.php
@@ -807,16 +807,16 @@ class COperation extends CCodable implements IPatientRelated {
     ) {
       $sejour->loadRefsOperations();
       if (count($sejour->_ref_operations) == 1) {
-        if (
-            isset($this->presence_preop) && (isset($this->horaire_voulu) ||
-            (isset($this->time_operation) && $this->time_operation != '00:00:00'))
-        ) {
+        if ($this->time_operation == '00:00:00') {
+          $this->time_operation = null;
+        }
+        if (isset($this->presence_preop) && (isset($this->horaire_voulu) || isset($this->time_operation))) {
           $sejour->entree_prevue = $this->date . ' ' . CMbDT::subTime($this->presence_preop, CValue::first($this->time_operation, $this->horaire_voulu));
           $do_store_sejour = true;
         }
         if (
-            isset($this->presence_postop) && isset($this->temp_operation) &&(isset($this->horaire_voulu) ||
-            (isset($this->time_operation) && $this->time_operation != '00:00:00'))
+            isset($this->presence_postop) && isset($this->temp_operation) &&
+            (isset($this->horaire_voulu) || isset($this->time_operation))
         ) {
           $time_postop = CMbDT::addTime($this->temp_operation, $this->presence_postop);
           $sejour->sortie_prevue = $this->date . ' ' . CMbDT::addTime($time_postop, CValue::first($this->time_operation, $this->horaire_voulu));
@@ -825,6 +825,9 @@ class COperation extends CCodable implements IPatientRelated {
 
         if ($do_store_sejour) {
           $sejour->updateFormFields();
+        }
+        if (!isset($this->time_operation)) {
+          $this->time_operation = '00:00:00';
         }
       }
     }
