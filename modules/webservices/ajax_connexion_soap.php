5945865253d755ba8ef0262e11342e0b31ebfb63
Laurent Le Moine <laurent.le.moine17@gmail.com>
2014-06-23 14:02:51 +0000

    bug: Problème de connexion aux services web utilisant l'authentification HTTP
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23660 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/webservices/ajax_connexion_soap.php
+++ modules/webservices/ajax_connexion_soap.php
@@ -33,7 +33,7 @@ $options = array(
 
 $soap_client = new CSOAPClient($exchange_source->type_soap);
 $soap_client->make(
-  $exchange_source->host, $exchange_source->user, $exchange_source->password, $exchange_source->type_echange, $options,
+  $exchange_source->host, $exchange_source->user, $exchange_source->getPassword(), $exchange_source->type_echange, $options,
   null, null, $exchange_source->local_cert, $exchange_source->passphrase, $exchange_source->safe_mode, $exchange_source->verify_peer,
   $exchange_source->cafile, $exchange_source->wsdl_external
 );
