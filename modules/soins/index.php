b72b75a21cf0ead94ff0a6d5a24a2b08beed9bf6
Fabien Ménager <fabien.menager@openxtrem.com>
2014-06-26 15:12:54 +0000

    fnc: ajout d'un onglet Dispensation dans le module Dossier de soins, configurable par établissement
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23731 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/index.php
+++ modules/soins/index.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * $Id:$
+ * $Id$
  *
  * @category Soins
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
  * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version  $Revision:$
+ * @version  $Revision$
  * @link     http://www.mediboard.org
  */
 
-$module = CModule::getInstalled(basename(dirname(__FILE__)));
+$module = CModule::getInstalled(basename(__DIR__));
 
 if (CAppUI::pref("vue_sejours") == "standard") {
   $module->registerTab("vw_idx_sejour"        , TAB_READ);
@@ -19,18 +19,26 @@ else {
   $module->registerTab("vw_sejours"           , TAB_READ);
 }
 
-if(CModule::getActive('dPprescription')){
+$current_group = CGroups::loadCurrent();
+
+if (CModule::getActive('dPprescription')) {
   $module->registerTab("vw_pancarte_service"  , TAB_READ);
   $module->registerTab("vw_bilan_prescription", TAB_READ);
   $module->registerTab("vw_plan_soins_service", TAB_READ);
-  if (CAppUI::conf("soins Other show_charge_soins", CGroups::loadCurrent()->_guid)) {
+  if (CAppUI::conf("soins Other show_charge_soins", $current_group)) {
     $module->registerTab("vw_ressources_soins"  , TAB_READ);
   }
   //$module->registerTab("vw_dossier_sejour"    , TAB_READ);
 }
 
+if (
+    CModule::getActive('pharmacie') &&
+    CAppUI::conf("pharmacie enable_v2") && 
+    CAppUI::conf("pharmacie Display show_dispensation_dossier_soins", $current_group)
+) {
+  $module->registerTab("vw_dispensation", TAB_READ);
+}
+
 if (isset(CModule::$active["dPstock"])) {
   $module->registerTab("vw_stocks_service"  , TAB_EDIT);
 }
-
-?>
---
+++ modules/soins/vw_dispensation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * $Id:$
+ *
+ * @package    Mediboard
+ * @subpackage Soins
+ * @author     SARL OpenXtrem <dev@openxtrem.com>
+ * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @version    $Revision:$
+ */
+
+CAppUI::requireModuleFile("pharmacie", "vw_idx_dispensation_v2");
