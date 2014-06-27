d0d237f4bf6e8a93cc4de038c788cc75aef04cd7
Aurélie Hébras <aureliehebras@gmail.com>
2014-06-27 07:38:51 +0000

    Fnc: La notion d'allergie connue est affichée par une image dans le bandeau patient des soins
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23739 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/httpreq_vw_antecedent_allergie.php
+++ modules/soins/httpreq_vw_antecedent_allergie.php
@@ -1,11 +1,12 @@
-<?php /* $Id: httpreq_vw_antecedent_allergie.php 13378 2011-10-04 12:13:24Z flaviencrochard $ */
-
+<?php
 /**
- * @package Mediboard
- * @subpackage soins
- * @version $Revision: 13378 $
- * @author SARL OpenXtrem
- * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
+ * $Id:$
+ *
+ * @category soins
+ * @package  Mediboard
+ * @author   SARL OpenXtrem <dev@openxtrem.com>
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @version  $Revision:$
  */
 
 $sejour_id = CValue::getOrSession("sejour_id");
@@ -26,6 +27,7 @@ $patient =& $sejour->_ref_patient;
 $patient->loadRefDossierMedical();
 $dossier_medical =& $patient->_ref_dossier_medical;
 if ($dossier_medical->_id) {
+  $dossier_medical->loadRefsAllergies();
   $dossier_medical->loadRefsAntecedents();
   $dossier_medical->countAntecedents(false);
   $dossier_medical->countAllergies();
@@ -37,10 +39,10 @@ if ($dossier_medical_sejour->_id && $dossier_medical->_id) {
 
 // Création du template
 $smarty = new CSmartyDP();
-$smarty->assign("sejour_id", $sejour->_id);
-$smarty->assign("antecedents_sejour", $dossier_medical_sejour->_ref_antecedents_by_type);
+$smarty->assign("sejour_id"             , $sejour->_id);
+$smarty->assign("antecedents_sejour"    , $dossier_medical_sejour->_ref_antecedents_by_type);
 $smarty->assign("dossier_medical_sejour", $dossier_medical_sejour);
-$smarty->assign("antecedents", $dossier_medical->_ref_antecedents_by_type);
-$smarty->assign("dossier_medical", $dossier_medical);
-$smarty->assign("patient_guid", $patient->_guid);
+$smarty->assign("antecedents"           , $dossier_medical->_ref_antecedents_by_type);
+$smarty->assign("dossier_medical"       , $dossier_medical);
+$smarty->assign("patient_guid"          , $patient->_guid);
 $smarty->display("inc_antecedents_allergies.tpl");
--- modules/soins/templates/inc_antecedents_allergies.tpl
+++ modules/soins/templates/inc_antecedents_allergies.tpl
@@ -1,23 +1,18 @@
-{{*
- * $Id$
- *  
- * @package    Mediboard
- * @subpackage soins
- * @author     SARL OpenXtrem <dev@openxtrem.com>
- * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version    $Revision$
- * @link       http://www.mediboard.org
-*}}
-
 {{mb_include module=soins template=inc_vw_antecedents}}
 
-{{if $dossier_medical->_id && $dossier_medical->_count_allergies}}
-  <script type="text/javascript">
-    ObjectTooltip.modes.allergies = {
-      module: "patients",
-      action: "ajax_vw_allergies",
-      sClass: "tooltip"
-    };
-  </script>
-  <img src="images/icons/warning.png" onmouseover="ObjectTooltip.createEx(this, '{{$patient_guid}}', 'allergies');" />
+{{if $dossier_medical->_id}}
+  {{if $dossier_medical->_count_allergies}}
+    <script>
+      ObjectTooltip.modes.allergies = {
+        module: "patients",
+        action: "ajax_vw_allergies",
+        sClass: "tooltip"
+      };
+    </script>
+    <img src="images/icons/warning.png" onmouseover="ObjectTooltip.createEx(this, '{{$patient_guid}}', 'allergies');" />
+    <img src="images/icons/allergies_warning.png" title="Allergie(s) connue(s)">
+
+  {{elseif $dossier_medical->_ref_allergies|@count}}
+    <img src="images/icons/allergies_ok.png" title="Pas d'allergie connue">
+  {{/if}}
 {{/if}}
