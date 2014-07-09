08cf1f0d6ece08ec871ee138a7c7492a13a841dc
Aurélie Hébras <aureliehebras@gmail.com>
2014-07-09 08:42:32 +0000

    Bug: Dans les consultations non cotées, il ne faut pas prendre en compte les pauses
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23929 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPfacturation/print_noncote.php
+++ modules/dPfacturation/print_noncote.php
@@ -27,6 +27,7 @@ $ljoin["plageconsult"] = "consultation.plageconsult_id = plageconsult.plageconsu
 $where = array();
 $where["tarif"] = " IS NULL ";
 $where["codes_ccam"] = " IS NULL";
+$where["patient_id"] = " IS NOT NULL";
 $where["secteur1"] = " = 0";
 $where["secteur2"] = " = 0";
 $where["plageconsult.chir_id"] = CSQLDataSource::prepareIn(array_keys($listPrat));
@@ -55,4 +56,3 @@ $smarty->assign("listConsults"       , $listConsults);
 $smarty->assign("listConsults_date"  , $listConsults_date);
 
 $smarty->display("print_noncote.tpl");
-?>
