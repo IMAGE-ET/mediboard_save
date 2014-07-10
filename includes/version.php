0097faf3ae6b0bdf666224c464d8192ed033ee5c
Alexis Granger <granger@openxtrem.com>
2014-07-10 14:26:48 +0000

    fnc: Remplacement d'une spécialité par sa DCI pour les médicaments et les perfusions
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23946 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 244,
+  "build" => 245,
 );
 
 $version["string"] = implode(".", $version);
