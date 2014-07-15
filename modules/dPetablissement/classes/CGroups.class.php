2577be0e45ae05cd83c1f3e1e1f2fe44f4e613d6
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-07-15 15:04:06 +0000

    Fnc: Ajout de la substition dans les extensions de produits.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23984 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPetablissement/classes/CGroups.class.php
+++ modules/dPetablissement/classes/CGroups.class.php
@@ -175,6 +175,7 @@ class CGroups extends CMbObject {
     $backProps["tarif_group"]               = "CTarif group_id";
     $backProps["extension_unites"]          = "CExtensionProduitUnite group_id";
     $backProps["extension_voies"]           = "CExtensionProduitVoie group_id";
+    $backProps["extension_produits_subst"]  = "CExtensionProduitSubstitution group_id";
     $backProps["daily_check_lists"]         = "CDailyCheckList group_id";
     $backProps["daily_check_list_types"]    = "CDailyCheckListType group_id";
     $backProps["grossesses"]                = "CGrossesse group_id";
