7863cf02370dbfe829a11fedbe8eb66dfb1e7aa1
Yohann Poiron <poiron@openxtrem.com>
2014-07-15 12:26:55 +0000

    bug: Double élément sortie
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@23973 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPurgences/classes/CRPUXMLDocument.class.php
+++ modules/dPurgences/classes/CRPUXMLDocument.class.php
@@ -214,7 +214,9 @@ class CRPUXMLDocument extends CMbXMLDocument {
       }
     }
     else {
-      $this->addElement($elParent, "SORTIE", CMbDT::transform($sejour->sortie_prevue, null, "%d/%m/%Y %H:%M"));
+      if (!$sortie) {
+        $this->addElement($elParent, "SORTIE", CMbDT::transform($sejour->sortie_prevue, null, "%d/%m/%Y %H:%M"));
+      }
       $this->addElement($elParent, "MODE_SORTIE", $mbObject->_mode_sortie);
       $this->addElement($elParent, "DESTINATION", $sejour->destination);
       $this->addElement($elParent, "ORIENT", strtoupper($mbObject->orientation));
