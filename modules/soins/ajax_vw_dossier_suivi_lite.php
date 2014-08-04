181940c51cc2d9f572b3c7885e2da7657ef134a6
Aurélie Hébras <aureliehebras@gmail.com>
2014-08-04 08:31:15 +0000

    Bug: Il manquait une partie des chargements en fonction d'une configuration
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@24265 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/ajax_vw_dossier_suivi_lite.php
+++ modules/soins/ajax_vw_dossier_suivi_lite.php
@@ -1,32 +1,34 @@
 <?php
-
 /**
- * $Id$
+ * $Id:$
  *
  * @package    Mediboard
  * @subpackage soins
  * @author     SARL OpenXtrem <dev@openxtrem.com>
  * @license    GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version    $Revision$
+ * @version    $Revision:$
  */
-
 
 $sejour_id = CValue::get("sejour_id");
 
 // Chargement des observations d'importance haute de moins de 7 jours
 $observation = new CObservationMedicale();
 $where = array();
-$where["degre"] = " = 'high'";
+$where["degre"]     = " = 'high'";
 $where["sejour_id"] = " = '$sejour_id'";
-$where["date"] = " >= '".CMbDT::dateTime("- 7 DAYS")."'";
+$where["date"]      = " >= '".CMbDT::dateTime("- 7 DAYS")."'";
 $observations = $observation->loadList($where);
 
 // Chargement des transmissions d'importance haute ou des macrocibles de moins de 7 jours
 $transmission = new CTransmissionMedicale();
 $where = array();
-$where["date"] = " >= '".CMbDT::dateTime("- 7 DAYS")."'";
+$where["date"]      = " >= '".CMbDT::dateTime("- 7 DAYS")."'";
 $where["sejour_id"] = " = '$sejour_id'";
-$where[] = "degre = 'high' OR category_prescription.cible_importante = '1'";
+$where[]            = "degre = 'high' OR category_prescription.cible_importante = '1'";
+
+if (CAppUI::conf("soins synthese transmission_date_limit", CGroups::loadCurrent()->_guid)) {
+  $where[] = "date_max >= '".CMbDT::dateTime()."' OR date_max IS NULL";
+}
 
 $ljoin = array();
 $ljoin["category_prescription"] = "transmission_medicale.object_id = category_prescription.category_prescription_id
@@ -35,12 +37,14 @@ $ljoin["category_prescription"] = "transmission_medicale.object_id = category_pr
 $transmissions = $transmission->loadList($where, null, null, null, $ljoin);
 
 $suivi = array();
+/* @var CObservationMedicale[] $observations*/
 foreach ($observations as $_observation) {
   $_observation->loadRefsFwd();
   $_observation->_ref_user->loadRefFunction();
   $suivi[$_observation->date.$_observation->_id] = $_observation;
 }
 
+/* @var CTransmissionMedicale[] $transmissions*/
 foreach ($transmissions as $_transmission) {
   $_transmission->loadRefsFwd();
   $_transmission->_ref_user->loadRefFunction();
@@ -51,6 +55,6 @@ krsort($suivi);
 
 // Création du template
 $smarty = new CSmartyDP();
-$smarty->assign("suivi", $suivi);
+$smarty->assign("suivi"   , $suivi);
 $smarty->assign("readonly", true);
 $smarty->display("inc_vw_dossier_suivi_lite.tpl");
