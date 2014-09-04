cb16da77b250d15e827d2f46bf27a2d3446c399b
Laurent Le Moine <laurent.le.moine17@gmail.com>
2014-09-04 14:56:45 +0000

    bug: Pas de vérification d'utilisation du nouveau mode de codage des actes CCAM avant de vérifier l'application des règles du CCodageCCAM dans le store CActeCCAM
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@24681 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPsalleOp/classes/CActeCCAM.class.php
+++ modules/dPsalleOp/classes/CActeCCAM.class.php
@@ -530,11 +530,14 @@ class CActeCCAM extends CActe {
         $this->signe = 0;
       }
     }
-    // Vérification de l'existence du codage
-    $codage = CCodageCCAM::get($this->loadRefObject(), $this->executant_id);
-    if (!$codage->_id) {
-      if ($msg = $codage->store()) {
-        return $msg;
+
+    if (CAppUI::conf('dPccam CCodeCCAM use_new_association_rules')) {
+      // Vérification de l'existence du codage
+      $codage = CCodageCCAM::get($this->loadRefObject(), $this->executant_id);
+      if (!$codage->_id) {
+        if ($msg = $codage->store()) {
+          return $msg;
+        }
       }
     }
     
@@ -543,13 +546,16 @@ class CActeCCAM extends CActe {
       return $msg;
     }
 
-    // Si on crée un nouvel acte, on relance l'analyse du codage
-    if (!$oldObject->_id) {
-      $codage->updateRule(true);
-      if ($msg = $codage->store()) {
-        return $msg;
+    if (CAppUI::conf('dPccam CCodeCCAM use_new_association_rules')) {
+      // Si on crée un nouvel acte, on relance l'analyse du codage
+      if (!$oldObject->_id) {
+        $codage->updateRule(true);
+        if ($msg = $codage->store()) {
+          return $msg;
+        }
       }
     }
+
     return null;
   }
 
