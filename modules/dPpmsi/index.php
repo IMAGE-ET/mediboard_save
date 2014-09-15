753300b2ee9ed0efcf2de83d055d3f6a9a75f08a
Romain Ollivier <ollivier@openxtrem.com>
2014-09-15 08:56:27 +0000

    erg: supression de l'onglet de codage d'acte au PMSI (maintenant affiché dans une modale)
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@24831 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPpmsi/index.php
+++ modules/dPpmsi/index.php
@@ -15,7 +15,7 @@ $module->registerTab("vw_dossier_pmsi"    , TAB_EDIT);
 //$module->registerTab("vw_dossier"         , TAB_READ);
 $module->registerTab("vw_list_hospi"      , TAB_READ);
 $module->registerTab("vw_list_interv"     , TAB_READ);
-$module->registerTab("edit_actes"         , TAB_READ);
+//$module->registerTab("edit_actes"         , TAB_READ);
 //$module->registerTab("labo_groupage"      , TAB_ADMIN);
 $module->registerTab("form_print_planning", TAB_READ);
 //$module->registerTab("vw_last_docs"       , TAB_EDIT);
