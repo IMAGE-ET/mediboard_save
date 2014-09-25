be87e5334113393e2becf2b05a42b17bff3fcbe4
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-09-25 15:00:18 +0000

    Ref: Dissociation de la gestion des alertes et signaux dans la prescription.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@24971 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 264,
+  "build" => 265,
 );
 
 $version["string"] = implode(".", $version);
