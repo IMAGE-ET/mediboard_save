47bef970aef63e993dd0d6fc0fd6bbc3e27d5986
Marie-Claude Armengaud <mc.armengaud@openxtrem.com>
2014-10-16 15:40:30 +0000

    bug: ajout de conditions pour l'accès à l'import sae
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25376 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPplanningOp/setup.php
+++ modules/dPplanningOp/setup.php
@@ -1809,7 +1809,13 @@ class CSetupdPplanningOp extends CSetup {
     $query = "SHOW TABLES LIKE 'type_autorisation_um'";
     $this->addDatasource("sae", $query);
 
-    $query = "SELECT * FROM `type_autorisation_um` WHERE `code_concat` = '07A';";
-    $this->addDatasource("sae", $query);
+    if (array_key_exists('sae', CAppUI::conf('db'))) {
+      $dsn = CSQLDataSource::get('sae');
+      $test = $dsn->exec('SHOW TABLES LIKE \'type_autorisation_um\';');
+      if ($test && $dsn->fetchRow($test)) {
+        $query = "SELECT * FROM `type_autorisation_um` WHERE `code_concat` = '07A';";
+        $this->addDatasource("sae", $query);
+      }
+    }
   }
 }
