21733d650d717e4335e5be1fa7437fad13f73232
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-11-05 08:36:26 +0000

    Fnc: Pouvoir ignorer les modules externes dans le script update.sh.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25710 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/update.sh
+++ shell/update.sh
@@ -20,6 +20,7 @@ then
   echo "     real : Performs the actual update"
   echo "  -r <revision> The revision number you want to update to, HEAD by default"
   echo "  -e <revision external> The external revision number, same behavior than the above option"
+  echo "  -i : ignore externals"
   exit 1
 fi
    
@@ -31,6 +32,7 @@ event=$MB_PATH/tmp/monitevent.txt
 prefixes="erg|fnc|fct|bug|war|edi|sys|svn"
 revision=HEAD
 revision_external=HEAD
+with_externals=1
 
 # First external
 externals=$(svn propget svn:externals modules)
@@ -40,7 +42,7 @@ path_external=$MB_PATH/modules/$first_external
 
 # Choose the target revision
 
-args=$(getopt r:e: $*)
+args=$(getopt r:e:i $*)
 
 if [ $? != 0 ] ; then
   echo "Invalid argument. Check your command line"; exit 0;
@@ -52,6 +54,7 @@ for i; do
   case "$i" in
     -r) revision=$2; shift 2;;
     -e) revision_external=$2; shift 2;;
+    -i) with_externals=0; shift;;
     --) shift ; break ;;
     -*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
   esac
@@ -86,13 +89,18 @@ case "$1" in
     rm -f $dif
     
     # Perform actual update
-    # If externals, update only the trunk without them and then update the externals
-    if [ "$revision_external" != "HEAD" ] && [ "$first_external" != "" ]
+    if [ "$with_externals" -eq "1" ]
     then
-      svn update --ignore-externals --revision $revision
-      svn update $all_externals --revision $revision_external
+      # If externals, update only the trunk without them and then update the externals
+      if [ "$revision_external" != "HEAD" ] && [ "$first_external" != "" ]
+      then
+        svn update --ignore-externals --revision $revision
+        svn update $all_externals --revision $revision_external
+      else
+        svn update $MB_PATH --revision $revision
+      fi
     else
-      svn update $MB_PATH --revision $revision
+      svn update $MB_PATH --ignore-externals --revision $revision
     fi
     check_errs $? "Failed to perform SVN update" "SVN updated performed!"
 
