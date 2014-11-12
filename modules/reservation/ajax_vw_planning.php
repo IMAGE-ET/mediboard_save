144ece9a533df9ab7db3c157986957a7affc7cc6
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-11-12 14:36:49 +0000

    Bug: Problème d'affichage des interventions dans le planning de la réservation.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25842 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/reservation/ajax_vw_planning.php
+++ modules/reservation/ajax_vw_planning.php
@@ -249,16 +249,16 @@ if ($show_operations) {
   foreach ($operations_by_salle as $salle_id => $_operations) {
     $i = array_search($salle_id, $salles_ids);
 
-    //en plage & non validé, skip
-    if ($_operation->plageop_id && !$_operation->rank) {
-      continue;
-    }
-
     $types_antecedent = CAntecedent::$types;
     $types_antecedent = array_diff($types_antecedent, array("alle"));
 
     foreach ($_operations as $_operation) {
       //CSQLDataSource::$trace = true;
+
+      //en plage & non validé, skip
+      if ($_operation->plageop_id && !$_operation->rank) {
+        continue;
+      }
 
       $_operation->_ref_salle = $salles[$_operation->salle_id];
 
