60b04772b93b82110d768f6bd19e5e568b2600e1
Kévin Grisel <k.grisel@openxtrem.com>
2014-11-19 14:35:15 +0000

    Ref : Incrémentation de la version.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25955 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 274,
+  "build" => 275,
 );
 
 $version["string"] = implode(".", $version);
