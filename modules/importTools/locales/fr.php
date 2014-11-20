5bb62ea87d0bb91c506a0235e16e87b11f262915
Fabien Ménager <fabien.menager@openxtrem.com>
2014-11-20 11:29:56 +0000

    ref: ajout de locales des modules Outils d'import et DMI
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25965 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/importTools/locales/fr.php
+++ modules/importTools/locales/fr.php
@@ -1,3 +1,5 @@
 <?php
+$locales['mod-importTools-tab-ajax_vw_table_data'] = 'Tableau des données';
+$locales['mod-importTools-tab-vw_database_explorer'] = 'Explorateur de base';
 $locales['module-importTools-court'] = 'Outils d\'import';
 $locales['module-importTools-long'] = 'Outils d\'import de base externe';
