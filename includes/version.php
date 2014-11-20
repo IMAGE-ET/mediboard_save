1cbb3192f3e1864df5f803296e534f4e15d63435
Kévin Grisel <k.grisel@openxtrem.com>
2014-11-20 13:39:33 +0000

    Bug : Le rafraîchissement de la modale de changement de statut d'une demande se fait après l'ajout de tâches.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@25966 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 275,
+  "build" => 276,
 );
 
 $version["string"] = implode(".", $version);
