e247c5033349fc7a037c5c1bd793d0641ff2472f
Marie-Claude Armengaud <mc.armengaud@openxtrem.com>
2014-11-28 14:34:07 +0000

    ref: meilleure gestion des caractères utf-8 en GET
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26112 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/magic_quotes_gpc.php
+++ includes/magic_quotes_gpc.php
@@ -15,7 +15,7 @@ function fixISOEncoding($str) {
 
 // UTF decode inputs from ajax requests
 if (isset($_REQUEST["ajax"]) && empty($_REQUEST["accept_utf8"])) {
-  $_GET     = array_map_recursive("utf8_decode", $_GET);
+  $_GET     = array_map_recursive("fixISOEncoding", $_GET);
   $_POST    = array_map_recursive("fixISOEncoding", $_POST);
   $_COOKIE  = array_map_recursive("utf8_decode", $_COOKIE);
   $_REQUEST = array_map_recursive("utf8_decode", $_REQUEST);
