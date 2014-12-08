404d4bac48ff72186723551e735016bb16268eda
Aurélie Hébras <aureliehebras@gmail.com>
2014-12-08 10:23:54 +0000

    Fnc: Selection auto des observations et transmissions dans la vue condensée en modale
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26230 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/soins/javascript/plan_soins.js
+++ modules/soins/javascript/plan_soins.js
@@ -778,7 +778,7 @@ PlanSoins = {
   },
 
    showModalAllTrans: function(sejour_id) {
-    loadSuivi(sejour_id, null, null, null, null, null, 1);
+    loadSuivi(sejour_id, null, null, 1, 1, null, 1);
     var modal_suivi_lite = Modal.open("dossier_suivi", { showClose: true, width: 800, height: 600 });
     modal_suivi_lite.observe("afterClose", PlanSoins.loadLiteSuivi.curry(sejour_id));
   },
