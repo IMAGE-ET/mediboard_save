2faa07875e9b0b4349ed3f6dc6d1417ad299abea
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-12-16 10:07:44 +0000

    Fnc: Ajout du volet Médicaments hors AMM depuis la pharmacie.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26433 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 289,
+  "build" => 290,
 );
 
 $version["string"] = implode(".", $version);
