a07844f62d9beda37b1833af9c73c7600bd5caa6
Alexis Granger <granger@openxtrem.com>
2014-12-17 16:27:18 +0000

    fnc: ajout d'une configuration pour afficher par défaut le formulaire d'ajout d'antecedent dans le mode grille
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26476 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcabinet/configuration.php
+++ modules/dPcabinet/configuration.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Id:$
+ * $Id$
  *  
  * @category Cabinet
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
  * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
- * @version  $Revision:$
+ * @version  $Revision$
  * @link     http://www.mediboard.org
  */
 
@@ -17,6 +17,9 @@ CConfiguration::register(
         "CPrescription" => array(
           "view_prescription"         => "bool default|0",
           "view_prescription_externe"  => "bool default|0"
+        ),
+        "CAntecedent" => array(
+          "show_form_add_atcd" => "bool default|0"
         )
       )
     )
