138245cc6eea9e5baddffa69a06e599b95c32b88
Kévin Grisel <k.grisel@openxtrem.com>
2014-12-18 16:48:28 +0000

    Fnc : Ajout d'un sélecteur de contexte.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26500 a21d6be2-e512-0410-a060-91f5a2946631

--- includes/version.php
+++ includes/version.php
@@ -16,7 +16,7 @@ $version = array (
   "patch" => 0,
 
   // Automated numbering (should be incremented at each commit)
-  "build" => 291,
+  "build" => 292,
 );
 
 $version["string"] = implode(".", $version);
