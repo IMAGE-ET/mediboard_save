5039c0bf5f09cd5e5ee7531dad0308a1a9d067fc
Flavien Crochard <flavien.crochard@openxtrem.com>
2014-12-24 14:50:20 +0000

    Ref: Ajout de la case à cocher Changement de propriétaire lors du verrou de document pour un autre utilisateur.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26583 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/system/ajax_password_action.php
+++ modules/system/ajax_password_action.php
@@ -13,13 +13,14 @@
 
 $user_id       = CValue::post("user_id");
 $user_password = CValue::post("user_password");
+$form_name     = CValue::post("form_name");
 $callback      = CValue::post("callback");
 
 $user = new CUser();
 $user->load($user_id);
 
 if ($user->_id && CUser::checkPassword($user->user_username, $user_password)) {
-  CAppUI::callbackAjax($callback, $user_id);
+  CAppUI::callbackAjax($callback, $user_id, $form_name);
 }
 else {
   CAppUI::setMsg("CUser-user_password-nomatch", UI_MSG_ERROR);
