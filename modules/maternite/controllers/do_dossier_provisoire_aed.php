2e46ae75299de06d6609a2ba47a4c6e3a67eb904
Charles-Etienne Masse <c.masse@openxtrem.com>
2015-01-15 16:43:19 +0000

    Bug: pour la sortie prévue d'un dossier provisoire
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26799 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/maternite/controllers/do_dossier_provisoire_aed.php
+++ modules/maternite/controllers/do_dossier_provisoire_aed.php
@@ -65,7 +65,7 @@ if (!$prenom) {
 $sejour_enfant = new CSejour();
 $sejour_enfant->patient_id = $patient->_id;
 $sejour_enfant->entree_prevue = CMbDT::dateTime();
-$sejour_enfant->sortie_prevue = $sejour->sortie;
+$sejour_enfant->sortie_prevue = max($sejour_enfant->entree_prevue, $sejour->sortie);
 $sejour_enfant->praticien_id = $praticien_id ? $praticien_id : $sejour->praticien_id;
 $sejour_enfant->group_id = $sejour->group_id;
 $sejour_enfant->_naissance = true;
