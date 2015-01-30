ca7f61e08bf9256a8703a985294862968466fcae
Flavien Crochard <flavien.crochard@openxtrem.com>
2015-01-30 13:55:58 +0000

    Bug: Correction de source HTML pour prince xml.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@26973 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/classes/CPrinceXMLConverter.class.php
+++ modules/dPcompteRendu/classes/CPrinceXMLConverter.class.php
@@ -22,7 +22,7 @@ class CPrinceXMLConverter extends CHtmlToPDFConverter {
     global $rootName;
 
     // Changer les src pour les images
-    $this->html = preg_replace("/src=\"\/".$rootName."/", "src=\"../", $this->html);
+    $this->html = preg_replace("/src=\"\/" . $rootName . "/", "src=\"../", $this->html);
 
     // Suppression du margin-top sur le body et le padding-top sur le hr
     $this->html = preg_replace("/body\s*\{\s*margin-top.*\s*\}\s*hr.pagebreak\s*\{\s*padding-top.*\s*\}/m", "", $this->html);
@@ -41,53 +41,68 @@ class CPrinceXMLConverter extends CHtmlToPDFConverter {
     }
 
     preg_match(
-      "/@page\s*{\s*margin-top:\s*([0-9.]+)cm;\s*".
-      "margin-right:\s*([0-9.]+)cm;\s*".
+      "/@page\s*{\s*margin-top:\s*([0-9.]+)cm;\s*" .
+      "margin-right:\s*([0-9.]+)cm;\s*" .
       "margin-bottom:\s*([0-9.]+)cm;\s*margin-left:\s*([0-9.]+)cm;/",
       $this->html,
       $matches
     );
     if (count($matches)) {
-      $margin_top = $matches[1];
+      $margin_top    = $matches[1];
       $margin_bottom = $matches[4];
     }
 
     // Format et orientation
-    $size = "";
+    $size = $format;
     if (is_array($format)) {
       $width  = $format[2] * 25.4 / 72;
       $height = $format[3] * 25.4 / 72;
-      $size = "{$width}mm {$height}mm";
+      $size   = "{$width}mm {$height}mm";
     }
-    else {
-      $size = $format;
-    }
-    $size.= " $orientation";
+
+    $size .= " $orientation";
 
     $style = "
       @page {
-        size:           $size;
-        margin-top:    {$header_height}px;
-        margin-bottom: {$footer_height}px;
+        size:           $size;";
 
+    if ($header_height) {
+      $style .= "margin-top:    {$header_height}px;";
+    }
+
+    if ($footer_height) {
+      $style .= "margin-bottom: {$footer_height}px;";
+    }
+
+    if ($header_height) {
+      $style .= "
         @top {
           content: flow(header);
           padding-top: {$margin_top}cm;
-        }
+        }";
+    }
+    if ($footer_height) {
+      $style .= "
         @bottom {
           content: flow(footer);
           padding-bottom: {$margin_bottom}cm;
-        }
-      }
+        }";
+    }
+    $style .= "}";
 
-      div#header {
-        flow: static(header);
-      }
+    if ($header_height) {
+      $style .= "
+        div#header {
+          flow: static(header);
+        }";
+    }
 
-      div#footer {
-        flow: static(footer);
-      }
-    ";
+    if ($footer_height) {
+      $style .= "
+        div#footer {
+          flow: static(footer);
+        }";
+    }
 
     $pos_end_style = strpos($this->html, "</style>");
 
