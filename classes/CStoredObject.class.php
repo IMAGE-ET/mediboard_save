e89e80b925a483fb47f8e95a58793c3094ecc5c7
Yohann Poiron <poiron@openxtrem.com>
2015-04-01 13:42:48 +0000

    ref: Affichage des group_by absent en mode debug seulement
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@27810 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/CStoredObject.class.php
+++ classes/CStoredObject.class.php
@@ -988,7 +988,7 @@ class CStoredObject extends CModelObject {
     }
 
     if (count($objects) != count($rows)) {
-      mbLog($query,  "Missing group by in $m / $action (rows : ".count($rows).", objects : ".count($objects).")");
+      CMbDebug::log($query,  "Missing group by in $m / $action (rows : ".count($rows).", objects : ".count($objects).")");
     }
 
     return $objects;
