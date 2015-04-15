9c19cd0fd06a332e2a0fcfc16dde204621dd2608
Flavien Crochard <flavien.crochard@openxtrem.com>
2015-04-15 07:01:16 +0000

    Fnc: Le scrit baseBackup peut sauvegarder plusieurs bases (argument 4) séparées par des virgules.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@27955 a21d6be2-e512-0410-a060-91f5a2946631

--- shell/baseBackup.sh
+++ shell/baseBackup.sh
@@ -12,11 +12,11 @@ announce_script "Database daily backup"
 
 if [ "$#" -lt 5 ]
 then 
-  echo "Usage: $0 <method> <username> <password> <database> <backup_path> options"
+  echo "Usage: $0 <method> <username> <password> <databases> <backup_path> options"
   echo " <method> is hotcopy or dump method"
   echo " <username> to access database"
   echo " <password> authenticate user"
-  echo " <database> to backup, eg mediboard"
+  echo " <databases> to backup, eg mediboard. Separate by ',' to backup several databases"
   echo " <backup_path> is the backup path, eg /var/backup"
   echo " Options:"
   echo "   [-t <time>] is time in days before removal of files, default 7"
@@ -60,10 +60,15 @@ done
 method=$1
 username=$2
 password=$3
-database=$4
-backup_path=$5
+databases=$(echo $4|xargs -n1 -d,|sort -u|xargs) # Remove duplicates from databases
+BACKUP_PATH=$5
+DATETIME=$(date +%Y-%m-%dT%H-%M-%S)
 
-info_script "Backuping '$database' database"
+# Save the old IFS because of the find command
+old_ifs=$IFS
+IFS=$','
+
+info_script "Backuping '$databases' databases"
 
 # Create lock file
 if [ -n "$lock" ]
@@ -72,13 +77,13 @@ then
 fi
 
 # Make backup path
-BACKUP_PATH=$5
 force_dir $BACKUP_PATH
 
 # Make database path
-BASE_PATH=${BACKUP_PATH}/$database-db
-force_dir $BASE_PATH
-cd ${BASE_PATH}
+for database in $databases; do
+  BASE_PATH=${BACKUP_PATH}/$database-db
+  force_dir $BASE_PATH
+done
 
 # Check free disk space
 mysql_conf=$(find /etc/ -name my.cnf 2>/dev/null|head -n 1)
@@ -87,20 +92,25 @@ then
   check_errs 2 "MySQL configuration file not found"
 fi
 
-mysql_data_root=$(grep datadir $mysql_conf|tr -s ' '|cut -d"=" -f 2)
+mysql_data_root=$(grep datadir $mysql_conf|tr -s ' '|cut -d"=" -f 2|sed -e 's/^[ \t]*//')
 
 if [ -z $mysql_data_root ]
 then
   mysql_data_root="/var/lib/mysql"
 fi
 
-mysql_data_base="$mysql_data_root/$database"
-database_size=$(du -k $mysql_data_base|tail -n 1|sed -r 's/\s+/\ /g'|cut -d" " -f 1)
-
-# Expanded size (database + tarball)
-needed_size=$((database_size*3/2))
 available_size=$(df -k $BACKUP_PATH|tail -n 1|sed -r 's/\s+/\ /g'|cut -d" " -f 4)
 available_size=$((available_size))
+needed_size=0
+
+for database in $databases; do
+  mysql_data_base="$mysql_data_root/$database"
+
+  database_size=$(du -k $mysql_data_base|tail -n 1|sed -r 's/\s+/\ /g'|cut -d" " -f 1)
+
+  # Expanded size (database + tarball)
+  needed_size=$((needed_size+database_size*3/2))
+done
 
 if [ $available_size -lt $needed_size ]
 then
@@ -124,89 +134,114 @@ check_errs $? "Failed to flush query cache" "Query cache flushed"
 ## Make MySQL method
 
 # removes previous hotcopy/dump if something went wrong
-rm -Rf $database
+for database in $databases; do
+  BASE_PATH=${BACKUP_PATH}/$database-db
+  cd $BASE_PATH
+  rm -Rf $database
+done
 
-DATETIME=$(date +%Y-%m-%dT%H-%M-%S)
+for database in $databases; do
+  BASE_PATH=${BACKUP_PATH}/$database-db
+  cd $BASE_PATH
 
-case $1 in
-  hotcopy)
-    result=$database/
-    
-    mysqlhotcopy --quiet -u $username -p $password $database $BASE_PATH
+  case $method in
+    hotcopy)
+      mysqlhotcopy --quiet -u $username -p $password $database $BASE_PATH
 
-    if [ $check_hotcopy -eq 1 ]; then
-      check_errs $? "Failed to create MySQL hot copy" "MySQL hot copy done!"
-    fi
+      if [ $check_hotcopy -eq 1 ]; then
+        check_errs $? "Failed to create MySQL hot copy for database $database" "MySQL hot copy done for database $database !"
+      fi
 
-    if [ $binary_log -eq 1 ]; then
-      databasebinlog=$database-${DATETIME}.binlog.position
-      mysql --user=$username --password=$password $database < $BASH_PATH/mysql_show_master_status.sql > $BACKUP_PATH/binlog-${DATETIME}.index
-      check_errs $? "Failed to create MySQL Binary log index" "MySQL Binary log index done!"
-    fi
-    ;;
-  dump)
-    result=$database.sql
-    mysqldump --opt -u ${username} -p${password} $database > $database.sql
-    check_errs $? "Failed to create MySQL dump" "MySQL dump done!"
-    ;;
-  *)
-    result=$database/
-    echo "Choose hotcopy or dump method"
+      if [ $binary_log -eq 1 ]; then
+        mysql --user=$username --password=$password $database < $BASH_PATH/mysql_show_master_status.sql > $BACKUP_PATH/binlog-${DATETIME}.index
+        check_errs $? "Failed to create MySQL Binary log index" "MySQL Binary log index done!"
+      fi
+      ;;
+    dump)
+      mysqldump --opt -u ${username} -p${password} $database > $database.sql
+      check_errs $? "Failed to create MySQL dump for database $database" "MySQL dump done for database $database !"
+      ;;
+    *)
+      echo "Choose hotcopy or dump method"
 
-    if [ -n "$lock" ]
-    then
-      rm $lock
-    fi
-    exit 1
-    ;;
-esac
+      if [ -n "$lock" ]
+      then
+        rm $lock
+      fi
+      exit 1
+      ;;
+  esac
+done
 
 # rotating files older than n days, all files if 0
-if [ $time -eq 0 ]; then
-  filter=""
-else
+filter=""
+if [ $time -ne 0 ]; then
   filter="-ctime +$time"
 fi
-if [ -n "$passphrase" ]; then
-  find ${BASE_PATH} -name "$database*.tar.gz.aes" $filter -exec /bin/rm '{}' ';'
-else
-  find ${BASE_PATH} -name "$database*.tar.gz" $filter -exec /bin/rm '{}' ';'
-fi
-check_errs $? "Failed to rotate files" "Files rotated"
+
+for database in $databases; do
+  BASE_PATH=${BACKUP_PATH}/$database-db
+  basename="$database*.tar.gz"
+  if [ -n "$passphrase" ]; then
+    basename="$basename.aes"
+  fi
+  # Restore the old IFS during the find command execution
+  IFS=$old_ifs
+  find ${BASE_PATH} -name "$basename" $filter -exec /bin/rm '{}' ';'
+  IFS=$','
+  check_errs $? "Failed to rotate files for database $database" "Files rotated for database $database"
+done
 
 # Compress archive and remove files
 
 # Make the tarball
-tarball=$database-${DATETIME}.tar.gz
-tar cfz $tarball $result
-check_errs $? "Failed to create backup tarball" "Tarball packaged!"
+for database in $databases; do
+  case $method in
+    hotcopy)
+      result=$database/
+      ;;
+    dump)
+      result=$database.sql
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
 
-# Crypt the tarball
-if [ -n "$passphrase" ]; then
-  openssl $cryptage -salt -in $tarball -out $tarball.aes -k $passphrase
-  check_errs $? "Failed to crypt the archive" "Archive crypted!"
-  # create a symlink
-  cp -s -f $tarball.aes $database-latest.tar.gz.aes
-  check_errs $? "Failed to create symlink of archive crypted" "Symlink of crypted archive created!"
-  rm $tarball
-  check_errs $? "Failed to delete non-crypted archive" "Archive non-crypted deleted!"
-else
-  # create a symlink
-  cp -s -f $tarball $database-latest.tar.gz
-  warn_errs $? "Failed to create symlink" "Symlink created!"
-fi
+  BASE_PATH=${BACKUP_PATH}/$database-db
+  cd $BASE_PATH
 
-# Remove temporary files
-rm -Rf $result
-check_errs $? "Failed to clean MySQL files" "MySQL files cleaning done!"
+  tarball=$database-${DATETIME}.tar.gz
+  tar cfz $tarball $result
+  check_errs $? "Failed to create backup tarball" "Tarball packaged for database $database !"
 
-if [ -n "$lock" ]
-then
-  rm $lock
-fi
+  # Crypt the tarball
+  if [ -n "$passphrase" ]; then
+    openssl $cryptage -salt -in $tarball -out $tarball.aes -k $passphrase
+    check_errs $? "Failed to crypt the archive" "Archive crypted for database $database !"
+    # create a symlink
+    cp -s -f $tarball.aes $database-latest.tar.gz.aes
+    check_errs $? "Failed to create symlink of archive crypted" "Symlink of crypted archive created for database $database !"
+    rm $tarball
+    check_errs $? "Failed to delete non-crypted archive" "Archive non-crypted deleted for database $database !"
+  else
+    # create a symlink
+    cp -s -f $tarball $database-latest.tar.gz
+    warn_errs $? "Failed to create symlink" "Symlink created for database $database !"
+  fi
 
-# Write event file
-event=$MB_PATH/tmp/monitevent.txt
+  # Remove temporary files
+  rm -Rf $result
+  check_errs $? "Failed to clean MySQL files" "MySQL files cleaning done for database $database !"
 
-echo "#$(date +%Y-%m-%dT%H:%M:%S)" >> $event
-echo "<strong>$database</strong> base backup: <strong>$method</strong> method" >> $event
+  if [ -n "$lock" ]
+  then
+    rm $lock
+  fi
+
+  # Write event file
+  event=$MB_PATH/tmp/monitevent.txt
+
+  echo "#$(date +%Y-%m-%dT%H:%M:%S)" >> $event
+  echo "<strong>$database</strong> base backup: <strong>$method</strong> method" >> $event
+done
