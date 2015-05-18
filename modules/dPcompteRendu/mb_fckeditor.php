ca3ae2a4690c0299aa79e55f8fd9874c44e2d6be
Flavien Crochard <flavien.crochard@openxtrem.com>
2015-05-18 14:38:07 +0000

    Fnc: Tri par ordre alphabétique des champs dans les modèles.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@28302 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPcompteRendu/mb_fckeditor.php
+++ modules/dPcompteRendu/mb_fckeditor.php
@@ -14,29 +14,32 @@
 $templateManager = unserialize(gzuncompress($_SESSION["dPcompteRendu"]["templateManager"]));
 header("Content-Type: text/javascript");
 
-$user = CMediusers::get();
-$use_apicrypt = false;
-if (!$user->isPraticien() && CModule::getActive("apicrypt")) {
-  $use_apicrypt = true;
+ksort($templateManager->sections);
+foreach ($templateManager->sections as $key => $_section) {
+  ksort($templateManager->sections[$key]);
+  foreach ($templateManager->sections[$key] as $_key => $_sub_section) {
+    ksort($templateManager->sections[$key][$_key]);
+  }
 }
-elseif ($user->mail_apicrypt && CModule::getActive("apicrypt")) {
-  $use_apicrypt = true;
+
+$user = CMediusers::get();
+
+$use_apicrypt = false;
+if (CModule::getActive("apicrypt")) {
+  $use_apicrypt = !$user->isPraticien() || $user->mail_apicrypt;
 }
 
 $use_mssante = false;
-if (!$user->isPraticien() && CModule::getActive('mssante')) {
-  $use_mssante = true;
-}
-elseif (CModule::getActive('mssante') && CMSSanteUserAccount::isUserHasAccount($user)) {
-  $use_mssante = true;
+if (CModule::getActive('mssante')) {
+  $use_mssante = !$user->isPraticien() || CMSSanteUserAccount::isUserHasAccount($user);
 }
 
 // Création du template
 $smarty = new CSmartyDP("modules/dPcompteRendu");
 
 $smarty->assign("templateManager", $templateManager);
-$smarty->assign("nodebug", true);
-$smarty->assign("use_apicrypt"  , $use_apicrypt);
-$smarty->assign('use_mssante'   , $use_mssante);
+$smarty->assign("nodebug"        , true);
+$smarty->assign("use_apicrypt"   , $use_apicrypt);
+$smarty->assign('use_mssante'    , $use_mssante);
 
 $smarty->display("mb_fckeditor.tpl");
