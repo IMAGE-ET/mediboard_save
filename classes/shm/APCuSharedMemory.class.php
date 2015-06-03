bc7af20a409648a46d0dcdd61d232f26ba95223a
Fabien Ménager <fabien.menager@openxtrem.com>
2015-06-03 10:23:07 +0000

    bug: meilleure gestion des clés APCu
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@28491 a21d6be2-e512-0410-a060-91f5a2946631

--- classes/shm/APCuSharedMemory.class.php
+++ classes/shm/APCuSharedMemory.class.php
@@ -17,7 +17,7 @@ require_once __DIR__."/APCSharedMemory.class.php";
  * Alternative PHP User Cache (APCu) based Memory class
  */
 class APCuSharedMemory extends APCSharedMemory {
-  protected $_cache_key = "key";
+  protected $_cache_key = "info";
 
   /**
    * @see parent::modDate()
@@ -57,7 +57,7 @@ class APCuSharedMemory extends APCSharedMemory {
     );
 
     foreach ($user_cache["cache_list"] as $_cache_info) {
-      if ($_cache_info["key"] == $key) {
+      if ($_cache_info["info"] == $key) {
         $cache_info["creation_date"]     = strftime(CMbDT::ISO_DATETIME, isset($_cache_info["ctime"]) ? $_cache_info["ctime"] : $_cache_info["creation_time"]);
         $cache_info["modification_date"] = strftime(CMbDT::ISO_DATETIME, isset($_cache_info["mtime"]) ? $_cache_info["mtime"] : $_cache_info["modification_time"]);
         $cache_info["num_hits"]          = $_cache_info["nhits"];
