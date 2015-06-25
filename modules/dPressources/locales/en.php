8efd8848db94a78804c825493fcf57e146d29cdc
Flavien Crochard <flavien.crochard@openxtrem.com>
2015-06-25 16:07:48 +0000

    Fnc: AJout des locales fr-be et nl-be.
    git-svn-id: svn://svn.code.sf.net/p/mediboard/code/trunk@28741 a21d6be2-e512-0410-a060-91f5a2946631

--- modules/dPressources/locales/en.php
+++ modules/dPressources/locales/en.php
@@ -3,4 +3,3 @@ $locales['CPlageressource'] = 'Plage de Ressources';
 $locales['CPlageressource.paye.'] = ' ';
 $locales['CPlageressource.paye.0'] = '0';
 $locales['CPlageressource.paye.1'] = '1';
-?>
